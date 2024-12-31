47c66e5c236bd7fc3bc40aa4eed1b4a74d94dfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:30 +0000

    Add package xf-cli/0.0.3/xf_cli-0.0.3.tar.gz

---
+++ packages/xf-cli/xf_cli-0.0.3.tar.gz/xf_cli-0.0.3/commands/base.py
@@ -0,0 +1,33 @@
+import os
+
+from utils.env import TRUE
+from utils.profiling import pyprofile
+
+TO_PROFILE_ENV = "XF_PROFILING"
+TO_PROFILE_HTML_ENV = "XF_PROFILING_SAVE_HTML"
+
+
+class BaseCommand:
+    command: str
+    imports: list[str]
+    packages: list[str]
+
+    def _import_packages(self):
+        for import_name in self.imports:
+            __import__(import_name)
+
+    def warm(self):
+        pass
+
+    def run(self, line: str):
+        pass
+
+    def _maybe_profiled_run(self, line: str):
+        if os.getenv(TO_PROFILE_ENV) == TRUE:
+
+            @pyprofile(save_html=os.getenv(TO_PROFILE_HTML_ENV) == TRUE)
+            def run(line: str):
+                return self.run(line)
+
+            return run(line)
+        return self.run(line)
---
+++ packages/xf-cli/xf_cli-0.0.3.tar.gz/xf_cli-0.0.3/main.py
@@ -0,0 +1,57 @@
+from typing import Annotated
+
+import typer
+
+from core.isolation import start
+
+
+def run(
+    selection: Annotated[
+        list[str] | None,
+        typer.Argument(
+            help="Select commands for your session. If not provided, all commands with available dependencies are included.",
+        ),
+    ] = None,
+    exclude: Annotated[
+        bool,
+        typer.Option(
+            "--exclude",
+            "-x",
+            help="By default, selected commands are included. Use this option to exclude them instead.",
+            is_flag=True,
+        ),
+    ] = False,
+    shell: Annotated[
+        bool,
+        typer.Option(
+            "--shell",
+            "-s",
+            is_flag=True,
+            help="In shell mode, commands are run through the shell by default, instead of the ipython console.",
+        ),
+    ] = False,
+    isolate: Annotated[
+        bool,
+        typer.Option(
+            "--isolate",
+            "-i",
+            help="Run the commands in an isolated environment using uvx. If using this option, you must explicitly select commands to include.",
+            is_flag=True,
+        ),
+    ] = False,
+):
+    included = excluded = []
+    if selection:
+        if exclude:
+            excluded = selection
+        else:
+            included = selection
+    start(shell, included, excluded, isolate)
+
+
+def main():
+    typer.run(run)
+
+
+if __name__ == "__main__":
+    main()
