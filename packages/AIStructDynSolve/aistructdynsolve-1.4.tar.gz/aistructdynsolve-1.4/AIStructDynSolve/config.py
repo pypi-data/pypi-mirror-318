ddfa74e5db6e124746013c95bda23b730bbd9d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:38 +0000

    Add package AIStructDynSolve/1.4/aistructdynsolve-1.4.tar.gz

---
+++ packages/AIStructDynSolve/aistructdynsolve-1.4.tar.gz/aistructdynsolve-1.4/AIStructDynSolve/config.py
@@ -0,0 +1,16 @@
+"""
+AIStructDynSolve
+A framework focused on solving structural dynamics problems using artificial intelligence (AI) methods
+solve the following ODE of MDOF
+M*U_dotdot+C*U_dot+K*U=Pt
+initial condition: U(t=0)=InitialU, and U_dot(t=0)=InitialU_dot
+Author: 杜轲 duke@iem.ac.cn
+Date: 2023/12/26
+"""
+import torch
+
+# 设置默认数据类型
+DEFAULT_DTYPE = torch.float64
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
