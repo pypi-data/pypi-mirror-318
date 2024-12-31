5f44c5f1b0ae0a79757307ffe65efb8655e7ae11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:30 +0000

    Add package pdf-text-extractor/0.1.4/pdf_text_extractor-0.1.4.tar.gz

---
+++ packages/pdf-text-extractor/pdf_text_extractor-0.1.4.tar.gz/pdf_text_extractor-0.1.4/pdf_text_extractor/__init__.py
@@ -0,0 +1,1 @@
+from pdf_text_extractor.extractor import PDFTextExtractor
---
+++ packages/pdf-text-extractor/pdf_text_extractor-0.1.4.tar.gz/pdf_text_extractor-0.1.4/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    if not os.path.exists(filename):
+        return []
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
+
+def read_me(filename):
+    if not os.path.exists(filename):
+        return None
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+long_description = read_me('README.md')
+setup(
+    name="pdf-text-extractor",
+    version="0.1.4",
+    description="Extract text and images from PDF files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shahzod",
+    author_email="baxromov.shahzodbek@gmail.com",
+    url="https://github.com/baxromov/pdf-text-extractor",
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
