26a0ea6f6039017470eb3679dd260513a9e873df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:27 +0000

    Add package core-pro/0.0.120/core_pro-0.0.120-py3-none-any.whl

---
+++ packages/core-pro/core_pro-0.0.120-py3-none-any.whl/core_pro/__init__.py
@@ -0,0 +1,10 @@
+from .config import GoogleAuthentication
+from .GSheet import Sheet, SheetFormat
+from .GMail import Gmail
+from .GDrive import Drive
+from .GSlide import Slide
+from .oop_sql import DataPipeLine
+from .cloud import AWS, Gcloud
+from .seatalk_bot import SEATALK
+from .download_url import DownloadImage, httpx_fetch
+from .download_url_v2 import ImageDownloader
