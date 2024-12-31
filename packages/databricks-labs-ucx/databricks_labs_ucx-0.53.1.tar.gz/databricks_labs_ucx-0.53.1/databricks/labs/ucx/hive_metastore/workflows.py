f3d518426bbd17a1c9b871f9a727c822f81de3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:14 +0000

    Add package databricks-labs-ucx/0.53.1/databricks_labs_ucx-0.53.1.tar.gz

---
+++ packages/databricks-labs-ucx/databricks_labs_ucx-0.53.1.tar.gz/databricks_labs_ucx-0.53.1/databricks/labs/ucx/hive_metastore/workflows.py
@@ -0,0 +1,158 @@
+from databricks.labs.ucx.assessment.workflows import Assessment
+from databricks.labs.ucx.contexts.workflow_task import RuntimeContext
+from databricks.labs.ucx.framework.tasks import Workflow, job_task
+from databricks.labs.ucx.hive_metastore.tables import What
+
+
+class TableMigration(Workflow):
+    def __init__(self):
+        super().__init__('migrate-tables')
+
+    @job_task(job_cluster="main", depends_on=[Assessment.crawl_tables])
+    def convert_managed_table(self, ctx: RuntimeContext):
+        """This workflow task converts managed HMS tables to external table if `managed_table_external_storage` is set to `CONVERT_TO_EXTERNAL
+        See documentation for more detail."""
+        ctx.tables_migrator.convert_managed_hms_to_external(
+            managed_table_external_storage=ctx.config.managed_table_external_storage
+        )
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables, convert_managed_table])
+    def migrate_external_tables_sync(self, ctx: RuntimeContext):
+        """This workflow task migrates the external tables that are supported by SYNC command from the Hive Metastore
+        to the Unity Catalog.
+        """
+        ctx.tables_migrator.migrate_tables(
+            what=What.EXTERNAL_SYNC, managed_table_external_storage=ctx.config.managed_table_external_storage
+        )
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables, convert_managed_table])
+    def migrate_dbfs_root_delta_tables(self, ctx: RuntimeContext):
+        """This workflow task migrates delta tables stored in DBFS root from the Hive Metastore to the Unity Catalog
+        using deep clone.
+        """
+        ctx.tables_migrator.migrate_tables(what=What.DBFS_ROOT_DELTA)
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables, convert_managed_table])
+    def migrate_dbfs_root_non_delta_tables(
+        self,
+        ctx: RuntimeContext,
+    ):
+        """This workflow task migrates non delta tables stored in DBFS root from the Hive Metastore to the Unity Catalog
+        using CTAS.
+        """
+        ctx.tables_migrator.migrate_tables(what=What.DBFS_ROOT_NON_DELTA)
+
+    @job_task(
+        job_cluster="user_isolation",
+        depends_on=[
+            Assessment.crawl_tables,
+            migrate_external_tables_sync,
+            migrate_dbfs_root_delta_tables,
+            migrate_dbfs_root_non_delta_tables,
+        ],
+    )
+    def migrate_views(self, ctx: RuntimeContext):
+        """This workflow task migrates views from the Hive Metastore to the Unity Catalog using create view sql
+        statement. It is dependent on the migration of the tables.
+        """
+        ctx.tables_migrator.migrate_tables(what=What.VIEW)
+
+    @job_task(job_cluster="user_isolation", depends_on=[migrate_views])
+    def update_migration_status(self, ctx: RuntimeContext):
+        """Refresh the migration status to present it in the dashboard."""
+        ctx.tables_migrator.get_remaining_tables()
+
+
+class MigrateHiveSerdeTablesInPlace(Workflow):
+    def __init__(self):
+        super().__init__('migrate-external-hiveserde-tables-in-place-experimental')
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables])
+    def migrate_hive_serde_in_place(self, ctx: RuntimeContext):
+        """This workflow task migrates ParquetHiveSerDe, OrcSerde, AvroSerDe tables in place from
+        the Hive Metastore to the Unity Catalog."""
+        ctx.tables_migrator.migrate_tables(
+            what=What.EXTERNAL_HIVESERDE,
+            hiveserde_in_place_migrate=True,
+        )
+
+    @job_task(
+        job_cluster="user_isolation",
+        depends_on=[Assessment.crawl_tables, migrate_hive_serde_in_place],
+    )
+    def migrate_views(self, ctx: RuntimeContext):
+        """This workflow task migrates views from the Hive Metastore to the Unity Catalog using create view sql statement.
+        It is dependent on the migration of the tables.
+        """
+        ctx.tables_migrator.migrate_tables(what=What.VIEW)
+
+    @job_task(job_cluster="user_isolation", depends_on=[migrate_views])
+    def update_migration_status(self, ctx: RuntimeContext):
+        """Refresh the migration status to present it in the dashboard."""
+        ctx.tables_migrator.get_remaining_tables()
+
+
+class MigrateExternalTablesCTAS(Workflow):
+    def __init__(self):
+        super().__init__('migrate-external-tables-ctas')
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables])
+    def migrate_other_external_ctas(self, ctx: RuntimeContext):
+        """This workflow task migrates non-SYNC supported and non HiveSerde external tables using CTAS"""
+        ctx.tables_migrator.migrate_tables(
+            what=What.EXTERNAL_NO_SYNC,
+        )
+
+    @job_task(job_cluster="user_isolation", depends_on=[Assessment.crawl_tables])
+    def migrate_hive_serde_ctas(self, ctx: RuntimeContext):
+        """This workflow task migrates HiveSerde tables using CTAS"""
+        ctx.tables_migrator.migrate_tables(
+            what=What.EXTERNAL_HIVESERDE,
+        )
+
+    @job_task(
+        job_cluster="user_isolation",
+        depends_on=[Assessment.crawl_tables, migrate_other_external_ctas, migrate_hive_serde_ctas],
+    )
+    def migrate_views(self, ctx: RuntimeContext):
+        """This workflow task migrates views from the Hive Metastore to the Unity Catalog using create view sql
+        statement. It is dependent on the migration of the tables.
+        """
+        ctx.tables_migrator.migrate_tables(what=What.VIEW)
+
+    @job_task(job_cluster="user_isolation", depends_on=[migrate_views])
+    def update_migration_status(self, ctx: RuntimeContext):
+        """Refresh the migration status to present it in the dashboard."""
+        ctx.tables_migrator.get_remaining_tables()
+
+
+class ScanTablesInMounts(Workflow):
+    def __init__(self):
+        super().__init__('scan-tables-in-mounts-experimental')
+
+    @job_task
+    def scan_tables_in_mounts_experimental(self, ctx: RuntimeContext):
+        """[EXPERIMENTAL] This workflow scans for Delta tables inside all mount points
+        captured during the assessment. It will store the results in the `tables` table,
+        replacing any existing content that might be present."""
+        ctx.tables_in_mounts.snapshot(force_refresh=True)
+
+    @job_task(job_cluster="user_isolation", depends_on=[scan_tables_in_mounts_experimental])
+    def update_migration_status(self, ctx: RuntimeContext):
+        """Refresh the migration status to present it in the dashboard."""
+        ctx.tables_migrator.get_remaining_tables()
+
+
+class MigrateTablesInMounts(Workflow):
+    def __init__(self):
+        super().__init__('migrate-tables-in-mounts-experimental')
+
+    @job_task(job_cluster="user_isolation", depends_on=[ScanTablesInMounts.scan_tables_in_mounts_experimental])
+    def migrate_tables_in_mounts_experimental(self, ctx: RuntimeContext):
+        """[EXPERIMENTAL] This workflow migrates `delta tables stored in mount points` to Unity Catalog using a Create Table statement."""
+        ctx.tables_migrator.migrate_tables(what=What.TABLE_IN_MOUNT)
+
+    @job_task(job_cluster="user_isolation", depends_on=[migrate_tables_in_mounts_experimental])
+    def update_migration_status(self, ctx: RuntimeContext):
+        """Refresh the migration status to present it in the dashboard."""
+        ctx.tables_migrator.get_remaining_tables()
