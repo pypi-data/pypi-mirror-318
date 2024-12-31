0e2b9527374897865bcfd915a48a965088242c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:37 +0000

    Add package discord-localization/1.1.4/discord_localization-1.1.4.tar.gz

---
+++ packages/discord-localization/discord_localization-1.1.4.tar.gz/discord_localization-1.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pathlib import Path
+
+VERSION = '1.1.4'
+DESCRIPTION = 'A discord.py extension for command localization.'
+
+# Setting up
+setup(
+    name="discord-localization",
+    version=VERSION,
+    author="Pearoo",
+    author_email="<contact@pearoo.dev>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=Path("README.md").read_text(),
+    packages=["discord.ext.localization"],
+    install_requires=["discord.py"],
+    keywords=['discord.py', 'i18n', 'translation', 'localisation', 'localization'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "Source": "https://github.com/rnxm/discord-localization"
+    }
+)
+# command: python setup.py sdist bdist_wheel
