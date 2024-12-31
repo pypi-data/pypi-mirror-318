6b4ee76cfb8ee445df52242eb20db280dbf28503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:43 +0000

    Add package byteai/0.1.3/byteai-0.1.3.tar.gz

---
+++ packages/byteai/byteai-0.1.3.tar.gz/byteai-0.1.3/byteai/client.py
@@ -0,0 +1,27 @@
+# byteai/client.py
+from .payment import StripeManager
+from .tokenizer import TokenizerTool
+
+class ByteClient:
+    def __init__(self, api_key: str):
+        self.stripe = StripeManager(api_key)
+        self.api_url = "https://api.byteai.dev"
+        
+    async def finetune(self, dataset: str, config: dict) -> str:
+        tokens = TokenizerTool().process_file(dataset)
+        payment = await self.stripe.charge("finetune", 100)
+        return await self._post("/finetune", {
+            "tokens": tokens,
+            "config": config,
+            "payment_id": payment.id
+        })
+
+    async def extract_features(self, model_id: str) -> list:
+        payment = await self.stripe.charge("features", 200)
+        return await self._post("/features", {
+            "model_id": model_id,
+            "payment_id": payment.id
+        })
+    
+    def tokenize(dataset:str):
+        tokens = TokenizerTool().process_file(dataset)
---
+++ packages/byteai/byteai-0.1.3.tar.gz/byteai-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byteai",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "tiktoken",
+        "stripe",
+        "aiohttp",
+        "numpy"
+    ],
+    author="Aaron Jerez",
+    author_email="aaronjerez1@gmail.com",
+    description="AI development tools",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
