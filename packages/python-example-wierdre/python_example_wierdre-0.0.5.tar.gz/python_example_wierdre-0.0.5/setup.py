3f68358f9c547f46c23145f4ff066258889cdefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:33 +0000

    Add package python-example-wierdre/0.0.5/python_example_wierdre-0.0.5.tar.gz

---
+++ packages/python-example-wierdre/python_example_wierdre-0.0.5.tar.gz/python_example_wierdre-0.0.5/setup.py
@@ -0,0 +1,34 @@
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+__version__ = "0.0.5"
+
+# The main interface is through Pybind11Extension.
+# * You can add cxx_std=11/14/17, and then build_ext can be removed.
+# * You can set include_pybind11=false to add the include directory yourself,
+#   say from a submodule.
+#
+# Note:
+#   Sort input source files if you glob sources to ensure bit-for-bit
+#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+
+ext_modules = [
+    Pybind11Extension(
+        "python_example_wierdre",
+        ["src/main.cpp"],
+        # Example: passing in the version to the compiled code
+        define_macros=[("VERSION_INFO", __version__)],
+    ),
+]
+
+setup(
+    name="python_example_wierdre",
+    version=__version__,
+    author="Wierdre",
+    description="A test project integrating C++ and Python by Wierdre",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.7",
+)
