bbd9e26fdc9df75c9b7e6b8f2c23917172497345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:03:33 +0000

    Add package scapi/0.3.0/scapi-0.3.0-py3-none-any.whl

---
+++ packages/scapi/scapi-0.3.0-py3-none-any.whl/scapi/__init__.py
@@ -0,0 +1,69 @@
+# Created by kakeruzoku / https://github.com/kakeruzoku/scapi
+# Special Thanks: Timmccool / https://github.com/TimMcCool/scratchattach
+
+__version__ = "0.3.0"
+
+from .others.common import (
+    create_ClientSession,
+    Response,
+    ClientSession,
+    api_iterative as _api_iterative,
+    split_int,
+    split,
+    to_dt,
+    empty_project_json,
+    BIG
+)
+from .others import error as exception
+del exception.TYPE_CHECKING
+from .others.other_api import (
+    get_csrf_token_sync
+)
+from .sites.base import (
+    _BaseSiteAPI,
+)
+from .sites.comment import (
+    CommentData,
+    Comment,
+    UserComment
+)
+from .sites.project import (
+    Project,
+    get_project,
+    create_Partial_Project,
+    explore_projects,
+    search_projects
+)
+from .sites.session import (
+    SessionStatus,
+    Session,
+    session_login,
+    login
+)
+from .sites.studio import (
+    Studio,
+    get_studio,
+    create_Partial_Studio,
+    explore_studios,
+    search_studios
+)
+from .sites.user import (
+    User,
+    get_user,
+    create_Partial_User
+)
+from .sites.activity import (
+    Activity,
+    ActivityType
+)
+from .sites.forum import (
+    ForumTopic,
+    ForumCategoryType,
+    ForumPost,
+    ForumStatus,
+    get_post,
+    get_topic,
+    get_topic_list,
+    create_Partial_ForumTopic,
+    create_Partial_ForumPost,
+)
