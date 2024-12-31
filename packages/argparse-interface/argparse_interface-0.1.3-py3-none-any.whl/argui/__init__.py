3d864a1222443af68f4bd841eb7e618c869abf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:14 +0000

    Add package argparse-interface/0.1.3/argparse_interface-0.1.3-py3-none-any.whl

---
+++ packages/argparse-interface/argparse_interface-0.1.3-py3-none-any.whl/argui/__init__.py
@@ -0,0 +1,6 @@
+# Argparse Interface
+from .Wrapper import Wrapper
+from .Interface import Interface
+from .ParserMap import ParserMap
+from .Demo import getDemoArgParser, runDemo
+from .ReturnCodes import ReturnCodes
