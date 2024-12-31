94cbc759e9991b23c8e58b2b39d8de6288125a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:56 +0000

    Add package llama-cpp-server-py-core/0.1.2/llama_cpp_server_py_core-0.1.2.tar.gz

---
+++ packages/llama-cpp-server-py-core/llama_cpp_server_py_core-0.1.2.tar.gz/llama_cpp_server_py_core-0.1.2/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 4372;
+char const *LLAMA_COMMIT = "e34c5af4";
+char const *LLAMA_COMPILER = "Ubuntu clang version 14.0.0-1ubuntu1.1";
+char const *LLAMA_BUILD_TARGET = "x86_64-pc-linux-gnu";
