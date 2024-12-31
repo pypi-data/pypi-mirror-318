1bc6f599ee2a43268a8752ca1995c89cbf00c653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:20 +0000

    Add package file-util-deepsec/1.0.0/file_util_deepsec-1.0.0.tar.gz

---
+++ packages/file-util-deepsec/file_util_deepsec-1.0.0.tar.gz/file_util_deepsec-1.0.0/README.md
@@ -0,0 +1,8 @@
+# File Utility Class
+
+A Python utility class for interacting with files like json, csv, and joblib using corresponding library. This class simplifies loading and saving data from these files.
+
+Currently, it support the following files:
+- json
+- csv
+- joblib
---
+++ packages/file-util-deepsec/file_util_deepsec-1.0.0.tar.gz/file_util_deepsec-1.0.0/src/file_util/__init__.py
@@ -0,0 +1,19 @@
+from .file_util import (
+    dump_data_to_json,
+    load_data_from_json,
+    load_data_from_csv,
+    dump_data_to_csv,
+    dump_model_to_file,
+    load_model_from_file,
+    load_test_data_and_target_from_csv
+)
+
+__all__ = [
+    "dump_data_to_json",
+    "load_data_from_json",
+    "load_data_from_csv",
+    "dump_data_to_csv",
+    "dump_model_to_file",
+    "load_model_from_file",
+    "load_test_data_and_target_from_csv"
+]
