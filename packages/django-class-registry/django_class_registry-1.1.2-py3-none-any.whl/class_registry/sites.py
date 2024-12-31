86cad4f1ca8e06e30be7453d4b8ec2eee8b67cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 06:03:38 +0000

    Add package django-class-registry/1.1.2/django_class_registry-1.1.2-py3-none-any.whl

---
+++ packages/django-class-registry/django_class_registry-1.1.2-py3-none-any.whl/class_registry/sites.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "Registry has moved. Change your imports to 'from class_registry import Registry'",
+    PendingDeprecationWarning,
+)
+
+from class_registry import Registry  # noqa
