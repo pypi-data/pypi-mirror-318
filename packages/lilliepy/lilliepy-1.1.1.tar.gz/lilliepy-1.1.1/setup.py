07fb1b05fbe44a6cabe266b5f5d5a56fd1792214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:03:26 +0000

    Add package lilliepy/1.1.1/lilliepy-1.1.1.tar.gz

---
+++ packages/lilliepy/lilliepy-1.1.1.tar.gz/lilliepy-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lilliepy",
+    version="1.1.1",
+    author="sarthak ghoshal",  
+    author_email="sarthak22.ghoshal@example.com",  
+    description="A Bundle of Pkgs for Lilliepy",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/webdebguy/lilliepy",  
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "reactpy",
+        "reactpy-router",
+        "reactpy-forms",
+        "reactpy-table",
+        "reactpy-select",
+        "reactpy-flake8",
+        "reactpy-apexcharts",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
