c0fdfba266c5ae3e94ea48b2eb093d250cdf742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:54 +0000

    Add package supertokens-python/0.27.0/supertokens_python-0.27.0-py3-none-any.whl

---
+++ packages/supertokens-python/supertokens_python-0.27.0-py3-none-any.whl/supertokens_python/recipe/thirdparty/providers/bitbucket.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2023, VRAI Labs and/or its affiliates. All rights reserved.
+#
+# This software is licensed under the Apache License, Version 2.0 (the
+# "License") as published by the Apache Software Foundation.
+#
+# You may not use this file except in compliance with the License. You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import annotations
+
+from typing import Dict, Any, Optional
+
+from supertokens_python.recipe.thirdparty.provider import (
+    ProviderConfigForClient,
+    ProviderInput,
+    Provider,
+)
+from .custom import GenericProvider, NewProvider
+
+from .utils import do_get_request
+from ..types import RawUserInfoFromProvider, UserInfo, UserInfoEmail
+
+
+class BitbucketImpl(GenericProvider):
+    async def get_config_for_client_type(
+        self, client_type: Optional[str], user_context: Dict[str, Any]
+    ) -> ProviderConfigForClient:
+        config = await super().get_config_for_client_type(client_type, user_context)
+
+        if config.scope is None:
+            config.scope = ["account", "email"]
+
+        return config
+
+    async def get_user_info(
+        self, oauth_tokens: Dict[str, Any], user_context: Dict[str, Any]
+    ) -> UserInfo:
+        _ = user_context
+        access_token = oauth_tokens.get("access_token")
+        if access_token is None:
+            raise Exception("Access token not found")
+
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+        }
+
+        raw_user_info_from_provider = RawUserInfoFromProvider({}, {})
+
+        user_info_from_access_token = await do_get_request(
+            "https://api.bitbucket.org/2.0/user",
+            query_params=None,
+            headers=headers,
+        )
+
+        raw_user_info_from_provider.from_user_info_api = user_info_from_access_token
+
+        user_info_from_email = await do_get_request(
+            "https://api.bitbucket.org/2.0/user/emails",
+            query_params=None,
+            headers=headers,
+        )
+
+        if raw_user_info_from_provider.from_id_token_payload is None:
+            # Actually this should never happen but python type
+            # checker is not agreeing so doing this:
+            raw_user_info_from_provider.from_id_token_payload = {}
+
+        raw_user_info_from_provider.from_id_token_payload["email"] = (
+            user_info_from_email
+        )
+
+        email = None
+        is_verified = False
+        for email_info in user_info_from_email["values"]:
+            if email_info["is_primary"]:
+                email = email_info["email"]
+                is_verified = email_info["is_confirmed"]
+
+        return UserInfo(
+            third_party_user_id=raw_user_info_from_provider.from_user_info_api["uuid"],
+            email=None if email is None else UserInfoEmail(email, is_verified),
+            raw_user_info_from_provider=raw_user_info_from_provider,
+        )
+
+
+def Bitbucket(input: ProviderInput) -> Provider:  # pylint: disable=redefined-builtin
+    if not input.config.name:
+        input.config.name = "Bitbucket"
+
+    if not input.config.authorization_endpoint:
+        input.config.authorization_endpoint = (
+            "https://bitbucket.org/site/oauth2/authorize"
+        )
+
+    if not input.config.token_endpoint:
+        input.config.token_endpoint = "https://bitbucket.org/site/oauth2/access_token"
+
+    if input.config.authorization_endpoint_query_params is None:
+        input.config.authorization_endpoint_query_params = {
+            "audience": "api.atlassian.com",
+        }
+
+    return NewProvider(input, BitbucketImpl)
---
+++ packages/supertokens-python/supertokens_python-0.27.0-py3-none-any.whl/supertokens_python/recipe/thirdparty/providers/github.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2021, VRAI Labs and/or its affiliates. All rights reserved.
+#
+# This software is licensed under the Apache License, Version 2.0 (the
+# "License") as published by the Apache Software Foundation.
+#
+# You may not use this file except in compliance with the License. You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+import base64
+from typing import Any, Dict, List, Optional
+
+from supertokens_python.recipe.thirdparty.providers.utils import (
+    do_get_request,
+    do_post_request,
+)
+from supertokens_python.recipe.thirdparty.types import UserInfo, UserInfoEmail
+
+from .custom import GenericProvider, NewProvider
+from ..provider import Provider, ProviderConfigForClient, ProviderInput
+
+
+class GithubImpl(GenericProvider):
+    async def get_config_for_client_type(
+        self, client_type: Optional[str], user_context: Dict[str, Any]
+    ) -> ProviderConfigForClient:
+        config = await super().get_config_for_client_type(client_type, user_context)
+
+        if config.scope is None:
+            config.scope = ["read:user", "user:email"]
+
+        return config
+
+    async def get_user_info(
+        self, oauth_tokens: Dict[str, Any], user_context: Dict[str, Any]
+    ) -> UserInfo:
+        headers = {
+            "Authorization": f"Bearer {oauth_tokens.get('access_token')}",
+            "Accept": "application/vnd.github.v3+json",
+        }
+
+        raw_response = {}
+
+        email_info: List[Any] = await do_get_request("https://api.github.com/user/emails", headers=headers)  # type: ignore
+        user_info = await do_get_request("https://api.github.com/user", headers=headers)
+
+        raw_response["emails"] = email_info
+        raw_response["user"] = user_info
+
+        result = UserInfo(
+            third_party_user_id=str(user_info.get("id")),
+        )
+
+        for info in email_info:
+            if info.get("primary"):
+                result.email = UserInfoEmail(
+                    email=info.get("email"), is_verified=info.get("verified")
+                )
+
+        return result
+
+
+def Github(input: ProviderInput) -> Provider:  # pylint: disable=redefined-builtin
+    if not input.config.name:
+        input.config.name = "Github"
+
+    if not input.config.authorization_endpoint:
+        input.config.authorization_endpoint = "https://github.com/login/oauth/authorize"
+
+    if not input.config.token_endpoint:
+        input.config.token_endpoint = "https://github.com/login/oauth/access_token"
+
+    if input.config.validate_access_token is None:
+        input.config.validate_access_token = validate_access_token
+
+    return NewProvider(input, GithubImpl)
+
+
+async def validate_access_token(
+    access_token: str, config: ProviderConfigForClient, _: Dict[str, Any]
+):
+    client_secret = "" if config.client_secret is None else config.client_secret
+    basic_auth_token = base64.b64encode(
+        f"{config.client_id}:{client_secret}".encode()
+    ).decode()
+
+    url = f"https://api.github.com/applications/{config.client_id}/token"
+    headers = {
+        "Authorization": f"Basic {basic_auth_token}",
+        "Content-Type": "application/json",
+    }
+
+    status, body = await do_post_request(url, {"access_token": access_token}, headers)
+    if status != 200:
+        raise ValueError("Invalid access token")
+
+    if "app" not in body or body["app"].get("client_id") != config.client_id:
+        raise ValueError("Access token does not belong to your application")
