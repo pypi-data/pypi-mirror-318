fe9faa03e5dc17c8224b86afb95d5492640bfb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:12 +0000

    Add package t-desktop/0.0.16/t_desktop-0.0.16.tar.gz

---
+++ packages/t-desktop/t_desktop-0.0.16.tar.gz/t_desktop-0.0.16/tests/test_t_desktop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Tests for `t_desktop` package."""
+
+
+class TestTDesktop:
+    """Smoke tests of the package."""
+
+    def test_example(self):
+        """Smoke test."""
+        assert True
