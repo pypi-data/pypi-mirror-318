73b82ebe794f69994ec0e31f1e8183953bc7bfb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 04:03:08 +0000

    Add package poorman-handshake/1.0.0a0/poorman_handshake-1.0.0a0.tar.gz

---
+++ packages/poorman-handshake/poorman_handshake-1.0.0a0.tar.gz/poorman_handshake-1.0.0a0/setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'poorman_handshake', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+
+setup(
+    name='poorman_handshake',
+    version=get_version(),
+    packages=['poorman_handshake', 'poorman_handshake.asymmetric',
+              'poorman_handshake.symmetric'],
+    url='https://github.com/JarbasHiveMind/poorman_handshake',
+    license='Apache-2.0',
+    author='jarbasAi',
+    install_requires=required("requirements.txt"),
+    author_email='jarbasai@mailfence.com',
+    description='poor man\'s key exchange, powered by RSA'
+)
