2ad002f16d03862ded8c178122d910adb820e2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 15:05:03 +0000

    Add package xphasenet/0.1.0/xphasenet-0.1.0.tar.gz

---
+++ packages/xphasenet/xphasenet-0.1.0.tar.gz/xphasenet-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="xphasenet",
+    version="0.1.0",
+    long_description="xphasenet",
+    long_description_content_type="text/markdown",
+    packages=["xphasenet"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
