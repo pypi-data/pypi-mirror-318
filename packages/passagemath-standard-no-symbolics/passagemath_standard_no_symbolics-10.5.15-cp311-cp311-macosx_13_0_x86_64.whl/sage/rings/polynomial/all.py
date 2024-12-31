4e920d3896d62c5740218cc53704111b62ab1e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:13 +0000

    Add package passagemath-standard-no-symbolics/10.5.15/passagemath_standard_no_symbolics-10.5.15-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.5.15-cp311-cp311-macosx_13_0_x86_64.whl/sage/rings/polynomial/all.py
@@ -0,0 +1,35 @@
+"""
+Polynomials
+"""
+# ****************************************************************************
+#       Copyright (C) 2005 William Stein <wstein@gmail.com>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from sage.rings.polynomial.all__sagemath_polyhedra import *
+
+from sage.misc.lazy_import import lazy_import
+
+# Generic convolution
+from sage.rings.polynomial.convolution import convolution
+
+# Boolean Polynomial Rings
+from sage.rings.polynomial.polynomial_ring_constructor import BooleanPolynomialRing_constructor as BooleanPolynomialRing
+
+# Integer-valued Univariate Polynomial Ring
+lazy_import('sage.rings.polynomial.integer_valued_polynomials',
+            'IntegerValuedPolynomialRing')
+lazy_import('sage.rings.polynomial.q_integer_valued_polynomials',
+            'QuantumValuedPolynomialRing')
+
+del lazy_import
