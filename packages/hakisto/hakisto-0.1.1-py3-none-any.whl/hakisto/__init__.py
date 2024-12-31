dadf76926161a7e6cbbdb154a5a5c47f4777e98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:30 +0000

    Add package hakisto/0.1.1/hakisto-0.1.1-py3-none-any.whl

---
+++ packages/hakisto/hakisto-0.1.1-py3-none-any.whl/hakisto/__init__.py
@@ -0,0 +1,30 @@
+#  hakisto - logging reimagined
+#
+#  Copyright (C) 2024  Bernhard Radermacher
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from ._logger import *
+from .colors import *
+from .file_handler import *
+from .handler import *
+from .indented_mixin import *
+from .severity import *
+from .single_line_mixin import *
+from .stderr_handler import *
+from .stream import *
+from .subject import *
+from .topic import *
+
+
