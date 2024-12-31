bbdca299f1fbc9915158ca0df65adcc7c3059da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:26 +0000

    Add package SuarimHelloPack/0.1.0/SuarimHelloPack-0.1.0-py3-none-any.whl

---
+++ packages/SuarimHelloPack/SuarimHelloPack-0.1.0-py3-none-any.whl/Suarimhellopack/hello.py
@@ -0,0 +1,8 @@
+# SuarimHelloPack/hello.py
+
+class HelloWorld:
+    def __init__(self, name: str = "World"):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, {self.name}!")
