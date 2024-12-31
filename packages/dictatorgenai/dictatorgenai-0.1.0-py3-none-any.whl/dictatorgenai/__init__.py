f9d981fde36da4538f4e7c6b801a37e221242af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 06:03:34 +0000

    Add package dictatorgenai/0.1.0/dictatorgenai-0.1.0-py3-none-any.whl

---
+++ packages/dictatorgenai/dictatorgenai-0.1.0-py3-none-any.whl/dictatorgenai/__init__.py
@@ -0,0 +1,46 @@
+# dictatorgenai/__init__.py
+__version__ = "0.1.0"
+
+from .agents.base_agent import BaseAgent
+from .agents.tool import tool
+from .agents.dictator import Dictator
+from .agents.general import General, TaskExecutionError
+from .regimes.regime import Regime, RegimeExecutionError
+from .command_chains.command_chain import CommandChain
+from .command_chains.default_command_chain import DefaultCommandChain
+from .conversations.base_conversation import BaseConversation
+from .conversations.group_chat import GroupChat
+from .conversations.nested_chat import NestedChat
+from .conversations.sequential_chat import SequentialChat
+from .conversations.two_agent_chat import TwoAgentChat
+from .events import BaseEventManager, EventManager, Event
+from .models.base_model import BaseModel, Message
+from .models.openai_model import OpenaiModel
+from .config.settings import DictatorSettings
+
+
+__all__ = [
+    "BaseAgent",
+    "tool",
+    "Dictator",
+    "General",
+    "TaskExecutionError",
+    "Regime",
+    "RegimeExecutionError",
+    "FailedAttemptsCondition",
+    "SpecificTaskFailureCondition",
+    "CommandChain",
+    "DefaultCommandChain",
+    "BaseModel",
+    "OpenaiModel",
+    "Message",
+    "BaseConversation",
+    "GroupChat",
+    "NestedChat",
+    "SequentialChat",
+    "TwoAgentChat",
+    "BaseEventManager",
+    "EventManager",
+    "Event",
+    "DictatorSettings",
+]
