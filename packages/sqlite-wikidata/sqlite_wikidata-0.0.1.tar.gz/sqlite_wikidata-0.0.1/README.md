567b6cf17d2c0a8db5c589ca38e596c5de5e56d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:02 +0000

    Add package sqlite-wikidata/0.0.1/sqlite_wikidata-0.0.1.tar.gz

---
+++ packages/sqlite-wikidata/sqlite_wikidata-0.0.1.tar.gz/sqlite_wikidata-0.0.1/README.md
@@ -0,0 +1,21 @@
+# sqlite-wikidata
+
+[![PyPI - Version](https://img.shields.io/pypi/v/sqlite-wikidata.svg)](https://pypi.org/project/sqlite-wikidata)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/sqlite-wikidata.svg)](https://pypi.org/project/sqlite-wikidata)
+
+-----
+
+## Table of Contents
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install sqlite-wikidata
+```
+
+## License
+
+`sqlite-wikidata` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/sqlite-wikidata/sqlite_wikidata-0.0.1.tar.gz/sqlite_wikidata-0.0.1/src/sqlite_wikidata/build_index.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def build_index(db_path):
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+    print('Database connected.')
+
+    # Function to create indexes
+    def create_index(conn, table, columns, unique=False):
+        unique_str = "UNIQUE" if unique else ""
+        index_name = f"{'_'.join(columns)}_index"
+        columns_str = ", ".join(columns)
+        sql = f"CREATE {unique_str} INDEX IF NOT EXISTS {index_name} ON {table} ({columns_str})"
+        conn.execute(sql)
+
+    # Creating indexes for WikipediaArticle
+    print('wikidataentityensitelink entity_id')
+    create_index(conn, 'wikidataentityensitelink', ['entity_id'])
+
+    print('wikidataentitylabel entity_id language')
+    create_index(conn, 'wikidataentitylabel', ['entity_id', 'language'])
+
+    print('wikidataentitylabel language value')
+    create_index(conn, 'wikidataentitylabel', ['language', 'value'])
+
+    print('wikidataentitydescriptions entity_id language')
+    create_index(conn, 'wikidataentitydescriptions', ['entity_id', 'language'])
+
+    print('wikidataentityaliases entity_id language')
+    create_index(conn, 'wikidataentityaliases', ['entity_id', 'language'])
+
+    print('wikidataclaim source_entity property_id target_entity')
+    create_index(conn, 'wikidataclaim', ['source_entity', 'property_id', 'target_entity'])
+
+    print('wikidataclaim property_id target_entity')
+    create_index(conn, 'wikidataclaim', ['property_id', 'target_entity'])
+
+    print('wikidataclaim target_entity')
+    create_index(conn, 'wikidataclaim', ['target_entity'])
+
+    print('Indexes created.')
+
+    # Close the connection
+    conn.close()
+    print('Database connection closed.')
