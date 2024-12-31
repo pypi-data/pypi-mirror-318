3ac2aedaff0bd81fdf1eb1011aade342625ea8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:03:26 +0000

    Add package gbnf/0.0.12/gbnf-0.0.12-py3-none-any.whl

---
+++ packages/gbnf/gbnf-0.0.12-py3-none-any.whl/gbnf/grammar_graph/graph_pointer.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Generic, TypeVar
+
+from .grammar_graph_types import UnresolvedRule
+
+if TYPE_CHECKING:
+    from .graph_node import GraphNode
+
+T = TypeVar("T", bound=UnresolvedRule)
+
+
+class GraphPointer(Generic[T]):
+    node: GraphNode[T]
+    parent: GraphPointer | None = None
+    id: str
+
+    def __init__(self, node: GraphNode[T], parent: GraphPointer | None = None):
+        if node is None:
+            raise ValueError("Node is undefined")
+        self.node = node
+        self.parent = parent
+        self.id = parent.id if parent else node.id
+
+    @property
+    def rule(self) -> T:
+        return self.node.rule
