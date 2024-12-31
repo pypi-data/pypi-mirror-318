efe8c9cd9644d2675c26b5e1dd92e08f522e4620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:20 +0000

    Add package unify-live-telegram-integration-api-client/0.0.7/unify_live_telegram_integration_api_client-0.0.7.tar.gz

---
+++ packages/unify-live-telegram-integration-api-client/unify_live_telegram_integration_api_client-0.0.7.tar.gz/unify_live_telegram_integration_api_client-0.0.7/unify_live_telegram_integration_api_client/models/bot_created_response.py
@@ -0,0 +1,75 @@
+from typing import Any, TypeVar
+from uuid import UUID
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+T = TypeVar("T", bound="BotCreatedResponse")
+
+
+@_attrs_define
+class BotCreatedResponse:
+    """
+    Attributes:
+        uuid (UUID): UUID of the bot
+        bot_user_name (str): Bot user name
+        bot_title (str): Bot title
+    """
+
+    uuid: UUID
+    bot_user_name: str
+    bot_title: str
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        uuid = str(self.uuid)
+
+        bot_user_name = self.bot_user_name
+
+        bot_title = self.bot_title
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "uuid": uuid,
+                "bot_user_name": bot_user_name,
+                "bot_title": bot_title,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        d = src_dict.copy()
+        uuid = UUID(d.pop("uuid"))
+
+        bot_user_name = d.pop("bot_user_name")
+
+        bot_title = d.pop("bot_title")
+
+        bot_created_response = cls(
+            uuid=uuid,
+            bot_user_name=bot_user_name,
+            bot_title=bot_title,
+        )
+
+        bot_created_response.additional_properties = d
+        return bot_created_response
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
