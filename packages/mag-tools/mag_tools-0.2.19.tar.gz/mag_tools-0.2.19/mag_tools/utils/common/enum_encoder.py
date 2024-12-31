2e8b10a234e0abf21baa6c4f3027533088433db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:14 +0000

    Add package mag-tools/0.2.19/mag_tools-0.2.19.tar.gz

---
+++ packages/mag-tools/mag_tools-0.2.19.tar.gz/mag_tools-0.2.19/mag_tools/utils/common/enum_encoder.py
@@ -0,0 +1,10 @@
+import json
+from enum import Enum
+
+
+
+class EnumEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Enum):
+            return obj.name
+        return super().default(obj)
