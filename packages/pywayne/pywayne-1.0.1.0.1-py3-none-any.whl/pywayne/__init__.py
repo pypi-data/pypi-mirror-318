ce152a53cb249a994b1c7bfd4032d0fa8f01ea0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:22 +0000

    Add package pywayne/1.0.1.0.1/pywayne-1.0.1.0.1-py3-none-any.whl

---
+++ packages/pywayne/pywayne-1.0.1.0.1-py3-none-any.whl/pywayne/__init__.py
@@ -0,0 +1,19 @@
+# from . import tools, tt_api
+
+__all__ = [
+    'tools',
+    'dsp',
+    'gui',
+    'math',
+    'data_structure',
+    'plot',
+    'ahrs',
+    'calibration',
+    'lark_custom_bot',
+    'lark_bot',
+    'tts',
+    # 'tt_api',
+    # 'ocs_api',
+]
+
+name = 'pywayne'
