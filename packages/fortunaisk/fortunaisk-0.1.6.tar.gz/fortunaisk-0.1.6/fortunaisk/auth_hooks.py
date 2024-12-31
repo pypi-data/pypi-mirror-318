f6e214c2ee0d1d49eb94c03b37fa72ad15944c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 06:03:37 +0000

    Add package fortunaisk/0.1.6/fortunaisk-0.1.6.tar.gz

---
+++ packages/fortunaisk/fortunaisk-0.1.6.tar.gz/fortunaisk-0.1.6/fortunaisk/auth_hooks.py
@@ -0,0 +1,40 @@
+# fortunaisk/auth_hooks.py
+"""
+Alliance Auth hooks for adding FortunaIsk to the navigation menu.
+"""
+
+# Alliance Auth
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+from . import urls
+
+
+class FortunaIskMenu(MenuItemHook):
+    """
+    Adds a menu item for FortunaIsk in the Alliance Auth navigation,
+    visible to users with the appropriate permissions.
+    """
+
+    def __init__(self):
+        super().__init__(
+            "FortunaIsk",
+            "fas fa-ticket-alt fa-fw",
+            "fortunaisk:lottery",
+            navactive=["fortunaisk:lottery"],
+        )
+
+    def render(self, request):
+        if request.user.has_perm("fortunaisk.view_ticketpurchase"):
+            return super().render(request)
+        return ""
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    return FortunaIskMenu()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    return UrlHook(urls, "fortunaisk", r"^fortunaisk/")
