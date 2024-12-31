53e3ef2c4a0800a3c86ef51bdf870b70a507314e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:03 +0000

    Add package httpdbg/0.29.2/httpdbg-0.29.2.tar.gz

---
+++ packages/httpdbg/httpdbg-0.29.2.tar.gz/httpdbg-0.29.2/httpdbg/mode_script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import importlib
+import sys
+import traceback
+from typing import List
+
+
+def run_script(argv: List[str]) -> None:
+    sys.argv = argv
+    if len(argv) == 0:
+        exit("script mode - error - python file required, but none set")
+    try:
+        spec = importlib.util.spec_from_file_location("__main__", argv[0])  # type: ignore
+        module = importlib.util.module_from_spec(spec)  # type: ignore
+    except AttributeError:
+        exit("script mode - error - the first argument shall be a python file")
+    try:
+        spec.loader.exec_module(module)  # type: ignore
+    except Exception:
+        traceback.print_exc()
---
+++ packages/httpdbg/httpdbg-0.29.2.tar.gz/httpdbg-0.29.2/httpdbg/webapp/static/main.js
@@ -0,0 +1,109 @@
+"use strict";
+
+let httpdbgApp = {
+    "config": {
+        "hide_group": {
+            "checkbox": "cinitiator",
+            "param": "hi",
+            "css": ".initiator {display: none;}",
+            "value": false
+        },
+        "hide_netloc": {
+            "checkbox": "curl",
+            "param": "hn",
+            "css": ".netloc {display: none;}",
+            "value": false
+        },
+        "hide_tag": {
+            "checkbox": "ctag",
+            "param": "ht",
+            "css": ".tag {display: none;}",
+            "value": false
+        },        
+        "details_wrap_default": {
+            "checkbox": "cwrap",
+            "param": "wl",
+            "elt_name": "ckwraptext",
+            "value": false
+        },
+        "details_raw_default": {
+            "checkbox": "craw",
+            "param": "rd",
+            "elt_name": "ckrawdata",
+            "value": false
+        }
+    }
+}
+
+function wait_for(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// use keys sequences as shortcut
+var keys_history = "";
+window.addEventListener('keydown', (event) => {
+
+    if (document.activeElement) {
+        if (document.activeElement.tagName.toLowerCase() === "input") {
+            if (document.activeElement.getAttribute("type").toLowerCase() === "text") {
+                return;
+            }
+        }
+    }
+
+    keys_history += event.key.toLowerCase();
+
+    if (keys_history.endsWith("rh")) {
+        opentab_headers();
+    }
+
+    if (keys_history.endsWith("rc")) {
+        opentab_cookies();
+    }
+
+    if (keys_history.endsWith("rp")) {
+        opentab_request();
+    }
+
+    if (keys_history.endsWith("rt")) {
+        opentab_response();
+    }
+
+    if (keys_history.endsWith("re")) {
+        opentab_exception();
+    }
+
+    if (keys_history.endsWith("rs")) {
+        opentab_stack();
+    }
+
+    if (keys_history.endsWith("oc")) {
+        config();
+    }
+
+    if (keys_history.endsWith("oh")) {
+        help();
+    }
+
+    if (keys_history.endsWith("lf")) {
+        select_first_request();
+    }
+
+    if (keys_history.endsWith("ll")) {
+        select_last_request();
+    }
+
+    if (keys_history.endsWith("ln")) {
+        select_next_request();
+    }
+
+    keys_history = keys_history.substr(-8, 8)
+});
+
+function help() {
+    if (document.getElementById("help").style.display == "block") {
+        document.getElementById("help").style.display = "none";
+    } else {
+        document.getElementById("help").style.display = "block";
+    }
+}
