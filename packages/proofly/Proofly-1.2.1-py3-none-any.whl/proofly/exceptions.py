6275c18674c7541edf82030ed7baffbce6520c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:19 +0000

    Add package proofly/1.2.1/Proofly-1.2.1-py3-none-any.whl

---
+++ packages/proofly/Proofly-1.2.1-py3-none-any.whl/proofly/exceptions.py
@@ -0,0 +1,18 @@
+from typing import Optional, List
+
+class ValidationError(Exception):
+    def __init__(self, message: str, invalid_fields: Optional[List[str]] = None):
+        super().__init__(message)
+        self.message = message
+        self.invalid_fields = invalid_fields or []
+
+class ConfigurationError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+class AnalysisError(Exception):
+    def __init__(self, message: str, error_code: Optional[str] = None):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
