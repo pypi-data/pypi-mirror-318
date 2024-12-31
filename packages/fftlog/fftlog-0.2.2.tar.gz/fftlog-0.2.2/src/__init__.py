b652621e889eb0168809ee49ebac04e58c7343c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:24 +0000

    Add package fftlog/0.2.2/fftlog-0.2.2.tar.gz

---
+++ packages/fftlog/fftlog-0.2.2.tar.gz/fftlog-0.2.2/src/__init__.py
@@ -0,0 +1,8 @@
+from fftlog._fftlog import fhti, fftl, fht, fhtq
+
+__all__ = ['fhti', 'fftl', 'fht', 'fhtq']
+
+# Version
+# Not currently using setuptools_scm, as in pyfftlog, because of meson-setup.
+# Has to be adjusted in pyproject.toml too!
+__version__ = '0.2.2'
