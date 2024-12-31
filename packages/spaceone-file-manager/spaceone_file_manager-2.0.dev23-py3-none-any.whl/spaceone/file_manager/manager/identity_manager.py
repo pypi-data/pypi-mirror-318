7541e79395a057952774ea1896c883c2532913f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:04:02 +0000

    Add package spaceone-file-manager/2.0.dev23/spaceone_file_manager-2.0.dev23-py3-none-any.whl

---
+++ packages/spaceone-file-manager/spaceone_file_manager-2.0.dev23-py3-none-any.whl/spaceone/file_manager/manager/identity_manager.py
@@ -0,0 +1,23 @@
+import logging
+
+from spaceone.core import config
+from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class IdentityManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_connector: SpaceConnector = self.locator.get_connector(
+            SpaceConnector, service="identity"
+        )
+
+    def check_workspace(self, workspace_id, domain_id):
+        system_token = config.get_global("TOKEN")
+        return self.identity_connector.dispatch(
+            "Workspace.check",
+            {"workspace_id": workspace_id, "domain_id": domain_id},
+            token=system_token,
+        )
