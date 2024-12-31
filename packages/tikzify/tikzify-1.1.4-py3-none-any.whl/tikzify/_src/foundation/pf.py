65c2aba50f4541e0af619a660b96bc9c6f30a4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 06:03:37 +0000

    Add package tikzify/1.1.4/tikzify-1.1.4-py3-none-any.whl

---
+++ packages/tikzify/tikzify-1.1.4-py3-none-any.whl/tikzify/_src/foundation/pf.py
@@ -0,0 +1,21 @@
+import sys
+from typing import IO, Any
+
+from .formatter import formatter
+
+__all__ = ['pf']
+
+
+def pf(string_to_be_printed: str,
+       file: IO[str] = sys.stdout,
+       end: str = '\n',
+       **kwargs: Any
+       ) -> None:
+    """Format the string using formatter and print it to file with the given ending character."""
+    print(formatter(string_to_be_printed, **kwargs), end=end, file=file)
+
+
+def tikz_option(name: str, value: str | None) -> str | None:
+    if value is None:
+        return None
+    return f"{name}={value}"
