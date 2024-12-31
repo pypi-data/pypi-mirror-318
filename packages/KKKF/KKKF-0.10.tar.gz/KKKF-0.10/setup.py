a297e3dd612cc9d108be38b7125cb31b9e3ec639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:04:02 +0000

    Add package KKKF/0.10/KKKF-0.10.tar.gz

---
+++ packages/KKKF/KKKF-0.10.tar.gz/KKKF-0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KKKF",
+    version="0.10",
+    packages=find_packages(),
+    install_requires=[
+        "numpy", 
+        "scipy", 
+        "scikit-learn"
+    ],
+    author="Diego Olguin-Wende",
+    author_email="dolguin@dim.uchile.cl",  
+    description="KKKF: a library for Python implementation of Kernel-Koopman-Kalman Filter.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/diegoolguinw/KKKF",  
+    classifiers=[                      
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
