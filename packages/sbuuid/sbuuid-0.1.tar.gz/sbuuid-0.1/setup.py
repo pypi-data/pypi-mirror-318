227350a60e4821940c716a4428e83bf9f56d423b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:48 +0000

    Add package sbuuid/0.1/sbuuid-0.1.tar.gz

---
+++ packages/sbuuid/sbuuid-0.1.tar.gz/sbuuid-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sbuuid",
+    version="0.1",
+    packages=find_packages(),
+    description="A package to generate globally unique IDs",
+    author="Shouvik Bajpayee",
+    author_email="shouvikbajpayee.private@gmail.com",
+    url="https://github.com/shouvikbj/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
