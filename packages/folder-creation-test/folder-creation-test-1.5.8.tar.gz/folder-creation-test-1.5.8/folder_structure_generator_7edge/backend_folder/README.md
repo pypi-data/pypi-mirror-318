09e9ef39bce1d79eeabca913cbaf47882e8deaca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:35 +0000

    Add package folder-creation-test/1.5.8/folder-creation-test-1.5.8.tar.gz

---
+++ packages/folder-creation-test/folder-creation-test-1.5.8.tar.gz/folder-creation-test-1.5.8/folder_structure_generator_7edge/backend_folder/README.md
@@ -0,0 +1,4 @@
+# Belina Payroll Backend APIs
+git config --global core.hooksPath .githooks/
+
+chmod +x .githooks/commit-msg
---
+++ packages/folder-creation-test/folder-creation-test-1.5.8.tar.gz/folder-creation-test-1.5.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+    name='folder-creation-test',
+    version='1.5.8',
+    description='CLI tool to generate folder structures for services',
+    author='AABK',
+    author_email='akhilesh.b@7edge.com',
+    url='https://github.com/yourusername/folder-structure-generator-7edge',
+    packages=find_packages(),
+    include_package_data=True,  # Include files specified in MANIFEST.in
+    package_data={
+        # Include specific file types
+        '': ['*.yml', '*.json', 'backend_folder/*', 'backend_folder/.gitignore', 'backend_folder/*/*'],
+        'folder_structure_generator_7edge': ['backend_folder/**/*'],
+
+
+    },
+    install_requires=[
+        'InquirerPy',
+        'pyyaml',  # Add pyyaml for YAML file handling
+    ],
+    entry_points={
+        'console_scripts': [
+            # Additional command
+            'start=folder_structure_generator_7edge.generator:create_folder_structure_with_files',
+            'create=folder_structure_generator_7edge.generator:create_service_structure',
+
+        ],
+    },
+)
