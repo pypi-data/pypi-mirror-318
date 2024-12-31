558c1027fecc0ab7f99051d18e00477ab5fa3709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:39 +0000

    Add package RCAIDE-LEADS/0.3.0/RCAIDE_LEADS-0.3.0-py3-none-any.whl

---
+++ packages/RCAIDE-LEADS/RCAIDE_LEADS-0.3.0-py3-none-any.whl/RCAIDE/Library/Mission/Segments/Single_Point/__init__.py
@@ -0,0 +1,14 @@
+# RCAIDE/Methods/Mission/Segments/Single_Point/__init__.py
+# 
+
+""" RCAIDE Package Setup
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+
+from . import Set_Speed_Set_Altitude
+from . import Set_Speed_Set_Throttle
+from . import Set_Speed_Set_Altitude_No_Propulsion
+from . import Set_Speed_Set_Altitude_AVL_Trimmed
