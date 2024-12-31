9fb18d75583ba2c1c58e1df3eb7af437d0a5b425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:25 +0000

    Add package jstreams/1.0.14/jstreams-1.0.14-py3-none-any.whl

---
+++ packages/jstreams/jstreams-1.0.14-py3-none-any.whl/jstreams/__init__.py
@@ -0,0 +1,43 @@
+from jstreams.stream import (
+    each,
+    dictUpdate,
+    Stream,
+    findFirst,
+    mapIt,
+    matching,
+    flatMap,
+    reduce,
+    takeWhile,
+    dropWhile,
+    isNotNone,
+    sort,
+    Opt,
+    ClassOps,
+    stream,
+    optional,
+)
+
+from jstreams.tryOpt import (
+    Try,
+    ErrorLog,
+)
+__all__ = [
+    "each",
+    "dictUpdate",
+    "Stream",
+    "findFirst",
+    "mapIt",
+    "matching",
+    "flatMap",
+    "reduce",
+    "takeWhile",
+    "dropWhile",
+    "isNotNone",
+    "sort",
+    "Opt",
+    "ClassOps",
+    "stream",
+    "optional",
+    "Try",
+    "ErrorLog",
+]
