8e3ed4089db5ac3090aa91294c928effdc990501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 15:04:37 +0000

    Add package lensedGW/1.0.4/lensedgw-1.0.4.tar.gz

---
+++ packages/lensedGW/lensedgw-1.0.4.tar.gz/lensedgw-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name= "lensedGW", # 包名，发布后可用 pip 安装
+    version= "1.0.4", # 版本号
+    description="lensed waveform for point mass model",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your_email@example.com",
+    url="https://github.com/yourusername/addone", # 你的项目地址
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    py_modules = ['main'],
+    entry_points = {"pycbc.waveform.fd":"main = main:lumDis",
+                    "pycbc.waveform.fd":"main = main:gravitational_lens",
+                    "pycbc.waveform.fd":"main = main:amp_factor",
+                    "pycbc.waveform.fd":"main = main:lensed_fd_waveform"
+                    },
+)
