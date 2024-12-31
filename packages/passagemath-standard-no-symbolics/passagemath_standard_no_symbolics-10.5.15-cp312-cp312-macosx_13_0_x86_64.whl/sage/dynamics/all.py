94f1e75d0b73858e0e10fe3b214cb85311fe3f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:13 +0000

    Add package passagemath-standard-no-symbolics/10.5.15/passagemath_standard_no_symbolics-10.5.15-cp312-cp312-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.5.15-cp312-cp312-macosx_13_0_x86_64.whl/sage/dynamics/all.py
@@ -0,0 +1,41 @@
+"""
+Flat surfaces
+
+Some code about flat surfaces and interval exchanges has been removed
+from Sage. The package ``surface_dynamics`` contains all that code
+and much more. For more information, see
+
+    https://www.labri.fr/perso/vdelecro/surface-dynamics/latest/
+
+You can install the ``surface_dynamics`` package via:
+
+    sage -pip install surface_dynamics
+
+If you do not have write access to the Sage installation you can
+alternatively do:
+
+    sage -pip install surface_dynamics --user
+"""
+# install the docstring of this module to the containing package
+from sage.misc.namespace_package import install_doc
+install_doc(__package__, __doc__)
+
+from sage.dynamics.all__sagemath_schemes import *
+
+try:
+    from sage.dynamics.all__sagemath_symbolics import *
+except ImportError:
+    pass
+
+from sage.misc.lazy_import import lazy_import
+
+from sage.dynamics.cellular_automata.all import *
+
+# Discrete dynamical systems
+lazy_import('sage.dynamics.finite_dynamical_system',
+            ['DiscreteDynamicalSystem'])
+
+lazy_import('sage.dynamics', 'finite_dynamical_system_catalog',
+            'finite_dynamical_systems')
+del lazy_import
+del install_doc
