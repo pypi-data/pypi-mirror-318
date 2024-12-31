f85e019e697a23210b63ff60136696307e293f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 04:03:56 +0000

    Add package pvmlib/1.4.4/pvmlib-1.4.4-py3-none-any.whl

---
+++ packages/pvmlib/pvmlib-1.4.4-py3-none-any.whl/pvmlib/info_lib.py
@@ -0,0 +1,23 @@
+class LibraryInfo:
+    version_lib = 'v1.4.4'
+    name = 'pvmlib'
+    author = "Jesus Lizarraga"
+    author_email = "jesus.lizarragav@coppel.com"
+    description = "Python library for PVM"
+    python_requires = '==3.12'
+    env = 'development'
+    
+    install_requires = [
+        "pydantic>=1.8.2,<2.0.0",
+        "typing-extensions>=3.10.0.0",
+        "pytz>=2021.1",
+        "logging",
+        "circuitbreaker>=1.3.2",
+        "requests>=2.25.1,<3.0.0",
+        "tenacity>=8.0.1",
+        "fastapi>=0.68.0",
+        "motor>=2.4.0",
+        "uvicorn>=0.15.0",
+        "urllib3>=1.26.5,<2.0.0",
+        "charset_normalizer>=2.0.0,<3.0.0"
+    ]
