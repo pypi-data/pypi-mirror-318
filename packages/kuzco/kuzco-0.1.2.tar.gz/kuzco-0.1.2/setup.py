cbc35bd1acf6f009608ef4fc6be05dbe1234af39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:43 +0000

    Add package kuzco/0.1.2/kuzco-0.1.2.tar.gz

---
+++ packages/kuzco/kuzco-0.1.2.tar.gz/kuzco-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kuzco",
+    version="0.1.2",
+    author="Yakov Perets",
+    author_email="yakov.perets@gmail.com",
+    description="Tool for managing Python monorepos",
+    # long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",  # Specify Markdown format
+    url="https://gitlab.com/yakov.perets/kuzco",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "kuzco=kuzco.cli.cli:cli",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
