92fcc4c0954adf48047441cafd235afd3c0d15b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:25 +0000

    Add package sqruff/0.22.0/sqruff-0.22.0.tar.gz

---
+++ packages/sqruff/sqruff-0.22.0.tar.gz/sqruff-0.22.0/crates/cli/src/commands_lint.rs
@@ -0,0 +1,36 @@
+use crate::commands::{Format, LintArgs};
+use crate::linter;
+use sqruff_lib::core::config::FluffConfig;
+use std::path::Path;
+
+pub(crate) fn run_lint(
+    args: LintArgs,
+    config: FluffConfig,
+    ignorer: impl Fn(&Path) -> bool + Send + Sync,
+) -> i32 {
+    let LintArgs { paths, format } = args;
+    let mut linter = linter(config, format);
+    linter.lint_paths(paths, false, &ignorer);
+
+    linter.formatter().unwrap().completion_message();
+    if linter.formatter().unwrap().has_fail() {
+        1
+    } else {
+        0
+    }
+}
+
+pub(crate) fn run_lint_stdin(config: FluffConfig, format: Format) -> i32 {
+    let read_in = crate::stdin::read_std_in().unwrap();
+
+    let linter = linter(config, format);
+    linter.lint_string(&read_in, None, false);
+
+    linter.formatter().unwrap().completion_message();
+
+    if linter.formatter().unwrap().has_fail() {
+        1
+    } else {
+        0
+    }
+}
