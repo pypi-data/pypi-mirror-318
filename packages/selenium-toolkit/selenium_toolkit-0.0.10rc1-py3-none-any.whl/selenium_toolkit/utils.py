8aa6e2144d078cb43da1fc6a55a648f68a3f6f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:14 +0000

    Add package selenium-toolkit/0.0.10rc1/selenium_toolkit-0.0.10rc1-py3-none-any.whl

---
+++ packages/selenium-toolkit/selenium_toolkit-0.0.10rc1-py3-none-any.whl/selenium_toolkit/utils.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+def create_locator(query_selector: str) -> tuple:
+    if query_selector[0] == '/':
+        locator = (By.XPATH, query_selector)
+    else:
+        locator = (By.CSS_SELECTOR, query_selector)
+
+    return locator
