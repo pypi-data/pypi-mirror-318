1f4b0920523968ab0e88d4ca5b29c98d9cda2558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 14:03:40 +0000

    Add package komm/0.15.1/komm-0.15.1.tar.gz

---
+++ packages/komm/komm-0.15.1.tar.gz/komm-0.15.1/src/komm/_lossless_coding/__init__.py
@@ -0,0 +1,11 @@
+from .FixedToVariableCode import FixedToVariableCode
+from .HuffmanCode import HuffmanCode
+from .TunstallCode import TunstallCode
+from .VariableToFixedCode import VariableToFixedCode
+
+__all__ = [
+    "FixedToVariableCode",
+    "HuffmanCode",
+    "TunstallCode",
+    "VariableToFixedCode",
+]
