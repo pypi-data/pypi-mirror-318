e76dcd278a1432003a0c16d2e2750fa0d73b8751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 06:03:29 +0000

    Add package audiobackend/0.1.0/audiobackend-0.1.0-py3-none-any.whl

---
+++ packages/audiobackend/audiobackend-0.1.0-py3-none-any.whl/audiobackend/__init__.py
@@ -0,0 +1,11 @@
+"""
+audiobackend - High-quality audio playback library with DSD support
+"""
+
+from .audiobackend import AudioBackend
+
+__version__ = "0.1.0"
+__author__ = "Niamorro"
+__email__ = "niamorrodev@gmail.com"
+
+__all__ = ["audiobackend"]
