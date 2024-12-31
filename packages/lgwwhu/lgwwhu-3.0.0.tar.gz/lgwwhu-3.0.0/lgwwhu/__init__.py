362a1c879889d05aa73a9454f2f33c881981dd09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:21 +0000

    Add package lgwwhu/3.0.0/lgwwhu-3.0.0.tar.gz

---
+++ packages/lgwwhu/lgwwhu-3.0.0.tar.gz/lgwwhu-3.0.0/lgwwhu/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 14:09:14 2024
+
+@author: xiaoxiami
+"""
+
+
+
+from lgwwhu.main import gravitational_lens_with_amplification 
+from lgwwhu.main import generate_waveform
+from lgwwhu.main import compute_lensed_waveform
