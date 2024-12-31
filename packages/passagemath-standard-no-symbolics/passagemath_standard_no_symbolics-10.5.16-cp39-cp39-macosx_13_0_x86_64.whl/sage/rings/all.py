e54c85d1a8986afb051e94c656530ef512b1ea65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 15:04:45 +0000

    Add package passagemath-standard-no-symbolics/10.5.16/passagemath_standard_no_symbolics-10.5.16-cp39-cp39-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.5.16-cp39-cp39-macosx_13_0_x86_64.whl/sage/rings/all.py
@@ -0,0 +1,68 @@
+"""
+Rings
+"""
+# ****************************************************************************
+#       Copyright (C) 2005 William Stein <wstein@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+from sage.misc.lazy_import import lazy_import
+
+from sage.rings.all__sagemath_categories import *
+
+# Finite fields
+from sage.rings.finite_rings.all import *
+
+from sage.rings.all__sagemath_combinat import *
+from sage.rings.all__sagemath_flint import *
+from sage.rings.all__sagemath_gap import *
+from sage.rings.all__sagemath_modules import *
+
+try:
+    from sage.rings.all__sagemath_symbolics import *
+except ImportError:
+    pass
+
+# Function field
+from sage.rings.function_field.all import *
+
+# Semirings
+from sage.rings.semirings.all import *
+
+# Double precision floating point numbers
+from sage.rings.real_double import RealDoubleField, RDF, RealDoubleElement
+
+
+# up to here (#36566)
+
+# Polynomial Rings and Polynomial Quotient Rings
+from sage.rings.polynomial.all import *
+
+# Following will go to all__sagemath_categories.py in #36566
+
+# c-finite sequences
+from sage.rings.cfinite_sequence import CFiniteSequence, CFiniteSequences
+
+from sage.rings.fast_arith import prime_range
+
+# Following will go to all__sagemath_categories.py in #36566
+
+# continued fractions
+from sage.rings.continued_fraction import (continued_fraction,
+                                           continued_fraction_list)
+
+# up to here (#36566)
+
+# asymptotic ring
+# from sage.rings.asymptotic.all import *
+lazy_import('sage.rings.asymptotic.asymptotic_ring', 'AsymptoticRing')
+lazy_import('sage.rings.asymptotic.asymptotic_expansion_generators',
+            'asymptotic_expansions')
+
+# Register classes in numbers abc
+from sage.rings import numbers_abc
+del lazy_import
