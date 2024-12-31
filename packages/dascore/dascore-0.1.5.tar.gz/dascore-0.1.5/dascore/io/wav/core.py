20c8452e0ed0cc62ce357e798980c99340307267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 15:05:04 +0000

    Add package dascore/0.1.5/dascore-0.1.5.tar.gz

---
+++ packages/dascore/dascore-0.1.5.tar.gz/dascore-0.1.5/dascore/io/wav/core.py
@@ -0,0 +1,84 @@
+"""Core module for wave format."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
+from scipy.io.wavfile import write
+
+from dascore.constants import ONE_SECOND, SpoolType
+from dascore.io.core import FiberIO
+from dascore.utils.patch import check_patch_coords
+
+
+class WavIO(FiberIO):
+    """IO support for wav (audio) format."""
+
+    name = "WAV"
+
+    def write(
+        self, spool: SpoolType, resource: str | Path, resample_frequency=None, **kwargs
+    ):
+        """
+        Write the contents of the patch to one or more wav files.
+
+        Parameters
+        ----------
+        resource
+            If a path that ends with .wav, write all the distance channels
+            to a single file. If not, assume the path is a directory and write
+            each distance channel to its own wav file.
+        resample_frequency
+            A resample frequency in Hz. If None, do not perform resampling.
+            Often DAS has non-int sampling rates, so the default resampling
+            rate is usually safe.
+
+        Notes
+        -----
+            - The sampling rate in the wav format must be an int, so the
+            patch's sampling rate is cast to an integer before writing wav file.
+            This may cause some distortion (but it isn't likely to be noticeable).
+
+            - The array data type is converted to np.float32 before writing. This
+            requires values to be between (-1, 1) so the data are detrended
+            and normalized before writing.
+
+            - If a single wavefile is specified with the path argument, and
+            the output the patch has more than one len along the distance
+            dimension, a multi-channel wavefile is created. There may be some
+            players that do not support multi-channel wavefiles.
+
+            - If using VLC, often it won't play the file unless the sampling
+            rate is 44100, in this case just set resample_frequency=44100 to
+            see if this fixes the issue.
+        """
+        resource = Path(resource)
+        assert len(spool) == 1, "Only single patch spools can be written to wav"
+        patch = spool[0]
+        # write a single wav file, maybe multi-channeled.
+        data, sr = self._get_wav_data(patch, resample_frequency)
+        if resource.name.endswith(".wav"):
+            write(filename=str(resource), rate=int(sr), data=data)
+        else:  # write data to directory, one file for each distance
+            resource.mkdir(exist_ok=True, parents=True)
+            distances = patch.coords.get_array("distance")
+            for ind, dist in enumerate(distances):
+                sub_data = np.take(data, ind, axis=1)
+                sub_path = resource / f"{dist}.wav"
+                write(filename=str(sub_path), rate=int(sr), data=sub_data)
+
+    @staticmethod
+    def _get_wav_data(patch, resample):
+        """Pre-condition patch data for writing. Return array and sample rate."""
+        check_patch_coords(patch, ("time", "distance"))
+        assert len(patch.dims) == 2, "only 2D patches supported for this function."
+        # handle resampling and normalization
+        pat = patch.transpose("time", "distance")
+        if resample is not None:
+            pat = pat.resample(time=1 / resample)
+        # normalize and detrend
+        pat = pat.detrend("time", "linear").normalize("time", norm="max")
+        data = pat.data
+        sample_rate = resample or np.round(ONE_SECOND / pat.attrs["time_step"])
+        return data.astype(np.float32), int(sample_rate)
---
+++ packages/dascore/dascore-0.1.5.tar.gz/dascore-0.1.5/tests/test_io/test_wav/test_wav.py
@@ -0,0 +1,45 @@
+"""Tests module for wave format."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+from scipy.io.wavfile import read as read_wav
+
+import dascore as dc
+
+
+class TestWriteWav:
+    """Tests for writing wav format to disk."""
+
+    @pytest.fixture(scope="class")
+    def audio_patch(self):
+        """Return the example sin wave patch."""
+        return dc.get_example_patch("sin_wav", sample_rate=500)
+
+    @pytest.fixture(scope="class")
+    def wave_dir(self, audio_patch, tmp_path_factory):
+        """Create a wave directory, return path."""
+        new = Path(tmp_path_factory.mktemp("wavs"))
+        dc.write(audio_patch, new, "wav")
+        return new
+
+    def test_directory(self, wave_dir, audio_patch):
+        """Sanity checks on wav directory."""
+        assert wave_dir.exists()
+        wavs = list(wave_dir.rglob("*.wav"))
+        assert len(wavs) == len(audio_patch.coords.get_array("distance"))
+
+    def test_write_single_file(self, audio_patch, tmp_path_factory):
+        """Ensure a single file can be written."""
+        path = tmp_path_factory.mktemp("wave_temp") / "temp.wav"
+        dc.write(audio_patch, path, "wav")
+        assert path.exists()
+
+    def test_resample(self, audio_patch, tmp_path_factory):
+        """Ensure resampling changes sampling rate in file."""
+        path = tmp_path_factory.mktemp("wav_resample") / "resampled.wav"
+        dc.write(audio_patch, path, "wav", resample_frequency=1000)
+        (sr, ar) = read_wav(str(path))
+        assert sr == 1000
