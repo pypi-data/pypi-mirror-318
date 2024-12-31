dbcc582db2a50810ec97280cee533e38fafabb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:24 +0000

    Add package hd2api.py/0.0.1.14.1/hd2api.py-0.0.1.14.1-py3-none-any.whl

---
+++ packages/hd2api.py/hd2api.py-0.0.1.14.1-py3-none-any.whl/hd2api/models/Base/__init__.py
@@ -0,0 +1,55 @@
+from .Assignment import Assignment
+from .Campaign import Campaign
+from .Cost import Cost
+from .Effects import PlanetActiveEffects
+from .GalaxyStats import GalaxyStats
+from .GlobalEvent import GlobalEvent
+from .HomeWorld import HomeWorld
+from .JointOperation import JointOperation
+from .NewsFeedItem import NewsFeedItem
+from .PlanetAttack import PlanetAttack
+from .PlanetCoordinates import PlanetCoordinates
+from .PlanetEvent import PlanetEvent
+from .PlanetInfo import PlanetInfo
+from .PlanetStats import PlanetStats
+from .PlanetStatus import PlanetStatus
+from .Reward import Reward
+from .Setting import Setting
+from .SpaceStation import SpaceStation, SpaceStationStatus
+from .SteamNewsRaw import SteamNewsRaw
+from .TacticalActions import TacticalAction
+from .Task import Task
+from .WarId import WarId
+from .WarInfo import WarInfo
+from .WarStatus import WarStatus
+from .WarSummary import WarSummary
+
+
+__all__ = [
+    "Assignment",
+    "Campaign",
+    "Cost",
+    "PlanetActiveEffects",
+    "GalaxyStats",
+    "GlobalEvent",
+    "HomeWorld",
+    "JointOperation",
+    "NewsFeedItem",
+    "PlanetAttack",
+    "PlanetCoordinates",
+    "PlanetEvent",
+    "PlanetInfo",
+    "PlanetStats",
+    "PlanetStatus",
+    "Reward",
+    "Setting",
+    "SpaceStation",
+    "SpaceStationStatus",
+    "SteamNewsRaw",
+    "TacticalAction",
+    "Task",
+    "WarId",
+    "WarInfo",
+    "WarStatus",
+    "WarSummary",
+]
