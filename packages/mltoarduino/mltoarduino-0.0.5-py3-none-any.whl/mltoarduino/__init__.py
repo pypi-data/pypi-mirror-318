0d5186c266a000a1cc16a30ee5fc192b66491cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:05 +0000

    Add package mltoarduino/0.0.5/mltoarduino-0.0.5-py3-none-any.whl

---
+++ packages/mltoarduino/mltoarduino-0.0.5-py3-none-any.whl/mltoarduino/__init__.py
@@ -0,0 +1,4 @@
+
+from .src.mltoarduino import (
+array_to_arduino,LinearRegToC ,get_cpp_code_from_tree,convert_DecTree_To_C,convert_RandForest_To_C,TreesCode,code_trees,XGBOOST_to_CPP,tf_model_to_arduino_code
+)
