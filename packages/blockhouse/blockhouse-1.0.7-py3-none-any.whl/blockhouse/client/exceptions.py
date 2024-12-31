40b53e882ce100816a465ec5e683e5a2b6eeb8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 04:03:11 +0000

    Add package blockhouse/1.0.7/blockhouse-1.0.7-py3-none-any.whl

---
+++ packages/blockhouse/blockhouse-1.0.7-py3-none-any.whl/blockhouse/client/exceptions.py
@@ -0,0 +1,7 @@
+class BlockhouseError(Exception):
+    """Base class for exceptions in the Blockhouse SDK."""
+    pass
+
+class S3UploadError(BlockhouseError):
+    """Exception raised for errors during S3 upload."""
+    pass
