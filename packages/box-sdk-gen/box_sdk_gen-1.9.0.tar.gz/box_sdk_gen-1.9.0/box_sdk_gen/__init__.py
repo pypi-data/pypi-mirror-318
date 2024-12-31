62878c8953e686c309a8ed7e9a81cd57769a05a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 04:03:07 +0000

    Add package box-sdk-gen/1.9.0/box_sdk_gen-1.9.0.tar.gz

---
+++ packages/box-sdk-gen/box_sdk_gen-1.9.0.tar.gz/box_sdk_gen-1.9.0/box_sdk_gen/__init__.py
@@ -0,0 +1,13 @@
+from box_sdk_gen.box import *
+
+from box_sdk_gen.serialization import *
+
+from box_sdk_gen.internal import *
+
+from box_sdk_gen.networking import *
+
+from box_sdk_gen.schemas import *
+
+from box_sdk_gen.managers import *
+
+from box_sdk_gen.client import *
