7625f517c470bfe9e86b94b11dccfcba446c09d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 16:03:53 +0000

    Add package bdshare/1.1.0/bdshare-1.1.0-py3-none-any.whl

---
+++ packages/bdshare/bdshare-1.1.0-py3-none-any.whl/bdshare/util/store.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+import pandas as pd
+from datetime import datetime
+import os
+
+
+class Store(object):
+    """
+        Store dataframe
+        :param data: dataframe,
+        :param name: str,
+        :param path: str,
+        :return: none
+    """
+    def __init__(self, data=None, name=None, path=None):
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        else:
+            raise RuntimeError('data type is incorrect')
+        self.name = name
+        self.path = path
+
+    def save(self, to='csv'):
+        if self.name is None:
+            self.name = f'{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+
+        file_path = '%s%s%s.%s'
+        if isinstance(self.name, str) and self.name != '':
+            if (self.path is None) or (self.path == ''):
+                file_path = '.'.join([self.name, to])
+                self.data.to_csv(file_path, index=False)
+            else:
+                try:
+                    if os.path.exists(self.path) is False:
+                        os.mkdir(self.path)
+                    file_path = file_path % (self.path, '', self.name, to)
+                    self.data.to_csv(file_path, index=False)
+                except:  # noqa: E722
+                    pass
+        else:
+            print('input error')
---
+++ packages/bdshare/bdshare-1.1.0-py3-none-any.whl/tests/test_random.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_
+'''
+Created on 2021-May-29
+@author: Raisul Islam
+'''
+from bdshare import get_current_trade_data
+
+df = get_current_trade_data()
+print(df.to_json(orient='records'))
+print(df.to_string())
