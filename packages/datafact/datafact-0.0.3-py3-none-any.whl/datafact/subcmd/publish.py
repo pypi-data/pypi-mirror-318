c38b274d63a1c19f709c30dd9bf3dcd373dbca4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:02 +0000

    Add package datafact/0.0.3/datafact-0.0.3-py3-none-any.whl

---
+++ packages/datafact/datafact-0.0.3-py3-none-any.whl/datafact/subcmd/publish.py
@@ -0,0 +1,77 @@
+import os
+import sys
+
+import click
+
+import dataset_sh as dsh
+
+from datafact.proj import DEFAULT_TARGET, DatasetLocalTarget, DatasetRemoteTarget, load_config, locate_draft_file
+
+
+def load_readme():
+    readme_path = './DATASET_README.md'
+    if os.path.exists(readme_path):
+        with open(readme_path, 'r') as f:
+            return f.read()
+    return None
+
+
+def publish_local(source, target: DatasetLocalTarget, tags: list[str]):
+    dataset = dsh.dataset(target['name'])
+    dataset.import_file(
+        source,
+        tags=tags
+    )
+    readme = load_readme()
+    if readme:
+        dataset.set_readme(readme)
+
+
+def publish_remote(source, target: DatasetRemoteTarget, tags: list[str]):
+    dataset = dsh.remote(target['host'], profile=target['profile']).dataset(target['name'])
+    dataset.upload_from_file(source, tags=tags)
+    readme = load_readme()
+    if readme:
+        dataset.set_readme(readme)
+
+
+@click.command(name='publish')
+@click.option('--source', '-s',
+              default='draft', type=str,
+              help="source dataset draft name.")
+@click.option('--target', '-t',
+              default=DEFAULT_TARGET, type=str,
+              help="publishing target name.")
+@click.option('--tag',
+              multiple=True,
+              help="List of tags. e.g., --tag tag1 --tag tag2.")
+def publish_cli(source, target, tag):
+    """
+    publish dataset draft to target.
+    """
+    cfg = load_config()
+    t = cfg.get_target(target)
+
+    tags = list(tag)
+
+    if t is None:
+        click.secho(f'Target {target} not found.', fg='red')
+        sys.exit(1)
+
+    source_file = locate_draft_file(source)
+
+    if not os.path.exists(source_file):
+        click.secho(f'Draft dataset file {source_file} not found.', fg='red')
+        sys.exit(1)
+
+    if t['type'] == 'local':
+        publish_local(source_file, t, tags)
+        click.secho(f'done.\n', fg='green')
+        click.secho("Hint: use the following command to view the published dataset:\n")
+        click.secho(f"dataset.sh gui {t['name']}\n", fg='cyan')
+    elif t['type'] == 'remote':
+        publish_remote(source_file, t, tags)
+        click.secho(f'done.', fg='green')
+    else:
+        click.secho(f'Unknown target type: {t["type"]}', fg='red')
+        sys.exit(1)
