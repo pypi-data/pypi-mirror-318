57ba2b8d90a84273a351c68d6e4607e7789dc42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:03:13 +0000

    Add package pvmlib/1.4.7/pvmlib-1.4.7-py3-none-any.whl

---
+++ packages/pvmlib/pvmlib-1.4.7-py3-none-any.whl/pvmlib/info_lib.py
@@ -0,0 +1,8 @@
+class LibraryInfo:
+    version_lib = 'v1.4.7'
+    name = 'pvmlib'
+    author = "Jesus Lizarraga"
+    author_email = "jesus.lizarragav@coppel.com"
+    description = "Python library for PVM"
+    python_requires = '>=3.12'
+    env = 'development'
