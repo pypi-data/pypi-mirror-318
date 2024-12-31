eae89bcd00ce9a1c75e9840c5b362e9f058d6d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 05:04:18 +0000

    Add package RCAIDE-TEST/0.0.0/rcaide_test-0.0.0.tar.gz

---
+++ packages/RCAIDE-TEST/rcaide_test-0.0.0.tar.gz/rcaide_test-0.0.0/RCAIDE/Framework/Optimization/Packages/__init__.py
@@ -0,0 +1,18 @@
+# RCAIDE/Optimization/Packages/__init__.py
+# 
+
+""" RCAIDE Package Setup
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ---------------------------------------------------------------------------------------------------------------------- 
+
+from . import additive 
+from . import ipopt 
+from . import particle_swarm
+from . import pyopt 
+from . import pyopt_sparse 
+from . import pyopt_surrogate
+from . import scipy
+from . import trmm
