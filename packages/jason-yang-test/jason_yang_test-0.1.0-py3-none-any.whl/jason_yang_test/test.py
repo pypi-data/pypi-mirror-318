c674c383b542883cd46dc57fe1867b10057b8782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 04:03:30 +0000

    Add package jason-yang-test/0.1.0/jason_yang_test-0.1.0-py3-none-any.whl

---
+++ packages/jason-yang-test/jason_yang_test-0.1.0-py3-none-any.whl/jason_yang_test/test.py
@@ -0,0 +1,12 @@
+def calculate_sum(a, b):
+    """
+    计算两个数的和
+    """
+    return a + b
+
+def show_result():
+    """
+    展示计算结果的函数
+    """
+    result = calculate_sum(10, 20)
+    print(f"计算结果是: {result}")
