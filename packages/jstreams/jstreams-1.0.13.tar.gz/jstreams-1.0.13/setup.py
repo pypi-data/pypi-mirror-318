21b9ba424742dce445a03a1dabe513eeb1456044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-31 03:06:27 +0000

    Add package jstreams/1.0.13/jstreams-1.0.13.tar.gz

---
+++ packages/jstreams/jstreams-1.0.13.tar.gz/jstreams-1.0.13/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="jstreams",
+    author="Cristian Trohin",
+    version="1.0.13",
+    package_data={"jstreams": ["py.typed"]},
+    packages=["jstreams"]
+)
