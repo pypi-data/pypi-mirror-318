67ea17a9b79a71e0672fe58da8312dc9f5b29927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:34 +0000

    Add package qdesc/0.0.1/qdesc-0.0.1.tar.gz

---
+++ packages/qdesc/qdesc-0.0.1.tar.gz/qdesc-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qdesc',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g., pandas if your function requires it
+    ],
+    author='Paolo Hilado',
+    author_email='datasciencepgh@proton.me',
+    description='A brief description of your package'
+)
