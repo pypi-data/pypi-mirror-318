745180b10a2faba829d0eea13d3104aa109ddb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:43 +0000

    Add package mouse-keyboard-tracker/0.1.2/mouse_keyboard_tracker-0.1.2.tar.gz

---
+++ packages/mouse-keyboard-tracker/mouse_keyboard_tracker-0.1.2.tar.gz/mouse_keyboard_tracker-0.1.2/mouse_keyboard_tracker/log_manager.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import time
+from datetime import datetime
+from mouse_keyboard_tracker.config import Config
+
+class LogManager:
+
+    @staticmethod
+    def verificar_ociosidade(horario_inicio):
+        """Verifica se houve períodos de ociosidade e registra no log."""
+        try:
+            ociosidade_log_path = os.path.join(Config.LOG_DIR, Config.OCIOSIDADE_LOG)
+            if not os.path.exists(ociosidade_log_path) or os.path.getsize(ociosidade_log_path) == Config.TAMANHO_MINIMO_ARQUIVO:
+                with open(ociosidade_log_path, 'a') as log_file:
+                    horario_fim = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    mensagem = f'Não foram registrados períodos de inatividade entre {horario_inicio} e {horario_fim}.'
+                    log_file.write(mensagem + '\n')
+                    print(mensagem)
+        except Exception as e:
+            mensagem = f"Erro ao verificar ociosidade: {e}"
+            print(mensagem)
+
+    @staticmethod
+    def esperar_arquivo_preenchido(arquivo_path):
+        """Espera até que o arquivo especificado seja preenchido."""
+        try:
+            while not os.path.exists(arquivo_path) or os.path.getsize(arquivo_path) == Config.TAMANHO_MINIMO_ARQUIVO:
+                print(f"Aguardando preenchimento do arquivo: {arquivo_path}")
+                time.sleep(Config.INTERVALO_VERIFICACAO_ARQUIVO)
+        except Exception as e:
+            mensagem = f"Erro ao esperar arquivo preenchido: {e}"
+            print(mensagem)
+
+    @staticmethod
+    def determinar_periodo():
+        """Determina o período do dia (matutino, vespertino, noturno)."""
+        try:
+            hora_atual = datetime.now().hour
+            if Config.HORA_INICIO_MATUTINO <= hora_atual < Config.HORA_INICIO_VESPERTINO:
+                return 'matutino'
+            elif Config.HORA_INICIO_VESPERTINO <= hora_atual < Config.HORA_INICIO_NOTURNO:
+                return 'vespertino'
+            else:
+                return 'noturno'
+        except Exception as e:
+            mensagem = f"Erro ao determinar período: {e}"
+            print(mensagem)
+            return 'desconhecido'
+
+    @staticmethod
+    def mover_logs_para_relatorios(horario_inicio):
+        """Move os logs para o diretório de relatórios."""
+        try:
+            LogManager.verificar_ociosidade(horario_inicio)
+
+            ociosidade_log_path = os.path.join(Config.LOG_DIR, Config.OCIOSIDADE_LOG)
+            LogManager.esperar_arquivo_preenchido(ociosidade_log_path)
+
+            timestamp = datetime.now().strftime('%Y-%m-%d')
+            periodo = LogManager.determinar_periodo()
+            horario_inicio = horario_inicio.replace(':', '_')
+            destino = os.path.join(Config.RELATORIOS_DIR, timestamp, periodo, horario_inicio)
+            os.makedirs(destino, exist_ok=True)
+
+            for arquivo in os.listdir(Config.LOG_DIR):
+                if arquivo.endswith('.log'):
+                    shutil.move(os.path.join(Config.LOG_DIR, arquivo), os.path.join(destino, arquivo))
+                    print(f"Arquivo {arquivo} movido para {destino}")
+        except Exception as e:
+            mensagem = f"Erro ao mover logs para relatórios: {e}"
+            print(mensagem)
---
+++ packages/mouse-keyboard-tracker/mouse_keyboard_tracker-0.1.2.tar.gz/mouse_keyboard_tracker-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mouse_keyboard_tracker',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'pynput',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mouse-keyboard-tracker=mouse_keyboard_tracker.cli:main',
+        ],
+    },
+    author='Victor L. Castro',
+    author_email='victorlcastro.dsa@gmail.com',
+    description='Uma biblioteca para monitorar atividades de teclado e mouse.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/victorlcastro-dsa/mouse-keyboard-tracker',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
