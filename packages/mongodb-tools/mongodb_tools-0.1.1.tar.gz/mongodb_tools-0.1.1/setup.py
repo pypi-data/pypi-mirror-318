3a8a1d2fb88a1ff0ac5d09558c02e25cc3142e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:18 +0000

    Add package mongodb-tools/0.1.1/mongodb_tools-0.1.1.tar.gz

---
+++ packages/mongodb-tools/mongodb_tools-0.1.1.tar.gz/mongodb_tools-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mongodb_tools',
+    version='0.1.1',
+    author='Shubham Akshit',
+    author_email='akshitshubhammas@gmail.com',
+    description='A simple database management package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shubhamakshit/easy_db',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo',
+        'twine',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
