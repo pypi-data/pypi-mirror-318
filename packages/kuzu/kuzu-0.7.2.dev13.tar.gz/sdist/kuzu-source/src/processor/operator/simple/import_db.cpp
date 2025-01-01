40eff5a671c26052bdd48a948a6b7fba39b05227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 15:04:20 +0000

    Add package kuzu/0.7.2.dev13/kuzu-0.7.2.dev13.tar.gz

---
+++ packages/kuzu/kuzu-0.7.2.dev13.tar.gz/sdist/kuzu-source/src/processor/operator/simple/import_db.cpp
@@ -0,0 +1,31 @@
+#include "processor/operator/simple/import_db.h"
+
+using namespace kuzu::common;
+using namespace kuzu::transaction;
+using namespace kuzu::catalog;
+
+namespace kuzu {
+namespace processor {
+
+using std::stringstream;
+
+void ImportDB::executeInternal(ExecutionContext* context) {
+    if (query.empty()) { // Export empty database.
+        return;
+    }
+    // TODO(Guodong): this is special for "Import database". Should refactor after we support
+    // multiple DDL and COPY statements in a single transaction.
+    // Currently, we split multiple query statements into single query and execute them one by one,
+    // each with an auto transaction.
+    if (context->clientContext->getTransactionContext()->hasActiveTransaction()) {
+        context->clientContext->getTransactionContext()->commit();
+    }
+    context->clientContext->queryInternal(query, std::nullopt /* queryID */);
+}
+
+std::string ImportDB::getOutputMsg() {
+    return "Imported database successfully.";
+}
+
+} // namespace processor
+} // namespace kuzu
