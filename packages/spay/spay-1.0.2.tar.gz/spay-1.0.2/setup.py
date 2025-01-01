ad0ede80cda45150a521c3f38e3ee103843e2cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:19 +0000

    Add package spay/1.0.2/spay-1.0.2.tar.gz

---
+++ packages/spay/spay-1.0.2.tar.gz/spay-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='spay',
+    version='1.0.2',
+    author='Saba Pardazesh',
+    author_email='info@sabapardazesh.net',
+    description='Python SDK for Spay internet gateway',
+    packages=['spay'],
+)
