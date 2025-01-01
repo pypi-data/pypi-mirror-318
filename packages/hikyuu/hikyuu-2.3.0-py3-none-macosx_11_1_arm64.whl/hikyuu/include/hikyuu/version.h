9203089dfb3d0ea8e5018065d75ae6f7d3640faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:48 +0000

    Add package hikyuu/2.3.0/hikyuu-2.3.0-py3-none-macosx_11_1_arm64.whl

---
+++ packages/hikyuu/hikyuu-2.3.0-py3-none-macosx_11_1_arm64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.3.0"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 3
+#define HKU_VERSION_ALTER 0
+#define HKU_VERSION_BUILD 202501010222
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.3.0 master.b50e5400 (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
