746e493963fce915a3cfd68ce7d55ce18c0e4658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:21 +0000

    Add package omu/0.8.2/omu-0.8.2-py3-none-any.whl

---
+++ packages/omu/omu-0.8.2-py3-none-any.whl/omu/extension/plugin/__init__.py
@@ -0,0 +1,10 @@
+from .package_info import PackageInfo
+from .plugin import PluginPackageInfo
+from .plugin_extension import PLUGIN_EXTENSION_TYPE, PluginExtension
+
+__all__ = [
+    "PackageInfo",
+    "PluginPackageInfo",
+    "PLUGIN_EXTENSION_TYPE",
+    "PluginExtension",
+]
