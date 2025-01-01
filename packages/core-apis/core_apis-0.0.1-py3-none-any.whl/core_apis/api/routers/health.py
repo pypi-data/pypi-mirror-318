7b4b30cc0dd7f80f8443836229bf4394b18c0382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:18 +0000

    Add package core-apis/0.0.1/core_apis-0.0.1-py3-none-any.whl

---
+++ packages/core-apis/core_apis-0.0.1-py3-none-any.whl/core_apis/api/routers/health.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import APIRouter
+from starlette.responses import PlainTextResponse
+from starlette.status import HTTP_200_OK
+
+
+health_router = APIRouter()
+
+
+@health_router.get(
+    path="/ping",
+    status_code=HTTP_200_OK,
+    response_class=PlainTextResponse,
+    description="Endpoint to ensure the API service is listening...")
+def health():
+    return "PONG"
