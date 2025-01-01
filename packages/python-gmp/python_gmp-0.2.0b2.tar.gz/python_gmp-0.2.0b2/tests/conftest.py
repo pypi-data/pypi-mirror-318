256adf51bd382af465adf1ca8a1f2e1e4a3e601c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:20 +0000

    Add package python-gmp/0.2.0b2/python_gmp-0.2.0b2.tar.gz

---
+++ packages/python-gmp/python_gmp-0.2.0b2.tar.gz/python_gmp-0.2.0b2/tests/conftest.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+import gmp
+from hypothesis import settings
+
+
+default = settings.get_profile("default")
+settings.register_profile("default",
+                          settings(default,
+                                   deadline=timedelta(seconds=300)))
+ci = settings.get_profile("ci")
+settings.register_profile("ci", settings(ci, max_examples=10000))
+
+def pytest_report_header(config):
+    print(f"""
+  The GNU GMP version: {gmp.gmp_info[3]}
+  Bits per limb:       {gmp.gmp_info[0]}
+  Nails bits:          {gmp.gmp_info[1]}
+  Size of a limb:      {gmp.gmp_info[2]}
+""")
