f5cd44cf34464a8161c47f3cbf3bccf8e0905b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:58 +0000

    Add package starcraft-data-orm/0.1.0/starcraft-data-orm-0.1.0.tar.gz

---
+++ packages/starcraft-data-orm/starcraft-data-orm-0.1.0.tar.gz/starcraft-data-orm-0.1.0/starcraft_data_orm/inject/Injectable.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+
+class Injectable(ABC):
+    @classmethod
+    @property
+    @abstractmethod
+    def __tableschema__(cls):
+        """Return the schema name. Must be implemented by subclasses."""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def process(cls, replay, session):
+        """Must be implemented by subclasses."""
+        pass
+
+    @classmethod
+    def get_data(cls, obj):
+        parameters = defaultdict(lambda:None)
+        for variable, value in vars(obj).items():
+            if variable in cls.columns:
+                parameters[variable] = value
+
+        return parameters
