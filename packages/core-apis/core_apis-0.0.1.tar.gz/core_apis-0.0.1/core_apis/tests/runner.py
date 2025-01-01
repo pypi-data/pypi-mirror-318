537bb23020251e2c26bdc32f9b84b03c30ff041d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:18 +0000

    Add package core-apis/0.0.1/core_apis-0.0.1.tar.gz

---
+++ packages/core-apis/core_apis-0.0.1.tar.gz/core_apis-0.0.1/core_apis/tests/runner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from click.decorators import group
+
+
+@group()
+def cli_api():
+    pass
+
+@cli_api.command("run-api")
+def run_api():
+    from core_apis.api import server
+    server.run()
