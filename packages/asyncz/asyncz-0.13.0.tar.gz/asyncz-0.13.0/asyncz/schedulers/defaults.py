9352fb9ae466f8ee274e58a82a5b3b19a88bb925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:26 +0000

    Add package asyncz/0.13.0/asyncz-0.13.0.tar.gz

---
+++ packages/asyncz/asyncz-0.13.0.tar.gz/asyncz-0.13.0/asyncz/schedulers/defaults.py
@@ -0,0 +1,25 @@
+triggers: dict[str, str] = {
+    "date": "asyncz.triggers.date:DateTrigger",
+    "interval": "asyncz.triggers.interval:IntervalTrigger",
+    "cron": "asyncz.triggers.cron.trigger:CronTrigger",
+    "and": "asyncz.triggers.combination:AndTrigger",
+    "or": "asyncz.triggers.combination:OrTrigger",
+    "shutdown": "asyncz.triggers.shutdown:ShutdownTrigger",
+}
+
+
+executors: dict[str, str] = {
+    "debug": "asyncz.executors.debug:DebugExecutor",
+    "pool": "asyncz.executors.pool:ThreadPoolExecutor",
+    "threadpool": "asyncz.executors.pool:ThreadPoolExecutor",
+    "processpool": "asyncz.executors.process_pool:ProcessPoolExecutor",
+    "asyncio": "asyncz.executors.asyncio:AsyncIOExecutor",
+}
+
+
+stores: dict[str, str] = {
+    "memory": "asyncz.stores.memory:MemoryStore",
+    "mongodb": "asyncz.stores.mongo:MongoDBStore",
+    "redis": "asyncz.stores.redis:RedisStore",
+    "sqlalchemy": "asyncz.stores.sqlalchemy:SQLAlchemyStore",
+}
