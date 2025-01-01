8445c0986d5a290cd8998a05bb7991094a92aaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:33 +0000

    Add package logicblocks.event.store/0.1.4a2/logicblocks_event_store-0.1.4a2.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.4a2.tar.gz/logicblocks_event_store-0.1.4a2/src/logicblocks/event/types/identifier.py
@@ -0,0 +1,81 @@
+import json
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+class Identifier(ABC):
+    @abstractmethod
+    def json(self) -> str:
+        raise NotImplementedError()
+
+
+@dataclass(frozen=True)
+class Log(Identifier):
+    def json(self) -> str:
+        return json.dumps({"type": "log"})
+
+    def __repr__(self) -> str:
+        return "identifier.Log()"
+
+    def __hash__(self):
+        return hash(self.json())
+
+
+@dataclass(frozen=True)
+class Category(Identifier):
+    category: str
+
+    def __init__(self, *, category: str):
+        object.__setattr__(self, "category", category)
+
+    def json(self) -> str:
+        return json.dumps(
+            {
+                "type": "category",
+                "category": self.category,
+            }
+        )
+
+    def __repr__(self) -> str:
+        return f"identifier.Category(category={self.category})"
+
+    def __hash__(self):
+        return hash(self.json())
+
+
+@dataclass(frozen=True)
+class Stream(Identifier):
+    category: str
+    stream: str
+
+    def __init__(self, *, category: str, stream: str):
+        object.__setattr__(self, "category", category)
+        object.__setattr__(self, "stream", stream)
+
+    def json(self) -> str:
+        return json.dumps(
+            {
+                "type": "stream",
+                "category": self.category,
+                "stream": self.stream,
+            }
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"identifier.Stream(category={self.category},stream={self.stream})"
+        )
+
+    def __hash__(self):
+        return hash(self.json())
+
+
+def target(
+    *, category: str | None = None, stream: str | None = None
+) -> Log | Category | Stream:
+    if category is not None and stream is not None:
+        return Stream(category=category, stream=stream)
+    elif category is not None:
+        return Category(category=category)
+    else:
+        return Log()
