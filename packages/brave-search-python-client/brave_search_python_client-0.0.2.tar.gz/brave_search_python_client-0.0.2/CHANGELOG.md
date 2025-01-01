02ec0b423936243615d1fd440ac81afe4dd18008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:44 +0000

    Add package brave-search-python-client/0.0.2/brave_search_python_client-0.0.2.tar.gz

---
+++ packages/brave-search-python-client/brave_search_python_client-0.0.2.tar.gz/brave_search_python_client-0.0.2/CHANGELOG.md
@@ -0,0 +1,28 @@
+[🦁 Brave Search Python Client](https://helmuthva.gitbook.io/brave-search-api-client)
+
+## [unreleased]
+
+### 📚 Documentation
+
+- *(gitbook)* Introduce gitbook site - ([f574b41](https://github.com/helmut-hoffer-von-ankershoffen/brave-search-python-client/commit/f574b41d40218915de3ff5095c41f3f6ffb6b04d))
+
+### 🎨 Styling
+
+- Remove superfluous pass - ([8726a6b](https://github.com/helmut-hoffer-von-ankershoffen/brave-search-python-client/commit/8726a6bd15a58cdcfc18105ccb2247157b870c16))
+
+
+## [0.0.1] - 2024-12-31
+
+### 📚 Documentation
+
+- *(readme)* Tweak - ([345ea07](https://github.com/helmut-hoffer-von-ankershoffen/brave-search-python-client/commit/345ea07ffcae7ab30717954eb00a70b1c4045dc7))
+
+### ⚙️ Miscellaneous Tasks
+
+- *(github)* Secret api key for e2e test, and baseline to check on secrets in code - ([5593b2b](https://github.com/helmut-hoffer-von-ankershoffen/brave-search-python-client/commit/5593b2b6975703797476d28731788e5d7e9e40e9))
+
+## New Contributors ❤️
+
+* @helmut-hoffer-von-ankershoffen made their first contribution
+
+<!-- built with love in Berlin by Helmut Hoffer von Ankershoffen -->
---
+++ packages/brave-search-python-client/brave_search_python_client-0.0.2.tar.gz/brave_search_python_client-0.0.2/tests/cli_test.py
@@ -0,0 +1,95 @@
+import json
+from unittest.mock import patch
+
+import pytest
+from typer.testing import CliRunner
+
+from brave_search_python_client import (
+    BraveSearch,
+    ImageSearchApiResponse,
+    NewsSearchApiResponse,
+    VideoSearchApiResponse,
+    WebSearchApiResponse,
+    __version__,
+)
+from brave_search_python_client.cli import cli
+
+with open("tests/fixtures/web_search_response.json") as f:
+    mock_web_search_response_data = json.load(f)
+mock_web_search_response = WebSearchApiResponse.model_validate(
+    mock_web_search_response_data
+)
+
+with open("tests/fixtures/image_search_response.json") as f:
+    mock_image_search_response_data = json.load(f)
+mock_image_search_response = ImageSearchApiResponse.model_validate(
+    mock_image_search_response_data
+)
+
+with open("tests/fixtures/video_search_response.json") as f:
+    mock_video_search_response_data = json.load(f)
+mock_video_search_response = VideoSearchApiResponse.model_validate(
+    mock_video_search_response_data
+)
+
+with open("tests/fixtures/news_search_response.json") as f:
+    mock_news_search_response_data = json.load(f)
+mock_news_search_response = NewsSearchApiResponse.model_validate(
+    mock_news_search_response_data
+)
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+def test_cli_built_with_love(runner):
+    """Check epilog shown."""
+    result = runner.invoke(cli, ["--help"])
+    assert result.exit_code == 0
+    assert "built with love in Berlin" in result.output
+    assert __version__ in result.output
+
+
+def test_cli_commands(runner: CliRunner):
+    """Check commands exist and show help and epilog."""
+    for command in ["web", "image", "video", "news"]:
+        result = runner.invoke(cli, [command, "--help"])
+        assert result.exit_code == 0
+        assert "The search query to perform" in result.output
+        assert f"Perform a {command} search" in result.output
+        assert __version__ in result.output
+
+
+def test_cli_search(runner: CliRunner):
+    """Check search triggered"""
+    with patch.object(BraveSearch, "web_search", return_value=mock_web_search_response):
+        result = runner.invoke(cli, ["web", "hello world"])
+        assert result.exit_code == 0
+        response = json.loads(result.output)
+        assert response["type"] == "search"
+
+    with patch.object(
+        BraveSearch, "image_search", return_value=mock_image_search_response
+    ):
+        result = runner.invoke(cli, ["image", "hello world"])
+        assert result.exit_code == 0
+        response = json.loads(result.output)
+        assert response["type"] == "images"
+
+    with patch.object(
+        BraveSearch, "video_search", return_value=mock_video_search_response
+    ):
+        result = runner.invoke(cli, ["video", "hello world"])
+        assert result.exit_code == 0
+        response = json.loads(result.output)
+        assert response["type"] == "videos"
+
+    with patch.object(
+        BraveSearch, "news_search", return_value=mock_news_search_response
+    ):
+        result = runner.invoke(cli, ["news", "hello world"])
+        assert result.exit_code == 0
+        response = json.loads(result.output)
+        assert response["type"] == "news"
