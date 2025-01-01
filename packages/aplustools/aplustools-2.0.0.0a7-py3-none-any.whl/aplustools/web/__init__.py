8f8737281edaecc81f99c69c48c1ffdf43921b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:55 +0000

    Add package aplustools/2.0.0.0a7/aplustools-2.0.0.0a7-py3-none-any.whl

---
+++ packages/aplustools/aplustools-2.0.0.0a7-py3-none-any.whl/aplustools/web/__init__.py
@@ -0,0 +1,15 @@
+"""TBA"""
+
+from ..package import setup_lazy_loaders as _setup_lazy_loaders
+from . import _direct
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+if _TYPE_CHECKING:
+    from ._direct import *
+    from . import request
+
+_setup_lazy_loaders(
+    globals(),
+    {
+        "request": ".web.request"
+    }, _direct)
