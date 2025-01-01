ca1d4bfa940240ad09f5329db09f86cf859d8391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:35 +0000

    Add package pywxm/0.2.0/pywxm-0.2.0.tar.gz

---
+++ packages/pywxm/pywxm-0.2.0.tar.gz/pywxm-0.2.0/src/pywxm/__init__.py
@@ -0,0 +1,37 @@
+"""An SDK for querying WeatherXM data."""
+
+from .api import (
+    AuthenticationError,
+    ForecastType,
+    WxmApi,
+    WxmClient,
+)
+from .model import (
+    BatteryState,
+    DailyForecast,
+    DeviceRelation,
+    ForecastForDate,
+    HourlyForecast,
+    HourlyWeatherData,
+    Location,
+    WeatherForecast,
+    WxmDevice,
+)
+
+# Explicitly export the public API to satisfy mypy strict type checking.
+# fmt:off
+__all__ = [
+    # API
+    "AuthenticationError", "WxmApi", "WxmClient", "ForecastType",
+    # Model
+    "BatteryState",
+    "DailyForecast",
+    "DeviceRelation",
+    "ForecastForDate",
+    "HourlyForecast",
+    "HourlyWeatherData",
+    "Location",
+    "WeatherForecast",
+    "WxmDevice",
+]
+# fmt:on
