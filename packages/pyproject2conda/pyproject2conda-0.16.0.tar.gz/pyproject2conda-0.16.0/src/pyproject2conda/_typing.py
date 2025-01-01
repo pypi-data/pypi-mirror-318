9f0a773d5d0aeb22f618348f193dccae62c2867a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:00 +0000

    Add package pyproject2conda/0.16.0/pyproject2conda-0.16.0.tar.gz

---
+++ packages/pyproject2conda/pyproject2conda-0.16.0.tar.gz/pyproject2conda-0.16.0/src/pyproject2conda/_typing.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, TypeVar
+
+R = TypeVar("R")
+T = TypeVar("T")
+
+
+if TYPE_CHECKING:
+    from typing import Literal, Tuple
+
+    from packaging.requirements import Requirement
+
+    from .overrides import OverrideDeps
+    from .utils import _Missing  # pyright: ignore[reportPrivateUsage]
+
+    MISSING_TYPE = Literal[_Missing.MISSING]
+
+    RequirementCommentPair = Tuple[Optional[Requirement], Optional[str]]
+    RequirementOverridePair = Tuple[Optional[Requirement], Optional[OverrideDeps]]
