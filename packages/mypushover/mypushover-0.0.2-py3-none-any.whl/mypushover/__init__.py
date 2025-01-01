f47b8d2ba5a92e6c3a97cd3719ef9baab43825fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:49 +0000

    Add package mypushover/0.0.2/mypushover-0.0.2-py3-none-any.whl

---
+++ packages/mypushover/mypushover-0.0.2-py3-none-any.whl/mypushover/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.0.2'
+__usage__ = """
+    USAGE:
+        As a module from any location
+            mypushover [-option value]
+
+    OPTIONS:
+        -c              config file path
+        -start          start service in background
+        -stop           stop service
+        -restart        restart service
+
+"""
