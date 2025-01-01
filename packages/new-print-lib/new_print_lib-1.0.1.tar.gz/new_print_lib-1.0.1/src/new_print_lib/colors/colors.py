e3c2afac89d7ccfce4d438a2c1bed95ba8379c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 15:04:17 +0000

    Add package new-print-lib/1.0.1/new_print_lib-1.0.1.tar.gz

---
+++ packages/new-print-lib/new_print_lib-1.0.1.tar.gz/new_print_lib-1.0.1/src/new_print_lib/colors/colors.py
@@ -0,0 +1,25 @@
+from .shades import (
+    BLUES,
+    BROWNS,
+    GREENS,
+    GREYS,
+    ORANGES,
+    PINKS,
+    PURPLES,
+    REDS,
+    WHITES,
+    YELLOWS,
+)
+
+COLORS = {
+    **BLUES,
+    **BROWNS,
+    **GREENS,
+    **GREYS,
+    **ORANGES,
+    **PINKS,
+    **PURPLES,
+    **REDS,
+    **WHITES,
+    **YELLOWS,
+}
