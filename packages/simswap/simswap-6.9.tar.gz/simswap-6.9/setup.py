78bc0fdb93ae68cd7c7e30865857d7c5bf4424c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:52 +0000

    Add package simswap/6.9/simswap-6.9.tar.gz

---
+++ packages/simswap/simswap-6.9.tar.gz/simswap-6.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simswap',
+    version='6.9',
+    packages=find_packages(),
+    install_requires = [
+        
+    ]
+)
---
+++ packages/simswap/simswap-6.9.tar.gz/simswap-6.9/simswap/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .main import simswap_the_world
