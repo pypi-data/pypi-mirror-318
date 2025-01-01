8b3e79cf404727fba2e4f5ad13bd164f2a91cd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:35 +0000

    Add package static-npm/1.0.10/static_npm-1.0.10.tar.gz

---
+++ packages/static-npm/static_npm-1.0.10.tar.gz/static_npm-1.0.10/tests/test_live_server.py
@@ -0,0 +1,40 @@
+"""
+Unit test file.
+"""
+
+import time
+import unittest
+
+import httpx  # type: ignore
+
+from static_npm import NpmTool
+
+PORT = 8375
+
+
+class MainTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_imports(self) -> None:
+        """Test command line interface (CLI)."""
+        live_server = NpmTool("live-server")
+        live_server.install()
+        proc = live_server.run([f"--port={PORT}", "--no-browser"])
+
+        timeout = time.time() + 120
+        while time.time() < timeout:
+            try:
+                response = httpx.get(f"http://localhost:{PORT}")
+                if response.status_code == 200:
+                    break
+            except httpx.ConnectError:
+                pass
+            time.sleep(1)
+        else:
+            self.fail("Server did not start.")
+
+        proc.terminate()
+
+
+if __name__ == "__main__":
+    unittest.main()
