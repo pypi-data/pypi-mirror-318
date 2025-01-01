8a7f69c00df5da7d47f37d047da147fc0f5ede87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:46 +0000

    Add package graphene-django-graphiql-static/0.8/graphene_django_graphiql_static-0.8-py3-none-any.whl

---
+++ packages/graphene-django-graphiql-static/graphene_django_graphiql_static-0.8-py3-none-any.whl/graphene_django_graphiql_static-0.8.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* prashant patel <prashantz7242@gmail.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/graphene-django-graphiql-static/graphene_django_graphiql_static-0.8-py3-none-any.whl/graphene_django_graphiql_static/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8
+from django.apps import AppConfig
+
+
+class GrapheneDjangoGraphiqlStaticConfig(AppConfig):
+    name = "graphene_django_graphiql_static"
+    verbose_name = "GraphiQL Static"
