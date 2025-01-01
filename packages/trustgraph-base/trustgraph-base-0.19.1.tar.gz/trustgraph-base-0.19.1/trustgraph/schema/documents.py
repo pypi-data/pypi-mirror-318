c73922429471471f717acdf0e01ec890afce5108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 05:04:12 +0000

    Add package trustgraph-base/0.19.1/trustgraph-base-0.19.1.tar.gz

---
+++ packages/trustgraph-base/trustgraph-base-0.19.1.tar.gz/trustgraph-base-0.19.1/trustgraph/schema/documents.py
@@ -0,0 +1,54 @@
+
+from pulsar.schema import Record, Bytes, String, Boolean, Integer, Array, Double
+from . topic import topic
+from . types import Error
+from . metadata import Metadata
+
+############################################################################
+
+# PDF docs etc.
+class Document(Record):
+    metadata = Metadata()
+    data = Bytes()
+
+document_ingest_queue = topic('document-load')
+
+############################################################################
+
+# Text documents / text from PDF
+
+class TextDocument(Record):
+    metadata = Metadata()
+    text = Bytes()
+
+text_ingest_queue = topic('text-document-load')
+
+############################################################################
+
+# Chunks of text
+
+class Chunk(Record):
+    metadata = Metadata()
+    chunk = Bytes()
+
+chunk_ingest_queue = topic('chunk-load')
+
+############################################################################
+
+# Doc embeddings query
+
+class DocumentEmbeddingsRequest(Record):
+    vectors = Array(Array(Double()))
+    limit = Integer()
+
+class DocumentEmbeddingsResponse(Record):
+    error = Error()
+    documents = Array(Bytes())
+
+document_embeddings_request_queue = topic(
+    'doc-embeddings', kind='non-persistent', namespace='request'
+)
+document_embeddings_response_queue = topic(
+    'doc-embeddings', kind='non-persistent', namespace='response',
+)
+
