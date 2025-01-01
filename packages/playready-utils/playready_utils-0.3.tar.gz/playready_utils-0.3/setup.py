ebb045ddbcf737968548c0e73ca099c87090fb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:43 +0000

    Add package playready-utils/0.3/playready_utils-0.3.tar.gz

---
+++ packages/playready-utils/playready_utils-0.3.tar.gz/playready_utils-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from playready_utils import __version__
+
+setup(
+    name='playready_utils',
+    version=__version__,
+    author="8c",
+    description='Tools built to work with playready and pyplayready',
+    packages=find_packages(include=['playready_utils', 'playready_utils.*']),
+    package_data={
+    },
+    entry_points={
+        'console_scripts': ['playready-utils=playready_utils.main:cli'],
+    },
+    include_package_data=True,
+    install_requires=[
+        "cloup",
+        'construct==2.8.8',
+        'coloredlogs',
+    ],
+)
