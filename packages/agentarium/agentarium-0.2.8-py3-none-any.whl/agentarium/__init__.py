db87f6bfb507196130c308067f69d9259775f262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:07 +0000

    Add package agentarium/0.2.8/agentarium-0.2.8-py3-none-any.whl

---
+++ packages/agentarium/agentarium-0.2.8-py3-none-any.whl/agentarium/__init__.py
@@ -0,0 +1,13 @@
+from .Agent import Agent
+# from .Environment import Environment
+from .AgentInteractionManager import AgentInteractionManager
+from .Interaction import Interaction
+
+__version__ = "0.2.8"
+
+__all__ = [
+    "Agent",
+    "Environment",
+    "AgentInteractionManager",
+    "Interaction"
+]
