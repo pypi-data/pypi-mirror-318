b232ead5137ebe7c1e15e5161a7074229141259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:20 +0000

    Add package firestoned/0.8.0/firestoned-0.8.0.tar.gz

---
+++ packages/firestoned/firestoned-0.8.0.tar.gz/firestoned-0.8.0/firestone/spec/__init__.py
@@ -0,0 +1,9 @@
+"""
+Firestone spec module.
+"""
+
+from firestone.spec import asyncapi
+from firestone.spec import cli
+from firestone.spec import openapi
+
+__all__ = ["asyncapi", "cli", "openapi"]
