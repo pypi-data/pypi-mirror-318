4c1f7548cf2c20785810d3b1bbfda3b3b3c9f8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:11:57 +0000

    Add package nexius/0.1.0/nexius-0.1.0-py3-none-any.whl

---
+++ packages/nexius/nexius-0.1.0-py3-none-any.whl/nexius/__init__.py
@@ -0,0 +1,19 @@
+"""
+Nexius - A Python Library
+Created by @roc4et
+"""
+
+from . import log
+from .log import (
+    log, 
+    success, 
+    error, 
+    warning, 
+    fatal, 
+    debug, 
+    info, 
+    printf
+)
+
+__version__ = "0.1.0"
+__author__ = "@roc4et" 
