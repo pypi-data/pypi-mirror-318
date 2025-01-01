19fef6b6a93829f6010e02db8c4b34d6bd866f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:17 +0000

    Add package testPackage-ghardy00143/0.1.0/testPackage_ghardy00143-0.1.0.tar.gz

---
+++ packages/testPackage-ghardy00143/testPackage_ghardy00143-0.1.0.tar.gz/testPackage_ghardy00143-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testPackage_ghardy00143",
+    version="0.1.0",
+    author="Grant Hardy",
+    author_email="ghardy00143@gmail.com",
+    description="Deploying a dummy package for self-learning",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
