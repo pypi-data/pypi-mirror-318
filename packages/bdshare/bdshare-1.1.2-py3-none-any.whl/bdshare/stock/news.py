dd41843a942b493ad1063fb9e6bbc27418b0c2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:11:51 +0000

    Add package bdshare/1.1.2/bdshare-1.1.2-py3-none-any.whl

---
+++ packages/bdshare/bdshare-1.1.2-py3-none-any.whl/bdshare/stock/news.py
@@ -0,0 +1,73 @@
+# from numpy import empty
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from bdshare.util import vars as vs
+
+
+def get_agm_news():
+    """
+        get stock agm declarations.
+        :return: dataframe
+    """
+    try:
+        r = requests.get(vs.DSE_URL + vs.DSE_AGM_URL)
+    except Exception as e:
+        print(e)
+    # soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.content, 'html5lib')
+    news = []  # a list to store quotes
+
+    table = soup.find('table')
+
+    for row in table.find_all('tr')[4:-6]:
+        cols = row.find_all('td')
+        news.append({'company': cols[0].text.strip(),
+                     'yearEnd': cols[1].text.strip(),
+                     'dividend': cols[2].text.strip(),
+                     'agmData': cols[3].text.strip(),
+                     'recordDate': cols[4].text.strip(),
+                     'vanue': cols[5].text.strip(),
+                     'time': cols[6].text.strip()
+                     })
+    df = pd.DataFrame(news)
+    return df
+
+
+def get_all_news(code=None):
+    """
+        get dse news.
+        :param start: str, Start date e.g.: '2020-03-01'
+        :param end: str, End date e.g.: '2020-03-02'
+        :param code: str, Instrument symbol e.g.: 'ACI'
+        :return: dataframe
+    """
+    # data to be sent to post request
+    data = {'inst': code,
+            'criteria': 3,
+            'archive': 'news'}
+    try:
+        r = requests.post(url=vs.DSE_URL + vs.DSE_NEWS_URL, params=data)
+    except Exception as e:
+        print(e)
+
+    # soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    # columns: Trading Code, News, Post Date
+    news = []  # a list to store quotes
+
+    table = soup.find('table', attrs={'class': 'table-news'})
+
+    for row in table.find_all('tr'):
+        heads = row.find_all('th')
+        cols = row.find_all('td')
+        if cols:
+            if heads[0].text.strip() == "News Title:":
+                news.append({"News Title": cols[0].text.strip()})
+            elif heads[0].text.strip() == "News:":
+                news.append({"News": cols[0].text.strip()})
+            elif heads[0].text.strip() == "Post Date:":
+                news.append({"Post Date": cols[0].text.strip()})
+    df = pd.DataFrame(news)
+    return df
---
+++ packages/bdshare/bdshare-1.1.2-py3-none-any.whl/bdshare/util/tickers.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+"""
+Created on 2024/03/04
+@author: Raisul Islam
+@group : bdshare.xyz
+@contact: raisul.me@gmail.com
+"""
+import json
+
+
+class Tickers(object):
+    def __init__(self):
+        self.f = open('tickers.json')
+        self.data = json.load(self.f)
+
+    def close(self):
+        self.f.close()
+
+    def ticker_data(self, ticker=None):
+        if ticker:
+            self.data['data']['companies' == ticker]
+        else:
+            self.data['data']['companies']
