8e8f3137b115d9dfcfc7cb2822edc48205cda580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:14 +0000

    Add package easy-ecs-sim/0.1.22/easy_ecs_sim-0.1.22-py3-none-any.whl

---
+++ packages/easy-ecs-sim/easy_ecs_sim-0.1.22-py3-none-any.whl/easy_ecs_sim/context.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+from typing import Type, Any, TypeVar
+
+T = TypeVar('T')
+
+
+class Context:
+    @classmethod
+    @lru_cache(maxsize=None)
+    def default(cls: Type[T]) -> T:
+        return cls()
+
+    def __init__(self, *initial_state: Any):
+        self.data: dict[Type, Any] = {}
+        for _ in initial_state:
+            self.register(_)
+
+    def register[T](self, data: T, ctype: Type[T] = None):
+        found_type = type(data)
+
+        if ctype is not None:
+            if not issubclass(found_type, ctype):
+                raise ValueError(f'incompatible type [{ctype}] for data of type [{found_type}]')
+
+        if ctype is not None:
+            assert issubclass(found_type, ctype)
+        else:
+            ctype = found_type
+        self.data[ctype] = data
+        return self
+
+    def find[T](self, ctype: Type[T]) -> T:
+        return self.data[ctype]
