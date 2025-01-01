59dd5795ee5650a986be1fb65c3f08051fd2bffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:45 +0000

    Add package blue-codespace/4.196.1/blue_codespace-4.196.1.tar.gz

---
+++ packages/blue-codespace/blue_codespace-4.196.1.tar.gz/blue_codespace-4.196.1/blue_codespace/__init__.py
@@ -0,0 +1,19 @@
+NAME = "blue_codespace"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
+
+VERSION = "4.196.1"
+
+REPO_NAME = "blue-codespace"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-codespace/marquee.png?raw=true"
+)
+
+ALIAS = "@codespace"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
