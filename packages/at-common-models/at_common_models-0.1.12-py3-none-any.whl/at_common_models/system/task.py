3a76c7802236abf02aa741f892730ff127e5edc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 15:04:11 +0000

    Add package at-common-models/0.1.12/at_common_models-0.1.12-py3-none-any.whl

---
+++ packages/at-common-models/at_common_models-0.1.12-py3-none-any.whl/at_common_models/system/task.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String, JSON
+from at_common_models.base import BaseModel
+
+class TaskModel(BaseModel):
+    __tablename__ = "system_tasks"
+
+    name = Column(String(255), primary_key=True, index=True)
+    data = Column(JSON, nullable=False)
