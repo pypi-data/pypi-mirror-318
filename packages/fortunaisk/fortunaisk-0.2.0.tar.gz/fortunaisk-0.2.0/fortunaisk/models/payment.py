918295cd56174475d1f25e16eb3be103bf022540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:28 +0000

    Add package fortunaisk/0.2.0/fortunaisk-0.2.0.tar.gz

---
+++ packages/fortunaisk/fortunaisk-0.2.0.tar.gz/fortunaisk-0.2.0/fortunaisk/models/payment.py
@@ -0,0 +1,30 @@
+# fortunaisk/models/payment.py
+
+# Django
+from django.db import models
+
+
+class ProcessedPayment(models.Model):
+    payment_id = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name="Payment ID",
+        help_text="Unique identifier for processed payments.",
+    )
+    processed_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Processed At",
+        help_text="Timestamp when the payment was processed.",
+    )
+
+    class Meta:
+        verbose_name = "Processed Payment"
+        verbose_name_plural = "Processed Payments"
+        default_permissions = ()  # Désactive les permissions automatiques
+        permissions = [
+            ("user", "can access this app"),
+            ("admin", "can admin this app"),
+        ]
+
+    def __str__(self):
+        return f"ProcessedPayment(payment_id={self.payment_id})"
