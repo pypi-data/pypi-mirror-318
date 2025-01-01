a3c6a912c1ba4a24adb8717fe2337a716e629983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 05:04:13 +0000

    Add package open-webui/0.5.3/open_webui-0.5.3-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.3-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.BucUzCW3.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Markdown.D36Un78Z.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.BucUzCW3.js.map
---
+++ packages/open-webui/open_webui-0.5.3-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.0V1KcHEN.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.DuGA_34m.js";export{t as start};
+//# sourceMappingURL=start.0V1KcHEN.js.map
