591253a8854c1cd4b8813dea2d1a034cf8452ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:49 +0000

    Add package liteutils/0.0.5/liteutils-0.0.5-py3-none-any.whl

---
+++ packages/liteutils/liteutils-0.0.5-py3-none-any.whl/liteutils/_arxiv/_arxiv_read.py
@@ -0,0 +1,18 @@
+
+import requests
+from io import BytesIO
+
+from liteutils import read_pdf
+
+
+def read_arxiv_pdf(id_or_link: str):
+    try:
+        if 'https' not in id_or_link:
+            id_or_link = f"https://arxiv.org/pdf/{id_or_link}"
+        response = requests.get(id_or_link)
+        pdf_content = BytesIO(response.content)
+        pdf_text = read_pdf(pdf_content)
+    except:
+        pdf_text = ""
+    return pdf_text
+
