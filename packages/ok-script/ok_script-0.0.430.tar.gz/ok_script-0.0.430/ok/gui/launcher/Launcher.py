48d5953c1dd538932522642111da76fd741f150c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:31 +0000

    Add package ok-script/0.0.430/ok_script-0.0.430.tar.gz

---
+++ packages/ok-script/ok_script-0.0.430.tar.gz/ok_script-0.0.430/ok/gui/launcher/Launcher.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from ok import ExitEvent
+from ok import Logger, config_logger
+from ok.gui.launcher.LauncherWindow import LauncherWindow
+from ok.gui.util.app import init_app_config, center_window
+from ok.update.GitUpdater import GitUpdater
+
+logger = Logger.get_logger(__name__)
+
+
+class Launcher:
+
+    def __init__(self, config):
+        self.app = None
+        self.locale = None
+        self.config = config
+        self.exit_event = ExitEvent()
+        config_logger(self.config, name='launcher')
+        self.app = None
+        self.locale = None
+        self.updater = None
+        logger.info(f'Launcher__init__ app_config = {config}')
+
+    def start(self):
+        try:
+            self.app, self.locale = init_app_config()
+            self.updater = GitUpdater(self.config, self.exit_event)
+            self.updater.list_all_versions()
+            logger.info(f'launcher start pid {os.getpid()} {self.locale}')
+            w = LauncherWindow(self.config, self.updater, self.exit_event)
+            center_window(self.app, w)
+            w.show()
+            self.app.exec_()
+        except Exception as e:
+            logger.error('launcher start error', e)
+            self.exit_event.set()
+            sys.exit(0)
