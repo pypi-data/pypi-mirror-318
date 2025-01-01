e5695c01fae746dee451bc414fa92923b17c4fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 15:04:12 +0000

    Add package at-common-models/0.1.12/at_common_models-0.1.12.tar.gz

---
+++ packages/at-common-models/at_common_models-0.1.12.tar.gz/at_common_models-0.1.12/at_common_models/system/workflow.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String, JSON
+from at_common_models.base import BaseModel
+
+class WorkflowModel(BaseModel):
+    __tablename__ = "system_workflows"
+
+    name = Column(String(255), primary_key=True, index=True)
+    data = Column(JSON, nullable=False)
