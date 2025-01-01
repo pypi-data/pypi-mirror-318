7fe5cb1368080dac49cbdf61ce2097f18ad27f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:49 +0000

    Add package nonebot-plugin-better-broadcast/1.0.6/nonebot_plugin_better_broadcast-1.0.6.tar.gz

---
+++ packages/nonebot-plugin-better-broadcast/nonebot_plugin_better_broadcast-1.0.6.tar.gz/nonebot_plugin_better_broadcast-1.0.6/nonebot_plugin_better_broadcast/__init__.py
@@ -0,0 +1,97 @@
+import json,asyncio,random
+from nonebot import on_command, require, get_plugin_config, logger
+from nonebot.plugin import PluginMetadata
+from nonebot.permission import SUPERUSER
+from nonebot.adapters import Event
+from nonebot.adapters.onebot.v11 import Bot,Message
+require("nonebot_plugin_waiter")
+from nonebot_plugin_waiter import waiter
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="更好的广播",
+    description="将你的信息广播到所有群聊，支持多种类型",
+    usage="发送广播",
+    type="application",
+    homepage="https://github.com/captain-wangrun-cn/nonebot-plugin-better-broadcast",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+)
+
+plugin_config = get_plugin_config(Config)
+last_bc_msg_id = []     # 上一条广播的消息id
+bc = on_command("发送广播", aliases={"广播","发送群聊广播","广播所有群聊"}, block=True, permission=SUPERUSER)
+recall_bc = on_command("撤回广播", block=True, permission=SUPERUSER)
+
+@bc.handle()
+async def _(bot: Bot):
+    await bc.send("请发送需要广播的内容（发送“取消”以取消）：")
+
+    @waiter(waits=["message"], keep_session=True)
+    async def check(event: Event):
+        return (event.get_message(),json.loads(event.json()))
+    
+    msg,data = await check.wait(timeout=120)
+    if msg is None:
+        await bc.finish("啊哦！输入超时了，请重试")
+    if msg.extract_plain_text() == "取消":
+        await bc.finish("已取消~")
+
+    forward_msg: bool = data["message"][0]["type"] == "forward"   # 是否为聊天记录
+
+    group_list = await bot.get_group_list()
+    blacklist = plugin_config.bc_blacklist
+    delay = plugin_config.bc_random_delay
+    fail,success = 0,0
+
+    for group in group_list:
+        if delay: await asyncio.sleep(random.randint(1,3))  # 随机延迟
+
+        gid = group["group_id"]
+        if str(gid) not in blacklist:
+            # 不在黑名单内，发送消息
+            try:
+                if forward_msg:
+                    # 聊天记录
+                    await bot.forward_group_single_msg(group_id=gid, message_id=data["message_id"])
+                else:
+                    # 其他消息
+                    msg_id = (await bot.send_group_msg(group_id=gid, message=msg))["message_id"]
+                    last_bc_msg_id.append(msg_id)    # 记录群聊对应的消息id，便于使用指令撤回
+                success += 1
+            except Exception as ex:
+                # 发送失败
+                logger.error(ex)
+                fail += 1
+
+
+    await bc.finish(f"广播完毕！\n成功发送了{success}个群\n有{fail}个群发送失败")
+
+
+@recall_bc.handle()
+async def _(bot: Bot):
+    await recall_bc.send("确定要尝试撤回上一条广播吗？（“确认”或“取消”）：")
+
+    @waiter(waits=["message"], keep_session=True)
+    async def check(event: Event):
+        return event.get_plaintext()
+
+    resp = await check.wait(timeout=120)
+    if resp is None:
+        await bc.finish("啊哦！输入超时了，请重试")
+    if resp == "确认":
+        fail,success = 0,0
+        for msg_id in last_bc_msg_id:
+            try:
+                await bot.delete_msg(message_id=msg_id)
+                success += 1
+            except Exception as ex:
+                # 撤回失败
+                logger.error(ex)
+                fail += 1
+        
+        last_bc_msg_id.clear()
+        await bc.finish(f"已尝试撤回广播\n成功撤回了{success}个群聊\n有{fail}个群撤回失败（可能是因为已经超时）")
+
+
+    await bc.finish("已取消~")
---
+++ packages/nonebot-plugin-better-broadcast/nonebot_plugin_better_broadcast-1.0.6.tar.gz/nonebot_plugin_better_broadcast-1.0.6/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_better_broadcast']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nonebot-adapter-onebot>=2.0.0,<3.0.0',
+ 'nonebot-plugin-waiter==0.8.0',
+ 'nonebot2>=2.2.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-better-broadcast',
+    'version': '1.0.6',
+    'description': 'nonebot2 plugin, boardcast your message to every groups',
+    'long_description': '<div align="center">\n  <a href="https://v2.nonebot.dev/store"><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>\n  <img src="https://github.com/WStudioGroup/hifumi-plugins/blob/main/remove.photos-removed-background.png" width="200">\n  <br>\n  <p><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>\n</div>\n\n<div align="center">\n\n# nonebot-plugin-better-broadcast\n\n_✨ 将你的信息广播到所有群聊，支持多种类型 ✨_\n\n\n<a href="./LICENSE">\n    <img src="https://img.shields.io/github/license/captain-wangrun-cn/nonebot-plugin-better-broadcast.svg" alt="license">\n</a>\n<a href="https://pypi.python.org/pypi/nonebot-plugin-better-broadcast">\n    <img src="https://img.shields.io/pypi/v/nonebot-plugin-better-broadcast.svg" alt="pypi">\n</a>\n<img src="https://img.shields.io/badge/python-3.9+-blue.svg" alt="python">\n\n</div>\n\n## 📖 介绍\n\n将你的信息广播到所有群聊，支持多种类型\n\n## 💿 安装\n\n<details open>\n<summary>使用 nb-cli 安装</summary>\n在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装\n\n    nb plugin install nonebot-plugin-better-broadcast\n\n</details>\n\n<details>\n<summary>使用包管理器安装</summary>\n在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令\n\n<details>\n<summary>pip</summary>\n\n    pip install nonebot-plugin-better-broadcast\n</details>\n\n\n打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分追加写入\n\n    plugins = ["nonebot_plugin_better_broadcast"]\n\n</details>\n\n## ⚙️ 配置\n\n在 nonebot2 项目的`.env`文件中添加下表中的必填配置\n\n| 配置项          | 类型   | 必填 | 默认值 | 说明                  |\n|:------------:|:----:|:---:|:---:|:-------------------:|\n| bc_blacklist | list | 否  | [ ]  | 群聊黑名单，广播时将不会发送到这些群聊 |\n| bc_random_delay | bool | 否  | 是  | 随机延迟，发送广播时将会有随机1-3秒的延迟 |\n\n## 🎉 使用\n### 指令表\n| 指令 | 权限 | 需要@ | 范围 | 说明 |\n|:-----:|:----:|:----:|:----:|:----:|\n| 发送广播 | 主人 | 否 | 私聊、群聊 | 顾名思义 |\n| 撤回广播 | 主人 | 否 | 私聊、群聊 | 撤回上一条广播的消息 |\n### 效果图\n<img src="imgs/QQ20241109-123325.png">\n<img src="imgs/QQ20241109-123336.png">\n\n## 📃 更新日志\n### 1.0.6（2025.01.01）\n- 🍟修复了一些问题\n### 1.0.5（2025.01.01）\n- 🍟修复了一些问题\n### 1.0.4（2024.11.15）\n- 🧋添加了“撤回广播”指令和取消广播，更改了插件信息\n',
+    'author': 'WR',
+    'author_email': 'wangrun114514@foxmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/captain-wangrun-cn/nonebot-plugin-better-broadcast',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
