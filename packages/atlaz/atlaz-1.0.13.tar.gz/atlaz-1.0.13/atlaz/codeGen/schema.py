cedcca87d49c5f73a09772cdd8d04d8da26ac791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:49 +0000

    Add package atlaz/1.0.13/atlaz-1.0.13.tar.gz

---
+++ packages/atlaz/atlaz-1.0.13.tar.gz/atlaz-1.0.13/atlaz/codeGen/schema.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class CodeGenRequest(BaseModel):
+    file_contents: list[dict]
+    directory_structure: str
+    openai_api_key: str
+    model_choice: Optional[str] = "o1"
