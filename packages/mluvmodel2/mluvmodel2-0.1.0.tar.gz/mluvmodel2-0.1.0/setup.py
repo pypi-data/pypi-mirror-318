c1ff6d08e831799630f48c0774be30887b152205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:30 +0000

    Add package mluvmodel2/0.1.0/mluvmodel2-0.1.0.tar.gz

---
+++ packages/mluvmodel2/mluvmodel2-0.1.0.tar.gz/mluvmodel2-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mluvmodel2",  # Choose a unique name for your package
+    version="0.1.0",
+    author="Ganesh",
+    author_email="ganesh4study@gmail.com",
+    description="A collection of educational ML and ANN functions",
+    long_description="no description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ganesh57803/mluvmodel",  # Link to your GitHub repository
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
