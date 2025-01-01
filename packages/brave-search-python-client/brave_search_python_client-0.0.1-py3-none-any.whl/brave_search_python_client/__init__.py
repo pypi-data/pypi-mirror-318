7c1ae3da9a39acab08edf45b51d67ec4811a7b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:33 +0000

    Add package brave-search-python-client/0.0.1/brave_search_python_client-0.0.1-py3-none-any.whl

---
+++ packages/brave-search-python-client/brave_search_python_client-0.0.1-py3-none-any.whl/brave_search_python_client/__init__.py
@@ -0,0 +1,31 @@
+import importlib.metadata
+import pathlib
+
+from .client import BraveSearch
+from .types import (
+    BraveSearchAPIError,
+    BraveSearchClientError,
+    BraveSearchError,
+    ImageSearchApiResponse,
+    NewsSearchApiResponse,
+    VideoSearchApiResponse,
+    WebSearchApiResponse,
+)
+
+__project_name__ = __name__.split(".")[0]
+__project_path__ = str(pathlib.Path(__file__).parent.parent.parent)
+__version__ = importlib.metadata.version(__project_name__)
+
+__all__ = [
+    "__version__",
+    "__project_name__",
+    "__project_path__",
+    "BraveSearch",
+    "BraveSearchError",
+    "BraveSearchClientError",
+    "BraveSearchAPIError",
+    "WebSearchApiResponse",
+    "ImageSearchApiResponse",
+    "NewsSearchApiResponse",
+    "VideoSearchApiResponse",
+]
