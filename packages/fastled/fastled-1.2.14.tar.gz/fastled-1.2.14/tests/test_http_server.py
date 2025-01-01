5c24334baa4c90c35e4453ce148c09f53b8d1ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:30 +0000

    Add package fastled/1.2.14/fastled-1.2.14.tar.gz

---
+++ packages/fastled/fastled-1.2.14.tar.gz/fastled-1.2.14/tests/test_http_server.py
@@ -0,0 +1,49 @@
+"""
+Unit test file.
+"""
+
+import time
+import unittest
+from pathlib import Path
+
+import httpx
+
+from fastled import Test
+
+HERE = Path(__file__).parent
+INDEX_HTML = HERE / "html" / "index.html"
+TIMEOUT = 120
+
+assert INDEX_HTML.exists()
+
+# def override(url) -> None:
+#     """Override the server url."""
+#     assert isinstance(url, str) and "localhost" in url
+
+# client_server.TEST_BEFORE_COMPILE = override
+
+
+class HttpServerTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_http_server(self) -> None:
+        """Test the http server."""
+        proc = Test.spawn_http_server(INDEX_HTML.parent, port=8081, open_browser=False)
+        time.sleep(1)
+        future_time = time.time() + TIMEOUT
+        while future_time > time.time():
+            try:
+                response = httpx.get("http://localhost:8081", timeout=1)
+                break
+            except Exception:
+                continue
+        else:
+            self.fail("Could not connect to server")
+        response = httpx.get("http://localhost:8081", timeout=5)
+        self.assertEqual(response.status_code, 200)
+        proc.kill()
+        proc.wait()
+
+
+if __name__ == "__main__":
+    unittest.main()
