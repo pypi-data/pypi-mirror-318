4f1bc7afc80b44a92a867bd751d55c1d5d4a685d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:04 +0000

    Add package hepai/1.1.20/hepai-1.1.20-py2.py3-none-any.whl

---
+++ packages/hepai/hepai-1.1.20-py2.py3-none-any.whl/hepai/__init__.py
@@ -0,0 +1,12 @@
+
+
+from hai import *
+
+from hai import __version__
+# from hai import HepAI
+from hai import HaiCompletions, ChatCompletion, ChatCompletionChunk, Stream
+from hai import HaiFile
+
+
+from .components.haiddf.hepai_client import HepAIClient as HepAI
+from .types import HRModel, LRModel, HModelConfig, HWorkerConfig, HWorkerAPP
