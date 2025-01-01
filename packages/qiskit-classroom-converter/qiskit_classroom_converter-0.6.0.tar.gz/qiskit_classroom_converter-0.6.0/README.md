6c8913587b7a9e6d74545db7344b0ab214bf9cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:31 +0000

    Add package qiskit-classroom-converter/0.6.0/qiskit_classroom_converter-0.6.0.tar.gz

---
+++ packages/qiskit-classroom-converter/qiskit_classroom_converter-0.6.0.tar.gz/qiskit_classroom_converter-0.6.0/README.md
@@ -0,0 +1,123 @@
+# qiskit-classroom-converter
+Qiskit classroom Converter
+
+## Version
+
+> 0.6.0 (2025-01-01) : The Version has been updated to be compatible with **Qiskit 1.3.1** and **Python 3.13**.
+> 0.5.0 (2024-03-06) : The Version has been updated to be compatible with **Qiskit 1.0.0** and **Python 3.12**.
+
+
+## Documents
+
+https://kmu-quantum-classroom.github.io/qiskit-classroom-converter/qiskit_class_converter.html
+
+## Official USER Guide
+
+https://kmu-quantum-classroom.github.io/documents/overview.html
+
+## Support convert method
+
+* quantum circuit to bra-ket notation
+* quantum circuit to matrix
+* matrix to quantum circuit
+* string to bra-ket notation
+
+---
+
+## Options
+
+| convert method | option                                   |
+|----------------|------------------------------------------|
+| QC_TO_BRA_KET  | expression{simplify, expand}, print{raw} |
+| QC_TO_MATRIX   | print{raw}                               |
+| MATRIX_TO_QC   | label{str}                               |
+| STR_TO_BRA_KET | print{raw}                               |
+
+```python
+from qiskit_class_converter import ConversionService
+
+ConversionService(conversion_type="QC_TO_BRA_KET", option={"expression": "simplify"})
+```
+
+## Required data
+
+* MATRIX_TO_QC
+  * User's QuantumCircuit object
+
+```python
+from qiskit import QuantumCircuit
+from qiskit_class_converter import ConversionService
+
+input_value = [
+    [1, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 0, 1, 0],
+    [0, 1, 0, 0]
+]
+sample_converter = ConversionService(conversion_type="MATRIX_TO_QC")
+result = sample_converter.convert(input_value=input_value)
+# using user's QuantumCircuit object
+quantum_circuit = QuantumCircuit(2, 2)
+quantum_circuit.append(result, [0, 1])
+```
+
+## How to Install
+
+```bash
+pip install qiskit-classroom-converter
+```
+
+## Docker Pull & Run
+
+Alternative installation with docker image.
+
+```shell
+docker pull ghcr.io/kmu-quantum-classroom/qiskit-classroom-converter
+docker run -p 8888:8888 ghcr.io/kmu-quantum-classroom/qiskit-classroom-converter
+```
+
+## Dependencies
+
+* qiskit
+
+## Usage
+
+```python
+from qiskit import QuantumCircuit
+from qiskit_class_converter import ConversionService
+# quantum circuit to matrix
+quantum_circuit = QuantumCircuit(2, 2)
+quantum_circuit.x(0)
+quantum_circuit.cx(0, 1)
+sample_converter = ConversionService(conversion_type="QC_TO_MATRIX")
+result = sample_converter.convert(input_value=quantum_circuit)
+```
+
+code : [example.py](example.py)
+
+## How to test the software
+
+```shell
+python -m unittest -v
+```
+
+or 
+
+```shell
+tox
+```
+
+## ARM Platform
+
+Mac ARM chips users may have issues running this package.
+
+We have provided a Dockerfile, which can be used docker-compose.
+
+```shell
+docker-compose up --build
+```
+
+## Acknowledgement
+
+- 국문 : "본 연구는 2022년 과학기술정보통신부 및 정보통신기획평가원의 SW중심대학사업의 연구결과로 수행되었음"(2022-0-00964)
+- English : "This research was supported by the MIST(Ministry of Science, ICT), Korea, under the National Program for Excellence in SW), supervised by the IITP(Institute of Information & communications Technology Planning & Evaluation) in 2022"(2022-0-00964)
---
+++ packages/qiskit-classroom-converter/qiskit_classroom_converter-0.6.0.tar.gz/qiskit_classroom_converter-0.6.0/qiskit_class_converter/converters/quantum_circuit_to_matrix.py
@@ -0,0 +1,84 @@
+"""
+QuantumCircuit to Matrix Converter
+"""
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+from typing import List
+
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.visualization import array_to_latex
+
+from qiskit_class_converter.converters.base import BaseConverter
+
+
+class QuantumCircuitToMatrixConverter(BaseConverter):
+    """
+    Converter class
+    """
+
+    def __init__(self, option=None):
+        super().__init__(option)
+        self.__programmable_variable_per_qubit = None
+
+    def insert_i_gate(self):
+        """
+        Trace empty space in the layer to enter the Identity gate.
+        """
+        arr = [f"I_{{q{i}}}" for i in range(self.__programmable_variable_per_qubit["total_qubits"])]
+        for i in self.__programmable_variable_per_qubit["gate_qubits"]:
+            for j in i["qubit"]:
+                arr[j] = i["name"]
+        arr = list(dict.fromkeys(arr))
+        arr.reverse()
+        return arr
+
+    def actual_convert_action(self):
+        self.logger.debug("quantum circuit to matrix")
+        matrix_list = {"gate": [], "name": []}
+        # type validate
+        if isinstance(self.input_value, (List, QuantumCircuit)):
+            self.input_value = self.input_value.remove_final_measurements(inplace=False)
+            dag = self.qiskit.converters.circuit_to_dag(self.input_value)
+        else:
+            raise TypeError("QuantumCircuit is required.")
+        layer_index = 0
+        for layer in dag.layers():
+            circuit = self.qiskit.converters.dag_to_circuit(layer['graph'])
+            matrix_list["gate"].append(self.qiskit.quantum_info.Operator(circuit).to_matrix())
+            self.__programmable_variable_per_qubit = {"total_qubits": circuit.num_qubits,
+                                                      "gate_qubits": []}
+            for _inst in circuit.data:
+                _inst_upper_name = _inst[0].name.upper()
+                qubit_indices = [circuit.find_bit(qubit)[0] for qubit in _inst[1]]
+                gate_name = (_inst_upper_name +
+                             "_{" + ", ".join(f"q{i}" for i in qubit_indices) + "}")
+                self.__programmable_variable_per_qubit["gate_qubits"].append(
+                    {"name": gate_name, "qubit": qubit_indices}
+                )
+            matrix_list["name"].append((layer_index, self.insert_i_gate()))
+            layer_index += 1
+        matrix_list["result"] = self.qiskit.quantum_info.Operator(self.input_value).to_matrix()
+        if self.option.get("print", False) == "raw":
+            latex_source_list = {"gate": [], "name": matrix_list["name"]}
+            for each_matrix in matrix_list["gate"]:
+                latex_source_list["gate"].append(
+                    array_to_latex(array=np.array(each_matrix), source=True))
+            latex_source_list["result"] = array_to_latex(
+                array=np.array(matrix_list["result"]), source=True)
+            return latex_source_list
+        return matrix_list
