f1ef9950a10ea57b088eb23fe2569f36ddd93b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:38 +0000

    Add package tdone/0.1.0/tdone-0.1.0.tar.gz

---
+++ packages/tdone/tdone-0.1.0.tar.gz/tdone-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='tdone',
+    version='0.1.0',    
+    description='TD ONE: A Python Package for Banking and Pricing',
+    url='https://github.com/tpdrg/tdone',
+    author='Behrang Dadsetan',
+    author_email='behrang.dadsetan@tpdrg.com',
+    license='BSD 2-clause',
+    packages=['tdone'],
+    install_requires=['pika>=1.3.2',                  
+                      ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Operating System :: Microsoft :: Windows',        
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
---
+++ packages/tdone/tdone-0.1.0.tar.gz/tdone-0.1.0/tdone/__init__.py
@@ -0,0 +1,9 @@
+"""
+tdone.
+
+TD ONE: A Python Package for Banking and Pricing.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Behrang Dadsetan'
+__credits__ = 'TOPDRAGON FINANCIAL TECHNOLOGY LIMITED'
