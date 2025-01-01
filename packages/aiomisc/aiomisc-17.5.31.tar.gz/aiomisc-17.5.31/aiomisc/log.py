e624d5d986585e9985e2c94193e45077911e3730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 03:12:09 +0000

    Add package aiomisc/17.5.31/aiomisc-17.5.31.tar.gz

---
+++ packages/aiomisc/aiomisc-17.5.31.tar.gz/aiomisc-17.5.31/aiomisc/log.py
@@ -0,0 +1,179 @@
+import asyncio
+import atexit
+import logging
+import logging.handlers
+import time
+import traceback
+from contextlib import suppress
+from functools import partial
+from socket import socket
+from typing import (
+    Any, Callable, Dict, Iterable, List, Optional, Tuple, Type, Union,
+)
+from weakref import finalize
+
+import aiomisc_log
+from aiomisc_log.enum import LogFormat, LogLevel
+
+from .thread_pool import run_in_new_thread
+
+
+def _thread_flusher(
+    handler: logging.handlers.MemoryHandler,
+    flush_interval: Union[float, int],
+    loop: asyncio.AbstractEventLoop,
+) -> None:
+    def has_no_target() -> bool:
+        return True
+
+    def has_target() -> bool:
+        return bool(handler.target)
+
+    is_target = has_no_target
+
+    if isinstance(handler, logging.handlers.MemoryHandler):
+        is_target = has_target
+
+    while not loop.is_closed() and is_target():
+        with suppress(Exception):
+            if handler.buffer:
+                handler.flush()
+
+        time.sleep(flush_interval)
+
+
+def suppressor(
+    callback: Callable[..., None],
+    exceptions: Tuple[Type[BaseException], ...] = (Exception,),
+) -> Callable[..., None]:
+    def wrapper() -> None:
+        with suppress(*exceptions):
+            callback()
+    return wrapper
+
+
+def wrap_logging_handler(
+    handler: logging.Handler,
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+    buffer_size: int = 1024,
+    flush_interval: Union[float, int] = 0.1,
+) -> logging.Handler:
+    buffered_handler = logging.handlers.MemoryHandler(
+        buffer_size,
+        target=handler,
+        flushLevel=logging.CRITICAL,
+    )
+
+    run_in_new_thread(
+        _thread_flusher, args=(
+            buffered_handler, flush_interval, loop,
+        ), no_return=True, statistic_name="logger",
+    )
+
+    at_exit_flusher = suppressor(handler.flush)
+    atexit.register(at_exit_flusher)
+    finalize(buffered_handler, partial(atexit.unregister, at_exit_flusher))
+
+    return buffered_handler
+
+
+class UnhandledLoopHook(aiomisc_log.UnhandledHook):
+    @staticmethod
+    def _fill_transport_extra(
+        transport: Optional[asyncio.Transport],
+        extra: Dict[str, Any],
+    ) -> None:
+        if transport is None:
+            return
+
+        extra["transport"] = repr(transport)
+
+        for key in (
+            "peername", "sockname", "compression",
+            "cipher", "peercert", "pipe", "subprocess",
+        ):
+            value = transport.get_extra_info(key)
+            if value:
+                extra[f"transport_{key}"] = value
+
+    def __call__(
+        self, loop: asyncio.AbstractEventLoop,
+        context: Dict[str, Any],
+    ) -> None:
+        context = dict(context)
+        message: str = context.pop("message", "unhandled loop exception")
+        exception: Optional[BaseException] = context.pop("exception", None)
+        future: Optional[asyncio.Future] = context.pop("future", None)
+        task: Optional[asyncio.Task] = context.pop("task", None)
+        handle: Optional[asyncio.Handle] = context.pop("handle", None)
+        protocol: Optional[asyncio.Protocol] = context.pop("protocol", None)
+        transport: Optional[asyncio.Transport] = context.pop("transport", None)
+        sock: Optional[socket] = context.pop("socket", None)
+        source_traceback: List[traceback.FrameSummary] = (
+            context.pop("source_traceback", None) or []
+        )
+
+        if exception is None:
+            if future is not None:
+                exception = future.exception()
+            elif task is not None and task.done():
+                exception = task.exception()
+
+        extra = context
+        if handle is not None:
+            extra["handle"] = repr(handle)
+        if protocol is not None:
+            extra["protocol"] = repr(protocol)
+        if sock is not None:
+            extra["sock"] = repr(sock)
+
+        self._fill_transport_extra(transport, extra)
+        self.logger.exception(message, exc_info=exception, extra=extra)
+        if source_traceback:
+            self.logger.error(
+                "".join(traceback.format_list(source_traceback)),
+            )
+
+
+def basic_config(
+    level: Union[int, str] = LogLevel.default(),
+    log_format: Union[str, LogFormat] = LogFormat.default(),
+    buffered: bool = True, buffer_size: int = 1024,
+    flush_interval: Union[int, float] = 0.2,
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+    handlers: Iterable[logging.Handler] = (),
+    **kwargs: Any,
+) -> None:
+    loop = loop or asyncio.get_event_loop()
+    unhandled_hook = UnhandledLoopHook(logger_name="asyncio.unhandled")
+
+    def wrap_handler(handler: logging.Handler) -> logging.Handler:
+        nonlocal buffer_size, buffered, loop, unhandled_hook
+
+        unhandled_hook.add_handler(handler)
+
+        if buffered:
+            return wrap_logging_handler(
+                handler=handler,
+                buffer_size=buffer_size,
+                flush_interval=flush_interval,
+                loop=loop,
+            )
+        return handler
+
+    aiomisc_log.basic_config(
+        level=level,
+        log_format=log_format,
+        handler_wrapper=wrap_handler,
+        handlers=handlers,
+        **kwargs,
+    )
+
+    loop.set_exception_handler(unhandled_hook)
+
+
+__all__ = (
+    "LogFormat",
+    "LogLevel",
+    "basic_config",
+)
---
+++ packages/aiomisc/aiomisc-17.5.31.tar.gz/aiomisc-17.5.31/aiomisc/service/tls.py
@@ -0,0 +1,241 @@
+import asyncio
+import logging
+import socket
+import ssl
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
+from typing import Any, Optional, Tuple, Union
+
+from ..utils import OptionsType, TimeoutType, awaitable, bind_socket
+from .base import SimpleServer
+from .tcp import RobustTCPClient, TCPClient
+
+
+PathOrStr = Union[Path, str]
+log = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class SSLOptionsBase:
+    cert: Optional[Path]
+    key: Optional[Path]
+    ca: Optional[Path]
+    verify: bool
+    require_client_cert: bool
+    purpose: ssl.Purpose
+
+
+class SSLOptions(SSLOptionsBase):
+    def __init__(
+        self, cert: Optional[PathOrStr], key: Optional[PathOrStr],
+        ca: Optional[PathOrStr], verify: bool, require_client_cert: bool,
+        purpose: ssl.Purpose,
+    ) -> None:
+        super().__init__(
+            cert=Path(cert) if cert else None,
+            key=Path(key) if key else None,
+            ca=Path(ca) if ca else None,
+            verify=verify,
+            require_client_cert=require_client_cert,
+            purpose=purpose,
+        )
+
+    def create_context(self) -> ssl.SSLContext:
+        context = ssl.create_default_context(
+            purpose=self.purpose, cafile=self.ca,
+        )
+
+        if self.ca and not self.ca.exists():
+            raise FileNotFoundError("CA file doesn't exists")
+
+        if self.require_client_cert:
+            context.verify_mode = ssl.VerifyMode.CERT_REQUIRED
+
+        if self.key and self.cert:
+            context.load_cert_chain(self.cert, self.key)
+
+        if not self.verify:
+            context.check_hostname = False
+
+        return context
+
+
+class TLSServer(SimpleServer):
+    PROTO_NAME = "tls"
+
+    def __init__(
+        self, *, address: Optional[str] = None, port: Optional[int] = None,
+        cert: PathOrStr, key: PathOrStr, ca: Optional[PathOrStr] = None,
+        require_client_cert: bool = False, verify: bool = True,
+        options: OptionsType = (), sock: Optional[socket.socket] = None,
+        **kwargs: Any,
+    ):
+
+        self.__ssl_options = SSLOptions(
+            cert, key, ca, verify, require_client_cert,
+            ssl.Purpose.CLIENT_AUTH,
+        )
+
+        if not sock:
+            if address is None or port is None:
+                raise RuntimeError(
+                    "You should pass socket instance or "
+                    '"address" and "port" couple',
+                )
+
+            self.make_socket = partial(
+                bind_socket,
+                address=address,
+                port=port,
+                options=options,
+            )
+        elif not isinstance(sock, socket.socket):
+            raise ValueError("sock must be socket instance")
+        else:
+            self.make_socket = lambda: sock     # type: ignore
+
+        self.socket: Optional[socket.socket] = None
+
+        super().__init__(**kwargs)
+
+    async def __client_handler(
+        self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
+    ) -> Any:
+        return await awaitable(self.handle_client)(reader, writer)
+
+    def make_client_handler(
+        self, reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+    ) -> asyncio.Task:
+        return self.create_task(self.__client_handler(reader, writer))
+
+    @abstractmethod
+    async def handle_client(
+        self, reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+    ) -> Any:
+        raise NotImplementedError
+
+    async def start(self) -> None:
+        ssl_context = await self.loop.run_in_executor(
+            None, self.__ssl_options.create_context,
+        )
+
+        self.socket = self.make_socket()
+
+        self.server = await asyncio.start_server(
+            self.make_client_handler,
+            sock=self.socket,
+            ssl=ssl_context,
+        )
+
+    async def stop(self, exc: Optional[Exception] = None) -> None:
+        await super().stop(exc)
+
+        if self.server:
+            await self.server.wait_closed()
+
+    @property
+    def __sockname(self) -> Optional[tuple]:
+        if self.socket:
+            return self.socket.getsockname()
+        return None
+
+    @property
+    def address(self) -> Optional[str]:
+        if self.__sockname:
+            return self.__sockname[0]
+        return None
+
+    @property
+    def port(self) -> Optional[int]:
+        if self.__sockname:
+            return self.__sockname[1]
+        return None
+
+
+class TLSClient(TCPClient, ABC):
+    __slots__ = "__ssl_options", "__server_hostname"
+
+    connect_attempts: int = 5
+    connect_retry_timeout: TimeoutType = 3
+
+    def __init__(
+        self, address: str, port: int, *,
+        cert: PathOrStr, key: PathOrStr,
+        ca: Optional[PathOrStr] = None, verify: bool = True,
+        **kwargs: Any,
+    ) -> None:
+        self.__ssl_options = SSLOptions(
+            cert, key, ca, verify, False, ssl.Purpose.SERVER_AUTH,
+        )
+
+        super().__init__(address, port, **kwargs)
+
+    async def connect(self) -> Tuple[
+        asyncio.StreamReader, asyncio.StreamWriter,
+    ]:
+        last_error: Optional[Exception] = None
+        for _ in range(self.connect_attempts):
+            try:
+                ssl_context: ssl.SSLContext = await self.loop.run_in_executor(
+                    None, self.__ssl_options.create_context,
+                )
+                reader, writer = await asyncio.open_connection(
+                    self.address, self.port,
+                    ssl=ssl_context,
+                )
+                log.info(
+                    "Connected to %s://%s:%d",
+                    self.PROTO_NAME, self.address, self.port,
+                )
+                return reader, writer
+            except ConnectionError as e:
+                last_error = e
+                await asyncio.sleep(self.connect_retry_timeout)
+
+        raise last_error or RuntimeError("Unknown exception occurred")
+
+
+class RobustTLSClient(RobustTCPClient, ABC):
+    __slots__ = "__ssl_options", "__server_hostname"
+
+    connect_attempts: int = 5
+    connect_retry_timeout: TimeoutType = 3
+
+    def __init__(
+        self, address: str, port: int, *,
+        cert: PathOrStr, key: PathOrStr,
+        ca: Optional[PathOrStr] = None, verify: bool = True,
+        **kwargs: Any,
+    ):
+        self.__ssl_options = SSLOptions(
+            cert, key, ca, verify, False, ssl.Purpose.SERVER_AUTH,
+        )
+
+        super().__init__(address, port, **kwargs)
+
+    async def connect(self) -> Tuple[
+        asyncio.StreamReader, asyncio.StreamWriter,
+    ]:
+        last_error: Optional[Exception] = None
+        for _ in range(self.connect_attempts):
+            try:
+                reader, writer = await asyncio.open_connection(
+                    self.address, self.port,
+                    ssl=await self.loop.run_in_executor(
+                        None, self.__ssl_options.create_context,
+                    ),
+                )
+                log.info(
+                    "Connected to %s://%s:%d",
+                    self.PROTO_NAME, self.address, self.port,
+                )
+                return reader, writer
+            except ConnectionError as e:
+                last_error = e
+                await asyncio.sleep(self.connect_retry_timeout)
+
+        raise last_error or RuntimeError("Unknown exception occurred")
