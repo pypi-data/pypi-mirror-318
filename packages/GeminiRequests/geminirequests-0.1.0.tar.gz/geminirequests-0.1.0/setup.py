63fd1ce9240a4f3a4ae3cdab5a7ea76b91214d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:27 +0000

    Add package GeminiRequests/0.1.0/geminirequests-0.1.0.tar.gz

---
+++ packages/GeminiRequests/geminirequests-0.1.0.tar.gz/geminirequests-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GeminiRequests",  # Уникальное имя вашего пакета
+    version="0.1.0",  # Версия проекта
+    author="Illya Lazarev",
+    author_email="sbdt.israel@gmail.com",
+    description="Gemini AI module (using Gemini AI API through requests lib)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Python-shik/ai-html-parse",  # URL на репозиторий или сайт проекта
+    packages=find_packages(),  # Автоматический поиск пакетов
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',  # Требуемая версия Python
+    #  зависимости requirements.txt
+    install_requires=[
+        "requests"
+    ],
+
+)
