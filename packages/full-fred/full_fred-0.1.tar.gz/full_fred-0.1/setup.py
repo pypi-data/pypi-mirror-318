8ffed4c65bbaa14a254c112e9180e298e40428b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:30 +0000

    Add package full-fred/0.1/full_fred-0.1.tar.gz

---
+++ packages/full-fred/full_fred-0.1.tar.gz/full_fred-0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import versioneer
+
+with open('README.md', encoding='utf-8') as readme:
+    readme = readme.read()
+
+version = versioneer.get_version()
+cmdclass = versioneer.get_cmdclass()
+
+INSTALL_REQUIRES = [
+    'pandas',
+    'requests',
+    'versioneer',
+]
+
+setup(
+    name="full_fred",
+    packages=find_packages(),
+    version=version,
+    cmdclass=cmdclass,
+    description="Full interface to Federal Reserve Economic Data (FRED)",
+    author="Zachary A. Kraehling",
+    author_email="zaknyy@protonmail.com",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=INSTALL_REQUIRES,
+    url="https://github.com/7astro7/full_fred",
+    project_urls={
+        "Tracker": "https://github.com/7astro7/full_fred/issues",
+        "Source": "https://github.com/7astro7/full_fred",
+    },
+    test_suite="full_fred.tests",
+    platforms=["Any"],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["economics", "API", "econ", "fred", "financial", "FRED",],
+)
