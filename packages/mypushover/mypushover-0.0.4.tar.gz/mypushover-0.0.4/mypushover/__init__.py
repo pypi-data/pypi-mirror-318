7e3c4b3adca50b7370d73471aeec68925933cdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:58 +0000

    Add package mypushover/0.0.4/mypushover-0.0.4.tar.gz

---
+++ packages/mypushover/mypushover-0.0.4.tar.gz/mypushover-0.0.4/mypushover/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.0.4'
+__usage__ = """
+    USAGE:
+        As a module from any location
+            mypushover [-option value]
+
+    OPTIONS:
+        -c              config file path (absolute path), if not set, use default path: site-packages/mypushover/pushover_conf.conf
+        -start          start service in background
+        -stop           stop service
+        -restart        restart service
+
+"""
