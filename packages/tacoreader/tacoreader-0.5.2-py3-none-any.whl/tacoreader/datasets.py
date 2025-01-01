2fcf5c93809b11d83c3bacdb5d5a21811e60beeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:32 +0000

    Add package tacoreader/0.5.2/tacoreader-0.5.2-py3-none-any.whl

---
+++ packages/tacoreader/tacoreader-0.5.2-py3-none-any.whl/tacoreader/datasets.py
@@ -0,0 +1,32 @@
+datasets = {
+    "tacofoundation:sen2naipv2-unet": [
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-unet.0000.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-unet.0001.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-unet.0002.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-unet.0003.part.taco",
+    ],
+    "tacofoundation:sen2naipv2-histmatch": [
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-histmatch.0000.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-histmatch.0001.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-histmatch.0002.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-histmatch.0003.part.taco",
+    ],
+    "tacofoundation:sen2naipv2-crosssensor": [
+        "https://huggingface.co/datasets/tacofoundation/SEN2NAIPv2/resolve/main/sen2naipv2-crosssensor.taco",
+    ],
+    "tacofoundation:cloudsen12-l1c": [
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l1c.0000.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l1c.0001.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l1c.0002.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l1c.0003.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l1c.0004.part.taco",
+    ],
+    "tacofoundation:cloudsen12-l2a": [
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0000.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0001.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0002.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0003.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0004.part.taco",
+        "https://huggingface.co/datasets/tacofoundation/cloudsen12/resolve/main/cloudsen12-l2a.0005.part.taco",
+    ],
+}
