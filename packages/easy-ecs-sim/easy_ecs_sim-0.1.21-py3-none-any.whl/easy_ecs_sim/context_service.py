abe018ea2e8275369f7a6c16e1652a0361a42621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:09 +0000

    Add package easy-ecs-sim/0.1.21/easy_ecs_sim-0.1.21-py3-none-any.whl

---
+++ packages/easy-ecs-sim/easy_ecs_sim-0.1.21-py3-none-any.whl/easy_ecs_sim/context_service.py
@@ -0,0 +1,18 @@
+from typing import Type, TypeVar
+
+from easy_ecs_sim.context import Context
+
+T = TypeVar('T')
+
+
+class ContextService:
+
+    @classmethod
+    def singleton(cls: Type[T]) -> T:
+        return cls(Context.default())
+
+    def __init__(self, ctx: Context):
+        self.ctx = ctx
+
+    def find[T](self, ctype: Type[T]):
+        return self.ctx.find(ctype)
