ec54ec9c7e0aeb38891dd86397988596563adcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 04:04:45 +0000

    Add package fy_library/0.5.8/fy_library-0.5.8-py3-none-any.whl

---
+++ packages/fy_library/fy_library-0.5.8-py3-none-any.whl/fy_library/mixins/property/mixin_import_map/abc_fy.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""fy
+from typing import Dict
+
+
+property mixin_import_map: Dict[str, str]
+fy"""
+
+from typing import Dict
+import abc
+
+
+# fy:start ===>>>
+class MixinImportMap_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _mixin_import_map(self) -> Dict[str, str]:
+        raise NotImplementedError()
+        # fy:end <<<===
