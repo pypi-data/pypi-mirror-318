b27c1fcb940264aa403b3a10b35313c38c03ff53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:46 +0000

    Add package static-npm/1.0.1/static_npm-1.0.1-py2.py3-none-any.whl

---
+++ packages/static-npm/static_npm-1.0.1-py2.py3-none-any.whl/static_npm/cli.py
@@ -0,0 +1,29 @@
+"""
+Main entry point.
+"""
+
+import argparse
+
+from static_npm.node import Node
+from static_npm.npm import Npm
+from static_npm.npx import Npx
+from static_npm.prog_exec import run
+
+
+def parse_args() -> tuple[argparse.Namespace, list[str]]:
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description="npm static binary")
+    return parser.parse_known_args()
+
+
+def main_npm() -> int:
+    """Main entry point for the template_python_cmd package."""
+    return run(Npm())
+
+
+def main_node() -> int:
+    return run(Node())
+
+
+def main_npx() -> int:
+    return run(Npx())
