5692db85f94f9d73f602a254ffa466db1042f924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:30 +0000

    Add package ss-pylib/0.0.12/ss-pylib-0.0.12.tar.gz

---
+++ packages/ss-pylib/ss-pylib-0.0.12.tar.gz/ss-pylib-0.0.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(
+  name='ss-pylib',
+  version='0.0.12',
+  description='SWEET-SOOP Python Library',
+  author='sweetsoop',
+  author_email='sweetsoop@gmail.com',
+  license='MIT',
+  packages=find_packages(),
+  python_requires='>=3.9',
+  entry_points={
+    'console_scripts': [
+      'hey = sslib.main:main'
+    ]
+  },
+  install_requires=[
+    'httpx',
+  ],
+)
