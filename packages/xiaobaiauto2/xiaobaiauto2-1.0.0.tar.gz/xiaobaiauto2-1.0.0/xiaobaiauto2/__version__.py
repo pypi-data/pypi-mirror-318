ffad927a15b1a448734eada131e640d380b68b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 14:03:29 +0000

    Add package xiaobaiauto2/1.0.0/xiaobaiauto2-1.0.0.tar.gz

---
+++ packages/xiaobaiauto2/xiaobaiauto2-1.0.0.tar.gz/xiaobaiauto2-1.0.0/xiaobaiauto2/__version__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+__project__ = 'xiaobaiauto2'
+__script__ = '__version__.py'
+__create_time__ = '2020/7/15 23:21'
+
+__version__ = '.'.join(map(str, (1, 0, 0)))
