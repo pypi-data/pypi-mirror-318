003ba473361ee76a7d026701f431912f580fc9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-01 15:04:17 +0000

    Add package new-print-lib/1.0.2/new_print_lib-1.0.2-py3-none-any.whl

---
+++ packages/new-print-lib/new_print_lib-1.0.2-py3-none-any.whl/new_print_lib/font_styles/font_styles.py
@@ -0,0 +1,13 @@
+FONT_STYLES = {
+    "bold": "\033[1m",
+    "dim": "\033[2m",
+    "italic": "\033[3m",
+    "underline": "\033[4m",
+    "blink": "\033[5m",
+    "inverse": "\033[7m",
+    "hidden": "\033[8m",
+    "strikethrough": "\033[9m",
+    "double-underline": "\033[21m",
+    "frame": "\033[51m",
+    "overline": "\033[53m",
+}
