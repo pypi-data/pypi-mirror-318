0a13f9e8da287dd7a63531148be47d5467b884f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 16:04:29 +0000

    Add package random-lib-debug/0.3.2/random_lib_debug-0.3.2-py3-none-any.whl

---
+++ packages/random-lib-debug/random_lib_debug-0.3.2-py3-none-any.whl/random_lib_debug/__init__.py
@@ -0,0 +1,6 @@
+
+import random
+
+def generate_random_int(min_value: int, max_value: int) -> int:
+    """Generate a random integer between min_value and max_value."""
+    return random.randint(min_value, max_value)
---
+++ packages/random-lib-debug/random_lib_debug-0.3.2-py3-none-any.whl/random_lib_debug/__main__.py
@@ -0,0 +1,5 @@
+import random
+
+def generate_random_int(min_value: int, max_value: int) -> int:
+    """Generate a random integer between min_value and max_value."""
+    return random.randint(min_value, max_value)
