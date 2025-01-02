c29eae6c7745e55dda4e242ac0caaff8f71319dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:57 +0000

    Add package quantflow/0.3.1/quantflow-0.3.1-py3-none-any.whl

---
+++ packages/quantflow/quantflow-0.3.1-py3-none-any.whl/quantflow/options/calibration.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field, replace
+from datetime import datetime
+from decimal import Decimal
+from typing import Any, Generic, NamedTuple, Sequence, TypeVar
+
+import numpy as np
+import pandas as pd
+from scipy.optimize import Bounds, OptimizeResult, minimize
+
+from quantflow.sp.base import StochasticProcess1D
+from quantflow.sp.heston import Heston
+from quantflow.utils import plot
+
+from .pricer import OptionPricer
+from .surface import OptionPrice, VolSurface
+
+M = TypeVar("M", bound=StochasticProcess1D)
+
+
+class ModelCalibrationEntryKey(NamedTuple):
+    maturity: datetime
+    strike: Decimal
+
+
+@dataclass
+class OptionEntry:
+    """Entry for a single option"""
+
+    ttm: float
+    moneyness: float
+    options: list[OptionPrice] = field(default_factory=list)
+    _prince_range: Bounds | None = None
+
+    def implied_vol_range(self) -> Bounds:
+        """Get the range of implied volatilities"""
+        implied_vols = tuple(option.implied_vol for option in self.options)
+        return Bounds(min(implied_vols), max(implied_vols))
+
+    def residual(self, price: float) -> float:
+        """Calculate the residual for a given price
+
+        when inside bid/offer, the residual is 0
+        """
+        return min(np.min(self.price_range().residual(price)), 0)
+
+    def price_range(self) -> Bounds:
+        """Get the range of prices"""
+        if self._prince_range is None:
+            prices = tuple(float(option.call_price) for option in self.options)
+            self._prince_range = Bounds(min(prices), max(prices))
+        return self._prince_range
+
+
+@dataclass
+class VolModelCalibration(ABC, Generic[M]):
+    """Calibration of a stochastic volatility model"""
+
+    pricer: OptionPricer[M]
+    """The option pricer for the model"""
+    vol_surface: VolSurface[Any]
+    """The volatility surface"""
+    minimize_method: str | None = None
+    """The optimization method to use"""
+    moneyness_weight: float = 0.5
+    """The weight for moneyness"""
+    options: dict[ModelCalibrationEntryKey, OptionEntry] = field(default_factory=dict)
+    """The options to calibrate"""
+
+    def __post_init__(self) -> None:
+        if not self.options:
+            self.vol_surface.bs()
+            for option in self.vol_surface.option_prices():
+                key = ModelCalibrationEntryKey(option.maturity, option.strike)
+                if key not in self.options:
+                    entry = OptionEntry(ttm=option.ttm, moneyness=option.moneyness)
+                    self.options[key] = entry
+                entry.options.append(option)
+
+    @abstractmethod
+    def get_params(self) -> np.ndarray:
+        """Get the parameters of the model"""
+
+    @abstractmethod
+    def set_params(self, params: np.ndarray) -> None:
+        """Set the parameters of the model"""
+
+    @property
+    def model(self) -> M:
+        """Get the model"""
+        return self.pricer.model
+
+    @property
+    def ref_date(self) -> datetime:
+        """Get the reference date"""
+        return self.vol_surface.ref_date
+
+    @property
+    def implied_vols(self) -> np.ndarray:
+        data: list[float] = []
+        for entry in self.options.values():
+            data.extend(option.implied_vol for option in entry.options)
+        return np.asarray(data)
+
+    def remove_implied_above(self, quantile: float = 0.95) -> VolModelCalibration:
+        exclude_above = np.quantile(self.implied_vols, quantile)
+        options = {}
+        for key, entry in self.options.items():
+            if entry.implied_vol_range().ub <= exclude_above:
+                options[key] = entry
+        return replace(self, options=options)
+
+    def get_bounds(self) -> Bounds | None:  # pragma: no cover
+        """Get the bounds for the calibration"""
+        return None
+
+    def get_constraints(self) -> Sequence[dict[str, Any]] | None:
+        """Get the constraints for the calibration"""
+        return None
+
+    def implied_vol_range(self) -> Bounds:
+        """Get the range of implied volatilities"""
+        return Bounds(
+            min(option.implied_vol_range().lb for option in self.options.values()),
+            max(option.implied_vol_range().ub for option in self.options.values()),
+        )
+
+    def fit(self) -> OptimizeResult:
+        """Fit the model"""
+        return minimize(
+            self.cost_function,
+            self.get_params(),
+            method=self.minimize_method,
+            bounds=self.get_bounds(),
+            constraints=self.get_constraints(),
+        )
+
+    def cost_weight(self, ttm: float, moneyness: float) -> float:
+        return np.exp(-self.moneyness_weight * moneyness)
+
+    def penalize(self) -> float:
+        """Penalize the cost function"""
+        return 0.0
+
+    def cost_function(self, params: np.ndarray) -> float:
+        """Calculate the cost function from the model prices"""
+        self.set_params(params)
+        self.pricer.reset()
+        cost = 0.0
+        for entry in self.options.values():
+            model_price = self.pricer.call_price(entry.ttm, entry.moneyness)
+            if residual := entry.residual(model_price):
+                weight = self.cost_weight(entry.ttm, entry.moneyness)
+                cost += weight * residual**2
+        return cost + self.penalize()
+
+    def plot(
+        self,
+        index: int = 0,
+        *,
+        max_moneyness_ttm: float | None = 1.0,
+        support: int = 51,
+        **kwargs: Any,
+    ) -> Any:
+        """Plot the implied volatility for market and model prices"""
+        cross = self.vol_surface.maturities[index]
+        options = tuple(self.vol_surface.option_prices(index=index))
+        cross = self.vol_surface.maturities[index]
+        model = self.pricer.maturity(cross.ttm(self.ref_date))
+        if max_moneyness_ttm is not None:
+            model = model.max_moneyness_ttm(
+                max_moneyness_ttm=max_moneyness_ttm, support=support
+            )
+        return plot.plot_vol_surface(
+            pd.DataFrame([d._asdict() for d in options]),
+            model=model.df,
+            **kwargs,
+        )
+
+
+@dataclass
+class HestonCalibration(VolModelCalibration[Heston]):
+    """A :class:`.VolModelCalibration` for the :class:`.Heston`
+    stochastic volatility model"""
+
+    feller_penalize: float = 0.0
+
+    def get_bounds(self) -> Sequence[Bounds] | None:
+        vol_range = self.implied_vol_range()
+        vol_lb = 0.5 * vol_range.lb[0]
+        vol_ub = 1.5 * vol_range.ub[0]
+        return Bounds(
+            [vol_lb * vol_lb, vol_lb * vol_lb, 0.0, 0.0, -0.9],
+            [vol_ub * vol_ub, vol_ub * vol_ub, np.inf, np.inf, 0],
+        )
+
+    def get_params(self) -> np.ndarray:
+        return np.asarray(
+            [
+                self.model.variance_process.rate,
+                self.model.variance_process.theta,
+                self.model.variance_process.kappa,
+                self.model.variance_process.sigma,
+                self.model.rho,
+            ]
+        )
+
+    def set_params(self, params: np.ndarray) -> None:
+        self.model.variance_process.rate = params[0]
+        self.model.variance_process.theta = params[1]
+        self.model.variance_process.kappa = params[2]
+        self.model.variance_process.sigma = params[3]
+        self.model.rho = params[4]
+
+    def penalize(self) -> float:
+        kt = 2 * self.model.variance_process.kappa * self.model.variance_process.theta
+        neg = max(0.5 * self.model.variance_process.sigma2 - kt, 0)
+        return self.feller_penalize * neg * neg
---
+++ packages/quantflow/quantflow-0.3.1-py3-none-any.whl/quantflow/sp/jump_diffusion.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import Generic, Self
+
+import numpy as np
+from pydantic import Field
+
+from ..ta.paths import Paths
+from ..utils.distributions import Normal
+from ..utils.types import FloatArrayLike, Vector
+from .base import StochasticProcess1D
+from .poisson import CompoundPoissonProcess, D
+from .weiner import WeinerProcess
+
+
+class JumpDiffusion(StochasticProcess1D, Generic[D]):
+    r"""A generic jump-diffusion model
+
+    .. math::
+        dx_t = \sigma d w_t + d N_t
+
+    where :math:`w_t` is a Weiner process with standard deviation :math:`\sigma`
+    and :math:`N_t` is a compound poisson process
+    with intensity :math:`\lambda` and jump distribution `D`
+    """
+
+    diffusion: WeinerProcess = Field(
+        default_factory=WeinerProcess, description="diffusion"
+    )
+    jumps: CompoundPoissonProcess[D] = Field(description="jump process")
+
+    def characteristic_exponent(self, t: FloatArrayLike, u: Vector) -> Vector:
+        return self.diffusion.characteristic_exponent(
+            t, u
+        ) + self.jumps.characteristic_exponent(t, u)
+
+    def sample(self, n: int, time_horizon: float = 1, time_steps: int = 100) -> Paths:
+        dw1 = Paths.normal_draws(n, time_horizon, time_steps)
+        return self.sample_from_draws(dw1)
+
+    def sample_from_draws(self, path_w: Paths, *args: Paths) -> Paths:
+        if args:
+            path_j = args[0]
+        else:
+            path_j = self.jumps.sample(path_w.samples, path_w.t, path_w.time_steps)
+        path_w = self.diffusion.sample_from_draws(path_w)
+        return Paths(t=path_w.t, data=path_w.data + path_j.data)
+
+    def analytical_mean(self, t: FloatArrayLike) -> FloatArrayLike:
+        return self.diffusion.analytical_mean(t) + self.jumps.analytical_mean(t)
+
+    def analytical_variance(self, t: FloatArrayLike) -> FloatArrayLike:
+        return self.diffusion.analytical_variance(t) + self.jumps.analytical_variance(t)
+
+
+class Merton(JumpDiffusion[Normal]):
+    """Merton jump-diffusion model"""
+
+    @classmethod
+    def create(
+        cls,
+        vol: float = 0.5,
+        diffusion_percentage: float = 0.5,
+        jump_intensity: float = 100,
+        jump_skew: float = 0.0,
+    ) -> Self:
+        """Create a Merton jump-diffusion model with a given volatility,
+        diffusion percentage, jump skewness, and jump intensity"""
+        variance = vol * vol
+        jump_std = 1.0
+        jump_mean = 0.0
+        if diffusion_percentage > 1:
+            raise ValueError("diffusion_percentage must be less than 1")
+        elif diffusion_percentage < 0:
+            raise ValueError("diffusion_percentage must be greater than 0")
+        elif diffusion_percentage == 1:
+            jump_intensity = 0
+        else:
+            jump_std = np.sqrt(variance * (1 - diffusion_percentage) / jump_intensity)
+            jump_mean = jump_skew / jump_intensity
+        return cls(
+            diffusion=WeinerProcess(sigma=np.sqrt(variance * diffusion_percentage)),
+            jumps=CompoundPoissonProcess[Normal](
+                intensity=jump_intensity, jumps=Normal(mu=jump_mean, sigma=jump_std)
+            ),
+        )
