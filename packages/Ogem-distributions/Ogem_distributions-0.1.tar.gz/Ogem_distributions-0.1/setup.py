b51d8a6c3d22b26b697265449ff11ce07e8dbc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:06:02 +0000

    Add package Ogem-distributions/0.1/Ogem_distributions-0.1.tar.gz

---
+++ packages/Ogem-distributions/Ogem_distributions-0.1.tar.gz/Ogem_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Ogem_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Ogem_distributions'],
+      zip_safe=False)
