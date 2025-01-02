e5708d56e8bbb0a6104730e92e68ac1af6da2103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:06:50 +0000

    Add package tombi/0.0.36/tombi-0.0.36.tar.gz

---
+++ packages/tombi/tombi-0.0.36.tar.gz/tombi-0.0.36/crates/formatter/src/format/value/string.rs
@@ -0,0 +1,40 @@
+use super::LiteralNode;
+
+impl LiteralNode for ast::BasicString {
+    fn token(&self) -> Option<syntax::SyntaxToken> {
+        self.token()
+    }
+}
+
+impl LiteralNode for ast::MultiLineBasicString {
+    fn token(&self) -> Option<syntax::SyntaxToken> {
+        self.token()
+    }
+}
+
+impl LiteralNode for ast::LiteralString {
+    fn token(&self) -> Option<syntax::SyntaxToken> {
+        self.token()
+    }
+}
+
+impl LiteralNode for ast::MultiLineLiteralString {
+    fn token(&self) -> Option<syntax::SyntaxToken> {
+        self.token()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_format;
+
+    test_format! {
+        #[test]
+        fn basic_string_key_value1(r#"key = "value""#) -> Ok(r#"key = "value""#);
+    }
+
+    test_format! {
+        #[test]
+        fn basic_string_key_value2(r#"key    = "value""#) -> Ok(r#"key = "value""#);
+    }
+}
