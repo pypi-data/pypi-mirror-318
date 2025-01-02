a93d4f4432baadf2e0bfd6028786bb95f7186001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 16:04:31 +0000

    Add package my-commandline-tool/0.6.4/my_commandline_tool-0.6.4.tar.gz

---
+++ packages/my-commandline-tool/my_commandline_tool-0.6.4.tar.gz/my_commandline_tool-0.6.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# @Author        : yuzijian
+# @Email         : yuzijian1010@163.com
+# @FileName      : setup.py
+# @Time          : 2024-12-30 11:51:53
+# @description   : 
+"""
+
+
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='my_commandline_tool',
+    version='0.6.4',  # 更新版本号
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycli=my_commandline_tool.cli:main',
+        ],
+    },
+    description='A simple command-line tool',
+    author='yuzijian',
+    author_email='yuzijian1010@163.com',
+    url='https://github.com/yourusername/my_commandline_tool',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
