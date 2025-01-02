4f29080b2b36d61a3995c8d23033b12a85ebc005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:37 +0000

    Add package versionhq/1.1.7.8/versionhq-1.1.7.8.tar.gz

---
+++ packages/versionhq/versionhq-1.1.7.8.tar.gz/versionhq-1.1.7.8/tests/clients/workflow_test.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+from versionhq.agent.model import Agent
+from versionhq.llm.model import LLM
+from versionhq.clients.workflow.model import Score, ScoreFormat, MessagingWorkflow, MessagingComponent
+
+MODEL_NAME = os.environ.get("LITELLM_MODEL_NAME", "gpt-3.5-turbo")
+LITELLM_API_KEY = os.environ.get("LITELLM_API_KEY")
+
+
+def test_store_scores():
+    """
+    Test if the final result will be calcurated using a random subject
+    """
+
+    messaging_component = MessagingComponent(message="demo")
+    score_raw = 15
+    messaging_component.store_scoring_result("demo", score_raw=score_raw)
+
+    assert messaging_component.score is not None
+    assert messaging_component.score.result() is not None
+
+
+def test_score_result():
+    messaging_component = MessagingComponent(message="demo")
+    score_raw = 15
+    messaging_component.store_scoring_result("demo", score_raw=score_raw)
+
+    result = messaging_component.score.result()
+
+    assert result is not None
+    assert result != 0
