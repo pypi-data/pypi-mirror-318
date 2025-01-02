fbbb64dd510a5ce18f8c52b844d6af18370f730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:37 +0000

    Add package nonebot-plugin-resolver2/1.6.0/nonebot_plugin_resolver2-1.6.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.6.0-py3-none-any.whl/nonebot_plugin_resolver2/matchers/__init__.py
@@ -0,0 +1,9 @@
+# 使用列表批量导入 matcher
+modules = ["bilibili", "douyin", "kugou", "twitter", "ncm", "ytb", "acfun", "tiktok", "weibo", "xiaohongshu"]
+for module in modules:
+    exec(f"from .{module} import {module}")
+    
+resolvers = {module: eval(module) for module in modules}
+
+# from .filter import *
+# commands = [enable_resolve, disable_resolve, check_resolve, enable_all_resolve, disable_all_resolve]
