466c012275d73f979d1a96ccb7b2b5e7ca8c603a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:00 +0000

    Add package python-snaptime/0.1.0/python_snaptime-0.1.0.tar.gz

---
+++ packages/python-snaptime/python_snaptime-0.1.0.tar.gz/python_snaptime-0.1.0/python_snaptime/__version__.py
@@ -0,0 +1,4 @@
+"""The package version."""
+
+__version__ = "0.1.0"
+__version_tuple__ = (0, 1, 0)
---
+++ packages/python-snaptime/python_snaptime-0.1.0.tar.gz/python_snaptime-0.1.0/python_snaptime/main.py
@@ -0,0 +1,48 @@
+"""Main package for Python Snaptime."""
+
+from __future__ import annotations
+
+import datetime
+from typing import overload
+from zoneinfo import ZoneInfo
+
+import pendulum
+
+from python_snaptime.parsers import parse_snaptime_string
+
+__all__ = ["snap"]
+
+
+@overload
+def snap(dtm: pendulum.DateTime, snap: str) -> pendulum.DateTime: ...
+
+
+@overload
+def snap(dtm: datetime.datetime, snap: str) -> datetime.datetime: ...
+
+
+def snap(dtm: pendulum.DateTime | datetime.datetime, snap: str) -> pendulum.DateTime | datetime.datetime:
+    """Transform datetimes using relative time modifiers.
+
+    Args:
+        dtm (pendulum.DateTime | datetime.datetime): The datetime to be transformed.
+        snap (str): The snaptime string defining the relative time transformation.
+
+    Returns:
+        pendulum.DateTime | datetime.datetime: The resulting snapped datetime.
+    """
+    if isinstance(dtm, pendulum.DateTime):
+        snap_dtm = parse_snaptime_string(snap, dtm)
+    elif isinstance(dtm, datetime.datetime):  # pyright: ignore[reportUnnecessaryIsInstance]
+        snap_dtm = parse_snaptime_string(snap, pendulum.instance(dtm))
+    else:
+        raise TypeError("Invalid datetime type. Must be pendulum.DateTime or datetime.datetime.")
+
+    if not isinstance(dtm, pendulum.DateTime):
+        if dtm.tzinfo is not None and dtm.tzinfo.utcoffset(dtm) is not None:
+            # aware
+            snap_dtm = datetime.datetime.fromtimestamp(snap_dtm.timestamp(), tz=snap_dtm.tz)
+        else:
+            # naive
+            snap_dtm = datetime.datetime.fromtimestamp(snap_dtm.naive().timestamp())  # noqa: DTZ006
+    return snap_dtm
