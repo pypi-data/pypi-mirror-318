f24d8626684d0abb8d533e73f2b08ecdfcc01482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:03 +0000

    Add package academic-metrics/1.0.93/academic_metrics-1.0.93-py3-none-any.whl

---
+++ packages/academic-metrics/academic_metrics-1.0.93-py3-none-any.whl/academic_metrics/constants/__init__.py
@@ -0,0 +1,8 @@
+from .dir_paths import (
+    INPUT_FILES_DIR_PATH,
+    LOG_DIR_PATH,
+    OUTPUT_FILES_DIR_PATH,
+    SPLIT_FILES_DIR_PATH,
+    locate_academic_metrics_root,
+    locate_src_root,
+)
