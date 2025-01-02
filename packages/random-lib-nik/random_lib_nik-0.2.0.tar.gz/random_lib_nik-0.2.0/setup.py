95b5836cd6ddaad97672bb18c0b310c8b55caa24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:30 +0000

    Add package random-lib-nik/0.2.0/random_lib_nik-0.2.0.tar.gz

---
+++ packages/random-lib-nik/random_lib_nik-0.2.0.tar.gz/random_lib_nik-0.2.0/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='random_lib_nik',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A library to generate random integers',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # ... other classifiers ...
+    ],
+)
