1ba9aff06814443fa8b127dbf4ef4915a310b05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:32 +0000

    Add package lxp/0.0.2/lxp-0.0.2.tar.gz

---
+++ packages/lxp/lxp-0.0.2.tar.gz/lxp-0.0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*
+import os
+from setuptools import setup
+
+dirs = ['lxp']
+modules = ['lxp']
+for path,dir_list,file_list in os.walk("./lxp"):
+    for dir_name in dir_list:
+        if dir_name.find("__")==-1 and dir_name.find("egg")==-1:
+            dirs.append((path+"/"+dir_name).replace("/",".").replace("..",""))
+    for file_name in file_list:
+        if file_name.find(".py")!=-1 and file_name.find("__")==-1 and file_name.find(".pyc")==-1:
+            modules.append((path+"."+file_name.replace(".py","")).replace("/",".").replace("..",""))
+# print(dirs,modules)
+setup(
+    name='lxp',
+    version='0.0.2',
+    author='lxp',
+    author_email='1282856796@qq.com',
+    url='https://makeblock.com',
+    description=u'libraries for makeblock electronic platform',
+    packages=dirs,
+    py_modules=modules,
+    install_requires=['makeblock'],
+)
+# setup.py
+# global-include *.pyc
+# global-exclude *.py
