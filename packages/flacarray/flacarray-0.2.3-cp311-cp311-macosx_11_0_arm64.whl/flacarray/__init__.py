c7c74781cf182cbfa6a9f4db4b1c4637a0d5946e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:58 +0000

    Add package flacarray/0.2.3/flacarray-0.2.3-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/flacarray/flacarray-0.2.3-cp311-cp311-macosx_11_0_arm64.whl/flacarray/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2024-2025 by the parties listed in the AUTHORS file.
+# All rights reserved.  Use of this source code is governed by
+# a BSD-style license that can be found in the LICENSE file.
+
+# FIXME: single-sourcing the package version from git without using
+# setuptools build backend in pyproject.toml seems difficult...
+
+import os
+import logging
+
+# Get the log level from the environment
+log_level = os.getenv("FLACARRAY_LOG_LEVEL", "INFO")
+
+# Set the log level
+logging.basicConfig(level=log_level)
+
+__version__ = "0.2.3"
+
+from .array import FlacArray
