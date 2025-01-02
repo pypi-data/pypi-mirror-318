b41ece37bcbbeaef1ca6a6d87e05473face9e955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:12 +0000

    Add package spellbot/14.2.0/spellbot-14.2.0-py3-none-any.whl

---
+++ packages/spellbot/spellbot-14.2.0-py3-none-any.whl/spellbot/spelltable.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import json
+import logging
+from typing import TYPE_CHECKING, Any
+
+from aiohttp.client_exceptions import ClientError
+from aiohttp_retry import ExponentialRetry, RetryClient
+
+from spellbot import __version__
+from spellbot.metrics import add_span_error
+from spellbot.settings import settings
+
+if TYPE_CHECKING:
+    from spellbot.models import GameDict
+
+logger = logging.getLogger(__name__)
+
+
+async def generate_spelltable_link(game: GameDict) -> str | None:
+    assert settings.SPELLTABLE_AUTH_KEY
+
+    headers = {
+        "user-agent": f"spellbot/{__version__}",
+        "key": settings.SPELLTABLE_AUTH_KEY,
+    }
+
+    data: dict[str, Any] | None = None
+    raw_data: bytes | None = None
+    try:
+        async with (
+            RetryClient(
+                raise_for_status=False,
+                retry_options=ExponentialRetry(attempts=5),
+            ) as client,
+            client.post(settings.SPELLTABLE_CREATE, headers=headers) as resp,
+        ):
+            # Rather than use `resp.json()`, which respects mimetype, let's just
+            # grab the data and try to decode it ourselves.
+            # https://github.com/inyutin/aiohttp_retry/issues/55
+            raw_data = await resp.read()
+            data = json.loads(raw_data)
+            if not data or "gameUrl" not in data:
+                logger.warning(
+                    "warning: gameUrl missing from SpellTable API response (%s): %s",
+                    resp.status,
+                    data,
+                )
+                return None
+            assert data is not None
+            returned_url = str(data["gameUrl"])
+            return returned_url.replace(
+                "www.spelltable.com",
+                "spelltable.wizards.com",
+            )
+    except ClientError as ex:
+        add_span_error(ex)
+        logger.warning(
+            "warning: SpellTable API failure: %s, data: %s, raw: %s",
+            ex,
+            data,
+            raw_data,
+            exc_info=True,
+        )
+        return None
+    except Exception as ex:
+        if raw_data == b"upstream request timeout":
+            return None
+
+        add_span_error(ex)
+        logger.exception("error: unexpected exception: data: %s, raw: %s", data, raw_data)
+        return None
---
+++ packages/spellbot/spellbot-14.2.0-py3-none-any.whl/spellbot/web/builder.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import logging
+from contextlib import suppress
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import aiohttp_jinja2
+import jinja2
+import pytz
+from aiohttp import web
+from babel.dates import format_datetime
+
+from spellbot.database import initialize_connection
+from spellbot.models import import_models
+from spellbot.settings import settings
+from spellbot.web.api import ping, record
+
+if TYPE_CHECKING:
+    from asyncio.events import AbstractEventLoop as Loop
+
+logger = logging.getLogger(__name__)
+
+TEMPLATES_ROOT = Path(__file__).resolve().parent / "templates"
+
+
+def humanize(ts: int, offset: int, zone: str) -> str:
+    d = datetime.fromtimestamp(ts / 1e3, tz=pytz.UTC) - timedelta(minutes=offset)
+    with suppress(pytz.UnknownTimeZoneError):
+        d = d.replace(tzinfo=pytz.timezone(zone))
+    return format_datetime(d, format="long")
+
+
+def build_web_app() -> web.Application:
+    import_models()
+    app = web.Application()
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.FileSystemLoader(TEMPLATES_ROOT),
+        filters={"humanize": humanize},
+    )
+    app.add_routes(
+        [
+            web.get(r"/", ping.endpoint),
+            web.get(r"/g/{guild}/c/{channel}", record.channel_endpoint),
+            web.get(r"/g/{guild}/u/{user}", record.user_endpoint),
+        ],
+    )
+    return app
+
+
+def launch_web_server(loop: Loop, port: int) -> None:  # pragma: no cover
+    app = build_web_app()
+    runner = web.AppRunner(app)
+    loop.run_until_complete(runner.setup())
+    loop.run_until_complete(initialize_connection("spellbot-web"))
+    site = web.TCPSite(runner, settings.HOST, port)
+    loop.run_until_complete(site.start())
+    logger.info("server running: http://%s:%s", settings.HOST, port)
