5c184375119800cd21e6af0c695399ef5c9eb168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:03:52 +0000

    Add package chkbit/4.2.2/chkbit-4.2.2.tar.gz

---
+++ packages/chkbit/chkbit-4.2.2.tar.gz/chkbit-4.2.2/README.md
@@ -0,0 +1,6 @@
+
+# chkbit
+
+This is the final/obsolete python chkbit release.
+
+To upgrade go to https://github.com/laktak/chkbit
---
+++ packages/chkbit/chkbit-4.2.2.tar.gz/chkbit-4.2.2/chkbit/__init__.py
@@ -0,0 +1,7 @@
+from .status import Status
+from .ignore import Ignore
+from .input_item import InputItem
+from .context import Context
+from .hashfile import hashfile, hashtext
+from .index import Index
+from .index_thread import IndexThread
