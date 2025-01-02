306518dc72fdb0b0ac2654f2dca841464ed45371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:05 +0000

    Add package dischub/2.0.6/dischub-2.0.6.tar.gz

---
+++ packages/dischub/dischub-2.0.6.tar.gz/dischub-2.0.6/dischub/main.py
@@ -0,0 +1,16 @@
+import requests
+class Dischub:
+    def __init__(self):
+        self.api_url = "http://dischub.co.zw/api/orders/create/" 
+    def create_payment(self, data):
+        try:
+            response = requests.post(self.api_url, json=data)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+            return response  # Assuming the response is in JSON format
+        except requests.exceptions.ConnectionError:
+            return {'status': 'error', 'message': 'connection failed. unable to reach the server.'}
+        except requests.exceptions.Timeout:
+            return {'status': 'error', 'message': 'request timed out.'}
+        except requests.exceptions.RequestException as e:
+            # Catch other exceptions like HTTP errors or invalid URLs
+            return {'status': 'error', 'message': f'an error occurred: {str(e)}'}
