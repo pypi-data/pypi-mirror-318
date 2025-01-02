e62f7632525a44662889fa863b2fdd6a7ae61536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:03 +0000

    Add package agentic-workflow/0.0.7/agentic_workflow-0.0.7-py3-none-any.whl

---
+++ packages/agentic-workflow/agentic_workflow-0.0.7-py3-none-any.whl/agentic_workflow/crud/connection.py
@@ -0,0 +1,18 @@
+from typing import List
+from agentic_workflow.crud.base import CRUDBase
+from agentic_workflow.adk.models.connection import ConnectionCore
+from agentic_workflow.db.models import Connection
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from agentic_workflow.utils.auth import User
+
+class CRUDConnection(CRUDBase[Connection, ConnectionCore, ConnectionCore]):
+    async def get_by_app_id(self, session: AsyncSession, *, app_id: str, version: str, user: User) -> List[Connection]:
+        """
+        Retrieve all connections associated with a specific app_id
+        """
+        statement = select(self.model).where(self.model.appId == app_id).where(self.model.appVersion == version).where(self.model.orgId == user.tenantModel.orgId)
+        result = await session.exec(statement)
+        return list(result.all())
+
+connection = CRUDConnection(Connection)
