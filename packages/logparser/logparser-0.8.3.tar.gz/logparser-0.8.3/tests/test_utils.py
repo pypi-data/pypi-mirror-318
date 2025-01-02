5e9172a7735eb74748fd9b27b94e1bb3fc6e6e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:06:40 +0000

    Add package logparser/0.8.3/logparser-0.8.3.tar.gz

---
+++ packages/logparser/logparser-0.8.3.tar.gz/logparser-0.8.3/tests/test_utils.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import os
+
+from logparser.run import STAR
+from logparser.utils import check_update
+
+
+def test_run_py():
+    assert STAR  # Test importing of logparser/logparser/run.py
+
+
+def test_check_update():
+    js = check_update(timeout=60)
+    if js:
+        assert 'latest_version' in js and 'info' in js
+    else:
+        print('Got empty js.')
+
+
+def test_main_pid_exit(psr):
+    psr(main_pid=os.getpid())
