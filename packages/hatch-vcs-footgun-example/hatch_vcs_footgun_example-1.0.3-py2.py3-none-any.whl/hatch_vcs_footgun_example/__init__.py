048a94703c2abefb073c830cef1c3bcab01f8b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:13 +0000

    Add package hatch-vcs-footgun-example/1.0.3/hatch_vcs_footgun_example-1.0.3-py2.py3-none-any.whl

---
+++ packages/hatch-vcs-footgun-example/hatch_vcs_footgun_example-1.0.3-py2.py3-none-any.whl/hatch_vcs_footgun_example/__init__.py
@@ -0,0 +1,9 @@
+"""Project initialization.
+
+It is a popular convention to define `__version__` in the top-level `__init__.py`.
+"""
+
+from hatch_vcs_footgun_example.version import __version__
+
+
+__all__ = ["__version__"]
