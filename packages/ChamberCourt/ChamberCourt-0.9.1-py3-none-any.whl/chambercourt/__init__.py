4e93a1ce5b14502c30ba64af2575d30dac682475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:06:06 +0000

    Add package ChamberCourt/0.9.1/ChamberCourt-0.9.1-py3-none-any.whl

---
+++ packages/ChamberCourt/ChamberCourt-0.9.1-py3-none-any.whl/chambercourt/__init__.py
@@ -0,0 +1,18 @@
+# © Reuben Thomas <rrt@sc3d.org> 2024
+# Released under the GPL version 3, or (at your option) any later version.
+
+import re
+import sys
+from typing import List
+
+from . import game
+from .game import app_main
+
+
+def main(argv: List[str] = sys.argv[1:]) -> None:
+    app_main(argv, game, game.Game)
+
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"__init__.py$", "chambercourt", sys.argv[0])
+    main()
