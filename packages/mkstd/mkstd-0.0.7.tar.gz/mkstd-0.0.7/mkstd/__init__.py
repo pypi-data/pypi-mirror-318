e6f8cd2747e15ab1d9220b3a78f8cb4a10ae83a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:05:45 +0000

    Add package mkstd/0.0.7/mkstd-0.0.7.tar.gz

---
+++ packages/mkstd/mkstd-0.0.7.tar.gz/mkstd-0.0.7/mkstd/__init__.py
@@ -0,0 +1,6 @@
+"""mkstd: the standard maker."""
+__version__ = "0.0.7"
+
+
+from .standards import *
+from .types import get_array_type
---
+++ packages/mkstd/mkstd-0.0.7.tar.gz/mkstd-0.0.7/mkstd/types/__init__.py
@@ -0,0 +1,2 @@
+"""Support for additional data types."""
+from .array import get_array_type
