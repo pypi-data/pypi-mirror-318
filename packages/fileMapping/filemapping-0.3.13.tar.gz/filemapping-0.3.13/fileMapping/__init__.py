20607685f49fcda4dfb6f99c5eff4ad51f729d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:32 +0000

    Add package fileMapping/0.3.13/filemapping-0.3.13.tar.gz

---
+++ packages/fileMapping/filemapping-0.3.13.tar.gz/filemapping-0.3.13/fileMapping/__init__.py
@@ -0,0 +1,8 @@
+from fileMapping import pluginLoading
+from fileMapping.plugIns import File, pathConversion, configConvertTodict
+from fileMapping import config as fileMappingConfig
+
+# File: plugIns.File = plugIns.File
+# pathConversion: plugIns.pathConversion = plugIns.pathConversion
+
+
