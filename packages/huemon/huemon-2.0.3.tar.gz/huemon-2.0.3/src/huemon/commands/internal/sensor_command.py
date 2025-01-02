7c71b04a04ef5fd75ef13359c9294e7356a0fdaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:09:04 +0000

    Add package huemon/2.0.3/huemon-2.0.3.tar.gz

---
+++ packages/huemon/huemon-2.0.3.tar.gz/huemon-2.0.3/src/huemon/commands/internal/sensor_command.py
@@ -0,0 +1,53 @@
+# Copyright (c) Ely Deckers.
+#
+# This source code is licensed under the MPL-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+from huemon.api.api_interface import ApiInterface
+from huemon.commands.hue_command_interface import HueCommand
+from huemon.infrastructure.logger_factory import create_logger
+from huemon.utils.assertions import assert_exists, assert_num_args
+
+LOG = create_logger()
+
+
+class SensorCommand(HueCommand):
+    def __init__(
+        self, config: dict, api: ApiInterface
+    ):  # pylint: disable=unused-argument
+        self.api = api
+
+    def __get_sensor(self, device_id):
+        return HueCommand.get_by_unique_id(device_id, self.api.get_sensors())
+
+    def __map_sensor(self, unique_id, mapper):
+        return mapper(self.__get_sensor(unique_id))
+
+    __SENSOR_ACTION_MAP = {
+        "battery:level": HueCommand._mapper("config.battery", float),
+        "light:level": HueCommand._mapper("state.lightlevel", float),
+        "presence": HueCommand._mapper("state.presence", int),
+        "reachable": HueCommand._mapper("config.reachable", int),
+        "temperature": lambda device: float(device["state"]["temperature"] / 100),
+    }
+
+    @staticmethod
+    def name():
+        return "sensor"
+
+    def exec(self, arguments):
+        LOG.debug(
+            "Running `%s` command (arguments=%s)", SensorCommand.name(), arguments
+        )
+        assert_num_args(2, arguments, SensorCommand.name())
+
+        device_id, action = arguments
+
+        assert_exists(list(SensorCommand.__SENSOR_ACTION_MAP), action)
+
+        HueCommand._process(
+            self.__map_sensor(device_id, SensorCommand.__SENSOR_ACTION_MAP[action])
+        )
+        LOG.debug(
+            "Finished `%s` command (arguments=%s)", SensorCommand.name(), arguments
+        )
---
+++ packages/huemon/huemon-2.0.3.tar.gz/huemon-2.0.3/src/huemon/infrastructure/config_factory.py
@@ -0,0 +1,45 @@
+# Copyright (c) Ely Deckers.
+#
+# This source code is licensed under the MPL-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+from os import environ, path
+from pathlib import Path
+
+import yaml
+from genericpath import isfile
+
+from huemon.utils.errors import exit_fail
+
+CONFIG_PATH_LOCAL = path.join(str(Path(__file__).parent.parent), "config.yml")
+CONFIG_PATH_ENV_VARIABLE = environ.get("HUEMON_CONFIG_PATH")
+
+CONFIG_PATHS_ORDERED_PREFERENCE = list(
+    filter(
+        None,
+        [
+            CONFIG_PATH_ENV_VARIABLE,
+            CONFIG_PATH_LOCAL,
+        ],
+    )
+)
+
+
+def __first_existing_config_file():
+    for config_path in CONFIG_PATHS_ORDERED_PREFERENCE:
+        if isfile(config_path):
+            return config_path
+
+    return None
+
+
+def create_config():
+    maybe_config_path = __first_existing_config_file()
+    if not maybe_config_path:
+        exit_fail(
+            "No configuration file found in %s",
+            ",".join(CONFIG_PATHS_ORDERED_PREFERENCE),
+        )
+
+    with open(maybe_config_path, "r") as file:
+        return yaml.safe_load(file.read())
