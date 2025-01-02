70ceb0ea3bbf5e0cc992df96f6d72b81d4a9a7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:06:10 +0000

    Add package blue-codespace/4.200.1/blue_codespace-4.200.1.tar.gz

---
+++ packages/blue-codespace/blue_codespace-4.200.1.tar.gz/blue_codespace-4.200.1/blue_codespace/__init__.py
@@ -0,0 +1,19 @@
+NAME = "blue_codespace"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} a github codespaces terraform."
+
+VERSION = "4.200.1"
+
+REPO_NAME = "blue-codespace"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-codespace/marquee.png?raw=true"
+)
+
+ALIAS = "@codespace"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
