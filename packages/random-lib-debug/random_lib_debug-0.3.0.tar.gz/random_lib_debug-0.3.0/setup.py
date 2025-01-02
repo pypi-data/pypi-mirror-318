e7c0f883b111bac482304b4c893268b67792d762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:55 +0000

    Add package random-lib-debug/0.3.0/random_lib_debug-0.3.0.tar.gz

---
+++ packages/random-lib-debug/random_lib_debug-0.3.0.tar.gz/random_lib_debug-0.3.0/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='random_lib_debug',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A library to generate random integers',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # ... other classifiers ...
+    ],
+)
