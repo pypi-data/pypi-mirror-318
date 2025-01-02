3b7e262e695e1375d21258a961df83c5a36544dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:00 +0000

    Add package abcli/9.440.1/abcli-9.440.1.tar.gz

---
+++ packages/abcli/abcli-9.440.1.tar.gz/abcli-9.440.1/abcli/.abcli/plugins/ssm.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+function abcli_ssm() {
+    local task=$1
+
+    if [[ "$task" == "help" ]]; then
+        abcli_ssm_get "$@"
+        abcli_ssm_put "$@"
+        abcli_ssm_rm "$@"
+        return
+    fi
+
+    local function_name=abcli_ssm_$task
+    if [[ $(type -t $function_name) == "function" ]]; then
+        $function_name "${@:2}"
+        return
+    fi
+
+    python3 -m abcli.plugins.ssm "$@"
+}
+
+abcli_source_caller_suffix_path /ssm
+
+# to get around a pip import issue.
+abcli_env_dot_load \
+    caller,ssm,plugin=abcli,suffix=/../../..
