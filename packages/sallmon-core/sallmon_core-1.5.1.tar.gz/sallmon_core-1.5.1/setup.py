1af1e16a2904b72b290e0ae09e7182ce4a30ae51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:06:39 +0000

    Add package sallmon-core/1.5.1/sallmon_core-1.5.1.tar.gz

---
+++ packages/sallmon-core/sallmon_core-1.5.1.tar.gz/sallmon_core-1.5.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# Read the README file for a detailed description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sallmon-core",
+    version="1.5.1",  # Increment version
+    description="Sallmon Blockchain FastAPI Server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Andrew Polykandriotis",
+    author_email="andrew@minakilabs.com",
+    url="https://github.com/minakilabs/sallmon-sdk",
+    packages=find_packages(include=["sallmon_core", "sallmon_core.*"]),
+    include_package_data=True,
+    install_requires=[
+        "fastapi>=0.101.0",
+        "uvicorn>=0.27.1",
+        "websockets>=10.4",
+        "requests>=2.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sallmon-server=sallmon_core.server:start",
+
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
