c2d92d746891fb14e8746be0afe34115403eff99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:07:19 +0000

    Add package plothic/0.3.3/plothic-0.3.3.tar.gz

---
+++ packages/plothic/plothic-0.3.3.tar.gz/plothic-0.3.3/src/plothic/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+@Author: Zijie Jiang
+@Contact: jzjlab@163.com
+@File: version.py
+@Time: 2025/1/1 18:54
+@Function: Version of PlotHiC
+"""
+
+__version__ = "0.3.3"
