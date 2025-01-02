23d3cee2af9a8393a71236dcebc2d1dd18a24c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:50 +0000

    Add package lvmgort/1.4.0/lvmgort-1.4.0.tar.gz

---
+++ packages/lvmgort/lvmgort-1.4.0.tar.gz/lvmgort-1.4.0/src/gort/overwatcher/helpers/dome.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2024-10-27
+# @Filename: dome.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import asyncio
+import enum
+
+from typing import TYPE_CHECKING
+
+from lvmopstools.retrier import Retrier
+
+from sdsstools.utils import GatheringTaskGroup
+
+from gort.exceptions import GortError
+
+
+if TYPE_CHECKING:
+    from gort.overwatcher.overwatcher import Overwatcher
+
+
+__all__ = ["DomeHelper", "DomeStatus"]
+
+
+class DomeStatus(enum.Flag):
+    """An enumeration of dome statuses."""
+
+    OPEN = enum.auto()
+    CLOSED = enum.auto()
+    OPENING = enum.auto()
+    CLOSING = enum.auto()
+    MOVING = enum.auto()
+    UNKNOWN = enum.auto()
+
+
+class DomeHelper:
+    """Handle dome movement."""
+
+    def __init__(self, overwatcher: Overwatcher):
+        self.overwatcher = overwatcher
+        self.gort = overwatcher.gort
+        self.log = overwatcher.log
+
+        self._action_lock = asyncio.Lock()
+        self._move_lock = asyncio.Lock()
+
+    @Retrier(max_attempts=3, delay=1)
+    async def status(self):
+        """Returns the status of the dome."""
+
+        status = await self.gort.enclosure.status()
+        labels = status["dome_status_labels"]
+
+        if "MOTOR_OPENING" in labels:
+            return DomeStatus.OPENING | DomeStatus.MOVING
+        elif "MOTOR_CLOSING" in labels:
+            return DomeStatus.CLOSING | DomeStatus.MOVING
+        elif "OPEN" in labels:
+            if "MOVING" in labels:
+                return DomeStatus.OPENING | DomeStatus.MOVING
+            return DomeStatus.OPEN
+        elif "CLOSED" in labels:
+            if "MOVING" in labels:
+                return DomeStatus.CLOSING | DomeStatus.MOVING
+            return DomeStatus.CLOSED
+
+        return DomeStatus.UNKNOWN
+
+    async def is_opening(self):
+        """Returns True if the dome is open or opening."""
+
+        status = await self.status()
+
+        if status & DomeStatus.OPENING:
+            return True
+        if status & DomeStatus.OPEN:
+            return True
+
+        return False
+
+    async def is_closing(self):
+        """Returns True if the dome is closed or closed."""
+
+        status = await self.status()
+
+        if status & DomeStatus.CLOSING:
+            return True
+        if status & DomeStatus.CLOSED:
+            return True
+
+        return False
+
+    async def is_moving(self):
+        """Returns True if the dome is moving."""
+
+        status = await self.status()
+
+        if status & DomeStatus.MOVING:
+            return True
+
+        return False
+
+    async def wait_until_idle(self):
+        """Waits until the dome is idle."""
+
+        while await self.is_moving():
+            await asyncio.sleep(5)
+
+    @Retrier(max_attempts=2, delay=5)
+    async def _move_with_retries(
+        self,
+        open: bool = False,
+        park: bool = True,
+        retry_without_parking: bool = False,
+    ):
+        """Moves the dome with retries."""
+
+        if open:
+            await self.gort.enclosure.open(park_telescopes=park)
+        else:
+            await self.gort.enclosure.close(
+                park_telescopes=park,
+                retry_without_parking=retry_without_parking,
+                force=True,
+            )
+
+    async def _move(
+        self,
+        status: DomeStatus,
+        open: bool = False,
+        park: bool = True,
+        retry_without_parking: bool = False,
+    ):
+        """Moves the dome."""
+
+        if open and not self.overwatcher.state.safe:
+            raise GortError("Cannot open the dome when conditions are unsafe.")
+
+        is_local = await self.gort.enclosure.is_local()
+        if is_local:
+            raise GortError("Cannot move the dome in local mode.")
+
+        failed: bool = False
+
+        try:
+            if status & DomeStatus.MOVING:
+                self.log.warning("Dome is already moving. Stopping.")
+                await self.stop()
+
+            await self._move_with_retries(
+                open=open,
+                park=park,
+                retry_without_parking=retry_without_parking,
+            )
+
+        except Exception:
+            await asyncio.sleep(3)
+
+            status = await self.status()
+
+            if status & DomeStatus.MOVING:
+                self.log.warning("Dome is still moving after an error. Stopping.")
+                await self.stop()
+
+            # Sometimes the open/close could fail but actually the dome is open/closed.
+            if open and not (status & DomeStatus.OPEN):
+                failed = True
+            elif not open and not (status & DomeStatus.CLOSED):
+                failed = True
+
+            if failed:
+                await self.overwatcher.notify(
+                    "The dome has failed to open/close. Disabling the Overwatcher "
+                    "to prevent further attempts. Please check the dome immediately, "
+                    "it may be partially or fully open.",
+                    level="critical",
+                )
+
+                # Release the lock here. force_disable() may require closing the dome.
+                if self._move_lock.locked():
+                    self._move_lock.release()
+
+                await self.overwatcher.force_disable()
+                raise
+
+    async def open(self, park: bool = True):
+        """Opens the dome."""
+
+        async with self._move_lock:
+            status = await self.status()
+            if status == DomeStatus.OPEN:
+                self.log.debug("Dome is already open.")
+                return
+
+            if status == DomeStatus.OPENING:
+                self.log.debug("Dome is already opening.")
+                return
+
+            if status == DomeStatus.UNKNOWN:
+                self.log.warning("Dome is in an unknown status. Stopping and opening.")
+                await self.stop()
+                status = await self.status()
+
+            await self._move(status, open=True, park=park)
+
+    async def close(self, park: bool = True, retry: bool = False):
+        """Closes the dome."""
+
+        async with self._move_lock:
+            status = await self.status()
+            if status == DomeStatus.CLOSED:
+                self.log.debug("Dome is already closed.")
+                return
+
+            if status == DomeStatus.CLOSING:
+                self.log.debug("Dome is already closing.")
+                return
+
+            if status == DomeStatus.UNKNOWN:
+                self.log.warning("Dome is in an unknown status. Stopping and closing.")
+                await self.stop()
+                status = await self.status()
+
+            await self._move(
+                status,
+                open=False,
+                park=park,
+                retry_without_parking=retry,
+            )
+
+    async def stop(self):
+        """Stops the dome."""
+
+        await self.gort.enclosure.stop()
+
+        await asyncio.sleep(1)
+        status = await self.status()
+
+        if status & DomeStatus.MOVING:
+            raise GortError("Dome is still moving after a stop command.")
+
+    async def startup(self):
+        """Runs the startup sequence."""
+
+        async with self._action_lock:
+            self.log.info("Starting the dome startup sequence.")
+            await self.gort.startup(open_enclosure=False, focus=False)
+
+            # Now we manually open. We do not focus here since that's handled
+            # by the observer module.
+            await self.open()
+
+    async def shutdown(
+        self,
+        retry: bool = False,
+        force: bool = False,
+        park: bool = True,
+    ):
+        """Runs the shutdown sequence."""
+
+        is_closing = await self.is_closing()
+        if is_closing and not force:
+            return
+
+        async with self._action_lock:
+            self.log.info("Running the shutdown sequence.")
+
+            async with GatheringTaskGroup() as group:
+                self.log.info("Turning off all lamps.")
+                group.create_task(self.gort.nps.calib.all_off())
+
+                self.log.info("Making sure guiders are idle.")
+                group.create_task(self.gort.guiders.stop())
+
+                self.log.info("Closing the dome.")
+                group.create_task(self.close(retry=retry))
+
+            self.log.info("Parking telescopes for the night.")
+            if park:
+                await self.gort.telescopes.park()
---
+++ packages/lvmgort/lvmgort-1.4.0.tar.gz/lvmgort-1.4.0/src/gort/overwatcher/overwatcher.py
@@ -0,0 +1,444 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2024-03-26
+# @Filename: overwatcher.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import asyncio
+import dataclasses
+import pathlib
+from copy import copy
+from time import time
+
+from typing import TYPE_CHECKING, cast
+
+from sdsstools import Configuration
+from sdsstools.utils import GatheringTaskGroup
+
+from gort.exceptions import GortError
+from gort.overwatcher.alerts import ActiveAlert
+from gort.overwatcher.core import OverwatcherBaseTask, OverwatcherModule
+from gort.overwatcher.helpers import DomeHelper
+from gort.overwatcher.helpers.notifier import NotifierMixIn
+from gort.overwatcher.helpers.tasks import DailyTasks
+from gort.overwatcher.troubleshooter.troubleshooter import Troubleshooter
+from gort.tools import LogNamespace, decap
+
+
+if TYPE_CHECKING:
+    from gort.gort import Gort
+    from gort.overwatcher.helpers.notifier import NotificationLevel
+
+
+@dataclasses.dataclass
+class OverwatcherState:
+    """Dataclass with the overwatcher state values."""
+
+    running: bool = False
+    enabled: bool = False
+    observing: bool = False
+    calibrating: bool = False
+    troubleshooting: bool = False
+    focusing: bool = False
+    night: bool = False
+    safe: bool = False
+    allow_calibrations: bool = True
+    dry_run: bool = False
+
+
+class OverwatcherTask(OverwatcherBaseTask):
+    """Overwatcher task that is aware of the overwatcher instance."""
+
+    def __init__(self, overwatcher: Overwatcher):
+        super().__init__()
+
+        self.overwatcher = overwatcher
+
+        # A bit configuring but _log is used internally, mainly for
+        # OverwatcherBaseTask.run() and log is for external use.
+        self._log = self.overwatcher.log
+        self.log = self._log
+
+
+class OverwatcherMainTask(OverwatcherTask):
+    """The main overwatcher task."""
+
+    name = "overwatcher_task"
+    keep_alive = True
+    restart_on_error = True
+
+    def __init__(self, overwatcher: Overwatcher):
+        super().__init__(overwatcher)
+
+        self.previous_state = OverwatcherState()
+        self._pending_close_dome: bool = False
+
+    async def task(self):
+        """Main overwatcher task."""
+
+        await asyncio.sleep(1)
+
+        ow = self.overwatcher
+
+        while True:
+            self.previous_state = copy(ow.state)
+
+            try:
+                is_safe, _ = ow.alerts.is_safe()
+                is_night = ow.ephemeris.is_night()
+                is_troubleshooting = ow.troubleshooter.is_troubleshooting()
+
+                ow.state.night = is_night
+                ow.state.safe = is_safe
+                ow.state.observing = ow.observer.is_observing
+                ow.state.focusing = ow.observer.focusing
+                ow.state.troubleshooting = (
+                    ow.state.troubleshooting or is_troubleshooting
+                )
+
+                running_calibration = ow.calibrations.get_running_calibration()
+                ow.state.calibrating = running_calibration is not None
+
+                # TODO: should these handlers be scheduled as tasks? Right now
+                # they can block for a good while until the dome is open/closed.
+
+                if not is_safe:
+                    await self.handle_unsafe()
+
+                if not is_night:
+                    await self.handle_daytime()
+
+                if not ow.state.enabled:
+                    await self.handle_disabled()
+
+                # Run daily tasks.
+                await ow.daily_tasks.run_all()
+
+            except Exception as err:
+                await ow.notify(
+                    f"Error in main overwatcher task: {decap(err)}",
+                    level="error",
+                )
+
+                # Avoid rapid fire errors. Sleep a bit longer before trying again.
+                await asyncio.sleep(30)
+
+            await asyncio.sleep(5)
+
+    async def handle_unsafe(self):
+        """Closes the dome if the conditions are unsafe."""
+
+        ow = self.overwatcher
+
+        closed = await ow.dome.is_closing()
+
+        observing = ow.observer.is_observing
+        cancelling = ow.observer.is_cancelling
+        calibrating = ow.state.calibrating
+
+        _, alerts_status = ow.alerts.is_safe()
+        is_raining = bool(alerts_status & ActiveAlert.RAIN)
+
+        if not closed or observing or calibrating:
+            try:
+                async with asyncio.timeout(delay=30):
+                    await ow.notify("Unsafe conditions detected.", level="warning")
+
+                    if observing and not cancelling:
+                        try:
+                            await ow.observer.stop_observing(
+                                immediate=True,
+                                reason="unsafe conditions",
+                            )
+                        except Exception as err:
+                            await ow.notify(
+                                f"Error stopping observing: {decap(err)}",
+                                level="error",
+                            )
+                            await ow.notify(
+                                "I will close the dome anyway.",
+                                level="warning",
+                            )
+
+                    if calibrating:
+                        await ow.calibrations.cancel()
+
+            except asyncio.TimeoutError:
+                await ow.notify(
+                    "Timed out while handling unsafe conditions.",
+                    level="error",
+                )
+
+            except Exception as err:
+                await ow.notify(
+                    f"Error handling unsafe conditions: {decap(err)}",
+                    level="error",
+                )
+
+            finally:
+                if not closed:
+                    await ow.notify("Closing the dome due to unsafe conditions.")
+                    await ow.dome.shutdown(retry=True, park=True)
+
+                    # If we have to close because of unsafe conditions, we don't want
+                    # to reopen too soon. We lock the dome for some time.
+                    timeout = ow.config["overwatcher.lock_timeout_on_unsafe"]
+                    ow.alerts.locked_until = time() + timeout
+
+                    await ow.notify(
+                        f"The dome will be locked for {int(timeout)} seconds.",
+                        level="warning",
+                    )
+
+        if ow.state.enabled:
+            if is_raining:
+                await ow.notify(
+                    "Disabling the Overwatcher due to rain. "
+                    "Manually re-enable it when safe.",
+                    level="warning",
+                )
+                ow.state.enabled = False
+
+    async def handle_daytime(self):
+        """Handles daytime."""
+
+        # Don't do anything if we are calibrating. If a calibration script opened the
+        # dome it should close it afterwards.
+        if self.overwatcher.state.calibrating:
+            return
+
+        # Also don't do anything if the overwatcher is not enabled.
+        # TODO: maybe we should close the dome if it's open?
+        if not self.overwatcher.state.enabled:
+            return
+
+        observing = self.overwatcher.observer.is_observing
+        cancelling = self.overwatcher.observer.is_cancelling
+
+        if observing and not cancelling:
+            # Decide whether to complete the current exposure or stop immediately.
+            exposure_finishes_at = self.overwatcher.observer.next_exposure_completes
+            now = time()
+
+            if exposure_finishes_at > 0 and (exposure_finishes_at - now) < 300:
+                immediate = False
+                notification_message = "Finishing this exposure and closing the dome."
+
+            else:
+                immediate = True
+                notification_message = "Cancelling exposure and closing the dome."
+
+            await self.overwatcher.notify("Twilight reached. " + notification_message)
+
+            await self.overwatcher.observer.stop_observing(
+                immediate=immediate,
+                reason="daytime conditions",
+            )
+            self._pending_close_dome = True
+
+        if not observing and not cancelling and self._pending_close_dome:
+            try:
+                closed = await self.overwatcher.dome.is_closing()
+                if not closed:
+                    await self.overwatcher.dome.shutdown(retry=True, park=True)
+            finally:
+                self._pending_close_dome = False
+
+    async def handle_disabled(self):
+        """Handles the disabled state."""
+
+        observing = self.overwatcher.observer.is_observing
+        cancelling = self.overwatcher.observer.is_cancelling
+
+        if observing and not cancelling:
+            # Disable after this tile.
+            await self.overwatcher.observer.stop_observing(
+                immediate=False,
+                reason="overwatcher was disabled",
+            )
+
+
+class OverwatcherPingTask(OverwatcherTask):
+    """Emits a ping notification every five minutes."""
+
+    name = "overwatcher_ping"
+    keep_alive = True
+    restart_on_error = True
+
+    delay: float = 900
+
+    async def task(self):
+        """Ping task."""
+
+        while True:
+            await asyncio.sleep(self.delay)
+            self.log.debug("I am alive!")
+
+
+class Overwatcher(NotifierMixIn):
+    """Monitors the observatory."""
+
+    instance: Overwatcher | None = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.instance:
+            cls.instance = super(Overwatcher, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(
+        self,
+        gort: Gort | None = None,
+        verbosity: str = "warning",
+        calibrations_file: str | pathlib.Path | None = None,
+        dry_run: bool = False,
+        **kwargs,
+    ):
+        from gort import Gort  # Needs to be imported here to avoid circular imports.
+        from gort.overwatcher import (
+            AlertsOverwatcher,
+            CalibrationsOverwatcher,
+            EphemerisOverwatcher,
+            EventsOverwatcher,
+            HealthOverwatcher,
+            ObserverOverwatcher,
+            SafetyOverwatcher,
+            TransparencyOverwatcher,
+        )
+
+        # Check if the instance already exists, in which case do nothing.
+        if hasattr(self, "gort"):
+            return
+
+        self.gort = gort or Gort(verbosity=verbosity, **kwargs)
+        self.config = cast(Configuration, self.gort.config)
+        self.log = LogNamespace(self.gort.log, header=f"({self.__class__.__name__}) ")
+
+        self.state = OverwatcherState()
+        self.state.dry_run = dry_run
+
+        self.dome = DomeHelper(self)
+        self.troubleshooter = Troubleshooter(self)
+        self.tasks: list[OverwatcherTask] = [
+            OverwatcherMainTask(self),
+            OverwatcherPingTask(self),
+        ]
+
+        # A series of tasks that must be run once every day.
+        self.daily_tasks = DailyTasks(self)
+
+        self.safety = SafetyOverwatcher(self)
+        self.ephemeris = EphemerisOverwatcher(self)
+        self.calibrations = CalibrationsOverwatcher(self, calibrations_file)
+        self.observer = ObserverOverwatcher(self)
+        self.alerts = AlertsOverwatcher(self)
+        self.transparency = TransparencyOverwatcher(self)
+        self.events = EventsOverwatcher(self)
+        self.health = HealthOverwatcher(self)
+
+    async def run(self):
+        """Starts the overwatcher tasks."""
+
+        if self.state.running:
+            raise GortError("Overwatcher is already running.")
+
+        if not self.gort.is_connected():
+            await self.gort.init()
+
+        async with GatheringTaskGroup() as group:
+            for module in OverwatcherModule.instances:
+                self.log.info(f"Starting overwatcher module {module.name!r}")
+                group.create_task(module.run())
+
+        async with GatheringTaskGroup() as group:
+            for task in self.tasks:
+                group.create_task(task.run())
+
+        self.state.running = True
+        await self.notify(
+            "Overwatcher is now running.",
+            payload={"dry-run": self.state.dry_run},
+        )
+
+        if self.state.dry_run:
+            self.log.warning("Overatcher is running in dry-mode.")
+
+        return self
+
+    async def shutdown(
+        self,
+        reason: str = "undefined",
+        level: NotificationLevel = "warning",
+        retry: bool = True,
+        park: bool = True,
+        disable_overwatcher: bool = False,
+    ):
+        """Shuts down the observatory."""
+
+        dome_closed = await self.dome.is_closing()
+        enabled = self.state.enabled
+        observing = self.observer.is_observing
+
+        if dome_closed and not enabled and not observing:
+            return
+
+        if not reason.endswith("."):
+            reason += "."
+
+        await self.notify(f"Triggering shutdown. Reason: {decap(reason)}", level=level)
+
+        if disable_overwatcher:
+            await self.notify("The Overwatcher will be disabled.", level="warning")
+
+        if not self.state.dry_run:
+            stop = asyncio.create_task(
+                self.observer.stop_observing(
+                    immediate=True,
+                    reason=reason,
+                )
+            )
+            shutdown = asyncio.create_task(
+                self.dome.shutdown(
+                    retry=retry,
+                    park=park,
+                )
+            )
+        else:
+            self.log.warning("Dry run enabled. Not shutting down.")
+            return
+
+        try:
+            await asyncio.gather(stop, shutdown)
+        except Exception as err:
+            await self.notify(
+                f"Error during shutdown: {decap(err)}",
+                level="critical",
+                error=err,
+            )
+
+        if disable_overwatcher:
+            self.state.enabled = False
+
+    async def force_disable(self):
+        """Disables the overwatcher."""
+
+        await self.observer.stop_observing(immediate=True)
+        await self.calibrations.cancel()
+
+        self.state.enabled = False
+        await self.notify("Overwatcher is now disabled.", level="warning")
+
+    async def cancel(self):
+        """Cancels the overwatcher tasks."""
+
+        for module in OverwatcherModule.instances:
+            self.gort.log.debug(f"Cancelling overwatcher module {module.name!r}")
+            await module.cancel()
+
+        for task in self.tasks:
+            await task.cancel()
+
+        self.state.running = False
