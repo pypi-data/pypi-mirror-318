38b806150eb720ba8bdb9bb580752cf870a8be1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:03:59 +0000

    Add package modular-intelligence/0.0.9/modular_intelligence-0.0.9-py3-none-any.whl

---
+++ packages/modular-intelligence/modular_intelligence-0.0.9-py3-none-any.whl/modular_intelligence/database/config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+class Config:
+    APP_NAME = "modular_intelligence"
+    IMAGE_PATH = Path.home() / f".{APP_NAME}"
+    DATABASE = Path.home() / f".{APP_NAME}" / f"{APP_NAME}.db"
+    DEBUG = True
+    PORT = 5000
+    ENABLE_FOREIGN_KEYS = True
+    SCHEMA_PATH = os.path.join(os.path.dirname(__file__), 'schema.sql')
+    TABLES = {
+    'BOTS': 'Bots',
+    'CHECKPOINTS': 'Checkpoints',
+    'STACKS': 'Stacks',
+    'STACKSLOTS': 'StackSlots'
+    }
+    QDRANT_COLLECTION_NAME = "assistant_memory"
+    VECTOR_SIZE = 768
+
+    # Add any additional configuration settings here
+    SQLITE_URI = f'sqlite:///{DATABASE}'
