20b869ce48ad64d13a0df4bc08c7003df0bb9e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:03 +0000

    Add package finance-core/0.1.4/finance_core-0.1.4-cp38-cp38-win_amd64.whl

---
+++ packages/finance-core/finance_core-0.1.4-cp38-cp38-win_amd64.whl/finance_core/__init__.py
@@ -0,0 +1,15 @@
+from ._finance_core import (
+    ExponentialMovingAverage,
+    Maximum,
+    Minimum,
+    MovingAverageConvergenceDivergence,
+    SimpleMovingAverage,
+)
+
+__all__ = [
+    "ExponentialMovingAverage",
+    "Maximum",
+    "Minimum",
+    "MovingAverageConvergenceDivergence",
+    "SimpleMovingAverage",
+]
