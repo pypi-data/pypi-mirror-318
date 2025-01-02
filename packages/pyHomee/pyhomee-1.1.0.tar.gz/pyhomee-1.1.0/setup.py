f0963c02014c1d718ff50e73dbe49eb36f598c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:05:14 +0000

    Add package pyHomee/1.1.0/pyhomee-1.1.0.tar.gz

---
+++ packages/pyHomee/pyhomee-1.1.0.tar.gz/pyhomee-1.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+__version__ = "1.1.0"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyHomee",
+    version=__version__,
+    author="Taraman17",
+    description="a python library to interact with homee",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Taraman17/pyHomee",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
