5d15562fab165d7745bfb3b0e7ece4fb3b4eece7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:44 +0000

    Add package pybehave-selenium-test-framework/1.0.4/pybehave_selenium_test_framework-1.0.4.tar.gz

---
+++ packages/pybehave-selenium-test-framework/pybehave_selenium_test_framework-1.0.4.tar.gz/pybehave_selenium_test_framework-1.0.4/app/features/steps/all_steps.py
@@ -0,0 +1,3 @@
+from features.steps.example import login_steps  # noqa: F401
+from features.steps.example import database_steps  # noqa: F401
+from features.steps.example import api_steps  # noqa: F401
---
+++ packages/pybehave-selenium-test-framework/pybehave_selenium_test_framework-1.0.4.tar.gz/pybehave_selenium_test_framework-1.0.4/app/support/locators/general.py
@@ -0,0 +1,9 @@
+"""Provides general locators enum"""
+
+from support.locators.locator import Locator
+
+
+class GeneralLocator(Locator):
+    """
+    General locators.
+    """
