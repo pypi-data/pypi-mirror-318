71da6e827c22871a0ed5ed5a0441b3dbca73e491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:08:12 +0000

    Add package evrim/1.8.0/evrim-1.8.0-py3-none-any.whl

---
+++ packages/evrim/evrim-1.8.0-py3-none-any.whl/evrim/resources/prod/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .bulk import (
+    BulkResource,
+    AsyncBulkResource,
+    BulkResourceWithRawResponse,
+    AsyncBulkResourceWithRawResponse,
+    BulkResourceWithStreamingResponse,
+    AsyncBulkResourceWithStreamingResponse,
+)
+from .prod import (
+    ProdResource,
+    AsyncProdResource,
+    ProdResourceWithRawResponse,
+    AsyncProdResourceWithRawResponse,
+    ProdResourceWithStreamingResponse,
+    AsyncProdResourceWithStreamingResponse,
+)
+from .blank import (
+    BlankResource,
+    AsyncBlankResource,
+    BlankResourceWithRawResponse,
+    AsyncBlankResourceWithRawResponse,
+    BlankResourceWithStreamingResponse,
+    AsyncBlankResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "BlankResource",
+    "AsyncBlankResource",
+    "BlankResourceWithRawResponse",
+    "AsyncBlankResourceWithRawResponse",
+    "BlankResourceWithStreamingResponse",
+    "AsyncBlankResourceWithStreamingResponse",
+    "BulkResource",
+    "AsyncBulkResource",
+    "BulkResourceWithRawResponse",
+    "AsyncBulkResourceWithRawResponse",
+    "BulkResourceWithStreamingResponse",
+    "AsyncBulkResourceWithStreamingResponse",
+    "ProdResource",
+    "AsyncProdResource",
+    "ProdResourceWithRawResponse",
+    "AsyncProdResourceWithRawResponse",
+    "ProdResourceWithStreamingResponse",
+    "AsyncProdResourceWithStreamingResponse",
+]
