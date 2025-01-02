99810b007a29003515fc7e068f7aa55168d18956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:15 +0000

    Add package airthings-ble/1.0.0b1/airthings_ble-1.0.0b1-py3-none-any.whl

---
+++ packages/airthings-ble/airthings_ble-1.0.0b1-py3-none-any.whl/airthings_ble/__init__.py
@@ -0,0 +1,10 @@
+"""Parser for Airthings BLE advertisements."""
+
+from __future__ import annotations
+
+from .device_type import AirthingsDeviceType
+from .parser import AirthingsBluetoothDeviceData, AirthingsDevice
+
+__version__ = "1.0.0b1"
+
+__all__ = ["AirthingsBluetoothDeviceData", "AirthingsDevice", "AirthingsDeviceType"]
