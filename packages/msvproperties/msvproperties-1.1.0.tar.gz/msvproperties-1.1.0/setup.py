47ff6f5d6b2a9c4e4124da374902c4f926e68539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:09:12 +0000

    Add package msvproperties/1.1.0/msvproperties-1.1.0.tar.gz

---
+++ packages/msvproperties/msvproperties-1.1.0.tar.gz/msvproperties-1.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msvproperties',
+    version='1.1.0',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'asgiref==3.8.1',
+        'certifi==2024.12.14',
+        'charset-normalizer==3.4.0',
+        'idna==3.10',
+        'numpy==2.0.0',
+        'pandas==2.2.3',
+        'probableparsing==0.0.1',
+        'python-crfsuite==0.9.11',
+        'python-dateutil==2.9.0.post0',
+        'python-dotenv==1.0.1',
+        'pytz==2024.2',
+        'requests==2.32.3',
+        'six==1.17.0',
+        'sqlparse==0.5.3',
+        'typing_extensions==4.12.2',
+        'tzdata==2024.2',
+        'urllib3==2.2.3',
+        'usaddress==0.5.11',
+    ],
+    python_requires='==3.9.6',
+    description='A Library for using in our CRM',
+    author='Alireza',
+    author_email='alireza@msvproperties.net',
+    url='https://github.com/alireza-msvproperties/msvproperties/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
