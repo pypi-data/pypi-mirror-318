f6bac9d799ce13b3de4448904c6cb1eb0c4f32cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:36 +0000

    Add package app-page/0.0.14/app-page-0.0.14.tar.gz

---
+++ packages/app-page/app-page-0.0.14.tar.gz/app-page-0.0.14/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+package_name = "app-page"
+version = '0.0.14'
+long_description = open("README.md", encoding="utf-8").read()
+
+setuptools.setup(
+    name=package_name,
+    version=version,
+    author="xiaohuicat",  # 作者名称
+    author_email="1258702350@qq.com", # 作者邮箱
+    description="python page application framework", # 库描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xiaohuicat/python-app-page", # 库的官方地址
+    license="MIT",
+    packages=setuptools.find_packages(),
+    package_data={'app_page': ['*', 'assets/*', 'assets/*/*']},
+    install_requires=[
+        "app-page-core",
+        "nanoid",
+        "PySide6",
+        "pywin32",
+        "numpy",
+        "openpyxl",
+        "paho-mqtt==1.6.1",
+        "pillow"
+    ],
+    zip_safe=False,
+)
