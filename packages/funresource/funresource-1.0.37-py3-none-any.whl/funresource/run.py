e518e8a1c195aa32213f23556fc98e00b83f7d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:05 +0000

    Add package funresource/1.0.37/funresource-1.0.37-py3-none-any.whl

---
+++ packages/funresource/funresource-1.0.37-py3-none-any.whl/funresource/run.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+from __future__ import annotations
+
+import argparse
+from typing import List
+
+from funresource.db.base import ResourceManage
+from funresource.generator import AcoooderGenerate, RSSGenerate, TelegramChannelGenerate
+from funresource.generator.base import BaseGenerate
+from funutil import getLogger
+
+logger = getLogger("funresource")
+
+
+def run():
+    manage = ResourceManage()
+    generator_list: List[BaseGenerate] = [
+        AcoooderGenerate(),
+        RSSGenerate(),
+        TelegramChannelGenerate(),
+    ]
+
+    for generator in generator_list:
+        try:
+            generator.run(manage)
+        except Exception as e:
+            print(e)
+
+
+def funresource():
+    parser = argparse.ArgumentParser(prog="PROG")
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # 添加子命令
+    build_parser = subparsers.add_parser("build", help="build package")
+    build_parser.set_defaults(func=run)  # 设置默认函数
+
+    args = parser.parse_args()
+    args.func(args)
