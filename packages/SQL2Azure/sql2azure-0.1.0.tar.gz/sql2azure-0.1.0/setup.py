ee157e66b34c40230c125e11d5ff4303c1f5a9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:09:24 +0000

    Add package SQL2Azure/0.1.0/sql2azure-0.1.0.tar.gz

---
+++ packages/SQL2Azure/sql2azure-0.1.0.tar.gz/sql2azure-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SQL2Azure',
+    version='0.1.0',
+    description='Load data from an on-prem SQL database to Azure Storage as JSON or CSV.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/mr-speedster/SQL2Azure',
+    author='Ajith D',
+    author_email='ajithd78564@gmail.com',
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'pyodbc',
+        'azure-storage-blob',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
