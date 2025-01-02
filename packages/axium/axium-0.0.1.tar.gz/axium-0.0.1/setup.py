6af3c9fe1445d6dae066b27b1520fc89147745b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:08:55 +0000

    Add package axium/0.0.1/axium-0.0.1.tar.gz

---
+++ packages/axium/axium-0.0.1.tar.gz/axium-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from setuptools import find_packages, setup
+
+setup(
+    name="axium",
+    version="0.0.1",
+    description="This is a placeholder package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="CuB3y0nd",
+    author_email="root@cubeyond.net",
+    url="https://github.com/CuB3y0nd/axium",
+    packages=find_packages(),
+)
