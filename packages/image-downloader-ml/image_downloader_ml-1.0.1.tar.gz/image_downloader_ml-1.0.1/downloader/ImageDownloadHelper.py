306ba8ef1609791dace761d93c89ad9480930eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:33 +0000

    Add package image-downloader-ml/1.0.1/image_downloader_ml-1.0.1.tar.gz

---
+++ packages/image-downloader-ml/image_downloader_ml-1.0.1.tar.gz/image_downloader_ml-1.0.1/downloader/ImageDownloadHelper.py
@@ -0,0 +1,33 @@
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
+
+from WebDriverManager import WebDriverManager
+from engine.GoogleEngine import GoogleEngine
+
+class ImageDownloadHelper:
+    @staticmethod
+    def download_images_concurrently(results: List, manager):
+        if not results:
+            return
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(manager.download_image, result, idx + 1) for idx, result in enumerate(results)]
+
+            for future in futures:
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Error in concurrent download: {e}")
+
+    @staticmethod
+    def download(config):
+        engines = GoogleEngine()
+        real_configs = config.parse_config()
+
+        for config in real_configs:
+            manager = WebDriverManager(config)
+            driver = manager.get(engines)
+            results = engines.parse_image(manager)
+            ImageDownloadHelper.download_images_concurrently(results, manager)
+            manager.quit()
+            print("loop end")
---
+++ packages/image-downloader-ml/image_downloader_ml-1.0.1.tar.gz/image_downloader_ml-1.0.1/downloader/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+
+
+# 从内部模块导入需要对外公开的内容
+from .common.ImageParseResult import ImageParseResult
