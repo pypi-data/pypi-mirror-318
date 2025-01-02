3a0d844e18a218b4b548117a3ea74618c27b91fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:15 +0000

    Add package teju/0.0.1/teju-0.0.1.tar.gz

---
+++ packages/teju/teju-0.0.1.tar.gz/teju-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="teju",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Shane Holloman",
+    description="Python project bootstrapper using uv",
+    url="https://github.com/shaneholloman/teju",
+)
