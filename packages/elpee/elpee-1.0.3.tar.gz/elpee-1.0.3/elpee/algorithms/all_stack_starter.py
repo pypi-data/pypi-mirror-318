35c5f529cf0566473097dcb08a52b59cf33f5d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:05 +0000

    Add package elpee/1.0.3/elpee-1.0.3.tar.gz

---
+++ packages/elpee/elpee-1.0.3.tar.gz/elpee-1.0.3/elpee/algorithms/all_stack_starter.py
@@ -0,0 +1,300 @@
+# Copyright 2024-2025 Navindu De Silva
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Literal, Union
+from elpee.datahandler.data_handler import save_file
+from elpee.utils.protocols.handler import DataHandler
+from elpee.utils.feasible import FeasibleHandler
+from elpee.utils.protocols.lp_problem import LinearProblem
+from elpee.utils.protocols.st_problem import StandardProblem
+from elpee.alternator import AlternateSolver
+from elpee.algorithms.big_m import check_artificial_basic_vars
+from elpee.utils.utilities import create_ratio_col, round_off_simplex_matrix, select_pivot_col, subsitute_big_M_for_row
+from elpee.utils.printer import SimplexPrinter
+
+class AllStackStarter():
+    """
+    A class used to solve problems using All Stack Starting Method
+
+    Attributes
+    ----------
+    problem : elpee.StandardProblem
+        The problem given to the AllStackSolver to be solved
+    is_max : bool
+        Whether given LP problem is a maximization or 
+        minimization problem
+    n_decision_vars : int
+        Number of decision variables (X_i) in given LP problem
+    n_slack_vars : int
+        Number of slack variables (S_i) in given LP problem
+    n_artificials : int
+        Number of artificial variables (A_i) in LP problem
+    n_cols : int
+        Number of variables present in the simplex table of 
+        the LP problem
+    n_constraints : int
+        Number of constraints in given LP problem
+    feasible_count : int
+        Step counter to optimal solution counting number of
+        intermediate feasible solutions generated
+    simplex_printer : elpee.utils.SimplexPrinter 
+        An instance of printer to visualize the simplex table
+        of given LP problem
+    feasible_handler : elpee.utils.FeasibleHandler
+        An instance of handler object to retain feasibility of
+        LP problem and its solutions
+
+    Methods
+    -------
+    solver() -> elpee.StandardProblem
+        Solves the Standardized LP problem given to AllStackStarter instance
+    """
+
+    def __init__(self, problem: Union[StandardProblem, LinearProblem]):
+        """
+        Parameters
+        ----------
+        problem : elpee.StandardProblem
+            LP problem to be solved using All Stack Starting Method
+        """
+
+        self.problem = problem
+        self.is_max = problem.is_max
+        self.n_decision_vars = problem.n_decision_vars
+        self.n_slack_vars = problem.n_slack_vars
+        self.n_artificials = problem.n_artificials
+        self.n_cols = len(problem.obj_row)
+        self.n_constraints = problem.n_constraints
+        self.feasible_count = 0
+        self.infeasible_sol_count = 0
+
+        self.simplex_printer = SimplexPrinter()
+        self.feasible_handler = FeasibleHandler()
+        self.data_handler = None
+
+    def __is_optimal(self) -> bool:
+        """
+        Returns true if the objective row of the matrix (first row without 
+        solution value) indicates optimal solution 
+        """
+
+        # substituting for M with large number (1,000,000) before doing comparision
+
+        if self.is_max:
+            # for maximization, all coefficients of objective row should be non-negative
+            return all(element >= 0 for element in subsitute_big_M_for_row(self.problem.obj_row))
+        else:
+            # for minimization, all coefficients of objective row should be non-positive
+            return all(element <= 0 for element in subsitute_big_M_for_row(self.problem.obj_row))
+
+    def __optimize(self) -> bool:
+        """
+        Core function for optimizing the matrix by changing the basic variables
+        When matrix cannot be optimized will return None
+        """
+
+        M = 1000000
+        blocked_cols = []
+        while len(blocked_cols) < self.n_cols:
+            pivot_col_var = select_pivot_col(self.problem.obj_row, self.is_max, blocked_cols)
+            if pivot_col_var == -1:
+                # No suitable pivot column available
+                print("\nNo suitable entering varaible for selection")
+                
+                # unsuccessfully optimized
+                return False
+            ratio_col = create_ratio_col(self.problem.matrix, pivot_col_var)
+            if ratio_col == []:
+                # if the ratio column cannot be created due to a lack of constraints
+                print("\nNo constraints for optimization - the solution is unbounded")
+
+                # return cannot optimize
+                return False
+            if min(ratio_col) == M:
+                # invalid ratio column with no suitable selections to be made
+                # will block the pivot col from being selected again 
+                # and find an alternate entering variable
+                blocked_cols.append(pivot_col_var)
+            else:
+                # leaving varaible identified
+                pivot_row_var = ratio_col.index(min(ratio_col)) + 1
+                break
+        self.problem.basic_vars[pivot_row_var] = pivot_col_var
+        
+        # successfully optimized
+        return True
+    
+    def __generate_initial_feasible_sol_step(self) -> None:
+        """
+        Function to obtain the initial feasible solutions step by step
+        """
+
+        self.infeasible_sol_count += 1
+        if self.data_handler.freq == "all":
+            save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/infeasible_sol_{self.infeasible_sol_count}.{self.data_handler.file_format}")
+        self.__make_feasible()
+    
+    def __optimize_step(self) -> None:
+        """
+        Function that contains 1 iteration of the optimization step
+        """
+
+        old_basic_vars = self.problem.basic_vars.copy()
+        optimizable = self.__optimize()
+        if not optimizable:
+            print("\nCannot be optimized further")
+        else:
+            self.simplex_printer.print_entering_leaving_vars(old_basic_vars, self.problem)
+        self.problem.update_optimal_reachability_status(optimizable)
+    
+    def __make_feasible(self) -> None:
+        """
+        Function that converts the optimal solution to a feasible optimal 
+        solution
+        """
+
+        old_basic_vars = self.problem.basic_vars.copy()
+        self.problem = self.feasible_handler.get_feasible(self.problem)
+        self.problem.update_feasible_status(self.feasible_handler.is_feasible(self.problem))
+        if not self.problem.is_optimal_reachable:
+            if self.feasible_count != 0:
+                print("\nNo further feasible solution found")
+            else:
+                print("\nNo feasible solution found")
+        else:
+            self.simplex_printer.print_entering_leaving_vars(old_basic_vars, self.problem)
+
+    def __increment_feasible_sol_num(self) -> None:
+        """
+        Function to increment the number of feasible solutions count
+        """
+        
+        self.feasible_count = self.feasible_count + 1
+
+    def __display_new_feasible_sol(self) -> None:
+        """
+        Function to display the current contents of the StandardProblem object
+        as a simplex table 
+
+        This function is called only after generating a feasible solution 
+        at each iteration of the all stack starting method
+        """
+
+        self.__increment_feasible_sol_num()
+        print(f"\nFeasible Solution # {self.feasible_count}")
+        self.simplex_printer.print_simplex_table_cli(self.problem)
+        if self.data_handler.freq == "all":
+            save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/sol_step_{self.feasible_count}.{self.data_handler.file_format}")
+
+    def __set_infeasible_status(self) -> None:
+        """
+        Function to update the status attributes of the StandardProblem object
+        when the problem is infeasible to be solved
+        """
+
+        self.problem.update_feasible_status(False)
+        self.problem.update_optimal_reachability_status(False)
+        self.problem.update_optimal_status(False)
+
+    def solver(
+            self, 
+            do_step : bool = False, 
+            show_steps : bool =True, 
+            show_interpret : bool =True,
+            file_format: Literal['json', 'yaml', None] = None,
+            freq: Literal['all','final', None] = None) -> StandardProblem:
+        """
+        Executing function to solve the linear programming problems using 
+        all stack starting method 
+
+        Parameters
+        ---------
+        do_step : `bool` (default : `False`)
+            Execute solution one step at a time when `do_step=True`
+        show_steps : `bool` (default : `True`)
+            Display all iterations occurring in the simplex matrix
+        show_interpret : `bool` (default : `True`)
+            Display the interpretation of the solution at each iteration
+        file_format : `str` (default : `None`) (Options : ["yaml","json",`None`]) 
+            Save the steps on solution in `json` or `yaml` file formats if provided
+        freq : str (default : None) (Options : ["all","final",`None`]) 
+            Save the steps in the solved LP problem. The freuency is 
+            overidden to None if the file_format is `None`. The frequency 
+            is overidden to `all` if a file format is specified. Expected 
+            options are 
+            "all"   : For saving all steps
+            "final" : For saving final result only
+            None    : No saving
+
+        Return
+        ------
+        StandardProblem object after optimizing using the all stack starting 
+        method. Will return a suboptimal or infeasible StandardProblem object if
+        the problem cannot be optimized. 
+        """
+
+        self.simplex_printer = SimplexPrinter(show_steps, show_interpret)
+        self.data_handler = DataHandler(file_format=file_format, freq=freq)
+
+        while not(self.feasible_handler.is_feasible(self.problem)):
+            self.problem.update_feasible_status(False)
+            print("\n...Generating Initial Feasible Solution for")
+            self.simplex_printer.print_simplex_table_cli(self.problem) 
+            self.__generate_initial_feasible_sol_step()
+            if not self.problem.is_optimal_reachable:
+                self.problem.matrix = round_off_simplex_matrix(self.problem.matrix)
+                if self.data_handler.freq == "all":
+                    save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/infeasible_sol_{self.infeasible_sol_count}.{self.data_handler.file_format}")
+                # no feasible solution
+                return self.problem
+        
+        self.problem.update_feasible_status(True)
+        if self.feasible_count != 0:
+            self.feasible_count -= 1
+        self.__display_new_feasible_sol()
+        
+        while not(self.__is_optimal()):
+            self.__optimize_step()
+            if not self.problem.is_optimal_reachable:
+                # update the last file saved with reachability status
+                if freq == "all":
+                    save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/sol_step_{self.feasible_count}.{self.data_handler.file_format}")
+                self.problem.matrix = round_off_simplex_matrix(self.problem.matrix)
+                # cannot be optimized
+                return self.problem
+        
+            self.__make_feasible()
+            if not self.problem.is_feasible:
+                self.problem.matrix = round_off_simplex_matrix(self.problem.matrix)
+                # no further feasible solution
+                return self.problem
+            
+            self.__display_new_feasible_sol()
+            if do_step:
+                break
+        
+        if (self.__is_optimal()):
+            if (check_artificial_basic_vars(self.problem)):
+                self.problem.matrix = round_off_simplex_matrix(self.problem.matrix)
+                self.__set_infeasible_status()
+                if freq == "all":
+                    save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/sol_step_{self.feasible_count}.{self.data_handler.file_format}")
+                print("\nArtificial variables found in optimal soltion.\nProblem is infeasible.")
+                return self.problem
+            else:
+                self.problem.update_optimal_status(True)
+                if freq == "all":
+                    save_file(self.problem, file_format=self.data_handler.file_format, file_path=f"solution/sol_step_{self.feasible_count}.{self.data_handler.file_format}")
+                print("\nOptimized Solution Received!")
+        
+        alternator = AlternateSolver(self.problem)
+        if alternator.n_alternates != 0:
+            print(f"There are {alternator.n_alternates} Alternate Solutions for this problem!")
+            # print(f">> Use alternate_solutions.extract_alternate_solution() method using version numbers from 1 to {num_alternate_sols}.")
+            # print(f">> Use alternate_solutions.display_all_alternate_solutions() method to display all alternate solutions")
+
+        # round off coefficients in matrix
+        # self.problem.matrix = round_off_simplex_matrix(self.problem.matrix)
+        return self.problem
+
+            
---
+++ packages/elpee/elpee-1.0.3.tar.gz/elpee-1.0.3/elpee/alternator.py
@@ -0,0 +1,229 @@
+# Copyright 2024-2025 Navindu De Silva
+# SPDX-License-Identifier: Apache-2.0
+
+from elpee.utils.feasible import FeasibleHandler
+from elpee import StandardProblem
+from elpee.utils.printer import SimplexPrinter
+from elpee.utils.utilities import create_ratio_col, get_subsets
+
+class AlternateSolver():
+    """
+    A class to assist the generation of alternate solutions for the given optimal problems
+
+    Attributes
+    ----------
+    problem : elpee.StandardProblem
+        The optimized problem given to find alternate solutions
+    n_alternates : int
+        Number of alternate solutions available for given optimal StandardProblem
+    simplex_printer : elpee.utils.SimplexPrinter 
+        An instance of printer to visualize the simplex table
+        of given LP problem
+    feasible_handler : elpee.utils.FeasibleHandler
+        An instance of handler object to retain feasibility of
+        LP problem and its solutions
+
+    Methods
+    -------
+    check_alternate_solutions() -> bool
+        Returns True/False if any alternate solutions are present
+    extract_alternate_solution(version_num) -> elpee.StandardProblem
+        Returns one of the alternate solutions. Version num is in the range of
+        1 to n_alternates inclusive
+    display_all_alternate_solutions() -> None
+        Displays all alternate solutions for given optimal problem
+    """
+
+    def __init__(self, problem:StandardProblem, show_simplex_table : bool = True, show_interpret : bool = True):
+        self.problem = problem
+        self.printer = SimplexPrinter(show_steps=show_simplex_table, show_interpret=show_interpret)
+        self.feasible_handler = FeasibleHandler()
+        self.n_alternates = len(self.get_alterations_combo_list())
+        self.__update_num_alternates()
+
+    def __update_num_alternates(self):
+        self.problem.set_num_alternates(self.n_alternates)
+        
+    def __get_entering_cols_for_alternates(self):
+        """
+        Returns a list of columns that can be used to create alternative optimal solutions
+        Returns a list of column indexes with 0 in the objective row except in basic variable columns
+        """
+
+        cols_for_alternates = []
+        for i in range(len(self.problem.obj_row)):
+            # for every zero in the objective row that is not a basic variable
+            if i+1 not in self.problem.basic_vars:
+                if self.problem.obj_row[i] == 0:
+
+                    # check if that column has at least 1 non-zero value. 
+                    zero_col = []
+                    for j in range(self.problem.n_constraints):
+                        zero_col.append(self.problem.matrix[j+1][i])
+                    if not all(element == 0 for element in zero_col):
+                        cols_for_alternates.append(i+1)
+                    # If the whole column is zero values - cannot do a pivot change
+                        # Helps avoid some of the infeasible alternate solutions
+
+        return cols_for_alternates
+    
+    def __apply_simplex_update(self, pivot_col_var):
+        """
+        Applies simplex update for the given pivot col to get an updated simplex table
+        Returns updated basic_vars list and matrix
+        """
+
+        M = 1000000
+        
+        new_var_name = self.printer.print_var_name(pivot_col_var, self.problem)
+        # create a ratio column for the pivot col provided
+        ratio_col = create_ratio_col(self.problem.matrix, pivot_col_var)
+        if min(ratio_col) == M:
+            return None, None 
+        else:
+            # selecting the leaving variable
+            pivot_row = ratio_col.index(min(ratio_col)) + 1
+        pivot_row_name = self.printer.print_var_name(self.problem.basic_vars[pivot_row], self.problem)
+        print(f"Taking {pivot_row_name} = 0 for & setting {new_var_name} as a Basic Variable for the alternate solution")
+        # make the change in basic variables list
+        self.problem.basic_vars[pivot_row] = pivot_col_var
+        
+        # apply feasibility fixes for the matrix with basic variables updated
+        self.problem = self.feasible_handler.get_feasible(self.problem)
+        if self.problem.matrix == None: 
+            return None, None
+        
+        # return the updated basic_vars, matrix after simplex updates
+        return self.problem.basic_vars, self.problem.matrix
+    
+    def get_alternate_solutions(self, alteration_combo):
+        """
+        For a given set of columns for which an alternate solution can be derived,
+        apply simplex updates to all the columns provided in alteration combo list and prints final alternate solution
+        """
+
+        for pivot_col in alteration_combo:
+            self.problem.basic_vars, self.problem.matrix = self.__apply_simplex_update(pivot_col)
+            if self.problem.matrix == None:
+                print("\nFeasible alternate solution not found")
+                return None, None
+        # print the final alternate solution based on set of columns provided
+        self.printer.print_simplex_table_cli(self.problem)
+    
+    def get_alterations_combo_list(self):
+        """
+        Provides the combinations of changes done to the original LP problem 
+        to derive alternate optimal solutions
+        """
+        
+        # obtain list of variables for generating alternate solutions
+        alternate_cols = self.__get_entering_cols_for_alternates()
+        # obtain a list of combinations of columns for creating all possible alternate solutions
+            # exclude the null set when extracting the subsets
+        alterations_combo_list = get_subsets(alternate_cols)[1:]
+        return alterations_combo_list
+
+
+def check_alternate_solutions(problem: StandardProblem):
+    """
+    Checks if the given problem has alternate solutions
+
+    Parameters
+    ----------
+    problem : `elpee.StandardProblem`
+        Optimized LP problem to check for alternate optimal solutions
+
+    Return
+    ------
+    True/False  
+    """
+
+    alternator = AlternateSolver(problem=problem)
+
+    # Checks if the simplex matrix has any alternate optimal solutions
+    # Compares and checks if there are combinations of changing the basic variables 
+        # with get_alterations_combo_list() function
+    # Returns True when there are alternative optimal solutions
+    if alternator.problem.is_optimal:
+        if len(alternator.get_alterations_combo_list()) > 0:
+            return True
+        else:
+            return False
+    else:
+        print("\nSolution not optimal")
+        return False
+    
+def extract_alternate_solution(problem: StandardProblem, version_num: int, 
+                               show_simplex_table : bool = True, show_interpret : bool = True) -> StandardProblem:
+    """
+    Extracts an alternate solution based on version_num provided 
+
+    Parameters
+    ----------
+    problem : `elpee.StandardProblem`
+        Optimized LP problem to check for alternate optimal solutions
+    version_num : int
+        index of the alternate solution to generate in the range 1 to num_alternates
+    show_simplex_table : bool (default : True)
+        Display the simplex tables of all alternate optimal solutions
+    show_interpret : bool (default : True)
+        Provide interpretation of all alternate optimal solutions
+    
+    Return
+    ------
+    elpee.StandardProblem containing alternate solution based on given index
+    """
+
+    alternator = AlternateSolver(problem=problem, show_simplex_table=show_simplex_table, show_interpret=show_interpret)
+
+    if not alternator.problem.is_optimal:
+        print(f"\nGiven problem is not optimal. No alternate solutions exist.")
+        return None
+
+    if alternator.problem.num_alternates != 0:
+        alterations_combo_list = alternator.get_alterations_combo_list()
+
+        if version_num <= len(alterations_combo_list):
+            print(f"\nAlternate Solution #{version_num}")
+            alternator.get_alternate_solutions(alterations_combo_list[version_num-1])
+            return alternator.problem
+        else:
+            print(f"\nThere are only {len(alterations_combo_list)} versions for Alternate Solutions!")
+            print(f"Cannot return Alternate Solution #{version_num}")
+            return None
+    else:
+        print("There are no alternate solutions!")
+        return None
+
+def display_all_alternate_solutions(problem : StandardProblem, show_simplex_table : bool = True, show_interpret : bool = True):
+    """
+    Display all alternate solutions for given optimal problem
+
+    Parameters
+    ----------
+    problem : `elpee.StandardProblem`
+        Optimized LP problem to display alternate optimal solutions
+    show_simplex_table : bool (default : True)
+        Display the simplex tables of all alternate optimal solutions
+    show_interpret : bool (default : True)
+        Provide interpretation of all alternate optimal solutions
+    """
+
+    alternator = AlternateSolver(problem=problem, show_simplex_table=show_simplex_table, show_interpret=show_interpret)
+    
+    print("\nDisplaying all Alternate Optimal Solutions for LP Problem provided...")
+
+    if not alternator.problem.is_optimal:
+        print("\nGiven problem is not optimal. No alternate solutions exist.")
+    else:
+        if alternator.problem.num_alternates != 0:
+            alterations_combo_list = alternator.get_alterations_combo_list()
+            initial_problem = alternator.problem.copy()
+
+            for i, alteration_combo in enumerate(alterations_combo_list):
+                print(f"\nAlternate Solution #{i+1}")
+                alternator.get_alternate_solutions(alteration_combo)
+
+                alternator.problem = initial_problem
+        else:
+            print("\nThere are no alternate solutions to display!")
