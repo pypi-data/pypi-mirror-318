4ff90c6906b7b49b3a4496e7c0b50b9a1bd6c222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:03:56 +0000

    Add package accessible-space/0.1.12/accessible_space-0.1.12.tar.gz

---
+++ packages/accessible-space/accessible_space-0.1.12.tar.gz/accessible_space-0.1.12/accessible_space/__main__.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "validation":
+            import accessible_space.apps.validation
+            accessible_space.apps.validation.main()
+        elif sys.argv[1] == "test":
+            import pytest
+            # subprocess.run([
+            #     "pytest",
+            #     os.path.abspath(os.path.dirname(__file__)),
+            #     "--doctest-modules",
+            # ])
+            pytest.main([
+                # os.path.abspath(os.path.dirname(__file__)),
+                # "--filterwarnings=ignore:Inferring attacking direction:UserWarning",
+                # "--filterwarnings=ignore:Range of tracking Y coordinates:UserWarning",
+                # "--filterwarnings=ignore:Range of tracking X coordinates:UserWarning",
+            ])
+        elif sys.argv[1] == "demo":
+            import accessible_space.apps.readme
+            accessible_space.apps.readme.main()
+        else:
+            raise ValueError(f"Invalid argument: {sys.argv[1]}. Available arguments: 'validation', 'test', 'demo'.")
+    else:
+        raise ValueError("No arguments provided. Available arguments: 'validation', 'test', 'demo'.")
+
+
+if __name__ == '__main__':
+    main()
