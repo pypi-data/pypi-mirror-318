736b6de98da14c1905f4534187df59be3b270554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 14:05:38 +0000

    Add package mmd-critic/0.1.2/mmd_critic-0.1.2.tar.gz

---
+++ packages/mmd-critic/mmd_critic-0.1.2.tar.gz/mmd_critic-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mmd-critic',
+    version='0.1.2',
+    description='Python package for the MMD Critic method',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PhysBoom/mmd_critic',
+    author='Matthew Chak',
+    author_email='mchak@calpoly.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'numexpr'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
