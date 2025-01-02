5d089aaaad5ebeaa78147d15e1138a671211bf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:48 +0000

    Add package simpleCan/0.1/simplecan-0.1.tar.gz

---
+++ packages/simpleCan/simplecan-0.1.tar.gz/simplecan-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpleCan',
+    version='0.1',
+    packages=find_packages(),
+    author='JIAJIE LIU',
+    author_email='your.email@example.com',
+    description='This package realizes sending can message functionality',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
