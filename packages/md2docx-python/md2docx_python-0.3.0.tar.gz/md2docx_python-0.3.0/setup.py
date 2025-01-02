478cbfbb1e41fcb104f9edd2848b323eb4a04121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:09 +0000

    Add package md2docx-python/0.3.0/md2docx_python-0.3.0.tar.gz

---
+++ packages/md2docx-python/md2docx_python-0.3.0.tar.gz/md2docx_python-0.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='md2docx_python',
+    version='0.3.0',
+    url='https://github.com/shloktech/md2docx-python',
+    author='Shlok Tadilkar',
+    author_email='shloktadilkar@gmail.com',
+    license='MIT',
+    description="""Markdown to Word Converter.
+                Simple and straight forward Python utility 
+                that converts a Markdown file (`.md`) to a Microsoft 
+                Word document (`.docx`). It supports basic Markdown 
+                elements, including headings, bold and italic text, 
+                and both unordered and ordered lists.""",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['markdown', 'python-docx', 'beautifulsoup4'],
+    python_requires=">=3.9.0",
+)
+
+
