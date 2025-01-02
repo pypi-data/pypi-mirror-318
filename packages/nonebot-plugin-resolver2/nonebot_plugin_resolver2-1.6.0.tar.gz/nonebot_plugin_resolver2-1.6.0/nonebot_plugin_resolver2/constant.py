c7855074da0b665b1fa02c72e9587bc989e189ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 04:04:37 +0000

    Add package nonebot-plugin-resolver2/1.6.0/nonebot_plugin_resolver2-1.6.0.tar.gz

---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.6.0.tar.gz/nonebot_plugin_resolver2-1.6.0/nonebot_plugin_resolver2/constant.py
@@ -0,0 +1,15 @@
+"""
+通用头请求
+"""
+COMMON_HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 '
+                  'UBrowser/6.2.4098.3 Safari/537.36'
+}
+
+"""
+视频最大大小（MB）
+"""
+VIDEO_MAX_MB = 100
+
+# 解析列表文件名
+DISABLE_GROUPS = "disable_group_list.json"
