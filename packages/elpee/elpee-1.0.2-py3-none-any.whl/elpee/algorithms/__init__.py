df76adea482327a09bf86a4289a938c1d579339a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 03:08:14 +0000

    Add package elpee/1.0.2/elpee-1.0.2-py3-none-any.whl

---
+++ packages/elpee/elpee-1.0.2-py3-none-any.whl/elpee/algorithms/__init__.py
@@ -0,0 +1,13 @@
+# elpee/algorithms/__init__.py
+
+from .all_stack_starter import AllStackStarter
+"""
+Export Algorithms to solve Linear Programming Problems
+
+Modeules exported by this package:
+
+- `AllStackStarter` : Provide implementation for All Atack Starting Method
+- `bigM_handler` : Provide implementation for Big M method
+- `DualSimplexSolver` : Provide implementation for Dual Simplex method
+
+"""
