68582cb97ef8842b2c2846b3ffd05c89336b18ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 05:04:15 +0000

    Add package brom-drake/0.3.0/brom_drake-0.3.0-py3-none-any.whl

---
+++ packages/brom-drake/brom_drake-0.3.0-py3-none-any.whl/brom_drake/file_manipulation/urdf/drakeify.py
@@ -0,0 +1,54 @@
+"""
+convenience.py
+Description:
+
+    This file contains a couple of convenience functions that are useful for the user of the URDF conversion
+    feature.
+"""
+
+from typing import Union
+from pathlib import Path
+
+# Internal Imports
+from .DrakeReadyURDFConverter import DrakeReadyURDFConverter, MeshReplacementStrategy
+
+def drakeify_my_urdf(
+    urdf_file_path: Union[str, Path],
+    overwrite_old_logs: bool = False,
+    overwrite_old_models: bool = False,
+    log_file_name: str = "conversion.log",
+    collision_mesh_replacement_strategy: MeshReplacementStrategy = MeshReplacementStrategy.kWithObj,
+) -> Path:
+    """
+    Description:
+
+        This function provides a convenience function for the user to convert a URDF file
+        into a "Drake-ready" URDF file.
+    :param urdf_file:
+    :param output_dir:
+    :param overwrite_old_logs:
+    :param log_file_name:
+    :return:
+    """
+    # Input Processing
+    if not isinstance(urdf_file_path, str) and not isinstance(urdf_file_path, Path):
+        raise ValueError(
+            f"urdf_file_path must be of type str or Path; received {urdf_file_path} of type {type(urdf_file_path)}."
+        )
+
+    if isinstance(urdf_file_path, str):
+        urdf_file_path = Path(urdf_file_path)
+
+    # Use converter
+    converter = DrakeReadyURDFConverter(
+        urdf_file_path,
+        overwrite_old_logs=overwrite_old_logs,
+        overwrite_old_models=overwrite_old_models,
+        log_file_name=log_file_name,
+        collision_mesh_replacement_strategy=collision_mesh_replacement_strategy,
+    )
+
+    # Convert the URDF
+    new_urdf_path = converter.convert_urdf()
+
+    return new_urdf_path
