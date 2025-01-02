170fcf9a4a9390e1e6181880c2f08be4a311cbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-02 15:04:25 +0000

    Add package antigranular-enterprise/1.4.0/antigranular_enterprise-1.4.0.tar.gz

---
+++ packages/antigranular-enterprise/antigranular_enterprise-1.4.0.tar.gz/antigranular_enterprise-1.4.0/antigranular_enterprise/__init__.py
@@ -0,0 +1,23 @@
+"""
+Antigranular client package
+"""
+
+
+from .client import login
+from .client import read_config
+from .client import write_config
+from .client import load_config
+# Package version dunder
+__version__ = "1.3.0"
+
+# Package author dunder
+__author__ = "Oblivious Software Pvt. Ltd."
+
+# Package * imports dunder
+__all__ = ["login", "read_config", "write_config", "load_config", "__version__", "__author__"]
+
+# Read default config on import
+# try:
+#     read_config()
+# except Exception:
+#     pass # Ignore if config file is not found
