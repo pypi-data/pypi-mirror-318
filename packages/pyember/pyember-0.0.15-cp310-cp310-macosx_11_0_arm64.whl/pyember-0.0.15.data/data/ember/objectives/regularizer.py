0438fbddc5a93871784a2c61cdc0bb5b6f6ea63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:26 +0000

    Add package pyember/0.0.15/pyember-0.0.15-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pyember/pyember-0.0.15-cp310-cp310-macosx_11_0_arm64.whl/pyember-0.0.15.data/data/ember/objectives/regularizer.py
@@ -0,0 +1,15 @@
+class Regularizer(): 
+    
+    def __init__(self): 
+        pass 
+
+class RidgeRegularizer(Regularizer): 
+    
+    def __init__(self): 
+        super().__init__()
+
+class LassoRegularizer(Regularizer): 
+
+    def __init__(self): 
+        super().__init__()
+ 
