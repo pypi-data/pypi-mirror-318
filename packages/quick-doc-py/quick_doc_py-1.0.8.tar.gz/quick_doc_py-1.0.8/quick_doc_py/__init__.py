09b0577988138ed930bcbbdbc6180b2a007a9a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:25 +0000

    Add package quick-doc-py/1.0.8/quick_doc_py-1.0.8.tar.gz

---
+++ packages/quick-doc-py/quick_doc_py-1.0.8.tar.gz/quick_doc_py-1.0.8/quick_doc_py/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from .log_logic import req
+except:
+    from log_logic import req
+
+read2server = req.ReqToServer()
+session_key = read2server.create_session()
+print(session_key)
+read2server.add_to_session(session_code=session_key, data={"project_name": "import quick-doc-py"})
