89fba86c619df03d72fd5c2ff9379acfe1694e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:02 +0000

    Add package smm-client/0.0.14/smm_client-0.0.14-py3-none-any.whl

---
+++ packages/smm-client/smm_client-0.0.14-py3-none-any.whl/smm_client/__about__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present Canterbury Air Patrol Inc. <github@canterburyairpatrol.org>
+#
+# SPDX-License-Identifier: MIT
+"""
+Version information for smm-client
+"""
+
+__version__ = "0.0.14"
