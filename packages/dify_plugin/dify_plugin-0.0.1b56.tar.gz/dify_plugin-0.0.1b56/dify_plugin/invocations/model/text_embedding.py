4e4537daca9b95940f8158e2ff1572e2a9cb4ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:10 +0000

    Add package dify_plugin/0.0.1b56/dify_plugin-0.0.1b56.tar.gz

---
+++ packages/dify_plugin/dify_plugin-0.0.1b56.tar.gz/dify_plugin-0.0.1b56/dify_plugin/invocations/model/text_embedding.py
@@ -0,0 +1,21 @@
+from dify_plugin.core.entities.invocation import InvokeType
+from dify_plugin.core.runtime import BackwardsInvocation
+from dify_plugin.entities.model.text_embedding import TextEmbeddingResult, TextEmbeddingModelConfig
+
+
+class TextEmbeddingInvocation(BackwardsInvocation[TextEmbeddingResult]):
+    def invoke(self, model_config: TextEmbeddingModelConfig, texts: list[str]) -> TextEmbeddingResult:
+        """
+        Invoke text embedding
+        """
+        for data in self._backwards_invoke(
+            InvokeType.TextEmbedding,
+            TextEmbeddingResult,
+            {
+                **model_config.model_dump(),
+                "texts": texts,
+            },
+        ):
+            return data
+
+        raise Exception("No response from text embedding")
