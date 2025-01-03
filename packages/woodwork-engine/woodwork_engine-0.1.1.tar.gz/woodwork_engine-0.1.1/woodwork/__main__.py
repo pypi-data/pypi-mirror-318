0d42db3991fa9b420472b84e2613c556d8ab89e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:27 +0000

    Add package woodwork-engine/0.1.1/woodwork_engine-0.1.1.tar.gz

---
+++ packages/woodwork-engine/woodwork_engine-0.1.1.tar.gz/woodwork_engine-0.1.1/woodwork/__main__.py
@@ -0,0 +1,33 @@
+from woodwork.dependencies import init, activate_virtual_environment, install_all
+from woodwork.helper_functions import set_globals
+
+import sys
+
+
+def main():
+    # woodwork
+    if len(sys.argv) == 1:
+        activate_virtual_environment()
+
+        from woodwork.config_parser import main_function
+
+        main_function()
+
+    # woodwork --debug
+    elif sys.argv[1] == "--debug":
+        set_globals(mode="debug")
+        activate_virtual_environment()
+
+        from woodwork.config_parser import main_function
+
+        main_function()
+
+    # woodwork init
+    elif sys.argv[1] == "init":
+        if len(sys.argv) == 2:
+            init()
+        else:
+            if sys.argv[2] == "--isolated":
+                init({"isolated": True})
+            if sys.argv[2] == "--all":
+                install_all()
