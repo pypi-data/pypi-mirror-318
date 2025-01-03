6ad9241a64ffb2a9258d1c0bb05aec74090df2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:06:22 +0000

    Add package fake-bge-module-latest/20250103/fake_bge_module_latest-20250103.tar.gz

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20250103.tar.gz/fake_bge_module_latest-20250103/bl_console_utils/autocomplete/intellisense/__init__.pyi
@@ -0,0 +1,32 @@
+import typing
+import collections.abc
+import typing_extensions
+
+def complete(line: str, cursor: int, namespace: dict, private: bool):
+    """Returns a list of possible completions:
+
+    :param line: incomplete text line
+    :type line: str
+    :param cursor: current character position
+    :type cursor: int
+    :param namespace: namespace
+    :type namespace: dict
+    :param private: whether private variables should be listed
+    :type private: bool
+    :return: list of completions, word
+    """
+
+def expand(line: str, cursor: int, namespace, *, private: bool = True) -> int | str:
+    """This method is invoked when the user asks auto-completion,
+    e.g. when Ctrl+Space is clicked.
+
+        :param line: incomplete text line
+        :type line: str
+        :param cursor: current character position
+        :type cursor: int
+        :param namespace: namespace
+        :param private: whether private variables should be listed
+        :type private: bool
+        :return: current expanded line, updated cursor position and scrollback
+        :rtype: int | str
+    """
