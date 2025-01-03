89a35914e89c27ed0405b62d77fbd63d08ae98b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:05:47 +0000

    Add package ss-pylib/0.0.26/ss_pylib-0.0.26-py3-none-any.whl

---
+++ packages/ss-pylib/ss_pylib-0.0.26-py3-none-any.whl/sslib/helper/slack.py
@@ -0,0 +1,11 @@
+import httpx
+
+
+class Slack:
+  def __init__(self, webHook:str):
+    self.__webHook = webHook
+    
+  async def send_message(self, message:str) -> bool:
+    async with httpx.AsyncClient() as client:
+      res = await client.post(self.__webHook, json={'text':message})
+      return True if res.status_code == httpx.codes.OK else False
