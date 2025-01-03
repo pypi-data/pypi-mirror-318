cf584ddb6d2234e1e21fa887a8a2120a1c6b2d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:06:27 +0000

    Add package Dhelpers/0.1.6/Dhelpers-0.1.6-py3-none-any.whl

---
+++ packages/Dhelpers/Dhelpers-0.1.6-py3-none-any.whl/Dpowers/editing/__init__.py
@@ -0,0 +1,40 @@
+#
+#
+# Copyright (c) 2020-2025 DPS, dps@my.mail.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+from .ressource_classes import *
+from .imagepower import *
+from .mp3power import *
+
+
+
+class Image(ImageBase):
+    adaptor = ImageAdaptor(_primary_name="Image.adaptor")
+    
+    
+class mp3tag(mp3tagBase):
+    adaptor = mp3tagAdaptor(_primary_name="mp3tag.adaptor")
+
+
+
+from Dhelpers.file_iteration import FilelistCreator
+
+
+class PlaylistCreator(FilelistCreator):
+    editor_class = mp3tag
+    filelist_extension = ".m3u"
+    #file_start = "#EXTM3U"
