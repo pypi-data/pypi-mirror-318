cc4bde8f4b2f2152a1c30a593118795d6fb96cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:06:22 +0000

    Add package PyUdisk/1.1.0a0/PyUdisk-1.1.0a0-py3-none-any.whl

---
+++ packages/PyUdisk/PyUdisk-1.1.0a0-py3-none-any.whl/pyudisk/models/__init__.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class SystemPartitions(BaseModel):
+    """System partitions' mountpoints and fstypes."""
+
+    system_mountpoints: List[str] = Field(
+        default_factory=lambda: [
+            "/sys",
+            "/proc",
+            "/dev",
+            "/run",
+            "/boot",
+            "/tmp",
+            "/var",
+            "/snap",
+            "/sys/kernel",
+            "/sys/fs",
+            "/var/lib/docker",
+            "/dev/loop",
+            "/run/user",
+            "/run/snapd",
+        ]
+    )
+    system_fstypes: List[str] = Field(
+        default_factory=lambda: [
+            "sysfs",
+            "proc",
+            "devtmpfs",
+            "tmpfs",
+            "devpts",
+            "fusectl",
+            "securityfs",
+            "overlay",
+            "hugetlbfs",
+            "debugfs",
+            "cgroup2",
+            "configfs",
+            "bpf",
+            "binfmt_misc",
+            "efivarfs",
+            "fuse",
+            "nsfs",
+            "squashfs",
+            "autofs",
+            "tracefs",
+            "pstore",
+        ]
+    )
