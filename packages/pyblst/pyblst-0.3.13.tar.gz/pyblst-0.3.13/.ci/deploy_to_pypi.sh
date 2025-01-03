946ce831261c496819494b0f4e9ced93b5bd4b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:21 +0000

    Add package pyblst/0.3.13/pyblst-0.3.13.tar.gz

---
+++ packages/pyblst/pyblst-0.3.13.tar.gz/pyblst-0.3.13/.ci/deploy_to_pypi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker run \
+       --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
+       --rm \
+       -v "$(pwd)":/io \
+       ghcr.io/pyo3/maturin \
+       publish \
+       --interpreter python3.9 python3.10 python3.11 \
+       --username __token__ \
+       --password "$MATURIN_PASSWORD" \
+       --skip-existing
