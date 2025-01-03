0fa8c1c424c730f5d7398f3bd7fed1cb5a16b5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:10 +0000

    Add package muji-lang/1.0.0/muji_lang-1.0.0-py3-none-any.whl

---
+++ packages/muji-lang/muji_lang-1.0.0-py3-none-any.whl/muji/token_types.py
@@ -0,0 +1,31 @@
+INTEGER          = 'INTEGER'  
+FLOAT            = 'FLOAT'  
+PLUS             = 'PLUS'  
+MINUS            = 'MINUS'  
+MUL              = 'MUL'  
+DIV              = 'DIV'  
+LPAREN           = 'LPAREN'  
+RPAREN           = 'RPAREN'  
+IDENTIFIER       = 'IDENTIFIER'  
+ASSIGN           = 'ASSIGN'  
+SEMI             = 'SEMI'      # Semicolon  
+EOF              = 'EOF'       # End-of-file  
+INPUT            = 'INPUT'     # van muji  
+PRINT            = 'PRINT'     # her muji  
+VAR_DECL         = 'VAR_DECL'  # dey muji  
+IF               = 'IF'        # yedi muji  
+ELIF             = 'ELIF'      # natra bhane muji  
+ELSE             = 'ELSE'      # natra muji  
+PROGRAM_START    = 'PROGRAM_START'  # oe muji  
+PROGRAM_END      = 'PROGRAM_END'    # lala muji  
+LBRACE           = 'LBRACE'  
+RBRACE           = 'RBRACE'  
+EQ               = 'EQ'        # ==  
+NE               = 'NE'        # !=  
+LT               = 'LT'        # <  
+GT               = 'GT'        # >  
+LTE              = 'LTE'       # <=  
+GTE              = 'GTE'       # >=  
+STRING           = 'STRING'  
+FOR              = 'FOR'  
+WHILE            = 'WHILE'  
