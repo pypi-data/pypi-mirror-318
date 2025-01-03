aa5aee501da4c4547bd86ce8e68550cccc53f789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:50 +0000

    Add package entitled/0.4.2/entitled-0.4.2-py3-none-any.whl

---
+++ packages/entitled/entitled-0.4.2-py3-none-any.whl/entitled/policies.py
@@ -0,0 +1,81 @@
+"""Grouping of authorization rules around a particular resource type"""
+
+from typing import Any, Callable, Generic, TypeVar
+
+from entitled import exceptions
+from entitled.rules import Rule, RuleProtocol
+
+T = TypeVar("T")
+
+
+class Policy(Generic[T]):
+    """A grouping of rules refering the given resource type."""
+
+    def __init__(
+        self,
+        label: str | None = None,
+        rules: dict[str, list[Rule[T]]] | None = None,
+    ):
+        self._registry: dict[
+            str,
+            list[Rule[T]],
+        ] = {}
+        self.label = label
+
+        if not rules:
+            rules = {}
+
+        for action, rule in rules.items():
+            self.__register(action, *rule)
+
+    def rule(self, name: str) -> Callable[[RuleProtocol[T]], RuleProtocol[T]]:
+        def wrapped(func: RuleProtocol[T]):
+            rule_name = name
+            if self.label is not None:
+                rule_name = self.label + ":" + rule_name
+            new_rule = Rule[T](rule_name, func)
+            self.__register(name, new_rule)
+            return func
+
+        return wrapped
+
+    def __register(self, action: str, *rules: Rule[T]):
+        if action not in self._registry:
+            self._registry[action] = [*rules]
+
+    def grants(
+        self, actor: Any, resource: T | type[T], context: dict[str, Any] | None = None
+    ) -> dict[str, bool]:
+        return {
+            action: self.allows(action, actor, resource, context)
+            for action in self._registry
+        }
+
+    def allows(
+        self,
+        action: str,
+        actor: Any,
+        resource: T | type[T],
+        context: dict[str, Any] | None = None,
+    ) -> bool:
+        try:
+            return self.authorize(action, actor, resource, context)
+        except exceptions.AuthorizationException:
+            return False
+
+    def authorize(
+        self,
+        action: str,
+        actor: Any,
+        resource: T | type[T],
+        context: dict[str, Any] | None = None,
+    ) -> bool:
+        if action not in self._registry:
+            raise exceptions.UndefinedAction(
+                f"Action <{action}> undefined for this policy"
+            )
+
+        if not any(rule(actor, resource, context) for rule in self._registry[action]):
+            raise exceptions.AuthorizationException("Unauthorized")
+
+        return True
---
+++ packages/entitled/entitled-0.4.2-py3-none-any.whl/entitled/rules.py
@@ -0,0 +1,92 @@
+"""Basic building block of the Entitled library.
+
+Rules are essentially wrappers around boolean functions that determines a relationship
+between an actor and a resourc.
+
+Resource can essentially be any entity in an application.
+Actors represents any entity that, in an application, can act upon a resource.
+"""
+
+from typing import Any, Callable, ClassVar, Generic, Protocol, TypeVar
+
+from entitled import exceptions
+
+Resource = TypeVar("Resource", contravariant=True)
+
+
+class RuleProtocol(Protocol[Resource]):
+    """Defines valid functions for rules"""
+
+    def __call__(
+        self,
+        actor: Any,
+        resource: Resource | type[Resource],
+        context: dict[str, Any] | None = None,
+    ) -> bool: ...
+
+
+class Rule(Generic[Resource]):
+    """Base class for rules
+
+    Args:
+        name (str): The name given to this rule.
+        rule_function (Callable): The underlying boolean function.
+    """
+
+    _registry: ClassVar[dict[str, "Rule[Any]"]] = {}
+
+    @classmethod
+    def clear_registry(cls) -> None:
+        cls._registry = {}
+
+    @classmethod
+    def get_rule(cls, name: str) -> "Rule[Resource] | None":
+        return Rule._registry[name] if name in Rule._registry else None
+
+    def __init__(self, name: str, rule_function: RuleProtocol[Resource]):
+        self.name = name
+        self.rule = rule_function
+        self.__register()
+
+    def __register(self) -> None:
+        if self.name in Rule._registry:
+            print(f"A rule identified by '{self.name}' already exists.")
+            return
+
+        Rule._registry[self.name] = self
+
+    def __call__(
+        self,
+        actor: Any,
+        resource: Resource | type[Resource],
+        context: dict[str, Any] | None = None,
+    ) -> bool:
+        if not context:
+            context = {}
+        return self.rule(actor, resource, context)
+
+    def authorize(
+        self,
+        actor: Any,
+        resource: Resource | type[Resource],
+        context: dict[str, Any] | None = None,
+    ) -> bool:
+        if not self(actor, resource, context):
+            raise exceptions.AuthorizationException("Unauthorized")
+        return True
+
+    def allows(
+        self,
+        actor: Any,
+        resource: Resource | type[Resource],
+        context: dict[str, Any] | None = None,
+    ) -> bool:
+        return self(actor, resource, context)
+
+
+def rule(name: str) -> Callable[[RuleProtocol[Resource]], RuleProtocol[Resource]]:
+    def wrapped(func: RuleProtocol[Resource]):
+        _ = Rule[Resource](name, func)
+        return func
+
+    return wrapped
