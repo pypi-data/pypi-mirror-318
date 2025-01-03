45a1edaf9e192a21f504108efa5c392fc25a440c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:05:44 +0000

    Add package RCAIDE-TEST/0.4.6/rcaide_test-0.4.6.tar.gz

---
+++ packages/RCAIDE-TEST/rcaide_test-0.4.6.tar.gz/rcaide_test-0.4.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        include_package_data=True,
+        packages=find_packages(),
+        package_data={
+            'RCAIDE': ['VERSION'], 
+        }
+    )
