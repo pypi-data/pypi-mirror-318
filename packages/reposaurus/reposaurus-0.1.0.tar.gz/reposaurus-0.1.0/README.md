794d5336edb13a6a38b3ff05ac7713d98ba50c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:37 +0000

    Add package reposaurus/0.1.0/reposaurus-0.1.0.tar.gz

---
+++ packages/reposaurus/reposaurus-0.1.0.tar.gz/reposaurus-0.1.0/README.md
@@ -0,0 +1,7 @@
+# 🦖 Reposaurus
+
+A prehistoric tool for turning repositories into text files. Because documentation never goes extinct!
+
+## Installation
+```bash
+pip install reposaurus
---
+++ packages/reposaurus/reposaurus-0.1.0.tar.gz/reposaurus-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reposaurus",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'reposaurus=repo2txt.main:main',  # This makes the command 'reposaurus' available
+        ],
+    },
+    author="Andy Thomas",
+    author_email="your.email@example.com",  # Update this if you want
+    description="A prehistoric tool for turning repositories into text files 🦖",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/reposaurus",  # Update if you have a GitHub repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+    ],
+    python_requires=">=3.6",
+)
