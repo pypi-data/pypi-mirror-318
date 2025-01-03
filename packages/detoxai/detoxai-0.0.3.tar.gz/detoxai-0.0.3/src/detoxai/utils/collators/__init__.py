bb151f1e15cc933d1575541c8db15766f1a7af68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:42 +0000

    Add package detoxai/0.0.3/detoxai-0.0.3.tar.gz

---
+++ packages/detoxai/detoxai-0.0.3.tar.gz/detoxai-0.0.3/src/detoxai/utils/collators/__init__.py
@@ -0,0 +1,8 @@
+from .base_collator import BaseCollator  # noqa
+from .extended_collator import ExtendedCollator  # noqa
+from .binary_collator import BinaryCollator  # noqa
+from .binary_fairness_collator import BinaryClassFairnessCollator  # noqa
+from .multiclass_collator import MultiClassCollator  # noqa
+from .multiclass_fairness_collator import MultiClassFairnessCollator  # noqa
+from .fairness_collator import FairnessCollator  # noqa
+from . import utils  # noqa
