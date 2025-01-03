8f1d31bbdc38a87df9092346426bbea6ef2baf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 05:06:15 +0000

    Add package rite/0.0.3/rite-0.0.3-py3-none-any.whl

---
+++ packages/rite/rite-0.0.3-py3-none-any.whl/rite/file/geojson.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Provides JSON stuff
+
+...
+
+Examples:
+    ...
+
+Attributes:
+    ...
+
+Todo:
+
+"""
+
+
+# Import | Futures
+# […]
+
+# Import | Standard Library
+import json
+# […]
+
+# Import | Libraries
+import geojson
+# […]
+
+# Import | Local Modules
+# […]
+
+
+
+class FileGeoJSON:
+    """
+    A class used to represent a GeoJSON File
+
+    ...
+
+    Attributes
+    ----------
+
+
+    Methods
+    -------
+    test()
+        test method
+    """
+
+    # Static Methods
+
+    @staticmethod
+    def save_to_geojson(path, geometry):
+        """"""
+        # Serializing json
+        dump = geojson.dumps(geometry, sort_keys=True, indent=4)
+        # Writing to outfile
+        with open(path, "w") as outfile:
+            outfile.write(dump)
