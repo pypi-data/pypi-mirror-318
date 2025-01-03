6d49cbbd7077ef09da68df55c4c74157711c6628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:06:25 +0000

    Add package dl123/0.1/dl123-0.1.tar.gz

---
+++ packages/dl123/dl123-0.1.tar.gz/dl123-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dl123',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author="25/25 in lab",
+    author_email="your_email@example.com",
+    description="A custom Python package with multiple programs.",
+    python_requires='>=3.6',
+)
