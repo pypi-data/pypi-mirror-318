de1d5d330db9154df9b3ecfc71fa7d8fe260fe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:52 +0000

    Add package django-konst/3.0.0/django_konst-3.0.0.tar.gz

---
+++ packages/django-konst/django_konst-3.0.0.tar.gz/django_konst-3.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+
+name = "django-konst"
+description = "Convenient constants fields for Django"
+author = "Luke Burden"
+author_email = "lukeburden@gmail.com"
+url = "https://github.com/lukeburden/django-konst"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+tests_require = ["pytest", "pytest-django", "djangorestframework>=3.10"]
+
+setup(
+    name=name,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="3.0.0",
+    license="MIT",
+    url=url,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    install_requires=["django>=3.2"],
+    test_suite="runtests.runtests",
+    tests_require=tests_require,
+    zip_safe=False,
+)
