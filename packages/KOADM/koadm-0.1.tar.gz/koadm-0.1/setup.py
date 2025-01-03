79a8dfc4861af30f7b5bb2f80cc49862cb805ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:05:14 +0000

    Add package KOADM/0.1/koadm-0.1.tar.gz

---
+++ packages/KOADM/koadm-0.1.tar.gz/koadm-0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='KOADM',
+    version='0.1',
+    packages=find_packages(),
+    package_data={
+        '': ['train_data.csv'],  # 包含当前目录下的 train_data.csv 文件
+    },
+    include_package_data=True,
+    description='Knee osteoarthritis diagnosis model',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='QWB',
+    license='MIT',
+    install_requires=['pandas','numpy','time','argparse','sklearn','xgboost',
+    'lightgbm','keras','tensorflow']
+)
+
+
