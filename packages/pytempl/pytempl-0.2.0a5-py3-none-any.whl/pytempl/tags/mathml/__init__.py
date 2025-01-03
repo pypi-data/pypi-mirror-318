737bc215bbed744efc2a1d675158c17661653fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 05:04:55 +0000

    Add package pytempl/0.2.0a5/pytempl-0.2.0a5-py3-none-any.whl

---
+++ packages/pytempl/pytempl-0.2.0a5-py3-none-any.whl/pytempl/tags/mathml/__init__.py
@@ -0,0 +1,10 @@
+# https://developer.mozilla.org/en-US/docs/Web/MathML
+
+from ._base import BaseMathMLElement, GlobalMathMLAttributes
+from .math import Math
+
+__all__ = [
+    "BaseMathMLElement",
+    "GlobalMathMLAttributes",
+    "Math",
+]
