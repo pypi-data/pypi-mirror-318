3d762e0fac40d0438535e03d164fe778e981c9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:36 +0000

    Add package yophonebot/0.1.3/yophonebot-0.1.3.tar.gz

---
+++ packages/yophonebot/yophonebot-0.1.3.tar.gz/yophonebot-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yophonebot",
+    version="0.1.3",
+    description="Python package for interacting with YoPhone Bot API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nairi Hovhannisyan",
+    author_email="your.email@example.com",
+    url="https://github.com/nairihovh/yophone",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.20.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
