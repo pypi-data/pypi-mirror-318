f11bb6207188641ed3370bcdcfa962b18aa06227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:27 +0000

    Add package peor/0.0.1/peor-0.0.1-py3-none-any.whl

---
+++ packages/peor/peor-0.0.1-py3-none-any.whl/peor/android_manifest_util.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+
+class AndroidManifestPatcher:
+    def __init__(self, manifest_path: Path):
+        self.__manifest_path = manifest_path
+        self.__android_schema = AndroidManifestPatcher.__find_android_schema(manifest_path)
+
+        ET.register_namespace('android', self.__android_schema)
+
+    def allow_internet_permission(self):
+        INTERNET_PERMISSION = 'android.permission.INTERNET'
+        root = ET.fromstring(self.__get_content())
+
+        for permission in root.iter('uses-permission'):
+            if INTERNET_PERMISSION in permission.attrib.values():
+                return
+
+        internet_perm = ET.Element('uses-permission', {f'{{{self.__android_schema}}}:name': INTERNET_PERMISSION})
+        root.insert(0, internet_perm)
+
+        self.__set_content(ET.tostring(root))
+
+    def allow_native_libs_extraction(self):
+        root = ET.fromstring(self.__get_content())
+        application_elem = root.find('application')
+
+        native_permission = f'{{{self.__android_schema}}}extractNativeLibs'
+        application_elem.attrib.pop(native_permission, None)
+        application_elem.attrib[native_permission] = 'true'
+
+        self.__set_content(ET.tostring(root))
+
+    def find_app_entry_point(self) -> str:
+        root = ET.fromstring(self.__get_content())
+        application_elem = root.find('application')
+
+        for activity in application_elem.iter('activity'):
+            for intent_filter in activity.iter('intent-filter'):
+                action_elem = intent_filter.find('action')
+                category_elem = intent_filter.find('category')
+                android_name_tag = f'{{{self.__android_schema}}}name'
+
+                if (action_elem is None) or (category_elem is None):
+                    continue
+
+                if action_elem.get(android_name_tag) != 'android.intent.action.MAIN':
+                    continue
+
+                if category_elem.attrib.get(android_name_tag) != 'android.intent.category.LAUNCHER':
+                    continue
+
+                return activity.attrib.get(android_name_tag)
+
+        raise Exception('Couldn\'t find activity with MAIN action for LAUNCHER catagory')
+
+    def __get_content(self) -> str:
+        with open(self.__manifest_path, 'r') as manifest_file:
+            return manifest_file.read()
+
+    def __set_content(self, xml_content: bytes):
+        with open(self.__manifest_path, 'wb') as manifest_file:
+            manifest_file.write(xml_content)
+
+    @staticmethod
+    def __find_android_schema(manifest_path: Path) -> str:
+        with open(manifest_path, 'r') as manifest_file:
+            manifest = ET.parse(manifest_file)
+
+        root = manifest.getroot()
+        application_elem = root.find('application')
+
+        if not application_elem:
+            raise Exception('Manifest got no application tag')
+
+        for icon_key in application_elem.attrib.keys():
+            if icon_key.startswith('{') and icon_key.endswith('}icon'):
+                return icon_key.split('{')[1].split('}')[0]
+
+        raise Exception('Couldn\'t find the icon key at AndroidManifest <application> tag')
---
+++ packages/peor/peor-0.0.1-py3-none-any.whl/peor/frida_gadget_util.py
@@ -0,0 +1,104 @@
+import re
+import lzma
+import requests
+from pathlib import Path
+from shutil import copyfile
+from os.path import expanduser
+from typing import Optional, Iterable
+
+
+class FridaGadgetManager:
+    INJECTION_SMALI_CODE = '''
+    const-string v0, "rel-apkmod"
+    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V'''
+
+    def __init__(self, update_gadgets: bool = False, gadget_path: Optional[Path] = None):
+        if update_gadgets:
+            FridaGadgetManager.__download_latest_gadgets(DEFAULT_GADGETS_FOLDER)
+
+        if gadget_path:
+            self.__paths = [gadget_path]
+            return
+
+        DEFAULT_GADGETS_FOLDER = Path(expanduser('~/.frida-gadgets'))
+        DEFAULT_GADGETS_FOLDER.mkdir(parents=True, exist_ok=True)
+
+        self.__paths = list(FridaGadgetManager.__discover_folder_gadgets(DEFAULT_GADGETS_FOLDER))
+
+        if not self.__paths:
+            FridaGadgetManager.__download_latest_gadgets(DEFAULT_GADGETS_FOLDER)
+            self.__paths = list(FridaGadgetManager.__discover_folder_gadgets(DEFAULT_GADGETS_FOLDER))
+
+    def add_gadget_libs(self, decompiled_folder: Path, script_path: Path):
+        for gadget_path in self.__paths:
+            arch_lib_folder = decompiled_folder / 'lib' / FridaGadgetManager.__get_gadget_arch(gadget_path)
+            arch_lib_folder.mkdir(parents=True, exist_ok=True)
+
+            FridaGadgetManager.__write_libs(arch_lib_folder, gadget_path, script_path)
+
+    @staticmethod
+    def __discover_folder_gadgets(gadgets_folder: Path) -> Iterable[Path]:
+        for android_gadget in gadgets_folder.glob('frida-gadget-android-*.so'):
+            yield android_gadget
+
+    @staticmethod
+    def __download_latest_gadgets(gadgets_folder: Path):
+        LATEST_RELEASE_URL = 'https://api.github.com/repos/frida/frida/releases/latest'
+        latest_release_assets = requests.get(LATEST_RELEASE_URL).json()['assets']
+
+        for asset in latest_release_assets:
+            FRIDA_GADGET_ANDROID_REGEX = re.compile(r'frida-gadget-\d+\.\d+\.\d+-android-.*\.so\.xz')
+
+            if FRIDA_GADGET_ANDROID_REGEX.match(asset['name']):
+                compressed_gadget_bytes = requests.get(asset['browser_download_url']).content
+
+                if len(compressed_gadget_bytes) != asset['size']:
+                    raise Exception('Error downloaded size doesn\'t match asset size!')
+
+                gadget_file_name = re.sub(r'\d+\.\d+\.\d+-', '', asset['name'])[:-len('.xz')]
+
+                with open(gadgets_folder / gadget_file_name, 'wb') as gadget_file:
+                    gadget_file.write(lzma.decompress(compressed_gadget_bytes))
+
+    @staticmethod
+    def __get_gadget_arch(gadget_path: Path):
+        KNOWN_ABIS = {
+            'x86': 'x86',
+            'x86_64': 'x86_64',
+            'arm': 'armeabi-v7a',
+            'arm64': 'arm64-v8a'
+        }
+
+        pattern = r".*android-(.+)\.so"
+        match = re.search(pattern, gadget_path.name)
+
+        if not match:
+            raise Exception('Frida gadget path does not meet naming convention (.*android-<processor>.so)')
+
+        arch_abi_name = KNOWN_ABIS.get(match.group(1))
+
+        if not arch_abi_name:
+            raise Exception('Can not find gadget ABI name')
+
+        return arch_abi_name
+
+    @staticmethod
+    def __write_libs(arch_lib_folder: Path, gadget_path: Path, script_path: Path):
+        GADGET_PATH = './librel-apkmod.so'
+        SCRIPT_PATH = './librel-apkmod.js.so'
+        CONFIG_PATH = './librel-apkmod.config.so'
+
+        FRIDA_CONFIGURATIONS = f'''
+{{
+    "interaction":
+    {{
+        "type": "script",
+        "path": "{SCRIPT_PATH}"
+    }}
+}}
+'''
+
+        copyfile(gadget_path, arch_lib_folder / GADGET_PATH)
+        copyfile(script_path, arch_lib_folder / SCRIPT_PATH)
+
+        (arch_lib_folder / CONFIG_PATH).write_text(FRIDA_CONFIGURATIONS)
