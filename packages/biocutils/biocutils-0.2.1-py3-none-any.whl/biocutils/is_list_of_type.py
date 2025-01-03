2a229d9f4c12c155cac7687b0a0fdab8a010fc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 06:04:32 +0000

    Add package biocutils/0.2.1/biocutils-0.2.1-py3-none-any.whl

---
+++ packages/biocutils/biocutils-0.2.1-py3-none-any.whl/biocutils/is_list_of_type.py
@@ -0,0 +1,30 @@
+from typing import Callable, Union
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+
+def is_list_of_type(
+    x: Union[list, tuple], target_type: Callable, ignore_none: bool = False
+) -> bool:
+    """Checks if ``x`` is a list, and whether all elements of the list are of the same type.
+
+    Args:
+        x: A list or tuple of values.
+
+        target_type: Type to check for, e.g. ``str``, ``int``.
+
+        ignore_none: Whether to ignore Nones when comparing to ``target_type``.
+
+    Returns:
+        True if ``x`` is a list or tuple and all elements are of the target
+        type (or None, if ``ignore_none = True``). Otherwise, false.
+    """
+    if not isinstance(x, (list, tuple)):
+        return False
+
+    if not ignore_none:
+        return all(isinstance(item, target_type) for item in x)
+
+    return all((isinstance(item, target_type) or item is None) for item in x)
