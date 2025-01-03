0f7d0566e77c3d4c82d62204839d10c416e1f38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 05:06:17 +0000

    Add package syncmaster-commons/0.0.28/syncmaster_commons-0.0.28.tar.gz

---
+++ packages/syncmaster-commons/syncmaster_commons-0.0.28.tar.gz/syncmaster_commons-0.0.28/syncmaster_commons/gupshup/__init__.py
@@ -0,0 +1,11 @@
+from .incoming_payloads import GupshupIncomingPayLoad
+from .outgoing_payloads import TextPayload, ImagePayload, VideoPayload, AudioPayload, LocationPayload,  GupshupOutgoingPayload  # noqa: F401
+__all__ = ["GupshupIncomingPayLoad"
+                "TextPayload",
+                "ImagePayload",
+                "VideoPayload",
+                "AudioPayload",
+                "LocationPayload",
+                "GupshupOutgoingPayload"
+
+           ]
