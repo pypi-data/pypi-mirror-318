ef0448f5ba49d6292b883a610c3fc960840c9942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:05:09 +0000

    Add package sallmon-core/2.0.3/sallmon_core-2.0.3.tar.gz

---
+++ packages/sallmon-core/sallmon_core-2.0.3.tar.gz/sallmon_core-2.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sallmon-core",
+    version="2.0.3",
+    description="Sallmon Core: a utility chain invented to eat Stripe's lunch",
+    author="c80129b (Andrew Polykandriotis)",
+    author_email="andrew@minakilabs.com",
+    url="https://github.com/minakilabs/sallmon_core",
+    packages=find_packages(exclude=("tests", "docs")),
+    include_package_data=True,
+    install_requires=[
+        "click",       # CLI framework
+        "fastapi",     # WebSocket server
+        "uvicorn",     # ASGI server
+        "websockets",  # WebSocket management
+        "cryptography" # Cryptographic utilities
+    ],
+    entry_points={
+        "console_scripts": [
+            "sallmon=sallmon.cli.main:main",
+            "sallmon-server=sallmon.sallmon_core.server:start_server",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
