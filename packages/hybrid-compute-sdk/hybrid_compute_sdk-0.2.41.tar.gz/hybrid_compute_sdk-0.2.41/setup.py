eb76ab5a64986160ffd6f3c305df83c5a80400fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:38 +0000

    Add package hybrid-compute-sdk/0.2.41/hybrid_compute_sdk-0.2.41.tar.gz

---
+++ packages/hybrid-compute-sdk/hybrid_compute_sdk-0.2.41.tar.gz/hybrid_compute_sdk-0.2.41/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hybrid_compute_sdk",
+    version="0.2.41",
+    packages=find_packages(exclude=["tests", "venv"]),
+    install_requires=[
+        "jsonrpcserver",
+        "aiohttp",
+    ],
+    author="Boba",
+    author_email="",
+    description="A Python SDK for creating JSON-RPC servers with hybrid compute capabilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bobanetwork/aa-hc-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
