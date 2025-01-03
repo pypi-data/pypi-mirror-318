1ee61edde6e8514ab318f1897e4d8b67443469e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:20 +0000

    Add package pynanoinstantmeshes/0.0.3/pynanoinstantmeshes-0.0.3.tar.gz

---
+++ packages/pynanoinstantmeshes/pynanoinstantmeshes-0.0.3.tar.gz/pynanoinstantmeshes-0.0.3/tests/test_remeshing.py
@@ -0,0 +1,55 @@
+import pytest
+import trimesh
+import numpy as np
+
+import pynanoinstantmeshes as PyNIM
+
+
+@pytest.fixture
+def mesh():
+    # Just use a trimesh primitive (Capsule)
+    yield trimesh.primitives.Capsule(radius=1, height=2).to_mesh()
+
+
+@pytest.mark.parametrize(
+    "rosy, posy, expected_vertex_dim",
+    [
+        (2, 4, 4),
+        (4, 4, 4),
+    ],
+)
+@pytest.mark.parametrize("vertex_count", [100, 200])
+@pytest.mark.parametrize("creaseAngle", [0.0, 10.0, 20.0])
+@pytest.mark.parametrize("align_to_boundaries", [True, False])
+@pytest.mark.parametrize("smooth_iter", [0, 2, 4])
+class TestRemeshing:
+    def test_mesh_valid(
+        self,
+        mesh,
+        rosy,
+        posy,
+        expected_vertex_dim,
+        vertex_count,
+        creaseAngle,
+        align_to_boundaries,
+        smooth_iter,
+    ):
+        vertices = np.asarray(mesh.vertices)
+        faces = np.asarray(mesh.faces)
+
+        new_verts, new_faces = PyNIM.remesh(
+            vertices,
+            faces,
+            vertex_count,
+            rosy=rosy,
+            posy=posy,
+            creaseAngle=creaseAngle,
+            align_to_boundaries=align_to_boundaries,
+            smooth_iter=smooth_iter,
+        )
+
+        assert new_verts.shape[-1] == 3
+        if new_verts.shape[0] - 1 != new_faces.max():
+            # Skip test as the meshing failed
+            pytest.skip("Meshing failed")
+        assert new_faces.shape[-1] == expected_vertex_dim
