705a8001d064e755c8cc5db17ee60f05b888d388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:21 +0000

    Add package biopsykit/0.11.0/biopsykit-0.11.0.tar.gz

---
+++ packages/biopsykit/biopsykit-0.11.0.tar.gz/biopsykit-0.11.0/src/biopsykit/utils/exceptions.py
@@ -0,0 +1,34 @@
+"""A set of custom exceptions."""
+
+__all__ = [
+    "DataFrameTransformationError",
+    "EcgProcessingError",
+    "FeatureComputationError",
+    "FileExtensionError",
+    "ValidationError",
+    "ValueRangeError",
+]
+
+
+class ValidationError(Exception):
+    """An error indicating that data-object does not comply with the guidelines."""
+
+
+class FileExtensionError(Exception):
+    """An error indicating that the file name has the wrong file extension."""
+
+
+class DataFrameTransformationError(Exception):
+    """An error indicating that dataframe transformation failed."""
+
+
+class ValueRangeError(Exception):
+    """An error indicating that scores are not in the expected range."""
+
+
+class FeatureComputationError(Exception):
+    """An error indicating that feature computation failed."""
+
+
+class EcgProcessingError(Exception):
+    """An error indicating that ECG processing failed."""
