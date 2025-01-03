d971098dae9a2888efe4ac57eb1d94f3a2db2ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:28 +0000

    Add package insightxcpackage/0.1.0/insightxcpackage-0.1.0.tar.gz

---
+++ packages/insightxcpackage/insightxcpackage-0.1.0.tar.gz/insightxcpackage-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='insightxcpackage',
+version='0.1.0',
+author='Your Name',
+author_email='your.email@efdaxample.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
