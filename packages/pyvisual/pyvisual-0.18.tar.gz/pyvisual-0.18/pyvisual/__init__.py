d76659f824f80b0b896115de2c68c1a5f1437120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:17 +0000

    Add package pyvisual/0.18/pyvisual-0.18.tar.gz

---
+++ packages/pyvisual/pyvisual-0.18.tar.gz/pyvisual-0.18/pyvisual/__init__.py
@@ -0,0 +1,28 @@
+from pyvisual.ui.window import Window
+
+from pyvisual.ui.basic_button_v2 import BasicButton
+from pyvisual.ui.image_button import ImageButton
+from pyvisual.ui.more.basic_text_input import BasicTextInput
+
+from pyvisual.ui.text import Text
+from pyvisual.ui.image import Image
+
+# Shapes
+from pyvisual.ui.circle_shape import CircleShape
+from pyvisual.ui.rectangle_shape import RectangleShape
+from pyvisual.ui.line_shape import LineShape
+
+
+# Layouts
+from pyvisual.ui.group_layout import GroupLayout
+from pyvisual.ui.basic_tab import Tabs
+
+
+from pyvisual.ui.more.basic_menu import BasicMenuBar
+
+
+
+
+
+
+
