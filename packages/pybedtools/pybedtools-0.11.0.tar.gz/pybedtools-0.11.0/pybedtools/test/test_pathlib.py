c483440513b4c33cb2348b0767241ef668efa1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:22 +0000

    Add package pybedtools/0.11.0/pybedtools-0.11.0.tar.gz

---
+++ packages/pybedtools/pybedtools-0.11.0.tar.gz/pybedtools-0.11.0/pybedtools/test/test_pathlib.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+
+import pybedtools
+import pytest
+
+
+def test_pathlib_base():
+    file = "a.bed"
+    fn = os.path.join(pybedtools.filenames.data_dir(), file)
+    path = pathlib.PurePath(fn)
+    assert pybedtools.BedTool(path).fn == fn
+
+
+def test_pathlib_derived():
+    file = "a.bed"
+    fn = os.path.join(pybedtools.filenames.data_dir(), file)
+    path = pathlib.Path(fn)
+    assert pybedtools.BedTool(path).fn == fn
+
+
+# this may be unnecessary, as the check is performed after str conversion
+def test_pathlib_nonexistent_file():
+    fn = os.path.join(pybedtools.filenames.data_dir(), "this_file_is_missing")
+    path = pathlib.Path(fn)
+    with pytest.raises(FileNotFoundError):
+        pybedtools.BedTool(path)
