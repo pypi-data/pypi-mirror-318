f20776ab8bcce130ba8233d3b1c491ed8f5bb74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:11 +0000

    Add package exrio/0.2.0/exrio-0.2.0.tar.gz

---
+++ packages/exrio/exrio-0.2.0.tar.gz/exrio-0.2.0/src/exrio/__init__.py
@@ -0,0 +1,19 @@
+from .image import (
+    PRIMARY_CHROMATICITIES,
+    Chromaticities,
+    Colorspace,
+    ExrChannel,
+    ExrImage,
+    ExrLayer,
+    load,
+)
+
+__all__ = [
+    "load",
+    "ExrImage",
+    "ExrLayer",
+    "ExrChannel",
+    "Chromaticities",
+    "Colorspace",
+    "PRIMARY_CHROMATICITIES",
+]
