fcdad39c5576e0a3ef022d966c42cf2bdf4c9422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:09 +0000

    Add package csc-cia-stne/0.0.34/csc_cia_stne-0.0.34.tar.gz

---
+++ packages/csc-cia-stne/csc_cia_stne-0.0.34.tar.gz/csc_cia_stne-0.0.34/src/csc_cia_stne/utilitarios/functions/__init__.py
@@ -0,0 +1,4 @@
+from .func_titulo import titulo
+from .func_recriar_pastas import recriar_pasta
+from .func_b64 import b64decode, b64encode
+from .func_converters import convert_bquery_result_to_json
