ed2982a2745d4034e3e15891bc3ffba8b42b35cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:31 +0000

    Add package dektools/0.2.14/dektools-0.2.14.tar.gz

---
+++ packages/dektools/dektools-0.2.14.tar.gz/dektools-0.2.14/dektools/file/path.py
@@ -0,0 +1,43 @@
+import os
+import uuid
+
+
+def join_path(*args):
+    return os.path.normpath(os.path.join(*(args[0], *(x.strip('\\/') for x in args[1:]))))
+
+
+def normal_path(path, unix=False):
+    path = os.path.normpath(os.path.abspath(os.path.expanduser(os.fspath(path))))
+    if unix and os.name == 'nt':
+        path = path.replace('\\', '/')
+    return path
+
+
+def new_empty_path(*paths):
+    while True:
+        np = f"{os.path.join(*paths)}.{uuid.uuid4().hex}"
+        if not os.path.exists(np):
+            return np
+
+
+def path_ext(path):
+    filename = os.path.basename(path)
+    file, ext = os.path.splitext(filename)
+    if not ext and file.startswith('.'):
+        return file
+    return ext
+
+
+def multi_ext(path, limit=None):
+    filename = os.path.basename(path)
+    result = []
+    while True:
+        index = filename.rfind('.')
+        if index != -1:
+            result.insert(0, filename[index:])
+            if limit is not None and len(result) >= limit:
+                break
+            filename = filename[:index]
+        else:
+            break
+    return result
