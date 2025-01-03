458f14666ba97a921ccb895ca476a9ec73282a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:22 +0000

    Add package pull-md/0.1.0/pull_md-0.1.0.tar.gz

---
+++ packages/pull-md/pull_md-0.1.0.tar.gz/pull_md-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pull_md",
+    version="0.1.0",
+    packages=find_packages(),
+    url="https://github.com/chigwell/pull_md",
+    license="MIT",
+    author="Eugene Evstafev",
+    author_email="chigwel@gmail.com",
+    description="A simple Python package to convert URLs to Markdown using the pull.md service.",
+    install_requires=[
+        "requests>=2.25.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
