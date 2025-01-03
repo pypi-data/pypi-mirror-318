20fcf1b2ada68b629c68e0b671be03cddbee8a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:07 +0000

    Add package alita-tools/0.1.18/alita_tools-0.1.18.tar.gz

---
+++ packages/alita-tools/alita_tools-0.1.18.tar.gz/alita_tools-0.1.18/src/alita_tools/bitbucket/__init__.py
@@ -0,0 +1,40 @@
+from typing import Dict, List
+from .api_wrapper import BitbucketAPIWrapper
+from .tools import __all__
+from langchain_core.tools import BaseToolkit
+from langchain_core.tools import BaseTool
+
+name = "bitbucket"
+
+
+def get_tools(tool):
+    return AlitaBitbucketToolkit.get_toolkit(
+        url=tool['settings']['url'],
+        project=tool['settings']['project'],
+        repository=tool['settings']['repository'],
+        username=tool['settings']['username'],
+        password=tool['settings']['password'],
+        branch=tool['settings']['branch']
+    ).get_tools()
+
+class AlitaBitbucketToolkit(BaseToolkit):
+    tools: List[BaseTool] = []
+    
+    @classmethod
+    def get_toolkit(cls, selected_tools: list[str] | None = None, **kwargs):
+        if selected_tools is None:
+            selected_tools = []
+        if "cloud" not in kwargs and ("bitbucket.org" in kwargs.get('url')):
+            kwargs["cloud"] = True
+        bitbucket_api_wrapper = BitbucketAPIWrapper(**kwargs)
+        available_tools: List[Dict] = __all__
+        tools = []
+        for tool in available_tools:
+            if selected_tools:
+                if tool['name'] not in selected_tools:
+                    continue
+            tools.append(tool['tool'](api_wrapper=bitbucket_api_wrapper))
+        return cls(tools=tools)
+
+    def get_tools(self):
+        return self.tools
