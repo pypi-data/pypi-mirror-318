5b953607b2d590606e342e2fcd4f063e6ac7d5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:36 +0000

    Add package arpakitlib/1.7.22/arpakitlib-1.7.22.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.7.22.tar.gz/arpakitlib-1.7.22/arpakitlib/_arpakit_project_template/src/db/sqlalchemy_model.py
@@ -0,0 +1,5 @@
+from arpakitlib.ar_sqlalchemy_model_util import SimpleDBM
+from arpakitlib.ar_sqlalchemy_util import get_string_info_from_declarative_base
+
+if __name__ == '__main__':
+    print(get_string_info_from_declarative_base(SimpleDBM))
