3b1c9b7d15bde776d44e20b941d2ebd18437e7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 05:05:43 +0000

    Add package imessage-conversation-analyzer/2.5.0rc3/imessage_conversation_analyzer-2.5.0rc3-py3-none-any.whl

---
+++ packages/imessage-conversation-analyzer/imessage_conversation_analyzer-2.5.0rc3-py3-none-any.whl/ica/analyzers/attachment_totals.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+
+import pandas as pd
+
+import ica
+
+
+def main() -> None:
+    """
+    Generate count data by attachment type, including number of Spotify links
+    shared, YouTube videos, Apple Music, etc.
+    """
+    cli_args = ica.get_cli_parser().parse_args()
+    dfs = ica.get_dataframes(
+        contact_name=cli_args.contact_name, timezone=cli_args.timezone
+    )
+    totals_map = {
+        "gifs": dfs.attachments["mime_type"].eq("image/gif").sum(),
+        "youtube_videos": (
+            dfs.messages["text"]
+            .str.extract(
+                r"(https?://(?:www\.)(?:youtube\.com|youtu\.be)/(?:.*?)(?:\s|$))"
+            )
+            .drop_duplicates()
+            .count()
+            .item()
+        ),
+        "apple_music": (
+            dfs.messages["text"]
+            .str.extract(r"(https?://(?:music\.apple\.com)/(?:.*?)(?:\s|$))")
+            .drop_duplicates()
+            .count()
+            .item()
+        ),
+        "spotify": (
+            dfs.messages["text"]
+            .str.extract(r"(https?://(?:open\.spotify\.com)/(?:.*?)(?:\s|$))")
+            .drop_duplicates()
+            .count()
+            .item()
+        ),
+        "audio_messages": (dfs.attachments["filename"].str.endswith(".caf").sum()),
+        "recorded_videos": (dfs.attachments["mime_type"].eq("video/quicktime").sum()),
+    }
+    ica.output_results(
+        pd.DataFrame({"type": totals_map.keys(), "total": totals_map.values()})
+        .set_index("type")
+        .sort_values(by="total", ascending=False),
+        format=cli_args.format,
+        output=cli_args.output,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/imessage-conversation-analyzer/imessage_conversation_analyzer-2.5.0rc3-py3-none-any.whl/ica/analyzers/most_frequent_emojis.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import importlib.resources
+import json
+import re
+
+import pandas as pd
+
+import ica
+
+# The maximum number of most frequent emojis to output in the table
+EMOJI_DISPLAY_COUNT = 10
+
+
+def get_emoji_list() -> list[str]:
+    """
+    Fetch a list of the most popular emoji on the Web to use as a basis for
+    computing this metric
+    """
+    return json.loads(
+        importlib.resources.files(__package__).joinpath("emojis.json").read_text()
+    )
+
+
+# Output the occurrences of specific emojis
+def main() -> None:
+    """
+    Generates count data for the top 10 most frequently used emojis across the
+    entire conversation
+    """
+    cli_args = ica.get_cli_parser().parse_args()
+    dfs = ica.get_dataframes(
+        contact_name=cli_args.contact_name, timezone=cli_args.timezone
+    )
+    emojis = get_emoji_list()
+    text = dfs.messages.query("is_reaction == False").get("text")
+    emoji_patt = re.compile(
+        f"({'|'.join(re.escape(emoji) for emoji in reversed(emojis))})"
+    )
+    matches = (
+        text.str.findall(emoji_patt).apply(pd.Series).stack().reset_index(drop=True)
+    )
+    ica.output_results(
+        (
+            pd.DataFrame({"emoji": matches.value_counts()})
+            .rename({"emoji": "count"}, axis="columns")
+            .rename_axis("emoji", axis="index")
+            .head(EMOJI_DISPLAY_COUNT)
+        ),
+        format=cli_args.format,
+        output=cli_args.output,
+    )
+
+
+if __name__ == "__main__":
+    main()
