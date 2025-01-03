011ed554ad23d1a9475304ac514448487f8db80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:05 +0000

    Add package lonelypsp/0.0.15/lonelypsp-0.0.15-py3-none-any.whl

---
+++ packages/lonelypsp/lonelypsp-0.0.15-py3-none-any.whl/lonelypsp/compat.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass as fast_dataclass
+import sys
+from functools import partial
+from typing import TypeVar
+
+T = TypeVar("T")
+
+if sys.version_info >= (3, 10):
+
+    def make_dataclass_fast(x: T) -> T:
+        return partial(x, frozen=True, slots=True)  # type: ignore
+
+    fast_dataclass = make_dataclass_fast(fast_dataclass)
+else:
+
+    def make_dataclass_fast(x: T) -> T:
+        return partial(x, frozen=True)  # type: ignore
+
+    fast_dataclass = make_dataclass_fast(fast_dataclass)
