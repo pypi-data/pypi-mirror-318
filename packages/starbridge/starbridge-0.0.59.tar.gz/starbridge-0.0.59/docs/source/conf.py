bfb07d81a308c23cacf0058699474355257f389f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:23 +0000

    Add package starbridge/0.0.59/starbridge-0.0.59.tar.gz

---
+++ packages/starbridge/starbridge-0.0.59.tar.gz/starbridge-0.0.59/docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = "starbridge"
+copyright = "2025, Helmut Hoffer von Ankershoffen"
+author = "Helmut Hoffer von Ankershoffen"
+release = "0.0.59"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_copybutton",
+    "sphinx.ext.coverage",
+    "sphinx_mdinclude",
+    "sphinxext.opengraph",
+    "sphinx_inline_tabs",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_logo = "../../starbridge.png"
