70b3fe9d876f851cc777c1b01b00b338cdeeed31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:48 +0000

    Add package ann8/0.1/ann8-0.1.tar.gz

---
+++ packages/ann8/ann8-0.1.tar.gz/ann8-0.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ann8',  # Package name
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy'],  # List your dependencies (numpy is required for your code)
+    description='A simple neural network implementation',
+    long_description=open('README.md').read(),  # Long description from README.md
+    long_description_content_type='text/markdown',
+    author='wanderer22',
+    author_email='wanderingwolves22@gmail.com',
+    url='https://github.com/yourusername/mllab4',  # GitHub URL for your project (or any hosting service)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
