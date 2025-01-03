36fea496f9e6e85dee1482f48d248432001b3ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:06:00 +0000

    Add package FalconEDA/0.2.2/FalconEDA-0.2.2-py3-none-any.whl

---
+++ packages/FalconEDA/FalconEDA-0.2.2-py3-none-any.whl/falconEDA/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.2"
+__author__ = "Riley Heiman"
+
+
+from .main import run
+#from .app import main  # Expose the main function from app.py
+
+# Optional helper imports for ease of access
+#from .utils import load_excel
+#from .eda import perform_eda
+
