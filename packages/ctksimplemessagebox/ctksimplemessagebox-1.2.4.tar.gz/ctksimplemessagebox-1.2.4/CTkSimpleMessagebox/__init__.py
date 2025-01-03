c4fea088f9db6cb960f4b4563988ddcbfc5eb0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:45 +0000

    Add package ctksimplemessagebox/1.2.4/ctksimplemessagebox-1.2.4.tar.gz

---
+++ packages/ctksimplemessagebox/ctksimplemessagebox-1.2.4.tar.gz/ctksimplemessagebox-1.2.4/CTkSimpleMessagebox/__init__.py
@@ -0,0 +1,4 @@
+from CTkSimpleMessagebox.MessageBoxIcons import *
+from CTkSimpleMessagebox.Messageboxclass import MessageContainer 
+
+__all__ = ["MessageContainer", "MessageBoxIcons"]
---
+++ packages/ctksimplemessagebox/ctksimplemessagebox-1.2.4.tar.gz/ctksimplemessagebox-1.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ctksimplemessagebox',
+    version='1.2.4',
+    author='Scott',
+    author_email='ctksimplemessagebox@gmail.com',
+    description='Python Messagebox to display current Infos or Errors.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NotScottt/CTkSimpleMessageboxes',
+    packages=find_packages(),
+    package_data={
+        '': ['MessageBoxIcons/*.ico'],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
