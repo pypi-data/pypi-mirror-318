24ac92a44e87062ff588712c030f3826e4e0886c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:56 +0000

    Add package pygameplayy/1.0.0/pygameplayy-1.0.0.tar.gz

---
+++ packages/pygameplayy/pygameplayy-1.0.0.tar.gz/pygameplayy-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygameplayy',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pygame'
+        ],
+    include_package_data=True,
+    package_data={
+        '': ['main.py'],
+    },
+    description='A simple pygame player',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='petteer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
