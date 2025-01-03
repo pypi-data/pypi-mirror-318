a66007b880b614562b1446cce1282b85260df6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 15:05:53 +0000

    Add package svnm/0.1.6.5/svnm-0.1.6.5.tar.gz

---
+++ packages/svnm/svnm-0.1.6.5.tar.gz/svnm-0.1.6.5/svnm/config.py
@@ -0,0 +1,39 @@
+modelinfo={
+    "Cifar10":{
+        "filename":"cifar10-80.h5",
+        "repoid":"svnmurali1/cifar10-80",
+        "classes":{0: 'airplane', 
+                    1: 'automobile', 
+                    2: 'bird', 
+                    3: 'cat', 
+                    4: 'deer', 
+                    5: 'dog', 
+                    6: 'frog', 
+                    7: 'horse', 
+                    8: 'ship', 
+                    9: 'truck'},
+        "metrics":{"accuracy":"80%"}
+    },
+    "GenderDetection":{
+        "repoid":"svnmurali1/genderdetection",
+        "filename":"genderdetection.h5",
+        "classes":{
+            0:"female",
+            1:"male"
+        },
+        "metrics":{
+            "accuracy":"90%"
+        }
+    },
+    "FaceDetectionYolo":{
+        "repoid":"svnmurali1/facedetectionyolo",
+        "filename":"best.pt",
+        "classes":{
+            0:"face"
+        },
+        "metrics":{
+            "accuracy":"90%"
+        }
+    }
+    
+}
