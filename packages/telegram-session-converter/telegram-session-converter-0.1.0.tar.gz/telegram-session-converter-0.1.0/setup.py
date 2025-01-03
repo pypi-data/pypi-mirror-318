6d81eae3eaa5e1ceb37aaa6bcfb21991fb8e94e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:59 +0000

    Add package telegram-session-converter/0.1.0/telegram-session-converter-0.1.0.tar.gz

---
+++ packages/telegram-session-converter/telegram-session-converter-0.1.0.tar.gz/telegram-session-converter-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telegram-session-converter",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["telethon", "pydantic"],
+    author="bundemshake",
+    author_email="bundemshake@gmail.com",  # Added comma here
+    description="A simple library for converting .session files from/to telethon or pyrogram format",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Adjust according to your license
+    ],
+)
