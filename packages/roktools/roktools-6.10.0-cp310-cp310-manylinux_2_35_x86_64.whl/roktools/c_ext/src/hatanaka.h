9ba9fac72eaeadec83750a81310dea455102dbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:19 +0000

    Add package roktools/6.10.0/roktools-6.10.0-cp310-cp310-manylinux_2_35_x86_64.whl

---
+++ packages/roktools/roktools-6.10.0-cp310-cp310-manylinux_2_35_x86_64.whl/roktools/c_ext/src/hatanaka.h
@@ -0,0 +1,9 @@
+#ifndef _C_EXT_SRC_HATANAKA_H
+#define _C_EXT_SRC_HATANAKA_H
+
+#include <Python.h>
+
+extern PyObject *read_crx(PyObject* self, PyObject* args, PyObject* kwargs);
+
+#endif  // _C_EXT_SRC_HATANAKA_H
+
