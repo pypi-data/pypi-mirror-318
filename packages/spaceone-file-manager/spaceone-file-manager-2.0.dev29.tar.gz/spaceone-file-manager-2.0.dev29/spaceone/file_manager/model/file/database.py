fa22c540d30053257dcd4638b8e5d4e456d41aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:57 +0000

    Add package spaceone-file-manager/2.0.dev29/spaceone-file-manager-2.0.dev29.tar.gz

---
+++ packages/spaceone-file-manager/spaceone-file-manager-2.0.dev29.tar.gz/spaceone-file-manager-2.0.dev29/spaceone/file_manager/model/file/database.py
@@ -0,0 +1,56 @@
+import logging
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class FileReference(EmbeddedDocument):
+    resource_type = StringField(required=True)
+    resource_id = StringField(required=True)
+
+    def to_dict(self):
+        return dict(self.to_mongo())
+
+
+class File(MongoModel):
+    file_id = StringField(max_length=40, generate_id="file", unique=True)
+    name = StringField(max_length=255, required=True)
+    download_url = StringField(null=True, default=None)
+    tags = DictField()
+    reference = EmbeddedDocumentField(FileReference, null=True, default=None)
+    resource_group = StringField(
+        max_length=40, choices=("SYSTEM", "DOMAIN", "WORKSPACE", "PROJECT")
+    )
+    workspace_id = StringField(max_length=40, null=True, default=None)
+    domain_id = StringField(max_length=40, null=True, default=None)
+    project_id =   StringField(max_length=40, null=True, default=None)
+    created_at = DateTimeField(auto_now_add=True)
+
+    meta = {
+        "updatable_fields": ["download_url", "tags", "reference"],
+        "minimal_fields": [
+            "file_id",
+            "name",
+            "reference",
+            "resource_group",
+            "workspace_id",
+            "domain_id",
+            "project_id",
+        ],
+        "change_query_keys": {
+            "resource_type": "reference.resource_type",
+            "resource_id": "reference.resource_id",
+        },
+        "ordering": ["name"],
+        "indexes": [
+            "file_id",
+            "reference.resource_type",
+            "reference.resource_id",
+            "resource_group",
+            "workspace_id",
+            "project_id",
+            "domain_id",
+        ],
+    }
