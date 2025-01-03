fff9d5fd85c3f9a759ed93f982dfc965e7393b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:10 +0000

    Add package ggpy2/0.0.1/ggpy2-0.0.1.tar.gz

---
+++ packages/ggpy2/ggpy2-0.0.1.tar.gz/ggpy2-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ggpy2",
+    version="0.0.1",
+    description="A grammar of graphics for Python inspired by ggplot2",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
