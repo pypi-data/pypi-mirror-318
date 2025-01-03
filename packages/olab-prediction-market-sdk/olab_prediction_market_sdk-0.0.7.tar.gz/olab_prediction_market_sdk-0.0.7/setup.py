a6d70fcd422744d62a4e0945da2b9880e6a17075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:36 +0000

    Add package olab-prediction-market-sdk/0.0.7/olab_prediction_market_sdk-0.0.7.tar.gz

---
+++ packages/olab-prediction-market-sdk/olab_prediction_market_sdk-0.0.7.tar.gz/olab_prediction_market_sdk-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+NAME = "olab_prediction_market_sdk"
+VERSION = "0.0.7"
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "olab_open_api==0.0.5", "hexbytes~=1.2.1", "web3~=7.6.1", "eth-account"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="OLAB Prediction Market Open API",
+    author="nik.opinionlabs",
+    author_email="nik@opinionlabs.xyz",
+    url="",
+    keywords=["PredictionMarket"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+)
