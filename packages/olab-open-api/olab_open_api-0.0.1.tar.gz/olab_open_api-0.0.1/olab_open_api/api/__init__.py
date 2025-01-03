13899cecd609c26113691f8f3137a008cec3d7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:44 +0000

    Add package olab-open-api/0.0.1/olab_open_api-0.0.1.tar.gz

---
+++ packages/olab-open-api/olab_open_api-0.0.1.tar.gz/olab_open_api-0.0.1/olab_open_api/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.open_api_api import OpenAPIApi
+from openapi_client.api.v2_open_api_api import V2OpenAPIApi
