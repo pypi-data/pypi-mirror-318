1d3ea8672c215764d6fe2a9d195294f021957d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:03:55 +0000

    Add package brunns-builder/1.1.1/brunns_builder-1.1.1-py3-none-any.whl

---
+++ packages/brunns-builder/brunns_builder-1.1.1-py3-none-any.whl/brunns/builder/__init__.py
@@ -0,0 +1,1 @@
+from .base import Builder, a_boolean, a_string, an_integer, method, one_of  # noqa: F401
---
+++ packages/brunns-builder/brunns_builder-1.1.1-py3-none-any.whl/brunns/builder/file.py
@@ -0,0 +1,11 @@
+import logging
+from pathlib import Path
+
+from brunns.builder import Builder, a_string
+
+logger = logging.getLogger(__name__)
+
+
+class PathBuilder(Builder):
+    target = Path
+    args = lambda: [a_string(), a_string()]
