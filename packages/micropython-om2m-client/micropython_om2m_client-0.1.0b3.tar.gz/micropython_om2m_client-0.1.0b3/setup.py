7b3bdd8a260c7a9d61a485072bd024efaa67f2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:05 +0000

    Add package micropython-om2m-client/0.1.0b3/micropython_om2m_client-0.1.0b3.tar.gz

---
+++ packages/micropython-om2m-client/micropython_om2m_client-0.1.0b3.tar.gz/micropython_om2m_client-0.1.0b3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="micropython-om2m-client",
+    version="0.1.0b3",
+    description="A MicroPython client for interacting with OM2M CSE (Work in Progress).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ahmad Hammad, Omar Hourani",
+    author_email="ahmadhammad.uca@gmail.com, omar.hourani@ieee.org",
+    url="https://github.com/SCRCE/micropython-om2m-client",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: Implementation :: MicroPython",
+    ],
+    install_requires=[
+        "urequests>=0.9.0",
+        "ujson>=1.35",
+    ],
+)
