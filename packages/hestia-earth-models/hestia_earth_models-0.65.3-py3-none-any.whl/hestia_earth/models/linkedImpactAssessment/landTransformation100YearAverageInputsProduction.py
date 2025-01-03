51219e4cb94348bf9f4f1256b3b9be8bac18df41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:52 +0000

    Add package hestia-earth-models/0.65.3/hestia_earth_models-0.65.3-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.65.3-py3-none-any.whl/hestia_earth/models/linkedImpactAssessment/landTransformation100YearAverageInputsProduction.py
@@ -0,0 +1,41 @@
+from .utils import run_inputs_production
+
+REQUIREMENTS = {
+    "ImpactAssessment": {
+        "product": {"@type": "Term"},
+        "cycle": {
+            "@type": "Cycle",
+            "products": [{
+                "@type": "Product",
+                "primary": "True",
+                "value": "> 0",
+                "economicValueShare": "> 0"
+            }],
+            "inputs": [{
+                "@type": "Input",
+                "impactAssessment": {
+                    "@type": "ImpactAssessment",
+                    "emissionsResourceUse": [{
+                        "@type": "Indicator",
+                        "term.@id": [
+                            "landTransformation100YearAverageDuringCycle",
+                            "landTransformation100YearAverageInputsProduction"
+                        ],
+                        "value": "> 0"
+                    }]
+                }
+            }]
+        }
+    }
+}
+RETURNS = {
+    "Indicator": [{
+        "value": "",
+        "landCover": "",
+        "previousLandCover": ""
+    }]
+}
+TERM_ID = 'landTransformation100YearAverageInputsProduction'
+
+
+def run(impact_assessment: dict): return run_inputs_production(impact_assessment, TERM_ID)
