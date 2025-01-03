ba87513e69bf565a34fbca7afbf7573ad2adba1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:37 +0000

    Add package capybara-docsaid/0.5.3/capybara_docsaid-0.5.3-py3-none-any.whl

---
+++ packages/capybara-docsaid/capybara_docsaid-0.5.3-py3-none-any.whl/capybara/onnxengine/__init__.py
@@ -0,0 +1,6 @@
+from .engine import Backend, ONNXEngine
+from .metadata import get_onnx_metadata, write_metadata_into_onnx
+from .tools import get_onnx_input_infos, get_onnx_output_infos
+
+# 暫時無法使用
+# from .quantize import quantize, quantize_static
