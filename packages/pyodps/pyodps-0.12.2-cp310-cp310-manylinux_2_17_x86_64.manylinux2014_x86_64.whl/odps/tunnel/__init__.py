78266a09145c1fa6582f46faf908ae5cd6245017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:06:01 +0000

    Add package pyodps/0.12.2/pyodps-0.12.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyodps/pyodps-0.12.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/odps/tunnel/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 1999-2024 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .instancetunnel import InstanceDownloadSession, InstanceTunnel
+from .io import CompressOption
+from .tabletunnel import TableDownloadSession, TableTunnel, TableUploadSession
+from .volumetunnel import (
+    VolumeDownloadSession,
+    VolumeFSTunnel,
+    VolumeTunnel,
+    VolumeUploadSession,
+)
+
+TableUploadStatus = TableUploadSession.Status
+TableDownloadStatus = TableDownloadSession.Status
+VolumeUploadStatus = VolumeUploadSession.Status
+VolumeDownloadStatus = VolumeDownloadSession.Status
+InstanceDownloadStatus = InstanceDownloadSession.Status
