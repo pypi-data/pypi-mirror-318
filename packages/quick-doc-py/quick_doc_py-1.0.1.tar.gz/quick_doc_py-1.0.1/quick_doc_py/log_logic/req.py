6d5e40b99e82cd5be8506a6f54d92904802bab8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 06:04:51 +0000

    Add package quick-doc-py/1.0.1/quick_doc_py-1.0.1.tar.gz

---
+++ packages/quick-doc-py/quick_doc_py-1.0.1.tar.gz/quick_doc_py-1.0.1/quick_doc_py/log_logic/req.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class ReqToServer:
+    def __init__(self, link: str= "https://sdwwwwsvbvgfgfd.pythonanywhere.com"):
+        self.link = link
+
+    def create_session(self) -> str:
+        add = "/create_session"
+        full_link = f"{self.link}{add}"
+
+        responce = requests.post(full_link)
+        return responce.text
+    
+    def add_to_session(self, session_code: str, data: dict) -> None:
+        add = "/add_to_session"
+        full_link = f"{self.link}{add}"
+        new_data = {
+            "session_key": 1,
+        }
+        for key in list(data.keys()):
+            new_data[key] = data[key]
+
+        responce = requests.post(full_link, data=new_data)
+
+
