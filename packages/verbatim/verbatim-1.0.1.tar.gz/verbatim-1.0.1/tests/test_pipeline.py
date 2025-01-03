0175538a7f379a067b7054a245b55448672f6756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:38 +0000

    Add package verbatim/1.0.1/verbatim-1.0.1.tar.gz

---
+++ packages/verbatim/verbatim-1.0.1.tar.gz/verbatim-1.0.1/tests/test_pipeline.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import unittest
+
+from verbatim.audio.sources import FileAudioSource
+from verbatim.config import Config
+from verbatim.verbatim import Verbatim
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+class TestStringMethods(unittest.TestCase):
+    def test_verbatim(self):
+        pass
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s [%(levelname)s][%(filename)s:%(lineno)d][%(funcName)s] %(message)s',
+                        datefmt='%Y-%m-%dT%H:%M:%SZ')
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Set CUDA_VISIBLE_DEVICES to -1 to use CPU
+
+    print(os.getcwd())
+    config:Config = Config()
+    config.source_stream = FileAudioSource("tests/data/init.mp3")
+    config.lang = ["fr", "en"]
+    config.device = "cpu"
+    verbatim:Verbatim = Verbatim(config=config)
+    for utterance in verbatim.transcribe():
+        print(utterance.text)
