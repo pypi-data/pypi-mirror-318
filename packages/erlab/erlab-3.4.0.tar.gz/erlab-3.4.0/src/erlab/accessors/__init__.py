8b981f1d35ad29670899864843688f1202903882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:52 +0000

    Add package erlab/3.4.0/erlab-3.4.0.tar.gz

---
+++ packages/erlab/erlab-3.4.0.tar.gz/erlab-3.4.0/src/erlab/accessors/__init__.py
@@ -0,0 +1,22 @@
+"""
+Some `xarray accessors
+<https://docs.xarray.dev/en/stable/internals/extending-xarray.html>`_ for convenient
+data analysis and visualization.
+
+ERLabPy provides a collection of accessors for convenient data analysis and
+visualization. All methods and attributes are documented in the following sections.
+
+.. currentmodule:: erlab.accessors
+
+Modules
+=======
+
+.. autosummary::
+   :toctree: generated
+
+   utils
+   general
+   kspace
+   fit
+
+"""  # noqa: D205
