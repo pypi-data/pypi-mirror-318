7914b6f4ef00cfddbb69f8314424bdbe01f932a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 14:05:26 +0000

    Add package atorch/0.2.2/atorch-0.2.2-py3-none-any.whl

---
+++ packages/atorch/atorch-0.2.2-py3-none-any.whl/atorch/ops/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version = '0.2.2+052760ad'
+git_hash = '052760ad'
+git_branch = '0.2.2'
+installed_ops = {'quantization_optimizer': False, 'quantizer': False}
+compatible_ops = {'quantization_optimizer': True, 'quantizer': True, 'atorch_not_implemented': False}
+torch_info = {'version': '1.13', 'bf16_support': False, 'cuda_version': '11.6', 'nccl_version': '2.14'}
