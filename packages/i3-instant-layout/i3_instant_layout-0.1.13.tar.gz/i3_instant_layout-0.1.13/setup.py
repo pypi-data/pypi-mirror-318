9f1ae6b00107a404ffe7484a5546760071073c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 04:04:05 +0000

    Add package i3-instant-layout/0.1.13/i3_instant_layout-0.1.13.tar.gz

---
+++ packages/i3-instant-layout/i3_instant_layout-0.1.13.tar.gz/i3_instant_layout-0.1.13/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Setup file for i3-instant-layout.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 3.2.3.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+import sys
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
