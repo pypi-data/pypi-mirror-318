80e3db44fc0fcf79cdde4a93b7d23e7c1ede48ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:29 +0000

    Add package wikitoolkit/0.0.7/wikitoolkit-0.0.7-py3-none-any.whl

---
+++ packages/wikitoolkit/wikitoolkit-0.0.7-py3-none-any.whl/wikitoolkit/__init__.py
@@ -0,0 +1,10 @@
+from .api import *
+# from .dumps import *
+from .tools import *
+from .pageviews import *
+from .redirects import *
+from .revisions import *
+from .quality import *
+from .links import *
+from .topics import *
+from .clickstreams import *
