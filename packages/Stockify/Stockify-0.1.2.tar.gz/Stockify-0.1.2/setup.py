2a78ca7d2461541f0e51a4851485939148985216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 16:05:23 +0000

    Add package Stockify/0.1.2/Stockify-0.1.2.tar.gz

---
+++ packages/Stockify/Stockify-0.1.2.tar.gz/Stockify-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Stockify",
+    version="0.1.2",
+    author="Rayansh Singh",
+    author_email="rayansh365@gmail.com",
+    description="A Python library for financial data analysis and portfolio management",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ray-singh/Stockify",
+    packages=find_packages(exclude=["tests*", "tests.*"]),  # Exclude tests folder
+    install_requires=[
+        "yfinance",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "datetime",
+        "seaborn"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
