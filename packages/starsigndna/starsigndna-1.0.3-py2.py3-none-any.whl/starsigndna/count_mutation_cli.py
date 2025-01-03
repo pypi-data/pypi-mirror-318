03e7a426a1273687d486e45bacb546e6c8dc85b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 03:08:54 +0000

    Add package starsigndna/1.0.3/starsigndna-1.0.3-py2.py3-none-any.whl

---
+++ packages/starsigndna/starsigndna-1.0.3-py2.py3-none-any.whl/starsigndna/count_mutation_cli.py
@@ -0,0 +1,38 @@
+import bionumpy as bnp
+import numpy as np
+from bionumpy.variants import count_mutation_types_genomic
+from bionumpy.io.vcf_buffers import PhasedVCFMatrixBuffer
+from bionumpy.io.matrix_dump import matrix_to_csv
+from bionumpy import Genome
+import logging
+logging.basicConfig(level="INFO")
+
+
+def count_mutation(vcf_filename: str, fasta_filename: str, out_filename: str = None, numeric_chromosomes: bool = True, genotyped: bool= True):
+    """
+    Count mutation types in a VCF file.
+
+    Parameters
+    ----------
+    vcf_filename: str
+        VCF file of the variants (possibly many samples)
+    fasta_filename: str
+        Fasta file of the reference genome
+    out_filename: str
+    numeric_chromosomes: bool
+        True if chromosome names in vcf are '1', '2', '3'. False if 'chr1', 'chr2', 'chr3'
+    genotyped: bool
+        True if the VCF file has genotype information for many samples
+
+    """
+    buffer_type = PhasedVCFMatrixBuffer if genotyped else None
+    genome = Genome.from_file(fasta_filename)
+    variants = np.concatenate(list(bnp.open(vcf_filename, buffer_type=buffer_type).read_chunks()))
+    variants = genome.get_locations(variants, has_numeric_chromosomes=numeric_chromosomes)
+    counts = count_mutation_types_genomic(variants, genome.read_sequence(), genotyped=genotyped)
+    if out_filename is not None:
+        output = matrix_to_csv(counts.counts.astype(int), header=counts.alphabet, sep='\t')
+        open(out_filename, "wb").write(bytes(output.raw()))
+    else:
+        print(counts)
+    return counts
---
+++ packages/starsigndna/starsigndna-1.0.3-py2.py3-none-any.whl/starsigndna/refit.py
@@ -0,0 +1,58 @@
+import numpy as np
+import warnings
+import math
+from scipy.stats import poisson, entropy
+from numpy import count_nonzero
+from .main_fixed_denovo import Frobinous, Frobinous_reconstuct, running_simulation_refit
+from scipy import stats
+def refit(M: np.ndarray, S: np.ndarray, O: np.ndarray=None, lambd: float = int, n_iterations: int=1000) -> np.ndarray:
+    '''
+    Refit the signatures to the data
+    M: Matrix of observed mutational signatures
+    S: Mutational Signatures matrix from COSMIC
+    O: Matrix of mutational opportunity
+    lambd: a penalty parameter that control the rate trade-off between goodness of fit and L-1 norm of the solution
+    '''
+    warnings.filterwarnings('ignore')
+    np.random.seed(1000)
+    n_samples = len(M)
+    n_signatures = len(S)
+    n_mutations = 96
+    if O is None:
+        O = np.ones((n_samples, n_mutations), dtype=int)
+    elif O.shape !=M.shape:
+        O = np.tile(O, (M.shape[0], 1))
+    M, S, O = (np.asarray(a) for a in (M, S, O))
+#    n_mutations = 96
+    tmp = np.abs(np.random.laplace(loc=0, scale=1, size=n_samples * n_signatures).reshape(n_samples, n_signatures))
+    E = np.full_like(tmp, 1)
+    topt = np.float64("Inf")
+    # topt= -math.inf
+    tedge = np.float64("Inf")
+    expose_mat = []
+    pmf_s = []
+    mse_s = []
+    expo_step = []
+    E = running_simulation_refit(E, M, S, O, topt, tedge, lambd, n_steps=n_iterations)
+    if (np.any(E < 0)):
+        E = np.maximum(E, 0.00001)
+    mse_e = Frobinous(M, S, E, O)
+    loss = -poisson.logpmf(M, (E @ S) * O)
+    loss[loss == np.float64("Inf")] = 0
+    # E[E<=1e-6]=0
+    E /= E.sum(axis=-1, keepdims=True)
+    E[np.isnan(E)] = 0
+    sparsity = 1.0 - ( count_nonzero(E) / float(E.size) )
+    E_norm = np.linalg.norm(E, ord='fro', axis=None, keepdims=False)
+    mse_reconstruct, M_hat = Frobinous_reconstuct(M, S, E, O)
+    print("Sparsity", sparsity)
+    print("The MSE is:", np.mean(mse_e))
+    #print("E_norm",E_norm)
+    print("PMF",np.mean(loss))
+    loss_hat = np.mean(loss)
+    return E
+
+
+
+
+
