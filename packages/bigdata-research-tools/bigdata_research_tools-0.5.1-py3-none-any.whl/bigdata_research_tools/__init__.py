f3e0bcc302808fc2564776c63a30a80abea30d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 06:04:57 +0000

    Add package bigdata-research-tools/0.5.1/bigdata_research_tools-0.5.1-py3-none-any.whl

---
+++ packages/bigdata-research-tools/bigdata_research_tools-0.5.1-py3-none-any.whl/bigdata_research_tools/__init__.py
@@ -0,0 +1,8 @@
+"""
+Bigdata.com Research Tools
+
+Copyright (C) 2024, RavenPack | Bigdata.com. All rights reserved.
+Author: Shawn Azdam (sazdam@ravenpack.com)
+"""
+
+from .search import RateLimitedSearchManager, run_search
