bf1d9e5deed3df84e00d785415ba3a824b483e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-03 05:04:47 +0000

    Add package deeptensor/0.0.1/deeptensor-0.0.1.tar.gz

---
+++ packages/deeptensor/deeptensor-0.0.1.tar.gz/deeptensor-0.0.1/README.md
@@ -0,0 +1,5 @@
+# DeepTensor
+
+```bash
+pip install deeptensor
+```
---
+++ packages/deeptensor/deeptensor-0.0.1.tar.gz/deeptensor-0.0.1/tests/test_add.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import deeptensor as dt
+
+
+def test_add():
+    assert dt.add(1, 2) == 3
