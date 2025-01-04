b58ba0cdb37ec9ff84fd1d38b58ba4d66b52dec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:43 +0000

    Add package lesscode-flask/0.0.84/lesscode-flask-0.0.84.tar.gz

---
+++ packages/lesscode-flask/lesscode-flask-0.0.84.tar.gz/lesscode-flask-0.0.84/lesscode_flask/service/auth_permission_service.py
@@ -0,0 +1,7 @@
+# from lesscode_flask.model.auth_permission import AuthPermission
+# from lesscode_flask.service.base_service import BaseService
+#
+#
+# class AuthPermissionService(BaseService):
+#     def __init__(self):
+#         super().__init__(AuthPermission)
