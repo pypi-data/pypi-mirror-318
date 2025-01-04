d20bdccb5286d0ded8335f40e980cb3ccfd5cf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:05:50 +0000

    Add package excalibur-py/1.0.1/excalibur_py-1.0.1-py3-none-any.whl

---
+++ packages/excalibur-py/excalibur_py-1.0.1-py3-none-any.whl/excalibur/www/app.py
@@ -0,0 +1,19 @@
+import json
+
+from flask import Flask
+
+from .. import configuration as conf
+from .views import views
+
+
+def to_pretty_json(value):
+    value = json.loads(value)
+    return json.dumps(value, sort_keys=True, indent=4, separators=(",", ": "))
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+    app.config.from_object(conf)
+    app.register_blueprint(views)
+    app.jinja_env.filters["pretty"] = to_pretty_json
+    return app
