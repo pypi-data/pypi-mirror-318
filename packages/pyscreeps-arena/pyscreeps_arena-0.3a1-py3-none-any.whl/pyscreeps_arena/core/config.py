74f666b3d28977dfbbd3abed40f9125485dae559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:04:38 +0000

    Add package pyscreeps-arena/0.3a1/pyscreeps_arena-0.3a1-py3-none-any.whl

---
+++ packages/pyscreeps-arena/pyscreeps_arena-0.3a1-py3-none-any.whl/pyscreeps_arena/core/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# pyscreeps_arena - config.py
+# Author: 我阅读理解一直可以的
+# Template: V0.1
+# Versions:
+# .2025 01 01 - v0.1:
+#   Created.
+#
+
+from pyscreeps_arena.core import const
+
+language = 'cn'
