a60e4e045f937a71728fa59e807ea998f0cd1eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:18 +0000

    Add package ucsc-cse40/0.11.0/ucsc_cse40-0.11.0.tar.gz

---
+++ packages/ucsc-cse40/ucsc_cse40-0.11.0.tar.gz/ucsc_cse40-0.11.0/setup.py
@@ -0,0 +1,50 @@
+import os
+import setuptools
+
+THIS_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+LICENSE_PATH = os.path.join(THIS_DIR, 'LICENSE.txt')
+LONG_DESCRIPTION_PATH = os.path.join(THIS_DIR, 'README.md')
+REQUIREMENTS_PATH = os.path.join(THIS_DIR, 'requirements.txt')
+
+def get_description():
+    with open(LONG_DESCRIPTION_PATH, 'r') as file:
+        return file.read()
+
+setuptools.setup(
+    name = 'ucsc-cse40',
+    url = 'https://github.com/ucsc-cse-40/ucsc-cse40',
+
+    version = '0.11.0',
+    keywords = 'grading',
+
+    description = "Dependencies for UCSC's CSE 40 - ML Basics: Data Analysis and Empirical Methods",
+    long_description = get_description(),
+    long_description_content_type = 'text/markdown',
+
+
+    maintainer = 'Eriq Augustine',
+    maintainer_email = 'eaugusti@ucsc.edu',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    packages = setuptools.find_packages(),
+
+    install_requires = [
+        'jupyterlab==4.2.*',
+        'matplotlib==3.7.*',
+        'numpy>=1.24.0',
+        'pandas==2.0.*',
+        'scikit-learn==1.3.*',
+        'autograder-py==0.5.*',
+    ],
+
+    license_files = (LICENSE_PATH, ),
+
+    python_requires = '>=3.8'
+)
