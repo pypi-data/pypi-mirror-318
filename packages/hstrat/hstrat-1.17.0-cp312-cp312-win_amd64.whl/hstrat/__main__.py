e7f72235e2c67a96c16a06ac8213163364d474f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:53 +0000

    Add package hstrat/1.17.0/hstrat-1.17.0-cp312-cp312-win_amd64.whl

---
+++ packages/hstrat/hstrat-1.17.0-cp312-cp312-win_amd64.whl/hstrat/__main__.py
@@ -0,0 +1,18 @@
+from ._auxiliary_lib import get_hstrat_version
+
+if __name__ == "__main__":
+    print(f"hstrat v{get_hstrat_version()}")
+    print()
+    print("Available commands (stabilized API):")
+    print("$ python3 -m hstrat.dataframe.surface_unpack_reconstruct")
+    print("$ python3 -m hstrat.dataframe.surface_postprocess_trie")
+    print()
+    print("Available commands (experimental API):")
+    print("$ python3 -m hstrat._auxiliary_lib._alifestd_as_newick_asexual")
+    print(
+        "$ python3 -m hstrat._auxiliary_lib._alifestd_try_add_ancestor_list_col"
+    )
+    print()
+    print(
+        "For information on a command, " "invoke it with the --help flag.",
+    )
