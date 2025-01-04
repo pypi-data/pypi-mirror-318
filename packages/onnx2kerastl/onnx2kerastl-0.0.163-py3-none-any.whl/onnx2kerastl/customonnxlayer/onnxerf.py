f845f52346d9f07f164fb539c743e9d129748c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:49 +0000

    Add package onnx2kerastl/0.0.163/onnx2kerastl-0.0.163-py3-none-any.whl

---
+++ packages/onnx2kerastl/onnx2kerastl-0.0.163-py3-none-any.whl/onnx2kerastl/customonnxlayer/onnxerf.py
@@ -0,0 +1,11 @@
+from keras.src.layers import Layer
+import tensorflow as tf
+
+
+class OnnxErf(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        x = tf.math.erf(inputs)
+        return x
