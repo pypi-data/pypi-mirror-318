45f117a239cdf057e8371e29e6f3bc52908607e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:34 +0000

    Add package fastled/1.2.15/fastled-1.2.15.tar.gz

---
+++ packages/fastled/fastled-1.2.15.tar.gz/fastled-1.2.15/tests/test_http_server.py
@@ -0,0 +1,42 @@
+"""
+Unit test file.
+"""
+
+import time
+import unittest
+from pathlib import Path
+
+import httpx
+
+from fastled import Test
+
+HERE = Path(__file__).parent
+INDEX_HTML = HERE / "html" / "index.html"
+TIMEOUT = 120
+
+assert INDEX_HTML.exists()
+
+
+class HttpServerTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_http_server(self) -> None:
+        """Test the http server."""
+        proc = Test.spawn_http_server(INDEX_HTML.parent, port=8081, open_browser=False)
+        time.sleep(1)
+        future_time = time.time() + TIMEOUT
+        while future_time > time.time():
+            try:
+                response = httpx.get("http://localhost:8081", timeout=1)
+                break
+            except Exception:
+                continue
+        else:
+            self.fail("Could not connect to server")
+        response = httpx.get("http://localhost:8081", timeout=5)
+        self.assertEqual(response.status_code, 200)
+        proc.kill()
+
+
+if __name__ == "__main__":
+    unittest.main()
