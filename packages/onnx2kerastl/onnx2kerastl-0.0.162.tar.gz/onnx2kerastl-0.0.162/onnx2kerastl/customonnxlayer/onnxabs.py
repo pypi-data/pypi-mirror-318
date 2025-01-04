4ad9fa9a6d27ced8696b4ca82ac3cd7763909a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:12 +0000

    Add package onnx2kerastl/0.0.162/onnx2kerastl-0.0.162.tar.gz

---
+++ packages/onnx2kerastl/onnx2kerastl-0.0.162.tar.gz/onnx2kerastl-0.0.162/onnx2kerastl/customonnxlayer/onnxabs.py
@@ -0,0 +1,11 @@
+from keras.src.layers import Layer
+import tensorflow as tf
+
+
+class OnnxAbs(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        x = tf.math.abs(inputs)
+        return x
