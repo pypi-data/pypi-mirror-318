bee845536f4e1ff0d4d6e7d1ec9dcc6eba8be851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:20 +0000

    Add package arkas/0.0.1a7/arkas-0.0.1a7-py3-none-any.whl

---
+++ packages/arkas/arkas-0.0.1a7-py3-none-any.whl/arkas/state/__init__.py
@@ -0,0 +1,9 @@
+r"""Contain states."""
+
+from __future__ import annotations
+
+__all__ = ["AccuracyState", "BaseState", "PrecisionRecallState"]
+
+from arkas.state.accuracy import AccuracyState
+from arkas.state.base import BaseState
+from arkas.state.precision_recall import PrecisionRecallState
