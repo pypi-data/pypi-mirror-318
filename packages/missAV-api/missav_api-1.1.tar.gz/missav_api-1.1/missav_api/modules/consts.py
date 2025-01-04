78f8d8d215222a6ff5bd4935b6a8c40257398ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:38 +0000

    Add package missAV-api/1.1/missav_api-1.1.tar.gz

---
+++ packages/missAV-api/missav_api-1.1.tar.gz/missav_api-1.1/missav_api/modules/consts.py
@@ -0,0 +1,11 @@
+import re
+
+HEADERS = {
+    "Referer": "https://missav.com/dm9/en",
+}
+
+
+regex_title = re.compile(r'<h1 class="text-base lg:text-lg text-nord6">(.*?)</h1>')
+regex_video_code = re.compile(r'<span class="font-medium">(.*?)</span>')
+regex_publish_date = re.compile(r'class="font-medium">(.*?)</time>')
+regex_m3u8_js = re.compile(r"'m3u8(.*?)video")
