94f1b297d301f4cb13b99ad1b19f5ac0e4a861b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:05:15 +0000

    Add package SoftwareAI/0.4.9/SoftwareAI-0.4.9-py3-none-any.whl

---
+++ packages/SoftwareAI/SoftwareAI-0.4.9-py3-none-any.whl/softwareai/CoreApp/SoftwareAI/Functions_Submit_Outputs/autosave_submit_outputs.py
@@ -0,0 +1,37 @@
+
+#########################################
+# IMPORT SoftwareAI Libs 
+from softwareai.CoreApp._init_libs_ import *
+#########################################
+# IMPORT SoftwareAI Functions
+from softwareai.CoreApp.SoftwareAI.Functions._init_functions_ import *
+#########################################
+
+def submit_output_autosave(function_name,
+                                function_arguments,
+                                tool_call,
+                                threead_id,
+                                client,
+                                run
+                                ):
+
+
+    if function_name == 'autosave':
+        args = json.loads(function_arguments)
+        result = autosave(
+            code=args['code'],
+            path=args['path']
+        )
+        tool_call_id = tool_call.id
+        client.beta.threads.runs.submit_tool_outputs(
+            thread_id=threead_id,
+            run_id=run.id,
+            tool_outputs=[
+                {
+                    "tool_call_id": tool_call_id, 
+                    "output": json.dumps(result)  
+                }
+            ]
+        )
+
+
