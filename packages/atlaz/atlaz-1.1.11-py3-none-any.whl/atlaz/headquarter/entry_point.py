a22399a461c522b937527157fd5c1c0548d6c7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:30 +0000

    Add package atlaz/1.1.11/atlaz-1.1.11-py3-none-any.whl

---
+++ packages/atlaz/atlaz-1.1.11-py3-none-any.whl/atlaz/headquarter/entry_point.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import sys
+import time
+import threading
+from atlaz.frontend.python_backend.flask_server import app
+from atlaz.headquarter.client import AtlazClient
+
+def start_app_server():
+    """Starts the Flask app server."""
+    subprocess.run(["python", "-c", "from atlaz.frontend.python_backend.flask_server import app; app.run(debug=True, port=5050)"])
+
+def start_flask_server():
+    """Starts the Flask server."""
+    app.run(debug=True, port=5050)
+
+def start_frontend_client():
+    """Starts the AtlazClient."""
+    client = AtlazClient()
+    client.start_frontend()
+
+def start_full_chain():
+    app_thread = threading.Thread(target=start_flask_server)
+    app_thread.daemon = True
+    app_thread.start()
+    time.sleep(3)
+    start_frontend_client()
+
+def main():
+    cwd = os.getcwd()
+    if cwd not in sys.path:
+        sys.path.insert(0, cwd) # Adjust this to match your actual logic
+    start_full_chain()
+
+if __name__ == "__main__":
+    main()
