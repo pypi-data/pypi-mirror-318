11546d36151d1af4b8df517fa8df2fc32549e603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:29 +0000

    Add package jupyterlite-pyodide-lock-webdriver/0.1.1/jupyterlite_pyodide_lock_webdriver-0.1.1-py3-none-any.whl

---
+++ packages/jupyterlite-pyodide-lock-webdriver/jupyterlite_pyodide_lock_webdriver-0.1.1-py3-none-any.whl/jupyterlite_pyodide_lock_webdriver/__init__.py
@@ -0,0 +1,7 @@
+"""A WebDriver locker strategy for ``jupyterlite-pyodide-lock``."""
+# Copyright (c) jupyterlite-pyodide-lock contributors.
+# Distributed under the terms of the BSD-3-Clause License.
+
+__version__ = __import__("importlib.metadata").metadata.version(
+    "jupyterlite-pyodide-lock-webdriver"
+)
