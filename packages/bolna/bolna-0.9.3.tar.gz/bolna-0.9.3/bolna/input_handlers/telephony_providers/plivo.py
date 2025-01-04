9b16cef23b48eaed5294c91517884697ac6188cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 06:05:10 +0000

    Add package bolna/0.9.3/bolna-0.9.3.tar.gz

---
+++ packages/bolna/bolna-0.9.3.tar.gz/bolna-0.9.3/bolna/input_handlers/telephony_providers/plivo.py
@@ -0,0 +1,26 @@
+import os
+import plivo as plivosdk
+from dotenv import load_dotenv
+from bolna.input_handlers.telephony import TelephonyInputHandler
+from bolna.helpers.logger_config import configure_logger
+
+logger = configure_logger(__name__)
+load_dotenv()
+
+
+class PlivoInputHandler(TelephonyInputHandler):
+    def __init__(self, queues, websocket=None, input_types=None, mark_set=None, turn_based_conversation=False):
+        super().__init__(queues, websocket, input_types, mark_set, turn_based_conversation)
+        self.io_provider = 'plivo'
+        self.client = plivosdk.RestClient(os.getenv('PLIVO_AUTH_ID'), os.getenv('PLIVO_AUTH_TOKEN'))
+
+    async def call_start(self, packet):
+        start = packet['start']
+        self.call_sid = start['callId']
+        self.stream_sid = start['streamId']
+
+    async def disconnect_stream(self):
+        try:
+            self.client.calls.delete_all_streams(self.call_sid)
+        except Exception as e:
+            logger.info('Error deleting plivo stream: {}'.format(str(e)))
---
+++ packages/bolna/bolna-0.9.3.tar.gz/bolna-0.9.3/bolna/output_handlers/default.py
@@ -0,0 +1,56 @@
+import base64
+from dotenv import load_dotenv
+from bolna.helpers.logger_config import configure_logger
+
+logger = configure_logger(__name__)
+load_dotenv()
+
+
+class DefaultOutputHandler:
+    def __init__(self, io_provider='default', websocket=None, queue=None):
+        self.websocket = websocket
+        self.is_interruption_task_on = False
+        self.queue = queue
+        self.io_provider = io_provider
+        self.is_chunking_supported = True
+        self.is_last_hangup_chunk_sent = False
+        self.is_welcome_message_sent = False
+
+    # @TODO Figure out the best way to handle this
+    async def handle_interruption(self):
+        logger.info("#######   Sending interruption message ####################")
+        response = {"data": None, "type": "clear"}
+        await self.websocket.send_json(response)
+
+    def process_in_chunks(self, yield_chunks=False):
+        return self.is_chunking_supported and yield_chunks
+
+    def get_provider(self):
+        return self.io_provider
+
+    def hangup_sent(self):
+        return self.is_last_hangup_chunk_sent
+
+    def welcome_message_sent(self):
+        return self.is_welcome_message_sent
+
+    async def handle(self, packet):
+        try:
+            logger.info(f"Packet received:")
+            data = None
+            if packet["meta_info"]['type'] in ('audio', 'text'):
+                if packet["meta_info"]['type'] == 'audio':
+                    logger.info(f"Sending audio")
+                    data = base64.b64encode(packet['data']).decode("utf-8")
+                elif packet["meta_info"]['type'] == 'text':
+                    logger.info(f"Sending text response {packet['data']}")
+                    data = packet['data']
+
+                logger.info(f"Sending to the frontend {len(data)}")
+                response = {"data": data, "type": packet["meta_info"]['type']}
+                await self.websocket.send_json(response)
+
+            else:
+                logger.error("Other modalities are not implemented yet")
+        except Exception as e:
+            logger.error(f"something went wrong in speaking {e}")
