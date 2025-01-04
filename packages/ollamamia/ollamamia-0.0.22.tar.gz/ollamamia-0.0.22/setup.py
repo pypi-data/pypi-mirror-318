7e92e7f92178afc4a38f1236233c4d190380c47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:04:38 +0000

    Add package ollamamia/0.0.22/ollamamia-0.0.22.tar.gz

---
+++ packages/ollamamia/ollamamia-0.0.22.tar.gz/ollamamia-0.0.22/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ollamamia',
+    version='0.0.22',
+    packages=find_packages(where='src'),
+    package_dir={'ollamamia': 'src'},
+    py_modules=["agent_store", "models_manager", "ollamamia"],
+    exclude_package_data={'': ['.env']},
+    install_requires=[
+        'ollama==0.4.4',
+        'tqdm==4.66.4',
+        'readchar==4.0.5',
+        'termcolor==2.5.0',
+        "setuptools==69.5.1"
+    ],
+)
