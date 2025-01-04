907febf2fc71e92bebd235a8c7ba6efda45af5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 06:05:02 +0000

    Add package apimagic/0.0.1/apimagic-0.0.1.tar.gz

---
+++ packages/apimagic/apimagic-0.0.1.tar.gz/apimagic-0.0.1/apimagic/__init__.py
@@ -0,0 +1,7 @@
+
+import numpy
+import numpy as np 
+import pandas as pd 
+
+from .jtools1 import Sorter  # Assuming you have a class named Sorter in jtools1.py
+from .jtools2 import Adder   # Assuming you have a class named Adder  in jtools2.py
