4061982f09971c81be47722c6c5f4d60f3e124b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:59 +0000

    Add package dtob/1.1.1/dtob-1.1.1.tar.gz

---
+++ packages/dtob/dtob-1.1.1.tar.gz/dtob-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dtob",
+    version="1.1.1",
+    description="Convert dictionaries into objects with attribute-style access.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/husseinnaeemsec/dicttoobject",
+    author="Hussein Naeem",
+    author_email="husseinnaeemsec@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
