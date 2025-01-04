375cf94d7909dbd3be07b078794a5a9ed006016e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:17 +0000

    Add package padoru/0.1.0/padoru-0.1.0.tar.gz

---
+++ packages/padoru/padoru-0.1.0.tar.gz/padoru-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="padoru",
+    version="0.1.0",
+    license="MIT",
+    description="Hashire sori yo, kaze no you ni, tsukimihara wo, padoru padoru !",
+    author="BakaTaida",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
