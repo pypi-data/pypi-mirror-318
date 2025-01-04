2c0a9745203c014577c02e50373534a7a055f022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:51 +0000

    Add package biox-sdk/0.0.1/Biox_SDK-0.0.1-py3-none-any.whl

---
+++ packages/biox-sdk/Biox_SDK-0.0.1-py3-none-any.whl/samples/sample1.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from biox.ble.scanner import BluetoothScanner
+from biox.collector.collector import Collector
+
+if __name__ == '__main__':
+    async def main():
+        ble_device = None
+        while ble_device is None:
+            devices = await BluetoothScanner.scan()
+            for device in devices:
+                if device and device.device.name and "Biox" in device.device.name:
+                    ble_device = device
+                    break
+
+        collector = Collector(ble_device)
+        await collector.register_notify()
+        await collector.start()
+        await collector.stop_data_collection()
+        await collector.start_data_collection()
+        await collector.wait_for_stop()
+
+
+    asyncio.run(main())
---
+++ packages/biox-sdk/Biox_SDK-0.0.1-py3-none-any.whl/samples/sample2.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from biox.ble.scanner import BluetoothScanner
+
+if __name__ == '__main__':
+    async def main():
+        ble_device = None
+        while ble_device is None:
+            devices = await BluetoothScanner.scan()
+            for device in devices:
+                if device and device.device.name and "Biox" in device.device.name:
+                    ble_device = device
+                    break
+
+        resp = await ble_device.check_device_work_status()
+        print(resp)
+
+
+    asyncio.run(main())
