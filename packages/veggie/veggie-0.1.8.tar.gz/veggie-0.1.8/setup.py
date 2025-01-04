c93928a16f7796629c6964cac120d34e36399e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:05:08 +0000

    Add package veggie/0.1.8/veggie-0.1.8.tar.gz

---
+++ packages/veggie/veggie-0.1.8.tar.gz/veggie-0.1.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="veggie",
+    version="0.1.8",
+    author="Theodore Tsitsimis",
+    description="A tool to monitor and execute Celery tasks",
+    packages=find_packages(),
+    install_requires=[
+        "dash>=2.5.1",
+        "dash-extensions>=1.0.19",
+        "dash-mantine-components>=0.15.1",
+        "dash-iconify",
+        "loguru",
+        "celery[redis]",
+        "humanize",
+    ],
+)
