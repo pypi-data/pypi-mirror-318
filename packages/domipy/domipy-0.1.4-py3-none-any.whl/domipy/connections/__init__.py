ace0f25eca85834d82a37c2a7e7d8c68e3404618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:55 +0000

    Add package domipy/0.1.4/domipy-0.1.4-py3-none-any.whl

---
+++ packages/domipy/domipy-0.1.4-py3-none-any.whl/domipy/connections/__init__.py
@@ -0,0 +1,172 @@
+"""
+:author: Thomas Delaet <thomas@delaet.org>
+
+Port for Domintell
+:author: Zilvinas Binisevicius <zilvinas@binis.me>
+
+"""
+import socket
+import time
+import threading
+import logging
+from queue import Queue
+import ssl
+import serial
+import serial.threaded
+import websocket
+import domipy
+
+class Protocol(serial.threaded.Protocol):
+    """Serial protocol."""
+
+    def data_received(self, data):
+        # pylint: disable-msg=E1101
+        self.parser(data)
+
+
+class DomintellException(Exception):
+    """Domintell Exception."""
+    def __init__(self, value):
+        Exception.__init__(self)
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class DomintellConnection():
+    """
+    Generic Domintell connection
+    """
+    def send(self, message, callback=None):
+        """
+        :return: None
+        """
+        raise NotImplementedError
+
+class WSSConnection(DomintellConnection):
+    """
+        Wrapper for Secure Web Socket connection configuration
+    """
+    def __init__(self, address, parser=None, hello_interval=0):
+        DomintellConnection.__init__(self)
+        self.logger = logging.getLogger('domintell')
+        self._address = address
+        self.parser = parser
+        self.hello_interval = hello_interval
+        self.is_connected = False
+        self.reconnect_interval = 30
+
+        self.connect_socket()
+        # build a read thread
+        self._listen_process = threading.Thread(None, self.read_daemon,
+                                         "domintell-process-reader", (), {})
+        self._listen_process.daemon = True
+        self._listen_process.start()
+
+        # build a writer thread
+        self._write_queue = Queue()
+        self._write_process = threading.Thread(None, self.write_daemon,
+                                               "domintell-connection-writer", (), {})
+        self._write_process.daemon = True
+        self._write_process.start()
+
+        # build a ping thread
+        self._hello_process = threading.Thread(None, self.hello_daemon,
+                                            "domintell-ping-writer", (), {})
+        self._hello_process.daemon = True
+
+    def connect_socket(self):
+        try:
+            self._socket = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+            self._socket.connect(self._address)
+            self.is_connected = True
+
+        except OSError as osError:
+            self.is_connected = False
+            if osError.strerror == 'A socket operation was attempted to an unreachable host' :
+                self.logger.error("Could not open socket, host is unreacheble")
+                time.sleep(self.reconnect_interval)
+            else:
+                self.logger.error("Could not open socket, \
+                              no messages are read or written to the bus")
+                raise DomintellException("Could not open socket port") from osError
+                
+        except Exception as ex:
+            self.is_connected = False
+            self.logger.error("Could not open socket, \
+                              no messages are read or written to the bus")
+            raise DomintellException("Could not open socket port") from ex
+
+    def stop(self):
+        """Close socket."""
+        self.logger.warning("Stop executed")
+        try:
+            self._socket.close()
+        except Exception as ex:
+            self.logger.error("Error while closing socket")
+            raise DomintellException("Error while closing socket") from ex
+        time.sleep(1)
+
+    def feed_parser(self, data):
+        """Parse received message."""
+        
+        if(data.startswith("TE1")):
+            self.parser.feed(data)
+        else:
+            lines = data.splitlines()
+            for line in lines:
+                self.parser.feed(data)
+
+    def send(self, message, callback=None):
+        """Add message to write queue."""
+        assert isinstance(message, domipy.Message)
+        self._write_queue.put_nowait((message, callback))
+
+    def read_daemon(self):
+        """Reads incoming data."""
+        while True:
+            try:
+                data = self._socket.recv()
+                self.feed_parser(data)
+            except websocket.WebSocketConnectionClosedException :
+                self.logger.warning("Read Deamon, connection closed")
+                self.connect_socket() 
+
+    def write_daemon(self):
+        """Write thread."""
+        while True:
+            (message, callback) = self._write_queue.get(block=True)
+            self.logger.info("Sending message to socket: %s", str(message))
+            self.logger.debug("Sending controll message:  %s", message.to_string())
+            try:
+                if message.is_binary():
+                    self._socket.send(message.to_string())
+                else:
+                    self._socket.send(bytes(message.to_string(),'ascii'))
+            except websocket.WebSocketConnectionClosedException :
+                self.logger.warning("Write Deamon, connection closed")
+                self.connect_socket()
+
+            time.sleep(0.3)
+            if callback:
+                callback()
+
+    def hello_daemon(self):
+        """Put hello message into write thread every hello_interval sec"""
+        while True:
+            ping_message = domipy.messages.Hello()
+            self.send(ping_message)
+            time.sleep(self.hello_interval)
+
+    def start_hello(self, interval=-1):
+        """
+        Start sending HELLO message to server every minute.
+        Server will end Login 'session' if no messages received
+        in predefined interval (10mins default)
+        """
+        if interval > -1:
+            self.hello_interval = interval
+
+        if self.hello_interval > 0:
+            if not self._hello_process.is_alive():
+                self._hello_process.start()
---
+++ packages/domipy/domipy-0.1.4-py3-none-any.whl/domipy/messages/temperature_status.py
@@ -0,0 +1,109 @@
+"""
+Temperature status
+:author: Zilvinas Binisevicius <zilvinas@binis.me>
+"""
+import json
+import domipy
+
+TEM_COMMAND_CODE = "TEM"
+TE1_COMMAND_CODE = "TE1"
+TE2_COMMAND_CODE = "TE2"
+COOLING_MESSAGE_DATA_TYPE = 'U'
+HEATING_MESSAGE_DATA_TYPE = 'T'
+
+
+class GenericTemperaturetatusMessage(domipy.Message):
+    """
+    Generic Temperature input module status
+    """
+
+    def __init__(self, address=None):
+        domipy.Message.__init__(self)
+        self.moduleType = TEM_COMMAND_CODE
+        self.serialNumber = None
+        self.dataType = None
+        self._current = None
+        self._mode = None
+        self._regulation_mode = None
+        self._cooling_set_point = None
+        self._heating_set_point = None
+        self._range = None
+
+    def get_temperature(self):
+        return self._current
+    
+    def get_mode(self):
+        return self._mode
+
+    def get_regulation_mode(self):
+        return self._regulation_mode
+
+    def get_cooling_set_point(self):
+        return self._cooling_set_point
+   
+    def get_heating_set_point(self):
+        return self._heating_set_point
+     
+    def get_range(self):
+        return self._range
+    
+    def get_dataType(self):
+        return self.dataType
+
+    def populate(self, serialNumber, dataType, dataString):
+        """
+        :return: None
+        """
+        # assert isinstance(dataString, str)
+
+        self.serialNumber = serialNumber
+        self.dataType = dataType
+        # [T20.2 21.0 AUTO 21.0]
+        # [U20.2 21.0 HEATING 21.0]
+        data = dataString.split()
+        
+        self._current = float(data[0])
+        if len(data) == 9:
+            self._current = float(data[0])
+            self._mode = data[2]
+            self._regulation_mode = data[7]
+            self._cooling_set_point = float(data[1])
+            self._heating_set_point = float(data[6])
+
+        #if dataType == COOLING_MESSAGE_DATA_TYPE :
+        #    self._cooling_set_point = float(data[1])
+        #    self._mode = data[2]
+        #else :
+        #    self._heating_set_point = float(data[1])
+        #    self._regulation_mode = data[2]
+
+        self._range = float(data[3])
+
+    def to_json(self):
+        """
+        :return: str
+        """
+
+        json_dict = self.to_json_basic()
+        json_dict['current'] = self._current
+        json_dict['mode'] = self._mode
+        json_dict['regulation_mode'] = self._regulation_mode
+        json_dict['cooling_set_point'] = self._cooling_set_point
+        json_dict['heating_set_point'] = self._heating_set_point
+        json_dict['range'] = self._range
+        return json.dumps(json_dict)
+
+
+class TE1TemperaturetatusMessage(GenericTemperaturetatusMessage):
+    def __init__(self, address=None):
+        GenericTemperaturetatusMessage.__init__(self)
+        self.moduleType = TE1_COMMAND_CODE
+
+class TE2TemperaturetatusMessage(GenericTemperaturetatusMessage):
+    def __init__(self, address=None):
+        GenericTemperaturetatusMessage.__init__(self)
+        self.moduleType = TE2_COMMAND_CODE
+
+
+domipy.register_command(TE1_COMMAND_CODE, TE1TemperaturetatusMessage)
+domipy.register_command(TE2_COMMAND_CODE, TE2TemperaturetatusMessage)
