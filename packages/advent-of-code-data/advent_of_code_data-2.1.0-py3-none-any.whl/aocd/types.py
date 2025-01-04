5034549f68cbc8b622305c8916cc35275b6cb8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:51 +0000

    Add package advent-of-code-data/2.1.0/advent_of_code_data-2.1.0-py3-none-any.whl

---
+++ packages/advent-of-code-data/advent_of_code_data-2.1.0-py3-none-any.whl/aocd/types.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from datetime import timedelta
+from typing import Any
+from typing import Literal
+from typing import TypedDict
+
+
+AnswerValue = Any
+"""The answer to a puzzle, either a string or a number. Numbers are coerced to a string"""
+PuzzlePart = Literal["a", "b"]
+"""The part of a given puzzle, a or b"""
+
+
+class PuzzleStats(TypedDict):
+    """Your personal stats for a given puzzle
+
+    See https://adventofcode.com/<year>/leaderboard/self when logged in.
+    """
+
+    time: timedelta
+    rank: int
+    score: int
+
+
+class Submission(TypedDict):
+    """Record of a previous submission made for a given puzzle
+
+    Only applies to answers submitted with aocd.
+    """
+
+    part: PuzzlePart
+    value: str
+    when: str
+    message: str
