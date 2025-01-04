e8771c6ec8c443e6101e538eeed5b25451de79c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:35 +0000

    Add package email-safeguard/0.1.6/email_safeguard-0.1.6.tar.gz

---
+++ packages/email-safeguard/email_safeguard-0.1.6.tar.gz/email_safeguard-0.1.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name="email-safeguard",
+    version="0.1.6",
+    author="Chukwuka Ibejih",
+    author_email="chukaibejih@gmail.com",
+    description="A Python library for validating and suggesting corrections for email addresses.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chukaibejih/email-validator",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['data/*.txt'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "django>=3.0",
+        "python-Levenshtein",
+        "dnspython",
+    ],
+    python_requires='>=3.6',
+    test_suite='tests',
+)
