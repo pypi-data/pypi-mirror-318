b84bf34c1f087d14a0a81b75fcd161a1751dcf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:08:19 +0000

    Add package pyscreeps-arena/0.3a0/pyscreeps_arena-0.3a0-py3-none-any.whl

---
+++ packages/pyscreeps-arena/pyscreeps_arena-0.3a0-py3-none-any.whl/pyscreeps_arena/core/const.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# pyscreeps_arena - const.py
+# Author: 我阅读理解一直可以的
+# Template: V0.1
+# Versions:
+# .2025 01 01 - v0.1:
+#   Created.
+#
+import re
+
+VERSION = "0.1"
+AUTHOR = "我阅读理解一直可以的"
+STEAM_ID = "1029562896"
+GITHUB_NAME = "EagleBaby"
+
