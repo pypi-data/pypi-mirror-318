e3d6869636a3fa0ad02615a8da8e62c10ca1f5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:25 +0000

    Add package doimbu/0.7.0/doimbu-0.7.0-py3-none-any.whl

---
+++ packages/doimbu/doimbu-0.7.0-py3-none-any.whl/doimbu/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+EXECUTION_DIR = Path().cwd()
+
+BUILDER_CONFIG_FILE = 'doimbu.ini'
+BUILDER_CONFIG_PATH = EXECUTION_DIR / BUILDER_CONFIG_FILE
+DOCKER_IMAGE_VARIANTS_DIR = EXECUTION_DIR / 'variants'
+LOG_PATH = EXECUTION_DIR / 'build_output.log'
