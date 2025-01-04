b3c8c6812a37340b2eff083593d682aa4c79d3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 03:07:48 +0000

    Add package LTPpyTools/0.2.2/ltppytools-0.2.2.tar.gz

---
+++ packages/LTPpyTools/ltppytools-0.2.2.tar.gz/ltppytools-0.2.2/LTPpyTools/__init__.py
@@ -0,0 +1,13 @@
+"""this is Tools"""
+"""by LTPLAX"""
+"""version 0.1.0"""
+"""Please use Tools.init before use"""
+from .music import music
+from .flush import flush
+__all__=["music","flush"]
+
+def init():
+    print("LTPpyTools 0.1 Normal mode ready(this ver only have music\ntype:Tools.music.init())")
+
+
+
---
+++ packages/LTPpyTools/ltppytools-0.2.2.tar.gz/ltppytools-0.2.2/README.md
@@ -0,0 +1,9 @@
+This is a python package for any python delelop.  
+init():init Tools  
+----music----  
+init():init music  
+load():load file  
+play():play .wav file  
+stop():stop playing.  
+---flush(testing,see on my github)---  
+flush():flush text  
