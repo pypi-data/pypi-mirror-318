a8ff8c7ab6f7789a264ad505454e9388612d1535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:34 +0000

    Add package spotify-music-dl/0.0.1/spotify_music_dl-0.0.1.tar.gz

---
+++ packages/spotify-music-dl/spotify_music_dl-0.0.1.tar.gz/spotify_music_dl-0.0.1/spotify_music_dl/__init__.py
@@ -0,0 +1,11 @@
+"""A simple tool to download Spotify tracks and playlists"""
+
+__description__ = "A simple tool to download Spotify tracks and playlists"
+__url__ = ""
+
+__title__ = "spotify_music_dl"
+__author__ = "anekobtw"
+__license__ = "MIT"
+__version__ = "0.0.1"
+
+from downloader import SpotifyDownloader
