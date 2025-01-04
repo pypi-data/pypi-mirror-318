fa8f67c8330d90a6cb8e522c8bdf85dbc3c58dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:28 +0000

    Add package maps4fs/1.5.6/maps4fs-1.5.6.tar.gz

---
+++ packages/maps4fs/maps4fs-1.5.6.tar.gz/maps4fs-1.5.6/maps4fs/__init__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-module-docstring
+from maps4fs.generator.dtm import DTMProvider
+from maps4fs.generator.game import Game
+from maps4fs.generator.map import Map
+from maps4fs.generator.settings import (
+    BackgroundSettings,
+    DEMSettings,
+    GRLESettings,
+    I3DSettings,
+    SettingsModel,
+    SplineSettings,
+    TextureSettings,
+)
+from maps4fs.logger import Logger
