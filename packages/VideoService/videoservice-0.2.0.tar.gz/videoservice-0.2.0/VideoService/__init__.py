ff038f7793137ecd6c05cd0aa16d7ac02c06ece5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:06:05 +0000

    Add package VideoService/0.2.0/videoservice-0.2.0.tar.gz

---
+++ packages/VideoService/videoservice-0.2.0.tar.gz/videoservice-0.2.0/VideoService/__init__.py
@@ -0,0 +1,26 @@
+
+#################################################*
+##### ***  DOCUMENTATION  *** ###################*
+#################################################*
+
+"""
+VIDEO SERVICE
+=============
+The library you didn't know you needed 🗿
+
+----
+
+This library gives you all you need to create a video<br>
+service, Video management, DataBase management,<br>
+and an integrated Search Engine.
+"""
+
+#################################################*
+##### ***  IMPORTS  *** #########################*
+#################################################*
+
+# *** Internal modules *** #
+from .__main__ import VideoService
+from .video import Video
+from .playlist import Playlist
+from .__errors__ import *
