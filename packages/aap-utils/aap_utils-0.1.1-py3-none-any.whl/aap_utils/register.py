ad3346ee2babee9e5f01909d9791869b38c9aef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:05:04 +0000

    Add package aap-utils/0.1.1/aap_utils-0.1.1-py3-none-any.whl

---
+++ packages/aap-utils/aap_utils-0.1.1-py3-none-any.whl/aap_utils/register.py
@@ -0,0 +1,30 @@
+import socket
+import requests
+import time
+
+def register_demo(debug=False):
+    for attempt in range(10):
+        try:
+            local_ip = socket.gethostbyname(socket.gethostname())
+            response = requests.post(
+                "http://hocmay-svc/aiplatform-api/demo/register/", 
+                json={"demo_ip": local_ip}, 
+                headers={"Content-Type": "application/json"}
+            )
+            if response.ok:
+                if debug:
+                    print("IP registered successfully.")
+                return True
+            else:
+                if debug:
+                    print(f"Attempt {attempt + 1}: Failed: {response.text}")
+        except (socket.error, requests.exceptions.RequestException) as e:
+            if debug:  # Print errors only if debug is True
+                print(f"Attempt {attempt + 1}: Error: {e}")
+        
+        time.sleep(2)  # Wait for 2 seconds before retrying
+
+    if debug:
+        print("Failed to register IP after 10 attempts.")
+    print("The demo service is registed at port 80.")
+    return False
