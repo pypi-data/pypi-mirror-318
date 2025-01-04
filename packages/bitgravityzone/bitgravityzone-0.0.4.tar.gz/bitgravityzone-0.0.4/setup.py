7ac0a7b5952da2cd8e6e0e7f7885e0d558af8894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:34 +0000

    Add package bitgravityzone/0.0.4/bitgravityzone-0.0.4.tar.gz

---
+++ packages/bitgravityzone/bitgravityzone-0.0.4.tar.gz/bitgravityzone-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="bitgravityzone",
+    version="0.0.4",
+    author="son",
+    author_email="son@example.com",
+    description="GravityZone API",
+    long_description="GravityZone API https://www.bitdefender.com/business/support/en/77211-125277-public-api.html",
+    long_description_content_type="text/markdown",
+    url="https://github.com/sonpd2/bitgravityzone",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
