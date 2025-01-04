ed9fffdbb3448fa1abe22d62e0cb5321dacbd2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:05:14 +0000

    Add package bookworm_genai/0.13.1b103/bookworm_genai-0.13.1b103-py3-none-any.whl

---
+++ packages/bookworm_genai/bookworm_genai-0.13.1b103-py3-none-any.whl/bookworm_genai/metadata.py
@@ -0,0 +1,16 @@
+import enum
+from langchain_core.documents import Document
+
+from bookworm_genai import __version__
+from bookworm_genai.integrations import Browser
+
+
+class Metadata(str, enum.Enum):
+    Browser = "browser"
+    BookwormVersion = "bookworm_version"
+
+
+def attach_metadata(doc: Document, browser: Browser) -> Document:
+    doc.metadata[Metadata.Browser.value] = browser.value
+    doc.metadata[Metadata.BookwormVersion.value] = __version__
+    return doc
