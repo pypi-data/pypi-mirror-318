0ba4df4a4182f383e24920ac18232a3300671033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:30 +0000

    Add package pinginstaller/1.0.0a4/pinginstaller-1.0.0a4-py3-none-any.whl

---
+++ packages/pinginstaller/pinginstaller-1.0.0a4-py3-none-any.whl/pinginstaller/Install_Update_PINGMapper.py
@@ -0,0 +1,58 @@
+import os, sys
+import subprocess, re
+
+def install_housekeeping():
+    conda_key = os.environ.get('CONDA_EXE', 'conda')
+    print(f"conda_key: {conda_key}")
+
+    subprocess.run('''"{}" update -y conda'''.format(conda_key), shell=True)
+    subprocess.run('''"{}" clean -y --all'''.format(conda_key), shell=True)
+    subprocess.run('''python -m pip install --upgrade pip''', shell=True)
+
+def conda_env_exists(env_name):
+    result = subprocess.run('conda env list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    envs = result.stdout.splitlines()
+    for env in envs:
+        if re.search(rf'^{env_name}\s', env):
+            return True
+    return False
+
+def install(yml):
+    conda_key = os.environ.get('CONDA_EXE', 'conda')
+    print(f"conda_key: {conda_key}")
+
+    subprocess.run('''"{}" env create --file {}'''.format(conda_key, yml), shell=True)
+
+    subprocess.run('conda env list', shell=True)
+
+    return
+
+def update(yml):
+    conda_key = os.environ.get('CONDA_EXE', 'conda')
+    print(f"conda_key: {conda_key}")
+
+    subprocess.run('''"{}" env update --file {}'''.format(conda_key, yml), shell=True)
+
+    subprocess.run('conda env list', shell=True)
+
+    return
+
+
+# def install_update(conda_base, conda_key):
+def install_update(yml):
+
+    subprocess.run('conda env list', shell=True)
+
+    install_housekeeping()
+
+    if conda_env_exists(env_name):
+        print(f"Updating '{env_name}' environment ...")
+        # subprocess.run([os.path.join(directory, "Update.bat"), conda_base, conda_key, yml], shell=True)
+        update(yml)
+        
+    else:
+        print(f"Creating '{env_name}' environment...")
+        # subprocess.run([os.path.join(directory, "Install.bat"), conda_base, conda_key, yml], shell=True)
+        install(yml)
+
+    
