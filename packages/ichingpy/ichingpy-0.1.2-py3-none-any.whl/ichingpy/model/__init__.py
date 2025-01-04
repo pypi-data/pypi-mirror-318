637f6794c0ec88d4eafec92972d86773265e24a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:35 +0000

    Add package ichingpy/0.1.2/ichingpy-0.1.2-py3-none-any.whl

---
+++ packages/ichingpy/ichingpy-0.1.2-py3-none-any.whl/ichingpy/model/__init__.py
@@ -0,0 +1,6 @@
+from ichingpy.model.hexagram import Hexagram
+from ichingpy.model.line import Line
+from ichingpy.model.sexagenary_cycle import SexagenaryCycle
+from ichingpy.model.trigram import Trigram
+
+__all__ = ["Line", "Hexagram", "Trigram", "SexagenaryCycle"]
