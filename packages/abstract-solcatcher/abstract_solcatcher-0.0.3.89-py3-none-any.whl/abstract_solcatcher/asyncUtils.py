87f229d2d23cafc1c27b52331431a3ff9bec3425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:49 +0000

    Add package abstract-solcatcher/0.0.3.89/abstract_solcatcher-0.0.3.89-py3-none-any.whl

---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.3.89-py3-none-any.whl/abstract_solcatcher/asyncUtils.py
@@ -0,0 +1,41 @@
+import asyncio,httpx
+from .utils import *
+from abstract_apis import get_headers
+async def make_request(url, payload, headers=None):
+    try:
+        async with httpx.AsyncClient(timeout=10) as client:
+            response = await client.post(url, json=payload, headers=headers)
+            response.raise_for_status()  # Raise exception for non-2xx status codes
+            return response.json()       # or just return response if you want the full object
+    except Exception as e:
+        print(f"HTTP request failed: {str(e)} - Payload: {payload}")
+        return None
+
+
+async def async_call_solcatcher_ts(endpoint,*args,**kwargs):
+    payload = get_payload(*args,**kwargs)
+    url = getSolcatcherTsUrl(endpoint=endpoint)
+    return await  make_request(url, payload,headers=get_headers())
+
+async def async_call_solcatcher_py(endpoint,*args,**kwargs):
+    payload = get_payload(*args,**kwargs)
+    url = getSolcatcherPairCatchUrl(endpoint=endpoint)
+    return await make_request(url, payload,headers=get_headers())
+
+async def async_call_solcatcher_db(endpoint,*args,**kwargs):
+    apiKey = kwargs.get('apiKey')
+    if apiKey:
+        del kwargs['apiKey']
+    payload = get_payload(*args,**kwargs)
+    url = getSolcatcherDbCalls(endpoint=endpoint)
+    return await make_request(url, payload,headers=get_db_header(apiKey))
+
+def call_solcatcher_py(endpoint,*args,**kwargs):
+    return asyncio.run(async_call_solcatcher_py(endpoint,*args,**kwargs))
+
+def call_solcatcher_ts(endpoint,*args,**kwargs):
+    return asyncio.run(async_call_solcatcher_ts(endpoint,*args,**kwargs))
+
+def call_solcatcher_db(endpoint,*args, **kwargs):
+    return asyncio.run(async_call_solcatcher_db(endpoint,*args,**kwargs))
+
---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.3.89-py3-none-any.whl/abstract_solcatcher/utils.py
@@ -0,0 +1,64 @@
+import json
+from abstract_utilities import eatAll
+from abstract_security import get_env_value
+def getSolcatcherUrl(endpoint=None):
+  return getEndpointUrl(endpoint=endpoint,url='https://solcatcher.io')
+def getSolcatcherPairCatchUrl(endpoint=None):
+  return getEndpointUrl(endpoint=endpoint,url='https://solcatcher.io/pairCatch')
+def getSolcatcherTsUrl(endpoint=None):
+  return getEndpointUrl(endpoint=endpoint,url='https://solcatcher.io/ts')
+def getSolcatcherDbCalls(endpoint=None):
+  return getEndpointUrl(endpoint=endpoint,url="https://solcatcher.io/dbCalls")
+def get_db_header(api_key=None):
+    # Retrieve the key from environment variable or config
+    api_key = api_key or get_env_value('SOLCATCHER_DB_API_KEY')
+    # Make sure the server sees this header as "X-API-KEY"
+    return {
+        "X-API-KEY": api_key,
+        "Content-Type": "application/json"  # if your endpoint expects JSON
+    }
+def post_request(endpoint, **kwargs):
+    url = getSolcatcherDbCalls(endpoint=endpoint)
+    response = requests.post(url=url, data=json.dumps(kwargs), headers=get_headers())
+    return get_response(response)
+def getEndpointUrl(endpoint=None,url=None):
+  if endpoint:
+    url = eatAll(url,['/'])
+    endpoint = eatAll(endpoint,['/'])
+    url= f"{url}/{endpoint}"
+  return url
+def try_json_dumps(data):
+  if isinstance(data,dict):
+    try:
+      data = json.dumps(data)
+    except:
+      pass
+    return data
+def get_url(url=None):
+    if isinstance(url,dict):
+      url = url.get('url',url)
+    return url
+def getCallArgs(endpoint):
+  return {'getMetaData': ['signature'], 'getPoolData': ['signature'], 'getTransactionData': ['signature'], 'getPoolInfo': ['signature'], 'getMarketInfo': ['signature'], 'getKeyInfo': ['signature'], 'getLpKeys': ['signature'], 'process': ['signature']}.get(get_endpoint(endpoint))
+def ifListGetSection(listObj,section=0):
+  if isinstance(listObj,list):
+      if len(listObj)>section:
+          return listObj[section]
+  return listObj
+def updateData(data,**kwargs):
+  data.update(kwargs)
+  return data
+def get_method(method=None):
+  return method or 'default_method'
+def get_resp(response=None):
+  response = response or {}
+  if isinstance(response,dict):
+    response = {"response":response}
+  return response
+def get_payload(*args,**kwargs):
+    payload = args
+    if args and kwargs:
+        payload.append(kwargs)
+    else:
+        payload = kwargs
+    return payload
