fc53274fd92f6a545e36a94901ec1c107c05a4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 14:04:38 +0000

    Add package plapi-string/0.0.11/plapi_string-0.0.11-py3-none-any.whl

---
+++ packages/plapi-string/plapi_string-0.0.11-py3-none-any.whl/plapi/string/compression/compression.py
@@ -0,0 +1,16 @@
+def compress_string(s):
+    if not s:
+        return ""
+
+    compressed = []
+    count = 1
+
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            compressed.append(s[i - 1] + str(count))
+            count = 1
+
+    compressed.append(s[-1] + str(count))
+    return ''.join(compressed)
