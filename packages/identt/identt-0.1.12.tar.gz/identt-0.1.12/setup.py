561f8566553b3124010097bf8018a4b81612a841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 04:05:16 +0000

    Add package identt/0.1.12/identt-0.1.12.tar.gz

---
+++ packages/identt/identt-0.1.12.tar.gz/identt-0.1.12/setup.py
@@ -0,0 +1,13 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name="identt",
+    version=os.getenv("PACKAGE_VERSION", "0.1.0"),
+    packages=find_packages(where="."),
+    package_dir={"": "."},
+    install_requires=[],
+    author="brdge.ai",
+    description="Python SDK for Iden API interactions",
+)
