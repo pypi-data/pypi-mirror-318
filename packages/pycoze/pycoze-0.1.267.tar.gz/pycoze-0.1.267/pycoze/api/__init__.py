b28e4404c43e2c52ba787aadd12847c0a4211a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:49 +0000

    Add package pycoze/0.1.267/pycoze-0.1.267.tar.gz

---
+++ packages/pycoze/pycoze-0.1.267.tar.gz/pycoze-0.1.267/pycoze/api/__init__.py
@@ -0,0 +1,11 @@
+from .lib.window_cls import WindowCls
+from .lib.tab_cls import TabCls
+
+class Api:
+    def __init__(self) -> None:
+        self.window = WindowCls()
+        self.tab = TabCls()
+
+api = Api()
+
+# from ps_view import ViewCls, WebsiteViewCls, FileViewCls, DirectoryViewCls, WorkflowCls
