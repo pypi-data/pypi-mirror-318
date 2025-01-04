8187fc4ab1e7b73adc8b8b96286024933f198d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 05:04:47 +0000

    Add package jupyterlab-magic-wand/0.3.0/jupyterlab_magic_wand-0.3.0.tar.gz

---
+++ packages/jupyterlab-magic-wand/jupyterlab_magic_wand-0.3.0.tar.gz/jupyterlab_magic_wand-0.3.0/CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+<!-- <START NEW CHANGELOG ENTRY> -->
+
+## 0.3.0
+
+([Full Changelog](https://github.com/Zsailer/jupyterlab-magic-wand/compare/v0.2.1...00f011339fb873cf3ca6d0d819112bd5b1baff7c))
+
+### Enhancements made
+
+- Rename entrypoint [#2](https://github.com/Zsailer/jupyterlab-magic-wand/pull/2) ([@Zsailer](https://github.com/Zsailer))
+
+### Contributors to this release
+
+([GitHub contributors page for this release](https://github.com/Zsailer/jupyterlab-magic-wand/graphs/contributors?from=2025-01-03&to=2025-01-03&type=c))
+
+[@Zsailer](https://github.com/search?q=repo%3AZsailer%2Fjupyterlab-magic-wand+involves%3AZsailer+updated%3A2025-01-03..2025-01-03&type=Issues)
+
+<!-- <END NEW CHANGELOG ENTRY> -->
+
+## 0.2.1
+
+([Full Changelog](https://github.com/Zsailer/jupyterlab-magic-wand/compare/cc48eda7b10373ddadf84447b9209bea63af1868...118fc2d235dd3ea6811f673ed98e111fe2f75330))
+
+### Enhancements made
+
+- Get to a working state [#1](https://github.com/Zsailer/jupyterlab-magic-wand/pull/1) ([@Zsailer](https://github.com/Zsailer))
+
+### Contributors to this release
+
+([GitHub contributors page for this release](https://github.com/Zsailer/jupyterlab-magic-wand/graphs/contributors?from=2024-11-13&to=2025-01-03&type=c))
+
+[@Zsailer](https://github.com/search?q=repo%3AZsailer%2Fjupyterlab-magic-wand+involves%3AZsailer+updated%3A2024-11-13..2025-01-03&type=Issues)
---
+++ packages/jupyterlab-magic-wand/jupyterlab_magic_wand-0.3.0.tar.gz/jupyterlab_magic_wand-0.3.0/src/utils.ts
@@ -0,0 +1,147 @@
+import { DocumentWidget } from '@jupyterlab/docregistry';
+
+import {
+  Notebook,
+  NotebookPanel,
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+import { Cell, CodeCell } from '@jupyterlab/cells';
+
+import { Widget } from '@lumino/widgets';
+
+export function getNotebook(widget: Widget | null): Notebook | null {
+  if (!(widget instanceof DocumentWidget)) {
+    return null;
+  }
+
+  const { content } = widget;
+  if (!(content instanceof Notebook)) {
+    return null;
+  }
+
+  return content;
+}
+
+export function getActiveCell(widget: Widget | null): Cell | null {
+  const notebook = getNotebook(widget);
+  if (!notebook) {
+    return null;
+  }
+  return notebook.activeCell;
+}
+
+export type CellData = {
+  cell_id: string;
+  type: string;
+  source: string;
+  error: string | null;
+};
+
+export type CellContext = {
+  previous: CellData | null;
+  current: CellData;
+  next: CellData | null;
+};
+
+export function getCellData(cell: Cell): CellData {
+  const source = cell?.model.sharedModel.getSource();
+  cell?.model.selections;
+  let error: null | string = null;
+  if (cell?.model.type === 'code') {
+    const codeCell = cell as CodeCell;
+    const lastIndex = codeCell.outputArea.model.length - 1;
+    const lastOutput = codeCell.outputArea.model.get(lastIndex);
+    if (lastOutput && lastOutput.type === 'error') {
+      const err = lastOutput.data['application/vnd.jupyter.error'] as any;
+      if (err) {
+        error = err['ename'] + ': ' + err['evalue'];
+      }
+    }
+  }
+
+  return {
+    cell_id: cell?.model.sharedModel.getId(),
+    type: cell?.model.type,
+    error: error,
+    source: source
+  };
+}
+
+function cellFromIndex(notebook: Notebook, idx: number): Cell | undefined {
+  const cellId = notebook.model?.cells.get(idx)?.id;
+  if (cellId) {
+    const cell = notebook._findCellById(cellId)?.cell;
+    if (cell) {
+      return cell;
+    }
+  }
+}
+
+export function getActiveCellContext(
+  widget: Widget | null
+): CellContext | null {
+  const notebook = getNotebook(widget);
+  if (!notebook) {
+    return null;
+  }
+
+  const cellIdx = notebook.activeCellIndex;
+  const current = notebook.activeCell;
+  if (!current) {
+    return null;
+  }
+  const currentData = getCellData(current);
+
+  // Handle previous cell
+  const previous = cellFromIndex(notebook, cellIdx - 1);
+  let previousData = null;
+  if (previous) {
+    previousData = getCellData(previous);
+  }
+  const next = cellFromIndex(notebook, cellIdx + 1);
+  let nextData = null;
+  if (next) {
+    nextData = getCellData(next);
+  }
+
+  return {
+    previous: previousData,
+    current: currentData,
+    next: nextData
+  };
+}
+
+type ActiveNotebookCell = {
+  cell: Cell | undefined;
+  notebook: NotebookPanel | undefined;
+};
+
+export function findCell(
+  cellId: string,
+  notebookTracker: INotebookTracker
+): ActiveNotebookCell {
+  // First, try the current notebook in focuse
+  const currentNotebook = notebookTracker.currentWidget;
+  const cell =
+    notebookTracker.currentWidget?.content._findCellById(cellId)?.cell;
+  if (currentNotebook && cell) {
+    return {
+      cell: cell,
+      notebook: currentNotebook
+    };
+  }
+
+  // Otherwise iterate through notebooks to find the cell.
+  const notebookMatch = notebookTracker.find(notebook => {
+    const cell = notebook.content._findCellById(cellId)?.cell;
+    if (cell) {
+      return true;
+    }
+    return false;
+  });
+  return {
+    cell: cell,
+    notebook: notebookMatch
+  };
+}
