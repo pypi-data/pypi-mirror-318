6907137ad6fc94bdab25ebbda64b1001c5f2be28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-04 15:04:50 +0000

    Add package hikkalls/0.1.10/hikkalls-0.1.10-cp39-cp39-win_amd64.whl

---
+++ packages/hikkalls/hikkalls-0.1.10-cp39-cp39-win_amd64.whl/hikkalls/__init__.py
@@ -0,0 +1,11 @@
+from .custom_api import CustomApi
+from .hikkalls import HikkaLls
+from .stream_type import StreamType
+from .sync import idle
+
+__all__ = (
+    'idle',
+    'CustomApi',
+    'HikkaLls',
+    'StreamType',
+)
