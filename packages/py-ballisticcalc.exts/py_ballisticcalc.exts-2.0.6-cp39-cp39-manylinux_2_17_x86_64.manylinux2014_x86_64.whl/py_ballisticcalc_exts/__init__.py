1f09be61b4f26ecf5aff8bbcfd72a9a8ced0c439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 14:04:37 +0000

    Add package py-ballisticcalc.exts/2.0.6/py_ballisticcalc.exts-2.0.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/py-ballisticcalc.exts/py_ballisticcalc.exts-2.0.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/py_ballisticcalc_exts/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "o-murphy"
+__copyright__ = (
+    "Copyright 2023 Dmytro Yaroshenko (https://github.com/o-murphy)",
+    "Copyright 2024 David Bookstaber (https://github.com/dbookstaber)"
+)
+
+__credits__ = ["o-murphy", "dbookstaber"]
+
+from .trajectory_calc import *
