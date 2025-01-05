c41fee7365ba6a1bd3375e88db7831e42a6f40fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:17 +0000

    Add package grscheller.fp/1.2.0/grscheller_fp-1.2.0-py3-none-any.whl

---
+++ packages/grscheller.fp/grscheller_fp-1.2.0-py3-none-any.whl/grscheller/fp/__init__.py
@@ -0,0 +1,34 @@
+# Copyright 2023-2025 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""### Package grscheller.fp - Pythonic Functional Programming
+
+Tools to aid with functional programming in Python yet still endeavoring to
+remain Pythonic.
+
+#### Modules and sub-packages
+
+* module grscheller.fp.err_handling: monadic maybe and either
+* module grscheller.fp.functions: tools combine and partially apply functions
+* module grscheller.fp.iterables: tools for iterables
+* module grscheller.fp.lazy: lazy (non-strict) function evaluation
+* module grscheller.fp.singletons: useful types with but one instance
+* module grscheller.fp.state: handle state monadically
+
+"""
+__version__ = "1.2.0"
+__author__ = "Geoffrey R. Scheller"
+__copyright__ = "Copyright (c) 2023-2025 Geoffrey R. Scheller"
+__license__ = "Apache License 2.0"
+
