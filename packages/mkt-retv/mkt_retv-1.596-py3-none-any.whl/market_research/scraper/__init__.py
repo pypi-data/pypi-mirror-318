3827bcb200c18ea87ea60b876c562141a1deb42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:51 +0000

    Add package mkt-retv/1.596/mkt_retv-1.596-py3-none-any.whl

---
+++ packages/mkt-retv/mkt_retv-1.596-py3-none-any.whl/market_research/scraper/__init__.py
@@ -0,0 +1,11 @@
+from .models.specs.spec_l import ModelScraper_l as Specscraper_l
+from .models.specs.spec_s import ModelScraper_s as Specscraper_s
+from .models.specs.spec_se import ModelScraper_se as Specscraper_se
+from .models.specs.sepc_sjp import ModelScraper_sjp as Specscraper_sjp
+from .models.specs.spec_p import ModelScraper_p as Specscraper_p
+from .models.specs.spec_pjp import ModelScraper_pjp as Specscraper_pjp
+from .models.visualizer.data_visualizer import DataVisualizer
+
+from .rtings.rtings import Rtings
+from .rtings.rvisualizer import Rvisualizer
+from .rtings.rurlsearcher import Rurlsearcher
