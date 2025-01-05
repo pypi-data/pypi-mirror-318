a06014ee1ad0baf8203469dbc38e825d77e4a35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 14:04:42 +0000

    Add package videoutil/0.1.1/videoutil-0.1.1.tar.gz

---
+++ packages/videoutil/videoutil-0.1.1.tar.gz/videoutil-0.1.1/src/videoutil/__init__.py
@@ -0,0 +1,8 @@
+# src/videoutil/__init__.py
+__version__ = "0.1.1"
+
+from .generate import generate_videos
+from .combine import combine_videos
+from .rename import find_and_rename_pairs
+
+__all__ = ['generate_videos', 'combine_videos', 'find_and_rename_pairs']
