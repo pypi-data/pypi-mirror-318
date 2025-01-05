13421b87b0dd3de6261927b8ed077be155adce92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:12:34 +0000

    Add package ceo-py/0.9.5rc0/ceo_py-0.9.5rc0.tar.gz

---
+++ packages/ceo-py/ceo_py-0.9.5rc0.tar.gz/ceo_py-0.9.5rc0/ceo/exception/too_dumb_exception.py
@@ -0,0 +1,8 @@
+from langchain_core.language_models import BaseChatModel
+
+
+class TooDumbException(Exception):
+    def __init__(self, model: BaseChatModel):
+        model_name = model.dict().get('model_name', 'unknown')
+        super().__init__(f'The model "{model_name}" is too dumb to be my brain, '
+                         f'swap it or change its temperature.')
