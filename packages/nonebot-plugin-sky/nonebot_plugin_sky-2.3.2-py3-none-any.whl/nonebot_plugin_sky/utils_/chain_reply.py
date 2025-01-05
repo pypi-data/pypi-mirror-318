94958a510205f10e97029465d037cd3ebb2eafb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:49 +0000

    Add package nonebot-plugin-sky/2.3.2/nonebot_plugin_sky-2.3.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-sky/nonebot_plugin_sky-2.3.2-py3-none-any.whl/nonebot_plugin_sky/utils_/chain_reply.py
@@ -0,0 +1,26 @@
+from typing import List, Union
+
+from nonebot import get_plugin_config
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.adapters.onebot.v11.bot import Bot
+
+from .bot_loader import Config
+
+
+async def chain_reply(bot: Bot, msg_list: List[Union[str, MessageSegment]]) -> list:
+    """
+    构造聊天记录转发消息
+
+    :param bot:
+    :param msg:
+    :return:
+    """
+    nick = get_plugin_config(Config).nickname
+    chain = []
+    for msg in msg_list:
+        data = {
+            "type": "node",
+            "data": {"name": nick[0], "uin": f"{bot.self_id}", "content": msg},
+        }
+        chain.append(data)
+    return chain
