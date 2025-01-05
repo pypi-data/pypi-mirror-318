3d5026b09982c0ebfccae68a5d870a8b89dc3861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:30 +0000

    Add package pymocklib/1.1.0/pymocklib-1.1.0.tar.gz

---
+++ packages/pymocklib/pymocklib-1.1.0.tar.gz/pymocklib-1.1.0/pymocklib/__main__.py
@@ -0,0 +1,10 @@
+from pymocklib.transformers import style_doc, styles
+
+
+if __name__ == "__main__":
+    print(
+        "\n".join(
+            ["Here's what I can do:", ""]
+            + [f"{name}: {style_doc(name)}" for (name, _) in styles]
+        )
+    )
