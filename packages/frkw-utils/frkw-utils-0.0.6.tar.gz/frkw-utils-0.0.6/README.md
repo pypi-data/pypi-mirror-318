461ae9a327fc67b270f1cf988d3273d4285966aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:05:55 +0000

    Add package frkw-utils/0.0.6/frkw-utils-0.0.6.tar.gz

---
+++ packages/frkw-utils/frkw-utils-0.0.6.tar.gz/frkw-utils-0.0.6/README.md
@@ -0,0 +1,5 @@
+# frkw-utils
+
+This is a placeholder package for testing purposes.
+
+https://pypi.org/project/frkw-utils/
---
+++ packages/frkw-utils/frkw-utils-0.0.6.tar.gz/frkw-utils-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frkw-utils",
+    version="0.0.6",
+    description="A placeholder for frkw_utils package.",
+    long_description="This is an empty package for testing purposes.",
+    long_description_content_type="text/plain",
+    author="Koichi Furukawa",
+    author_email="frkw50@gmail.com",
+    url="https://gitlab.com/frkw50group/frkw-utils",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
