2f8e65b2f2048ab377e332fc64f46595f188516e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 16:07:56 +0000

    Add package check-working/0.1/check_working-0.1.tar.gz

---
+++ packages/check-working/check_working-0.1.tar.gz/check_working-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='check_working',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://yourlibraryhomepage.com',
+)
