e97071a5313b93d2339ff10b0395de7b001775cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:04:56 +0000

    Add package chrome-lens-py/1.5.0/chrome_lens_py-1.5.0.tar.gz

---
+++ packages/chrome-lens-py/chrome_lens_py-1.5.0.tar.gz/chrome_lens_py-1.5.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='chrome_lens_py',
+    version='1.5.0',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'requests',
+        'Pillow',
+        'filetype',
+        'lxml',
+        'json5',
+        'rich',
+        'PySocks',
+        'httpx==0.27.2',
+        'socksio',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lens_scan=chrome_lens_py.main:main',
+        ],
+    },
+    description='Library to use Google Lens OCR for free via API used in Chromium on python ',
+    long_description=README,
+    long_description_content_type='text/markdown',  # Указание типа содержимого
+    author='Bropines',
+    author_email='bropines@gmail.com',
+    url='https://github.com/bropines/chrome-lens-py',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
