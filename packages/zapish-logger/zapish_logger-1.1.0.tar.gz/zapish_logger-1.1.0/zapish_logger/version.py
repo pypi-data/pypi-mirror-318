8a9f248ab335a25397f9e635380a5c9865e80ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 14:04:35 +0000

    Add package zapish-logger/1.1.0/zapish_logger-1.1.0.tar.gz

---
+++ packages/zapish-logger/zapish_logger-1.1.0.tar.gz/zapish_logger-1.1.0/zapish_logger/version.py
@@ -0,0 +1,7 @@
+"""
+Holds the version information for the package
+"""
+__copyright__ = 'Copyright (c) 2024-2025 Benjamin P. Trachtenberg'
+__status__ = 'prod'
+__version_info__ = (1, 1, 0)
+__version__ = '.'.join(map(str, __version_info__))
