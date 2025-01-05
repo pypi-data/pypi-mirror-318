1f63fe769cbfb1095b22dc2fbef5f88066624817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 14:04:44 +0000

    Add package modstore/3.0.0/modstore-3.0.0-cp38-cp38-macosx_10_12_x86_64.whl

---
+++ packages/modstore/modstore-3.0.0-cp38-cp38-macosx_10_12_x86_64.whl/modstore/algorithms/searching/__init__.py
@@ -0,0 +1,13 @@
+from ._searching import (
+    Search,
+    SearchObject,
+    search_consistency,
+    int_only,
+)
+
+__all__ = [
+    "Search",
+    "SearchObject",
+    "search_consistency",
+    "int_only",
+]
