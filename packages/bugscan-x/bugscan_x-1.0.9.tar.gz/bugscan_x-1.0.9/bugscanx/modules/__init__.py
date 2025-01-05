041c513db62a5a30fa204509c0f0326b13aab6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:18 +0000

    Add package bugscan-x/1.0.9/bugscan_x-1.0.9.tar.gz

---
+++ packages/bugscan-x/bugscan_x-1.0.9.tar.gz/bugscan_x-1.0.9/bugscanx/modules/__init__.py
@@ -0,0 +1,5 @@
+from bugscanx.modules.utils.handler import *
+from bugscanx.modules.utils.other import banner
+from bugscanx.modules.utils.glance import display_message
+from bugscanx.modules.utils.utils import *
+from bugscanx.modules.utils.http_utils import HEADERS, USER_AGENTS, EXTRA_HEADERS
---
+++ packages/bugscan-x/bugscan_x-1.0.9.tar.gz/bugscan_x-1.0.9/bugscanx/modules/utils/other.py
@@ -0,0 +1,14 @@
+from rich.console import Console
+
+console = Console()
+
+def banner():
+    banner_text = """
+    [bold red]╔╗[/bold red] [turquoise2]╦ ╦╔═╗╔═╗╔═╗╔═╗╔╗╔═╗ ╦[/turquoise2]
+    [bold red]╠╩╗[/bold red][turquoise2]║ ║║ ╦╚═╗║  ╠═╣║║║╔╩╦╝[/turquoise2]
+    [bold red]╚═╝[/bold red][turquoise2]╚═╝╚═╝╚═╝╚═╝╩ ╩╝╚╝╩ ╚═[/turquoise2]
+        [bold magenta]Bʏ: Aʏᴀɴ Rᴀᴊᴘᴏᴏᴛ
+  Sᴜᴘᴘᴏʀᴛ: https://t.me/BugScanX[/bold magenta]
+    """
+    console.print(banner_text)
+
