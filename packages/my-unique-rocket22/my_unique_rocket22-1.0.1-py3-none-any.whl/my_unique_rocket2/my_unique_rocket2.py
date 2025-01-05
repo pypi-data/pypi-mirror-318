7cd55a95893d9f673091dd82a2fc052b8cc4cabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:51 +0000

    Add package my-unique-rocket22/1.0.1/my_unique_rocket22-1.0.1-py3-none-any.whl

---
+++ packages/my-unique-rocket22/my_unique_rocket22-1.0.1-py3-none-any.whl/my_unique_rocket2/my_unique_rocket2.py
@@ -0,0 +1,6 @@
+class Rocket:
+    def __init__(self):
+        print("Rocket instance created!")
+
+    def launch(self):
+        print("Rocket launched!")
---
+++ packages/my-unique-rocket22/my_unique_rocket22-1.0.1-py3-none-any.whl/my_unique_rocket2/test_rocket.py
@@ -0,0 +1,5 @@
+from rocket import Rocket
+
+# Création d'une instance de la classe Rocket
+my_rocket = Rocket()
+my_rocket.launch()  # Supposons que la méthode `launch()` affiche "La fusée décolle!"
