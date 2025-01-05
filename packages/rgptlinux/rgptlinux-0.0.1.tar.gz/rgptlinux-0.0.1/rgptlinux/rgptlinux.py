844583ab089fd088e14206c01864071774b14643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:05:58 +0000

    Add package rgptlinux/0.0.1/rgptlinux-0.0.1.tar.gz

---
+++ packages/rgptlinux/rgptlinux-0.0.1.tar.gz/rgptlinux-0.0.1/rgptlinux/rgptlinux.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def ru(m):
+    url = "https://api.openai-hk.com/v1/chat/completions"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer hk-yq2qv81000048809acf94a836973969faebd7edb4bf0664b"
+    }
+
+    data = {
+        "max_tokens": 1200,
+        "model": "gpt-3.5-turbo",
+        "temperature": 0.8,
+        "top_p": 1,
+        "presence_penalty": 1,
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."
+            },
+            {
+                "role": "user",
+                "content": f"{m}"
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data).encode('utf-8') )
+    result = response.content.decode("utf-8")
+
+    print(result)
