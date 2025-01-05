150c08b74173c880e33efa2a98c825ef0a987c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:48 +0000

    Add package controller-companion/0.7.1/controller_companion-0.7.1.tar.gz

---
+++ packages/controller-companion/controller_companion-0.7.1.tar.gz/controller_companion-0.7.1/controller_companion/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+from pathlib import Path
+
+APP_NAME = "Controller Companion"
+VERSION = f'v{version("controller-companion")}'
+PACKAGE_DIR = Path(__file__).parent.absolute()
+CONFIG_PATH = Path.home() / "Documents" / "Controller Companion" / "settings.json"
