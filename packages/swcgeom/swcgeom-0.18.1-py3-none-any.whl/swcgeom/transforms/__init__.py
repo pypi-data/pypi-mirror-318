9bab0c2ac782e6412c62edb4db4c9ff5427a9141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:12:08 +0000

    Add package swcgeom/0.18.1/swcgeom-0.18.1-py3-none-any.whl

---
+++ packages/swcgeom/swcgeom-0.18.1-py3-none-any.whl/swcgeom/transforms/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2022-2025 Zexin Yuan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""A series of transformations to compose codes."""
+
+from swcgeom.transforms.base import *
+from swcgeom.transforms.branch import *
+from swcgeom.transforms.branch_tree import *
+from swcgeom.transforms.geometry import *
+from swcgeom.transforms.image_preprocess import *
+from swcgeom.transforms.image_stack import *
+from swcgeom.transforms.images import *
+from swcgeom.transforms.mst import *
+from swcgeom.transforms.neurolucida_asc import *
+from swcgeom.transforms.path import *
+from swcgeom.transforms.population import *
+from swcgeom.transforms.tree import *
+from swcgeom.transforms.tree_assembler import *
