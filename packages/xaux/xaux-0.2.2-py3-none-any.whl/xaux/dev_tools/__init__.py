fe37a9eeb79f1767d0f757944d5ba6afeb1e8502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:03 +0000

    Add package xaux/0.2.2/xaux-0.2.2-py3-none-any.whl

---
+++ packages/xaux/xaux-0.2.2-py3-none-any.whl/xaux/dev_tools/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xaux Package.    #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .release_tools import make_release, make_release_branch, rename_release_branch
+from .package_manager import *
