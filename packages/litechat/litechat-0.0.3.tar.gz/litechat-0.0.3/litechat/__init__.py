d758f7933a8b4634fe9b77b684fe589716a67b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:41 +0000

    Add package litechat/0.0.3/litechat-0.0.3.tar.gz

---
+++ packages/litechat/litechat-0.0.3.tar.gz/litechat-0.0.3/litechat/__init__.py
@@ -0,0 +1,3 @@
+from ._core import completion,pp_completion,genai,LiteAI,OpenAI,litechat_server
+from ._codegpt import litechat_codegpt_server
+from ._gmail_auto import GmailAutomation,gmail,automail
