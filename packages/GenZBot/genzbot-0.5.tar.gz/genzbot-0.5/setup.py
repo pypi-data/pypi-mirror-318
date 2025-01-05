c1f49f6872843da117620797c0c77d10585fd511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:06:07 +0000

    Add package GenZBot/0.5/genzbot-0.5.tar.gz

---
+++ packages/GenZBot/genzbot-0.5.tar.gz/genzbot-0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GenZBot',
+    version='0.5',
+    author='Anurupa Karmakar',
+    author_email='anurupakarmakar.dgp18@gmail.com',
+    packages=find_packages(), 
+    package_data={'': ['Images_list/*']},
+    include_package_data=True,
+    description='Chatbot making packing',
+)
