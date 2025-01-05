660851173c3be82d0f9829078b9b0ba469910010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:16 +0000

    Add package ptpl-transcoder/1.0.1/ptpl-transcoder-1.0.1.tar.gz

---
+++ packages/ptpl-transcoder/ptpl-transcoder-1.0.1.tar.gz/ptpl-transcoder-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="ptpl-transcoder",
+    version="1.0.1",
+    py_modules=["transcoder"],
+    entry_points={
+        "console_scripts": [
+            "ptpl-transcoder=transcoder:main",
+        ],
+    },
+    install_requires=[
+        "lxml",
+    ],
+    description="A CLI tool to to decode .ptpl files into .xml and encode .xml files back into .ptpl format.",
+    author="KRW CLASSIC",
+    author_email="classic.krw@gmail.com",
+    url="https://github.com/KRWCLASSIC/PTPL-CLI-Transcoder",
+)
