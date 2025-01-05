a8359193eebde14f55aed29cd72f0cf91069bcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 14:04:35 +0000

    Add package tissuebox/2.0.1/tissuebox-2.0.1.tar.gz

---
+++ packages/tissuebox/tissuebox-2.0.1.tar.gz/tissuebox-2.0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+try:
+    fh = open("pypi.rst", "r")
+    long_description = fh.read()
+except FileNotFoundError:
+    long_description = ""
+
+setup(
+    name="tissuebox",
+    version="2.0.1",
+    description="Tissuebox :: Pythonic payload validator",
+    author="nehemiah",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author_email="nehemiah.jacob@gmail.com",
+    url="https://github.com/n3h3m/tissuebox.git",
+    packages=["tissuebox"],
+)
