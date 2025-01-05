5f2867600c316ae109b0b86ea1f0711f6f0695b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:06 +0000

    Add package miniworlds/3.0.2.8/miniworlds-3.0.2.8.tar.gz

---
+++ packages/miniworlds/miniworlds-3.0.2.8.tar.gz/miniworlds-3.0.2.8/miniworlds/base/manager/app_music_manager.py
@@ -0,0 +1,77 @@
+import pygame
+
+import miniworlds.base.app as app_mod
+
+
+class MusicManager:
+    def __init__(self, app: "app_mod.App"):
+        self.app: "app_mod.App" = app
+        self.path = None
+        pygame.mixer.init()
+
+    def change_music(self, path):
+        self.stop_music()
+        self.path = path
+        pygame.mixer.music.load(self.path)
+
+    def load_music(self, path):
+        self.path = path
+        pygame.mixer.music.load(self.path)
+
+    def play_music(self, path, loop):
+        """plays a music by path
+        """
+        if path:
+            self.load_music(path)
+        self.load_music(self.path)
+        pygame.mixer.music.play(loop)
+
+    def is_playing(self):
+        return pygame.mixer.music.get_busy()
+
+    def stop_music(self):
+        """stops current music
+        """
+        pygame.mixer.music.stop()
+
+    def fade_out(self, time):
+        """fades current music out
+        """
+        pygame.mixer.music.fadeout(time)
+
+    def fade_in(self, time):
+        """Alternative to play
+
+        Returns:
+
+        """
+        pygame.mixer.music.play(- 1, 0, time)
+
+    def pause(self):
+        pygame.mixer.music.pause()
+
+    def unpause(self):
+        pygame.mixer.music.unpause()
+
+    @property
+    def volume(self):
+        return self.get_volume()
+
+    @volume.setter
+    def volume(self, value):
+        return self.set_volume(value)
+
+    def set_volume(self, percent):
+        """Sets volume
+        min: 0
+        max: 100
+        """
+        pygame.mixer.music.set_volume(percent / 100)
+
+    def get_volume(self):
+        """Gets actual volume
+        min: 0
+        max: 100
+        """
+        return pygame.mixer.music.get_volume() * 100
+
