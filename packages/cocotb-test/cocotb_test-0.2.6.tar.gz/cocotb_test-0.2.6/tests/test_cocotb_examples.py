669ca10e51c699070dd867777fa91eff8f51d84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:54 +0000

    Add package cocotb-test/0.2.6/cocotb_test-0.2.6.tar.gz

---
+++ packages/cocotb-test/cocotb_test-0.2.6.tar.gz/cocotb_test-0.2.6/tests/test_cocotb_examples.py
@@ -0,0 +1,40 @@
+import os
+import cocotb
+import pytest
+
+from cocotb_test.compat import cocotb_2x_or_newer
+from cocotb_test.simulator import run
+
+if cocotb_2x_or_newer:
+    cocotb_repo_root = os.path.dirname(os.path.dirname(os.path.dirname(cocotb.__file__)))
+else:
+    cocotb_repo_root = os.path.dirname(os.path.dirname(cocotb.__file__))
+
+example_dir = os.path.join(cocotb_repo_root, "examples")
+
+if os.path.isdir(example_dir) == False:
+    raise IOError("Cocotb example directory not found. Please clone with git and install with `pip -e`")
+
+
+@pytest.mark.skipif(os.getenv("SIM") in ("ghdl", "nvc"), reason="Verilog not suported")
+def test_adder_verilog():
+    run(
+        verilog_sources=[os.path.join(example_dir, "adder", "hdl", "adder.sv")],
+        toplevel="adder",
+        python_search=[os.path.join(example_dir, "adder", "tests"), os.path.join(example_dir, "adder", "model")],
+        module="test_adder",
+        force_compile=True,
+    )
+
+
+@pytest.mark.skipif(os.getenv("SIM") in ("ghdl", "nvc"), reason="Errors in compilation cocotb example")
+@pytest.mark.skipif(os.getenv("SIM") in ["icarus", "verilator"], reason="VHDL not supported")
+def test_adder_vhdl():
+    run(
+        vhdl_sources=[os.path.join(example_dir, "adder", "hdl", "adder.vhdl")],
+        toplevel="adder",
+        python_search=[os.path.join(example_dir, "adder", "tests"), os.path.join(example_dir, "adder", "model")],
+        module="test_adder",
+        toplevel_lang="vhdl",
+        force_compile=True,
+    )
---
+++ packages/cocotb-test/cocotb_test-0.2.6.tar.gz/cocotb_test-0.2.6/tests/test_multitop.py
@@ -0,0 +1,38 @@
+from cocotb_test.simulator import run
+import pytest
+import os
+
+import cocotb
+from cocotb.triggers import Timer, ReadOnly
+
+tests_dir = os.path.dirname(__file__)
+
+@pytest.mark.skipif(os.getenv("SIM") not in ("icarus", "questa"), reason="Multitop feature only supported for Icarus and Questa")
+def test_dff_verilog():
+    run(
+        verilog_sources=[
+            os.path.join(tests_dir, "glbl.v"),
+            os.path.join(tests_dir, "glbl_sink.v"),
+        ],
+        toplevel=["glbl_sink", "glbl"],
+        module="test_multitop",
+        # time unit and precision set the same
+        timescale="1ns/1ns",
+    )
+
+
+@cocotb.test()
+async def glbl(dut):
+    await ReadOnly()
+    assert dut.rst.value != 0
+
+    # BEWARE: Timer(10) is equal to Timer(10, 'step') which is ten simulator *precision* steps.
+    # Only the same as Verilog delay '#10' (ten *time units*) when timescale directive sets
+    # the time unit same as precision ie. with timescale="1ns/1ns" or "1ps/1ps"
+    await Timer(10)
+    await ReadOnly()
+    assert dut.rst.value != 1
+
+
+if __name__ == "__main__":
+    test_dff_verilog()
