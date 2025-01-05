b10346455f3984e27b920555dd363c11bf5f3b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:12:10 +0000

    Add package Oge-distributions/0.1/Oge_distributions-0.1.tar.gz

---
+++ packages/Oge-distributions/Oge_distributions-0.1.tar.gz/Oge_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Oge_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Oge_distributions'],
+      zip_safe=False)
