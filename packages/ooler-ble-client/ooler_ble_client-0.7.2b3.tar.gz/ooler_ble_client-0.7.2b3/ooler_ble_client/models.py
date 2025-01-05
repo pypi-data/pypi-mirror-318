9d2411cddb47140174fa5224a94244509b0e2cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:05:49 +0000

    Add package ooler-ble-client/0.7.2b3/ooler_ble_client-0.7.2b3.tar.gz

---
+++ packages/ooler-ble-client/ooler_ble_client-0.7.2b3.tar.gz/ooler_ble_client-0.7.2b3/ooler_ble_client/models.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+@dataclass
+class OolerBLEState:
+    power: bool | None = None
+    mode: str | None = None
+    set_temperature: int | None = None
+    actual_temperature: int | None = None
+    pump_watts: int | None = None
+    water_level: int | None = None
+    # pump_watts: int | None = None
+    # clean: bool | None = None
+    connected: bool = False
