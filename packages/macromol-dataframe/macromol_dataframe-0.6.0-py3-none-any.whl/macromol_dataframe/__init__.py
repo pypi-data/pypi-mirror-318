bbf2acd60d8b61637d73d2c38ac8d1e44790087f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:15 +0000

    Add package macromol-dataframe/0.6.0/macromol_dataframe-0.6.0-py3-none-any.whl

---
+++ packages/macromol-dataframe/macromol_dataframe-0.6.0-py3-none-any.whl/macromol_dataframe/__init__.py
@@ -0,0 +1,11 @@
+"""
+Manipulate macromolecular coordinate data using data frames.
+"""
+
+__version__ = '0.6.0'
+
+from .mmcif import *
+from .pymol import *
+from .atoms import *
+from .coords import *
+from .error import *
