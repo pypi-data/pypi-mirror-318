0443851d3f100571ab577a45617b6df9f84b89d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 16:07:43 +0000

    Add package wadler-lindig/0.1.0/wadler_lindig-0.1.0.tar.gz

---
+++ packages/wadler-lindig/wadler_lindig-0.1.0.tar.gz/wadler_lindig-0.1.0/wadler_lindig/_doc_utils.py
@@ -0,0 +1,13 @@
+import typing
+
+_T = typing.TypeVar("_T")
+
+class _DocType(type):
+    pass
+
+
+def doc_obj(x: _T, doc: str) -> _T:
+    if getattr(typing, "GENERATING_DOCUMENTATION", False):
+        return _DocType("_", (), dict(__doc__=doc))  # pyright: ignore[reportReturnType]
+    else:
+        return x
