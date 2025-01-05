3017877dcfd06d0438027532915ae8eb7f502e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:23 +0000

    Add package data_market_index_fetcher/1.1.1/data_market_index_fetcher-1.1.1.tar.gz

---
+++ packages/data_market_index_fetcher/data_market_index_fetcher-1.1.1.tar.gz/data_market_index_fetcher-1.1.1/src/data_market_index_fetcher/indexes/ibov/TestIbovWebScrapper.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import logging
+from data_market_index_fetcher.indexes.ibov.IbovWebScrapperB3 import IBovWebScrapperB3, Periodicity
+
+# Suppress logs from IBovWebScrapperB3
+logging.getLogger("IBovWebScrapper").setLevel(logging.ERROR)
+logging.getLogger("LoggerUtil").setLevel(logging.ERROR)
+logging.getLogger("WebDriverUtil").setLevel(logging.ERROR)
+logging.getLogger("SeleniumUtil").setLevel(logging.ERROR)
+
+ibov_scrapper=IBovWebScrapperB3()
+
+periodicity_mapping = {
+    Periodicity.DAILY: "D",
+    Periodicity.WEEKLY: "W",
+    Periodicity.MONTHLY: "ME",
+    Periodicity.QUARTERLY: "Q",
+    Periodicity.SEMESTRAL: "2Q",
+    Periodicity.ANNUAL: "YE"
+}
+
+
+# Loop through each periodicity and fetch data
+# Loop through each periodicity and fetch data
+for periodicity, period_code in periodicity_mapping.items():
+    # periodicity is a Periodicity enum, e.g., Periodicity.DAILY
+    print(f"\nFetching data with {periodicity.value} periodicity.")
+    
+    # Pass the enum to fetch_data
+    ibov_data = ibov_scrapper.fetch_data('2020-10-01', '2025-10-01', periodicity)
+    
+    print(f"{periodicity.value.capitalize()} Data:")
+    print(ibov_data)
+
