873a187cc93cc411ef6f916b8b8738001acd0842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:42 +0000

    Add package apksearch/1.2.5/apksearch-1.2.5-py3-none-any.whl

---
+++ packages/apksearch/apksearch-1.2.5-py3-none-any.whl/apksearch/__init__.py
@@ -0,0 +1,7 @@
+from .sites.apkpure import APKPure
+from .sites.apkmirror import APKMirror
+from .sites.appteka import AppTeka
+from .sites.apkcombo import APKCombo
+from .sites.apkfab import APKFab
+
+__all__ = ["APKPure", "APKMirror", "AppTeka", "APKCombo", "APKFab"]
