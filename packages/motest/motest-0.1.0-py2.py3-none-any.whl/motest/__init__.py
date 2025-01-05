7fc261958271048464bafdaef3c4eec73df41929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:12:06 +0000

    Add package motest/0.1.0/motest-0.1.0-py2.py3-none-any.whl

---
+++ packages/motest/motest-0.1.0-py2.py3-none-any.whl/motest/__init__.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import pytest
+from pathlib import Path
+
+def motest(mofile: Path, *args):    
+    temp_path = Path("test_" + mofile.parts[-1])
+    
+    # Export the marimo notebook to Python
+    export_cmd = f"marimo export script --output={temp_path} {mofile}"
+    subprocess.run(export_cmd.split(), check=True)
+
+    retcode = pytest.main([str(temp_path), *args])
+    
+    # Clean up temporary file
+    temp_path.unlink()
+
+    # Relay exit code
+    return retcode
+
+if __name__ == "__main__":
+    mofile = Path(sys.argv[1])
+    retcode = motest(mofile, *sys.argv[2:])
+    # Exit with pytest exit code
+    exit(int(retcode))
+
