c60457e3beae2c6c4682adaa145c9c10ecbda3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:38 +0000

    Add package netcook-v2/2.5/netcook_v2-2.5.tar.gz

---
+++ packages/netcook-v2/netcook_v2-2.5.tar.gz/netcook_v2-2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netcook_v2',
+    version='2.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.31.0',
+        'tabulate==0.9.0',
+        'beautifulsoup4==4.12.3',
+        'googletrans==4.0.0-rc1',
+        'pyunpack==0.3',
+        'rarfile==4.2',
+        'PyDrive2==1.6.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'netcook_v2=netcook_v2.netcook:checked_cookies',
+        ],
+    },
+    author='NetCook',
+    description='A package to check Netflix cookies status.',
+    long_description='A package to check Netflix cookies status.',
+    long_description_content_type='text/plain',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
