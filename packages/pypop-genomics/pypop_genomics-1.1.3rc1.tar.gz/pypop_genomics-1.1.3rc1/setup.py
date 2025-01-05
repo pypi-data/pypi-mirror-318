a727a432439e0644680bd5281a88a0e75e26bdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:05:57 +0000

    Add package pypop-genomics/1.1.3rc1/pypop_genomics-1.1.3rc1.tar.gz

---
+++ packages/pypop-genomics/pypop_genomics-1.1.3rc1.tar.gz/pypop_genomics-1.1.3rc1/setup.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+
+# This file is part of PyPop
+
+# Copyright (C) 2003-2007.
+# The Regents of the University of California (Regents)
+# All Rights Reserved.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+# IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT,
+# INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+# LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+# DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED OF THE POSSIBILITY
+# OF SUCH DAMAGE.
+
+# REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING
+# DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS
+# IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+# UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+import sys, os
+from glob import glob
+from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.install import install as _install
+from distutils.command import clean
+from sysconfig import get_config_vars, get_config_var
+from src.PyPop import __pkgname__, __version_scheme__
+
+src_dir = "src"
+pkg_dir = "PyPop"
+
+# distutils doesn't currently have an explicit way of setting CFLAGS,
+# it takes CFLAGS from the environment variable of the same name, so
+# we set the environment to emulate that.
+#os.environ['CFLAGS'] = '-funroll-loops'
+
+class CleanCommand(clean.clean):
+    """Customized clean command - removes in_place extension files if they exist"""
+    def run(self):
+        DIR = os.path.join(os.path.dirname(__file__), src_dir)
+        # generate glob pattern from extension name and suffix
+        ext_files = [os.path.join(DIR, pkg_dir, ext.name.split(pkg_dir + '.').pop() + ("*.pyd" if sys.platform == "win32" else "*.so")) for ext in extensions]
+        for ext_file in ext_files:
+            for ext_file in glob(ext_file):
+                if os.path.exists(ext_file):
+                    print("Removing in-place extension {}".format(ext_file))
+                    os.unlink(ext_file)
+        clean.clean.run(self)
+
+# look for libraries in _PREFIX
+prefix = get_config_var('prefix')
+library_dirs = [os.path.join(prefix, "lib")]
+include_dirs = [os.path.join(prefix, "include")]
+# also look in LIBRARY_PATH, CPATH (needed for macports etc.)
+if "LIBRARY_PATH" in os.environ:
+    library_dirs += os.environ["LIBRARY_PATH"].rstrip(os.pathsep).split(os.pathsep)
+if "CPATH" in os.environ:
+    include_dirs += os.environ["CPATH"].rstrip(os.pathsep).split(os.pathsep)
+
+# generate the appropriate relative path to source directory, given
+# paths within that source directory (this means we need to define
+# this directory in just one place
+def path_to_src(source_path_list):
+    new_source_list = []
+    for file_path in source_path_list:
+        new_source_list.append(os.path.join(src_dir, file_path))
+    return new_source_list
+
+
+swig_opts = ["-I%s" % os.path.join(src_dir, "SWIG"), "-I%s" % os.path.join(src_dir)]
+
+# define each extension
+ext_Emhaplofreq = Extension("PyPop._Emhaplofreq",
+                            path_to_src(["emhaplofreq/emhaplofreq_wrap.i",
+                                         "emhaplofreq/emhaplofreq.c"
+                                         ]),
+                            swig_opts = swig_opts,
+                            include_dirs=include_dirs + path_to_src(["emhaplofreq"]),
+                            define_macros=[('__SWIG__', '1'),
+                                           ('DEBUG', '0'),
+                                           ('EXTERNAL_MODE', '1'),
+                                           ('XML_OUTPUT', '1')]
+                            )
+ext_EWSlatkinExact = Extension("PyPop._EWSlatkinExact",
+                               path_to_src(["slatkin-exact/monte-carlo_wrap.i",
+                                "slatkin-exact/monte-carlo.c"]),
+                               swig_opts = swig_opts,
+                               include_dirs=include_dirs,
+                               )
+
+ext_Pvalue = Extension("PyPop._Pvalue",
+                       path_to_src(["pval/pval_wrap.i",
+                        "pval/pval.c",
+                        "pval/pchisq.c",
+                        "pval/chebyshev.c",
+                        "pval/ftrunc.c",
+                        "pval/lgamma.c",
+                        "pval/mlutils.c",
+                        "pval/pgamma.c",
+                        "pval/fmin2.c",
+                        "pval/fmax2.c",
+                        "pval/dnorm.c",
+                        "pval/dpois.c",
+                        "pval/gamma.c",
+                        "pval/bd0.c",
+                        "pval/stirlerr.c",
+                        "pval/lgammacor.c",
+                        "pval/pnorm.c"]),
+                       swig_opts = swig_opts,
+                       include_dirs=include_dirs + path_to_src(["pval"]),
+                       define_macros=[('MATHLIB_STANDALONE', '1')]
+                       )
+
+ext_Gthwe_files = path_to_src(["gthwe/gthwe_wrap.i",
+                   "gthwe/hwe.c",
+                   "gthwe/cal_const.c",
+                   "gthwe/cal_n.c",
+                   "gthwe/cal_prob.c",
+                   "gthwe/check_file.c",
+                   "gthwe/do_switch.c",
+                   "gthwe/new_rand.c",
+                   "gthwe/ln_p_value.c",
+                   "gthwe/to_calculate_log.c",
+                   "gthwe/print_data.c",
+                   "gthwe/random_choose.c",
+                   "gthwe/read_data.c",
+                   "gthwe/select_index.c",
+                   "gthwe/stamp_time.c",
+                   "gthwe/test_switch.c",
+                   "gthwe/statistics.c"])
+
+
+ext_Gthwe_macros = [('__SWIG__', '1'),
+                    ('DEBUG', '0'),
+                    ('XML_OUTPUT', '1'),
+                    ('SUPPRESS_ALLELE_TABLE', '1'),
+                    ('INDIVID_GENOTYPES', '1')]
+
+ext_Gthwe = Extension("PyPop._Gthwe",
+                      ext_Gthwe_files,
+                      swig_opts = swig_opts,
+                      include_dirs=include_dirs + path_to_src(["gthwe"]),
+                      library_dirs=library_dirs,
+                      libraries=["gsl", "gslcblas"],
+                      define_macros=ext_Gthwe_macros
+                      )
+
+ext_Haplostats = Extension("PyPop._Haplostats",
+                       path_to_src(["haplo-stats/haplostats_wrap.i",
+                        "haplo-stats/haplo_em_pin.c"]),
+                       swig_opts = swig_opts,
+                       include_dirs=include_dirs + path_to_src(["haplo-stats", "pval"]),
+                       define_macros=[('MATHLIB_STANDALONE', '1'),
+                                      ('__SWIG__', '1'),
+                                      ('DEBUG', '0'),
+                                      ('R_NO_REMAP', '1')]
+                       )
+
+ext_HweEnum = Extension("PyPop._HweEnum",
+                      path_to_src(["hwe-enumeration/src/hwe_enum_wrap.i",
+                        "hwe-enumeration/src/hwe_enum.c",
+                        "hwe-enumeration/src/factorial.c",
+                        "hwe-enumeration/src/main.c",
+                        "hwe-enumeration/src/common.c",
+                        "hwe-enumeration/src/statistics.c",
+                        "hwe-enumeration/src/external.c"
+                        ]),
+                        swig_opts = swig_opts,
+                        include_dirs=include_dirs + [path_to_src(["hwe-enumeration/src/include"]),
+                                                     "/usr/include/glib-2.0",
+                                                     "/usr/include/glib-2.0/include",
+                                                     "/usr/lib/glib-2.0/include",
+                                                     "/usr/lib64/glib-2.0/include/",
+                                                     "/usr/include/libxml2",
+                                                     "/usr/include/gsl",
+                                                     ],
+                        libraries=["glib-2.0", "xml2", "popt",
+                                   "m", "gsl", "gslcblas"],
+                        define_macros=[('__SORT_TABLE__', '1'),
+                                       ('g_fprintf', 'pyfprintf'),
+                                       ('VERSION', '"internal"'),
+                                       ('PACKAGE_NAME','"hwe-enumeration"'),
+                                       ('INDIVID_GENOTYPES', '1'),
+                                       ('HAVE_LIBGSL', '1')]
+                        )
+
+ext_Emhaplofreq.depends=path_to_src(["SWIG/typemap.i", "emhaplofreq/emhaplofreq.h", "emhaplofreq/drand48.c"])
+ext_Pvalue.depends=path_to_src(["SWIG/typemap.i", "pval/Rconfig.h", "pval/Rmath.h", "pval/dpq.h", "pval/nmath.h"])
+ext_Gthwe.depends=path_to_src(["SWIG/typemap.i", "gthwe/func.h", "gthwe/hwe.h"])
+ext_Haplostats.depends=path_to_src(["SWIG/typemap.i", "haplo-stats/haplo_em_pin.h"])
+
+# default list of extensions to build
+extensions = [ext_Emhaplofreq, ext_EWSlatkinExact, ext_Pvalue, ext_Haplostats, ext_Gthwe]
+
+# don't include HWEEnum
+# extensions.append(ext_HweEnum)
+
+xslt_data_file_paths = []
+# xslt files are in a subdirectory
+xslt_files = [f + '.xsl' for f in ['text', 'html', 'lib', 'common', 'filter', 'hardyweinberg', 'homozygosity', 'emhaplofreq', 'meta-to-tsv', 'sort-by-locus', 'haplolist-by-group', 'phylip-allele', 'phylip-haplo']]
+xslt_data_file_paths.extend(xslt_files)
+
+citation_data_file_paths = []
+# citation files are in a subdirectory of PyPop, but not a separate module
+from src.PyPop.citation import citation_output_formats, convert_citation_formats
+citation_files = [os.path.join("citation", 'CITATION.' + suffix) for suffix in citation_output_formats]
+citation_data_file_paths.extend(citation_files)
+
+# currently disabled (these are built in a github action)
+class CustomBuildPy(_build_py):
+    def run(self):
+
+        # do standard build process
+        super().run()
+
+        # if not running from a CIBUILDWHEEL environment variable
+        # we need to create the citations
+        if os.environ.get('CIBUILDWHEEL') != '1':
+
+            # source citation path (single-source of truth)
+            citation_path = "CITATION.cff"
+
+            # then copy CITATION.cff to temp build directory
+            # use setuptools' temp build directory
+            build_lib = self.get_finalized_command('build').build_lib
+
+            convert_citation_formats(build_lib, citation_path)
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+setup (name = __pkgname__,
+       use_scm_version={
+           'write_to': os.path.join(src_dir, pkg_dir, "_version.py"),
+           'version_scheme': __version_scheme__,
+       },
+       description = "PyPop: Python for Population Genomics",
+       long_description=long_description,
+       long_description_content_type='text/x-rst',
+       url = "http://www.pypop.org/",
+       project_urls={
+           'Documentation': 'http://pypop.org/docs/',
+           'Changelog': 'https://github.com/alexlancaster/pypop/blob/main/NEWS.md',
+           'Source': 'https://github.com/alexlancaster/pypop/',
+           'Tracker': 'https://github.com/alexlancaster/pypop/issues',
+       },
+       author = "Alex Lancaster",
+       maintainer = "PyPop team",
+       license = "GNU GPL",
+       platforms = ["GNU/Linux", "Windows", "MacOS"],
+       keywords = ["bioinformatics", "population-genomics", "evolutionary-biology", "population-genetics"],
+       classifiers = ["Programming Language :: Python :: 3",
+                      "Intended Audience :: Science/Research",
+                      "Topic :: Scientific/Engineering :: Bio-Informatics",
+                      "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+                      "Operating System :: MacOS :: MacOS X",
+                      "Operating System :: Microsoft :: Windows",
+                      "Operating System :: POSIX :: Linux",
+                      ],
+       package_dir = {"": src_dir},
+       packages = ["PyPop", "PyPop.xslt"],
+       package_data = {"PyPop.xslt": xslt_data_file_paths,
+                       "PyPop": citation_data_file_paths},
+       install_requires = ["numpy <= 2.2.1",
+                           "lxml <= 5.3.0",
+                           "importlib-resources; python_version <= '3.8'",
+                           "importlib-metadata; python_version <= '3.8'"],
+       extras_require={
+           "test": ['pytest']
+           # FIXME:  "psutil <= 5.9.5", not currently used, 5.9.6 and later had problems with building on Windows PyPy
+           },
+       entry_points = {
+           'console_scripts': ['pypop=PyPop.pypop:main',
+                               'popmeta=PyPop.popmeta:main',
+                               'pypop-interactive=PyPop.pypop:main_interactive']
+       },
+       ext_modules=extensions,
+       cmdclass={'clean': CleanCommand,
+                 # enable the custom build
+                 'build_py': CustomBuildPy,
+                 },
+       )
---
+++ packages/pypop-genomics/pypop_genomics-1.1.3rc1.tar.gz/pypop_genomics-1.1.3rc1/src/PyPop/xslt/cdv.xsl
@@ -0,0 +1,136 @@
+<!--
+This file is part of PyPop
+
+  Copyright (C) 2003. The Regents of the University of California (Regents)
+  All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
+ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION
+TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+MODIFICATIONS.
+-->
+<xsl:stylesheet
+ version='1.0'
+ xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+ xmlns:exsl="http://exslt.org/common"
+ xmlns:str="http://exslt.org/strings"
+ extension-element-prefixes="exsl str">
+
+ <xsl:import href="lib.xsl"/>
+
+ <xsl:output method="text" omit-xml-declaration="yes"/>
+
+ <xsl:template match="text()"/>
+
+ <!--
+ <xsl:param name="limit" select="0.02"/>
+ -->
+ <xsl:param name="limit" select="20"/>
+
+ <xsl:template match="/">
+  <xsl:apply-templates/>
+  <!--
+  <xsl:for-each match"emhaplofreq/group[(string-length(@loci) -
+   string-length(translate(@loci, ':', '')))=2 and @role!='no-data']">
+   -->
+ </xsl:template>
+
+ <xsl:template match="emhaplofreq/group[(string-length(@loci) -
+ string-length(translate(@loci, ':', '')))=2 and @role!='no-data']">
+
+  <xsl:variable name="loci-array" select="str:tokenize(@loci, ':')"/>
+
+  <xsl:variable name="locus1" select="$loci-array[1]"/>
+  <xsl:variable name="locus2" select="$loci-array[2]"/>
+  <xsl:variable name="locus3" select="$loci-array[3]"/>
+
+  <xsl:variable name="locus-pair12" select="concat($locus1,':',$locus2)"/>
+  <xsl:variable name="locus-pair13" select="concat($locus1,':',$locus3)"/>
+  <xsl:variable name="locus-pair23" select="concat($locus2,':',$locus3)"/>
+
+  <!--
+  <xsl:for-each select="haplotypefreq/haplotype[frequency &gt;= $limit]">
+  -->
+  <xsl:for-each select="haplotypefreq/haplotype[numCopies &gt;= $limit]">
+
+   <xsl:sort select="frequency"/>
+
+   <xsl:variable name="allele-array" select="str:tokenize(substring(@name, 1,
+    string-length(@name)-1), ':')"/>
+
+   <xsl:variable name="allele1" select="$allele-array[1]"/>
+   <xsl:variable name="allele2" select="$allele-array[2]"/>
+   <xsl:variable name="allele3" select="$allele-array[3]"/>
+
+   <xsl:variable name="allele-name12" select="concat($allele1,':',$allele2,':')"/>
+   <xsl:variable name="allele-name13" select="concat($allele1,':',$allele3,':')"/>
+   <xsl:variable name="allele-name23" select="concat($allele2,':',$allele3,':')"/>
+
+   <xsl:value-of select="$locus1"/>  <!-- output the 3-locus haplotype -->
+   <xsl:text>*</xsl:text>
+   <xsl:value-of select="$allele1"/>
+   <xsl:text> </xsl:text>
+   <xsl:value-of select="$locus2"/>
+   <xsl:text>*</xsl:text>
+   <xsl:value-of select="$allele2"/>
+   <xsl:text> </xsl:text>
+   <xsl:value-of select="$locus3"/>
+   <xsl:text>*</xsl:text>
+   <xsl:value-of select="$allele3"/>
+   <xsl:text>   </xsl:text>
+   <xsl:value-of select="frequency"/> <!-- followed by its frequency -->
+
+   <xsl:call-template name="newline"/>
+
+   <xsl:value-of select="/dataanalysis/locus[@name=$locus1]/allelecounts/allele[@name=$allele1]/frequency"/>
+
+   <xsl:value-of select="/dataanalysis/emhaplofreq/group[@loci=$locus-pair12]/haplotypefreq/haplotype[@name=$allele-name12]/frequency"/>
+   <xsl:text> </xsl:text>
+   <xsl:value-of select="/dataanalysis/emhaplofreq/group[@loci=$locus-pair13]/haplotypefreq/haplotype[@name=$allele-name13]/frequency"/>
+
+   <xsl:call-template name="newline"/>
+
+   <xsl:value-of select="/dataanalysis/locus[@name=$locus2]/allelecounts/allele[@name=$allele2]/frequency"/>
+   <xsl:value-of select="/dataanalysis/emhaplofreq/group[@loci=$locus-pair23]/haplotypefreq/haplotype[@name=$allele-name23]/frequency"/>
+   <xsl:call-template name="newline"/>
+
+   <xsl:value-of select="/dataanalysis/locus[@name=$locus3]/allelecounts/allele[@name=$allele3]/frequency"/>
+
+   <xsl:call-template name="newline"/>
+
+  </xsl:for-each>
+
+ </xsl:template>
+
+</xsl:stylesheet>
+
+<!--
+Local variables:
+mode: xml
+sgml-default-dtd-file: "xsl.ced"
+sgml-indent-step: 1
+sgml-indent-data: 1
+End:
+-->
