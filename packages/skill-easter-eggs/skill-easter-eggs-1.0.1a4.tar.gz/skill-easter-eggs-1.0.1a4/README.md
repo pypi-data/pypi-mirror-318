bf7c7512d2e55d1ce845ea72261cb552d318e233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:05:13 +0000

    Add package skill-easter-eggs/1.0.1a4/skill-easter-eggs-1.0.1a4.tar.gz

---
+++ packages/skill-easter-eggs/skill-easter-eggs-1.0.1a4.tar.gz/skill-easter-eggs-1.0.1a4/README.md
@@ -0,0 +1,35 @@
+# Easter Eggs
+
+pop culture references skill
+
+## Description
+
+Some funny things for OpenVoiceOS, mostly quotes from movies
+
+## Examples
+
+- "Current stardate?"
+- "Open the pod bay doors."
+- "First law of robotics?"
+- "How to play rock paper scissors lizard spock?"
+- "How many languages do you speak?"
+- "Do you like Portal?"
+- "What would Duke Nukem say?"
+- "What would HAL 9000 say?"
+- "What would Arnold say?"
+- "What would GLaDOS say?"
+- "What would Bender say?"
+- "What would Malibu Stacey say?"
+- "You know the best band in the world? Wyld Stallions?"
+- "Sing me a song"
+
+## Grandma Mode
+
+This skill has some spicy language options. It starts in grandma mode by default. To disable grandma mode, say "hurt me plenty."
+
+If you want to go back into grandma mode, say "I'm too young to die."
+
+## Credits
+
+- JarbasAI
+- mikejgray (revival)
---
+++ packages/skill-easter-eggs/skill-easter-eggs-1.0.1a4.tar.gz/skill-easter-eggs-1.0.1a4/constants.py
@@ -0,0 +1,53 @@
+ANNUAL = 31556952  # seconds in a year, used for scheduling annual events
+
+SPICY_SOUNDS = [
+    "sounds/arnold/bullsh_dont.wav",
+    "sounds/arnold/fist.wav",
+    "sounds/arnold/no_sh.wav",
+    "sounds/arnold/sob.wav",
+    "sounds/arnold/t1_fu.wav",
+    "sounds/arnold/ugly.wav",
+    "sounds/bender/Bodies are for hookers.mp3",
+    "sounds/bender/Compare your lives to mine.mp3",
+    "sounds/bender/Dont make me kick your neck.mp3",
+    "sounds/bender/Im gonna go kill my self.mp3",
+    "sounds/bender/Lets go get drunk.mp3",
+    "sounds/bender/That dirty bastard.mp3",
+    "sounds/bender/Wanna kill all humans.mp3",
+]
+
+ASCII_SNOW = """
+                     *  .  *
+                   . _\/ \/_ .
+                    \  \ /  /             .      .
+      ..    ..    -==>: X :<==-           _\/  \/_
+      '\    /'      / _/ \_ \              _\/\/_
+        \\//       '  /\ /\  '         _\_\_\/\/_/_/_
+   _.__\\\///__._    *  '  *            / /_/\/\_\ \
+    '  ///\\\  '                           _/\/\_
+        //\\                               /\  /\
+      ./    \.             ._    _.       '      '
+      ''    ''             (_)  (_)                  <> \  / <>
+                            .\::/.                   \_\/  \/_/
+           .:.          _.=._\\//_.=._                  \\//
+      ..   \o/   ..      '=' //\\ '='             _<>_\_\<>/_/_<>_
+      :o|   |   |o:         '/::\'                 <> / /<>\ \ <>
+       ~ '. ' .' ~         (_)  (_)      _    _       _ //\\ _
+           >O<             '      '     /_/  \_\     / /\  /\ \
+       _ .' . '. _                        \\//       <> /  \ <>
+      :o|   |   |o:                   /\_\\><\\ \/
+           ':'        . ~~\  /~~ .       _//\\_
+jgs                   _\_._\/_._/_      \_\  /_/
+                       / ' /\ ' \                   \o/
+       o              ' __/  \__ '              _o/.:|:.\o_
+  o    :    o         ' .'|  |'.                  .\:|:/.
+    '.\'/.'                 .                 -=>>::>o<::<<=-
+    :->@<-:                 :                   _ '/:|:\' _
+    .'/.\'.           '.___/*\___.'              o\':|:'/o
+  o    :    o           \* \ / */                   /o\
+       o                 >--X--<
+                        /*_/ \_*\
+                      .'   \*/   '.
+                            :
+                            '
+"""
