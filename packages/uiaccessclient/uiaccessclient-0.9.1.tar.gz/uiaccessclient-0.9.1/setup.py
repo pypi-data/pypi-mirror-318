fb1480880bbd891808803a3b12b1c9744925e4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 04:04:57 +0000

    Add package uiaccessclient/0.9.1/uiaccessclient-0.9.1.tar.gz

---
+++ packages/uiaccessclient/uiaccessclient-0.9.1.tar.gz/uiaccessclient-0.9.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uiaccessclient",
+    version="0.9.1",
+    description="UniFi Access API",
+    keywords=["UniFi Access API"],
+    install_requires=[
+        "urllib3 >= 1.25.3, < 3.0.0",
+        "python_dateutil >= 2.8.2",
+        "pydantic >= 2",
+        "typing-extensions >= 4.7.1",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
