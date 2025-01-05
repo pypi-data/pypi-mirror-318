6e01120dc806631ad11b85c7bc3cfabca2b37bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 15:05:57 +0000

    Add package knify/1.6.76/knify-1.6.76.tar.gz

---
+++ packages/knify/knify-1.6.76.tar.gz/knify-1.6.76/knify/help.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+def get_pip_name():
+    return 'knify'
+
+def get_version():
+    return '1.6.76'
+
+def print_version():
+    print('''Knify %s
+Development tools for python.''' % get_version())
---
+++ packages/knify/knify-1.6.76.tar.gz/knify-1.6.76/knify/threadutil.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+import threading
+from . import listutil
+from . import logger
+
+def thread_partition_call(list_obj: list, func_, thread_num: int, partiton_num: int) -> None:
+    list_partiton = listutil.partition(list_obj, partiton_num)
+    threads = []
+    logger.info("=============start=============")
+    for list_for_process in list_partiton:
+        t = threading.Thread(target=func_, args=(list_for_process,))
+        t.start()
+        threads.append(t)
+        if len(threads) == thread_num:
+            for t_ in threads:
+                t_.join()
+            threads = []
+            logger.info("=============start=============")
+
