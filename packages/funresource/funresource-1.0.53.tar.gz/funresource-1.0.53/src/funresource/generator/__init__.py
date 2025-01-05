734e9559314d468c95bf2572da92b2f469220e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-05 03:11:52 +0000

    Add package funresource/1.0.53/funresource-1.0.53.tar.gz

---
+++ packages/funresource/funresource-1.0.53.tar.gz/funresource-1.0.53/src/funresource/generator/__init__.py
@@ -0,0 +1,9 @@
+from .acoooder import AcoooderGenerate
+from .rss import RSSGenerate
+from .telegram import TelegramChannelGenerate
+
+__all__ = [
+    "AcoooderGenerate",
+    "RSSGenerate",
+    "TelegramChannelGenerate",
+]
