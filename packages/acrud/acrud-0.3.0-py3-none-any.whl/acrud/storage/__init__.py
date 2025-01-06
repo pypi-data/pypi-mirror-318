d3cd5a50c2a643f5f20e716ebcee7abae1362b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:12:01 +0000

    Add package acrud/0.3.0/acrud-0.3.0-py3-none-any.whl

---
+++ packages/acrud/acrud-0.3.0-py3-none-any.whl/acrud/storage/__init__.py
@@ -0,0 +1,4 @@
+from .base import StorageBase, BaseModel
+from .google_drive import GoogleDriveStorage, GoogleDriveStorageConfig
+from .local import LocalStorage, LocalStorageConfig
+from .s3 import S3Storage, S3StorageConfig
