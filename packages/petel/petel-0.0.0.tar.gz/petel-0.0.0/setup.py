fe2ec4e325691e99bf152789934d65a208caefad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:45 +0000

    Add package petel/0.0.0/petel-0.0.0.tar.gz

---
+++ packages/petel/petel-0.0.0.tar.gz/petel-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='petel',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='yuda',
+    author_email='yuda03979@gmail.com',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/ollamamia',
+    classifiers=[],
+    python_requires='>=3.8',
+)
