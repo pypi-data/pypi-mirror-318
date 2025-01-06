9074232b174e30c74cdc677f4b40db558b3fb6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:49 +0000

    Add package atash/0.1.6/atash-0.1.6-py3-none-any.whl

---
+++ packages/atash/atash-0.1.6-py3-none-any.whl/atash/__init__.py
@@ -0,0 +1,24 @@
+from .atash import *
+
+__all__ = [
+    "connect_to_openeo",
+    "load_map",
+    "getBAP",
+    "get_date_input",
+    "get_start_and_end_dates",
+    "load_pre_ndvi",
+    "plot_pre_ndvi",
+    "load_post_ndvi",
+    "plot_post_ndvi",
+    "fire_detector_ndvi",
+    "load_pre_nbr",
+    "post_nbr",
+    "plot_nbr",
+    "fire_detector_nbr",
+    "fire_area_nbr",
+    "fire_area_ndvi",
+    "severity_ndvi",
+    "severity_nbr",
+    "fire_severity_multiclass",
+    "severity_kmeans"
+]
