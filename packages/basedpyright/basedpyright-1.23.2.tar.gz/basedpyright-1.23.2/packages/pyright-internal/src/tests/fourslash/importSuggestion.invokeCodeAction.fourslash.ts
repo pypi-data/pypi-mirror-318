cb61bb75d63afedfca871839aabefdb797f0acf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:57 +0000

    Add package basedpyright/1.23.2/basedpyright-1.23.2.tar.gz

---
+++ packages/basedpyright/basedpyright-1.23.2.tar.gz/basedpyright-1.23.2/packages/pyright-internal/src/tests/fourslash/importSuggestion.invokeCodeAction.fourslash.ts
@@ -0,0 +1,37 @@
+/// <reference path="typings/fourslash.d.ts" />
+
+// @filename: pyrightconfig.json
+//// {}
+
+// @filename: test.py
+//// [|/*import*/|]foo[|/*noImportMarker*/|]: [|/*startMarker*/|][|TracebackType/*range*/|][|/*endMarker*/|]
+{
+    const importRange = helper.getPositionRange('import');
+    const symbolRange = helper.getPositionRange('range');
+
+    const codeActions = {
+        codeActions: [
+            {
+                title: `from types import TracebackType`,
+                edit: {
+                    changes: {
+                        'file:///test.py': [
+                            // this is a useless TextEdit that replaces the text with the same thing for some reason.
+                            // keeping it in case there's ever code actions that rename it for whatever reason
+                            { range: symbolRange, newText: 'TracebackType' },
+                            { range: importRange, newText: 'from types import TracebackType\n\n\n' },
+                        ],
+                    },
+                },
+                kind: 'quickfix',
+            },
+        ],
+    };
+
+    //@ts-expect-error https://github.com/DetachHead/basedpyright/issues/86
+    await helper.verifyCodeActions('included', {
+        noImportMarker: { codeActions: [] },
+        startMarker: codeActions,
+        endMarker: codeActions,
+    });
+}
