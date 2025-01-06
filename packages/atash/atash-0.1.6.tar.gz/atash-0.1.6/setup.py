35c228ac3a7afa51e4f44b642fe383f7af93da44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:28 +0000

    Add package atash/0.1.6/atash-0.1.6.tar.gz

---
+++ packages/atash/atash-0.1.6.tar.gz/atash-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atash',
+    version='0.1.6',
+    authors='Bahman Amirsardary, Hadi Kheiri, Milad Ramezani Ziarani',
+    authors_email='bahman.amirsardary@mail.polimi.it, hadi.kheiri@mail.polimi.it, Milad.ramezani@mail.polimi.it',
+    description='A library for satellite image processing and fire analysis.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Bahman75/Geospatial-Processing',  
+    packages=find_packages(),
+    install_requires=[
+        'scipy',
+        'numpy',
+        'openeo',
+        'matplotlib',
+        'rasterio',
+        'ipyleaflet',
+        'ipywidgets',
+        'scikit-learn',
+        'scikit-image'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
