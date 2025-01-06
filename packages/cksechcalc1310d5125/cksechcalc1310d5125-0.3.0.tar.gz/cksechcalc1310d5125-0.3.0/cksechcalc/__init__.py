6457099838f91aa62e167e54af1302c02f6b6f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-06 03:11:29 +0000

    Add package cksechcalc1310d5125/0.3.0/cksechcalc1310d5125-0.3.0.tar.gz

---
+++ packages/cksechcalc1310d5125/cksechcalc1310d5125-0.3.0.tar.gz/cksechcalc1310d5125-0.3.0/cksechcalc/__init__.py
@@ -0,0 +1,13 @@
+"""
+Documentation for cksechcalc package.
+This can be viewed using help(cksechcalc) or help(cksechcalc.module)
+
+New addition: ver in ver.py
+"""
+__version__ = '0.3.0'
+
+from .add import add
+from .subtract import subtract
+from .multiply import multiply
+from .divide import divide
+from .ver import version
